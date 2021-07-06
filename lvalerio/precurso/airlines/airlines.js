8d024a688eb3f7a6725801567e3ccd26fe668c58
Lluc Valério <lluc.valerio@gmail.com>
2021-07-07 00:22:36 +0200

    Make some corrections
     - add language to html tag
     - delete octal literals (01 --> 1)
     - some for changed to for-of
     - delete duplicate semicolon

--- lvalerio/precurso/airlines/airlines.js
+++ lvalerio/precurso/airlines/airlines.js
@@ -3,29 +3,29 @@
 //
 // ********** VARIABLES **********
 let flights = [
-    { id: 00, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
-    { id: 01, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
-    { id: 02, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
-    { id: 03, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
-    { id: 04, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
-    { id: 05, to: 'London', from: 'Madrid', cost: 200, scale: false },
-    { id: 06, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
-    { id: 07, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
-    { id: 08, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
-    { id: 09, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
-    { id: 10, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false } ];
+    { id: 0, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
+    { id: 1, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
+    { id: 2, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
+    { id: 3, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
+    { id: 4, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
+    { id: 5, to: 'London', from: 'Madrid', cost: 200, scale: false },
+    { id: 6, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
+    { id: 7, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
+    { id: 8, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
+    { id: 9, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
+    { id: 10, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false }];
 
 airlines();
 // ********** FUNCIONS **********
-function airlines (){
+function airlines() {
     // Benvinguda al usuari.
-    let user = prompt("¿Cuál es tu nombre?","");
-    alert (`Bienvenido/a ${user}`);
+    let user = prompt("¿Cuál es tu nombre?", "");
+    alert(`Bienvenido/a ${user}`);
     // Llista tots els vols.
     console.log(`${user}, los vuelos disponibles son:`)
     console.log(`\n`)
-    for (let i=0; i<flights.length; i++){
-        console.log(` · El vuelo con origen: ${flights[i].from}, y destino: ${flights[i].to} tiene un coste de ${flights[i].cost}€ y ${getScale(i)}`);    
+    for (let i = 0; i < flights.length; i++) {
+        console.log(` · El vuelo con origen: ${flights[i].from}, y destino: ${flights[i].to} tiene un coste de ${flights[i].cost}€ y ${getScale(i)}`);
     }
     // Mitja aritmètica de tots els vols.
     console.log(`\n`);
@@ -37,31 +37,31 @@ function airlines (){
     console.log(`\n`);
     console.log(`Los últimos destinos del día son:`);
     let lastFive = flights.slice(-5);
-    for (let i=0;i<lastFive.length;i++){
-        console.log(lastFive[i].to)
-}
+    for (let value of lastFive) {
+        console.log(value.to)
+    }
     // Retorna un text en funció de si el vol té o no escala.
-function getScale (iterator) {
-        if (flights[iterator].scale){
-            return 'SI realiza escala.';;
+    function getScale(iterator) {
+        if (flights[iterator].scale) {
+            return 'SI realiza escala.';
         } else {
             return 'NO realiza ninguna escala.';
         }
     }
 }
 // Retorna la mitjana aritmètica del cost de tots els vols de l'array.
-function getArithMean () {
+function getArithMean() {
     let arithMean = 0;
-    for (let i=0;i<flights.length;i++){
-        arithMean = arithMean + flights[i].cost;
+    for (let value of flights) {
+        arithMean = arithMean + value.cost;
     }
     return (arithMean / flights.length);
 }
 // Retorna el nombre d'escales d'un array de vols.
-function getNumScales () {
-    let numScales=0;
-    for (let i =0; i<flights.length; i++) {
-        if (flights[i].scale) {
+function getNumScales() {
+    let numScales = 0;
+    for (let value of flights) {
+        if (value.scale) {
             numScales++;
         }
     }
