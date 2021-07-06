7e7836e65550df5a995455ac7aeabbd23d7b35b4
Nacho Martín <martin.nacho.31@gmail.com>
2021-07-06 19:54:48 +0200

    Change 'for' statement for 'for in' statement on currentFlights()
    As it itinerates in an array, the loop has been changed to improve efficiency

--- nacho-martin/precurso/airlines/-airlines-pro.js
+++ nacho-martin/precurso/airlines/-airlines-pro.js
@@ -35,7 +35,7 @@
  function currentFlights() { //Función para mostrar los vuelos
      // Se muestran por consola los vuelos progrmados
      console.log(`These are the flights scheduled for today:`)
-     for (let i = 0; i < flights.length; i++) { //A traves de un for se recorren los distintos vuelos del array
+     for (let i in flights) { //A traves de un for se recorren los distintos vuelos del array
          totalFlightsCost += flights[i].cost; //Se suman los precios de los vuelos para calcular el coste medio
 
          /*Con ayuda de un condicional se distinguen los vuelos con escalas de los que no hacen escalas,
