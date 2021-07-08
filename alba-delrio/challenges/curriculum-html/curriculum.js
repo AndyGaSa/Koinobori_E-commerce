f96edf026455df658bc9a7bbda653680e2726195
Alba del Rio <adelriocastellvi@gmail.com>
2021-07-08 08:53:40 +0200

    Add function to menu button in js document

--- alba-delrio/challenges/curriculum-html/curriculum.js
+++ alba-delrio/challenges/curriculum-html/curriculum.js
@@ -1,5 +1,5 @@
-const menuButton = querySelector('#menu-button');
+const menuButton = document.querySelector('#menu-button');
 
-menuButton.addEventListener('click', button =>{
+menuButton.addEventListener('click', button=> {
     document.querySelector('#menu-options').style.visibility ='visible';
 })
