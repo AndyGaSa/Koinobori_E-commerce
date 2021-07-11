a846d5054172ba1b43acf3400f39cd4d1e5c10b6
Xavier Lasierra <xlp1998@gmail.com>
2021-07-11 21:25:13 +0200

    Refine vertical scroll style

--- xavi-lasierra/challenges/rakuten-tv/rakuten.js
+++ xavi-lasierra/challenges/rakuten-tv/rakuten.js
@@ -3,6 +3,11 @@ const numberOfSlides = 6;
 const carouselContainer = document.querySelector(".scrollable__list");
 const carouselItemWidth = carouselContainer.scrollWidth / numberOfSlides;
 
+let indexV = 0;
+const numberOfSlidesV = 19;
+const carouselContainerV = document.querySelector(".scrollable__list-vertical");
+const carouselItemWidthV = carouselContainerV.scrollWidth / numberOfSlidesV;
+
 let nextButton = document.getElementById('next-button');
 nextButton.addEventListener("click", function(){
     next();
@@ -11,6 +16,16 @@ nextButton.addEventListener("click", function(){
 let backButton = document.getElementById('back-button');
 backButton.addEventListener("click", function(){
     back();
+});
+
+let nextButtonV = document.getElementById('next-button-vertical');
+nextButtonV.addEventListener("click", function(){
+    nextVertical();
+});
+
+let backButtonV = document.getElementById('back-button-vertical');
+backButtonV.addEventListener("click", function(){
+    backVertical();
 });
 
 function next(){
@@ -31,4 +46,38 @@ function back() {
         backButton.style.visibility= "hidden";
     else if(index===3)
         nextButton.style.visibility = "visible";
+}
+
+function nextVertical(){
+    if(indexV===19) return;
+
+    if(document.querySelector(".header-container").backgroundColor === "black"){
+        carouselContainerV.scrollBy(carouselItemWidthV*2, 0);
+        indexV += 2;
+    }
+    else{
+        console.log("ee");
+        carouselContainerV.scrollBy(carouselItemWidthV*7, 0);
+        indexV += 7;
+    }
+    if(indexV>0)
+        backButtonV.style.visibility= "visible";
+    else if(indexV>=18)
+        nextButtonV.style.visibility= "hidden";
+}
+
+function backVertical() {
+    if(indexV===0) return;
+    if(document.querySelector(".header-container").backgroundColor === "black"){
+        carouselContainerV.scrollBy(-carouselItemWidthV*2, 0);
+        indexV += 2;
+    }
+    else{
+        carouselContainerV.scrollBy(-carouselItemWidthV*7, 0);
+        indexV += 7;
+    }
+    if(indexV<=0)
+        backButtonV.style.visibility= "hidden";
+    else if(indexV>=18)
+        nextButtonV.style.visibility = "visible";
 }
