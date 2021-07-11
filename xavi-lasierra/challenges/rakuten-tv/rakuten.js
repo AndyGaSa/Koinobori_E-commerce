8749fc29ff35aa4ee808e1d0bf2132c026424864
Xavier Lasierra <xlp1998@gmail.com>
2021-07-11 16:43:40 +0200

    Refine full responsive scrollable photos

--- xavi-lasierra/challenges/rakuten-tv/rakuten.js
+++ xavi-lasierra/challenges/rakuten-tv/rakuten.js
@@ -1,20 +1,36 @@
 let index = 0;
-const speed = 5;
-const numberOfSlides = 5;
+const numberOfSlides = 6;
 const carouselContainer = document.querySelector(".scrollable__list");
 const carouselItemWidth = carouselContainer.scrollWidth / numberOfSlides;
-setInterval(() => {
-  carouselContainer.scrollBy(carouselItemWidth, 0);
-  timeoutId = setTimeout(() => {
-    index = index % numberOfSlides;
-    let childToMove = carouselContainer.querySelectorAll(`.scrollable__item`)[
-      index
-    ];
-    childToMove.style.order =
-      childToMove.style.order && childToMove.style.order === 0
-        ? 1
-        : +childToMove.style.order + 1;
+
+let nextButton = document.getElementById('next-button');
+nextButton.addEventListener("click", function(){
+    next();
+});
+
+let backButton = document.getElementById('back-button');
+backButton.addEventListener("click", function(){
+    back();
+});
+
+function next(){
+    if(index===5) return;
+    carouselContainer.scrollBy(carouselItemWidth, 0);
     index++;
-    clearTimeout(timeoutId);
-  }, 1000);
-}, speed * 1000);
+    if(index==1)
+        backButton.style.visibility= "visible";
+    else if(index===4)
+        nextButton.style.visibility= "hidden";
+}
+
+function back() {
+    if(index===0) return;
+    carouselContainer.scrollBy(-carouselItemWidth, 0);
+    index--;
+    if(index===0)
+        backButton.style.visibility= "hidden";
+    else if(index===3)
+        nextButton.style.visibility = "visible";
+}
+
+carouselContainer.scrollBy(0, 0);
