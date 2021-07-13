7d3af6e7e205e038368025dd9cf61a10a4b11632
Xavier Lasierra <xlp1998@gmail.com>
2021-07-13 17:16:53 +0200

    Create test and sum functions

--- xavi-lasierra/lectures/test/sum.test.js
+++ xavi-lasierra/lectures/test/sum.test.js
@@ -1,6 +1,22 @@
-test {
-    "describe test",
-    function () {
-        
+const { test, expect } = require("@jest/globals");
+
+const sum = function (a, b){
+    if(typeof a !== "number" || typeof b !== "number"){
+        throw new Error("Params should be numbers");
     }
+    return a + b;
 }
+
+test ("Sum 2 and 2 is 4", function () {
+    const result = sum(2,2);
+    expect(result).toBe(4);
+})
+
+test("Sum 1 and 0 is 1", function () {
+    const result = sum (1,0);
+    expect(result).toBe(1);
+})
+
+test("Sum of any value that are not numbers should return an error", function() {
+    expect(() => sum(true, "algo")).toThrow();
+})
