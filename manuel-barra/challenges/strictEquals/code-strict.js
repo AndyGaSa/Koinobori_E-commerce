324832d970721240eaf4b45455aef26db807c401
Manuel Barra <manuelbarralazo@gmail.com>
2021-07-13 17:21:54 +0200

    Testing con Jest

--- manuel-barra/challenges/strictEquals/code-strict.js
+++ manuel-barra/challenges/strictEquals/code-strict.js
@@ -1,7 +1,11 @@
-test(
-    "texto descriptivo",
+const { expect, test } = require("@jest/globals")
+
+test("Sum 2 and 2 is 4",
     function() {
+        expect(2 + 2).toBe(4)
+    })
 
-    }
-
-)
+test("Sum 2 and 2 is 4",
+    function() {
+        expect(2 + 2).toBe(4)
+    })
