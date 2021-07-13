a68598ee7ec6b6c48754ccc14013d9fddc8431b1
Lluc Valério <lluc.valerio@gmail.com>
2021-07-13 18:08:37 +0200

    Add strictEqual test file

---
+++ lluc-valerio/challenges/test/strictEqual.test.js
@@ -0,0 +1,14 @@
+function strictEquals (a,b) {
+    if(
+        (Object.is(a,b)) && 
+        (!(Object.is(a,NaN)) || !(Object.is())
+        
+        
+        ) { 
+        return true;
+    } else {
+        return false;
+    }
+}
+
+test 
