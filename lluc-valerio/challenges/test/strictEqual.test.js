e6e595ecc1118c0d1e1ba056403832ded42deb0e
Lluc Valério <lluc.valerio@gmail.com>
2021-07-13 19:26:56 +0200

    Add all functionalities
     - 9 test ok

--- lluc-valerio/challenges/test/strictEqual.test.js
+++ lluc-valerio/challenges/test/strictEqual.test.js
@@ -1,45 +1,44 @@
-function strictEquals (a,b) {
-    // if(
-    //     (Object.is(a,b)) && 
-    //     (!(Object.is(a,NaN)) || !(Object.is())
-        
-        
-    //     ) { 
-    //     return true;
-    // } else {
-    //     return false;
-    // }
-    if (Object.is(a,b)) {
+function strictEquals(a, b) {
+    if ((Object.is(a, NaN)) && (Object.is(b, NaN))) {
+        return false;
+    }
+    if ((Object.is(a, 0)) && (Object.is(b, -0))) {
+        return true;
+    }
+    if ((Object.is(a, -0)) && (Object.is(b, 0))) {
+        return true;
+    }
+    if (Object.is(a, b)) {
         return true;
     } else {
         return false;
     }
 }
 
-test ('Compare 1 to 1 is true', function () {
-    expect(strictEquals(1,1)).toBe(1===1);
+test('Compare 1 to 1 is true', function () {
+    expect(strictEquals(1, 1)).toBe(1 === 1);
 });
-test ('Compare NaN to Nan is false', function () {
-    expect(strictEquals(NaN,NaN)).toBe(NaN===NaN);
+test('Compare NaN to Nan is false', function () {
+    expect(strictEquals(NaN, NaN)).toBe(NaN === NaN);
 });
-test ('Compare 0 to -0 is true', function () {
-    expect(strictEquals(0,-0)).toBe(0===-0);
+test('Compare 0 to -0 is true', function () {
+    expect(strictEquals(0, -0)).toBe(0 === -0);
 });
-test ('Compare -0 to 0 is true', function () {
-    expect(strictEquals(-0,0)).toBe(-0===0);
+test('Compare -0 to 0 is true', function () {
+    expect(strictEquals(-0, 0)).toBe(-0 === 0);
 });
-test ('Compare 1 to "1" is false', function () {
-    expect(strictEquals(1,"1")).toBe(1==="1");
+test('Compare 1 to "1" is false', function () {
+    expect(strictEquals(1, "1")).toBe(1 === "1");
 });
-test ('Compare true to false is false', function () {
-    expect(strictEquals(true,false)).toBe(true===false);
+test('Compare true to false is false', function () {
+    expect(strictEquals(true, false)).toBe(true === false);
 });
-test ('Compare false to false is true', function () {
-    expect(strictEquals(false,false)).toBe(false===false);
+test('Compare false to false is true', function () {
+    expect(strictEquals(false, false)).toBe(false === false);
 });
-test ('Compare "Water" to "Oil" is false', function () {
-    expect(strictEquals("Water","Oil")).toBe("Water"==="Oil");
+test('Compare "Water" to "Oil" is false', function () {
+    expect(strictEquals("Water", "Oil")).toBe("Water" === "Oil");
 });
-test ('Compare "SkyLab" to "SkyLab" is true', function () {
-    expect(strictEquals("SkyLab","SkyLab")).toBe("SkyLab"==="SkyLab");
+test('Compare "SkyLab" to "SkyLab" is true', function () {
+    expect(strictEquals("SkyLab", "SkyLab")).toBe("SkyLab" === "SkyLab");
 });
