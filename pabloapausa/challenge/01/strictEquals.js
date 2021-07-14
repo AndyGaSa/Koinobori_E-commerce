06cd54966b1177bb53e23413a06a190d099f763a
pabloapausa <pabloapausa@outlook.com>
2021-07-14 09:47:48 +0200

    Make sure everything is up to date

--- pabloapausa/challenge/01/strictEquals.js
+++ pabloapausa/challenge/01/strictEquals.js
@@ -1,5 +1,16 @@
-function strictEquals(a, b) {
-
+function isTrue() {
+    console.log('true');
+    return true;
 }
 
-strictEquals();
+function isFalse() {
+    console.log('false');
+    return false; 
+}
+
+function strictEquals(a, b) {
+    if (a = b || Object.is(a, b)) return isTrue(); 
+    return isTrue();
+}
+
+strictEquals(1, '1');
