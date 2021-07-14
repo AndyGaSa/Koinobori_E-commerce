b7a02090298c281829bb221ff6825ef45f641a48
Marc Martin <marcmartinbarrios@gmail.com>
2021-07-14 10:00:25 +0200

    Improve all test strings to show the expected result

--- marc-martin/lectures/unit-testing/strictequals.test.js
+++ marc-martin/lectures/unit-testing/strictequals.test.js
@@ -6,25 +6,25 @@ return result
 
 strictEquals();
 
-test ("A function that checks if 2 numbers are strictly equal", function(){
+test ("A function that checks if 2 numbers are strictly equal, expects true", function(){
 
     expect(strictEquals(1,1)).toBe(true);
 })
 
 // usar toEqual para el challenge, toThrow
 
-test ("A function that checks if a number is strictly equal to a string", function(){
+test ("A function that checks if a number is strictly equal to a string, expects true", function(){
 
     expect(strictEquals("1",1)).toBe(false);
 })
 
-test ("A function that checks if null is strictly equal to a string", function(){
+test ("A function that checks if null is strictly equal to a string,expects false", function(){
 
     expect(strictEquals(null,"hola")).toBe(false);
 })
 
-test ("A function that checks if NaN is strictly equal NaN", function(){
+test ("A function that checks if NaN is strictly equal NaN,expects false", function(){
 
 
-    expect(strictEquals(NaN, NaN)).toEqual(true);
+    expect(strictEquals(NaN, NaN)).toEqual(false);
 })
