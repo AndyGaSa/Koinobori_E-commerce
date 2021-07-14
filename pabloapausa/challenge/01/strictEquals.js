51c5dd5c55f3ff0109b636156ba3b004ff7a2c90
pabloapausa <pabloapausa@outlook.com>
2021-07-14 10:10:48 +0200

    Rule exceptions on strictEquals.js

--- pabloapausa/challenge/01/strictEquals.js
+++ pabloapausa/challenge/01/strictEquals.js
@@ -1,16 +1,12 @@
-function isTrue() {
-    console.log('true');
-    return true;
-}
-
 function isFalse() {
     console.log('false');
     return false; 
 }
 
 function strictEquals(a, b) {
-    if (a = b || Object.is(a, b)) return isTrue(); 
-    return isTrue();
+    if (a != b) return isFalse();
+    if ((typeof a == 'number') && (typeof b != 'number')) return isFalse();
+    if ((typeof b == 'number') && (typeof a != 'number')) return isFalse();
+    console.log('true');
+    return true;
 }
-
-strictEquals(1, '1');
