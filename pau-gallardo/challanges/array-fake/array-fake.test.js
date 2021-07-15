1c49b05863502d72aed4805c74e86d5550eeab1a
Pau Gallardo <paugallrdocordon@gmail.com>
2021-07-15 18:13:34 +0200

    Create SkylabArray constructo
    - Add skylab_array atribut
    - Add skylab_length atribute

---
+++ pau-gallardo/challanges/array-fake/array-fake.test.js
@@ -0,0 +1,10 @@
+class SkylabArray {
+  constructor(skylabArray) {
+    this.skylab_array = skylabArray;
+    this.skylab_length = 0;
+  }
+
+  skylab_length() {
+    for (index in this.skylab_array) {} return index;
+  }
+}
