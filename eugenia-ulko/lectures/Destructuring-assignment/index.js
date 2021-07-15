dd9faa18be2f430a9fdef6b3de02ade7610d98a5
Matryoshka.es <eugenia.ulko@gmail.com>
2021-07-15 12:07:46 +0200

    Add more examples

--- eugenia-ulko/lectures/Destructuring-assignment/index.js
+++ eugenia-ulko/lectures/Destructuring-assignment/index.js
@@ -9,3 +9,19 @@ alert(firstName); // Ilya
 alert(surname);  // Kantor
 
 let [firstName, surname] = "Ilya Kantor".split(' ');
+
+
+// Trocando variáveis:
+var a = 1;
+var b = 3;
+
+[a, b] = [b, a];
+console.log(a); // 3
+console.log(b); // 1
+
+// Desestruturação de objeto, Atribuição básica
+var o = {p: 42, q: true};
+var {p, q} = o;
+
+console.log(p); // 42
+console.log(q); // true
