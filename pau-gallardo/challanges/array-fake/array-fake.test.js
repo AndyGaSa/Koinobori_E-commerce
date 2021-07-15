045468cc5b7319ca44442b7debf1ed2887aab146
Pau Gallardo <paugallrdocordon@gmail.com>
2021-07-15 22:40:47 +0200

    Add new fuctions
    - Add Push method
    - Add Some method

--- pau-gallardo/challanges/array-fake/array-fake.test.js
+++ pau-gallardo/challanges/array-fake/array-fake.test.js
@@ -1,17 +1,29 @@
 class SkylabArray {
   constructor(skylabArray) {
     this.skylabArray = skylabArray;
-    this.skyLength = 0;
+    this.skyLength = this.skylabLength();
   }
 
   skylabLength() {
+    this.skyLength = 0;
     for (const elem of this.skylabArray) {
       this.skyLength += 1;
+      this[this.skyLength] = elem;
     }
     return this.skyLength;
+  }
+
+  skylabPush(newElement) {
+    this[this.skyLength] = newElement;
+    this.skyLength += 1;
+  }
+
+  skylabSome(callback) {
+    for (let i = 0; i < this.skyLength; i += 1) {
+      if (callback(this[i])) return true;
+    }
+    return false;
   }
 }
 
 a = new SkylabArray([1, 2, 3]);
-a.skylabLength();
-console.log(a.skyLength);
