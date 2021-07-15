3833d05790b0f0a53cb44d54057693836c4ccc8b
Lola Rufino <lola.ruf.ar@gmail.com>
2021-07-15 21:04:59 +0200

    Add arraymethods file

---
+++ lola-rufino/challenges/arraymethods/arraymethods.js
@@ -0,0 +1,28 @@
+class SkylabArray {
+  constructor() {
+    this.length = 0;
+  }
+
+  push(param) {
+    this[this.length] = param;
+    this.length += 1;
+  }
+
+  pop() {
+    if (this.length === 0) {
+      return undefined;
+    }
+    this.length -= 1;
+    delete this[this.length];
+  }
+}
+
+const array1 = new SkylabArray();
+
+array1.push('Lola');
+array1.push('Alvaro');
+array1.pop();
+array1.pop();
+array1.pop();
+
+console.log(array1);
