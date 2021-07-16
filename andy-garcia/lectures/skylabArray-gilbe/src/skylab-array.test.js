85dfaa00902b69b687a1b03ffce1853d3961963e
“Andy <garcia.andy.tecnologies10@gmail.com>
2021-07-16 11:48:29 +0200

    Update test definition

--- andy-garcia/lectures/skylabArray-gilbe/src/skylab-array.test.js
+++ andy-garcia/lectures/skylabArray-gilbe/src/skylab-array.test.js
@@ -1,6 +1,12 @@
 const SkylabArray = require('./skylab-array');
 
-test('should have a length equals to zero', () => {
-  const myArray = new SkylabArray();
-  expect(myArray.length).toBe(0);
+describe('SkylabArray class', () => {
+  describe('Given a length porpierty', () => {
+    describe('When the object is first created', () => {
+      test('should be equal to zero', () => {
+        const myArray = new SkylabArray();
+        expect(myArray.length).toBe(0);
+      });
+    });
+  });
 });
