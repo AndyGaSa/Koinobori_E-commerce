d37a14e63c05c2036ecad204933089b84934370f
Gilberto Cao <gilbe.cao@gmail.com>
2021-07-16 15:42:18 +0200

    Add push method for one value

--- gilbe-cao/skylabArray/src/skylab-array.test.js
+++ gilbe-cao/skylabArray/src/skylab-array.test.js
@@ -9,11 +9,32 @@ const SkylabArray = require('./skylab-array');
  */
 
 describe('SkylabArray class', () => {
+  let myArray;
+
   describe('Given a length property', () => {
     describe('When the object is first created', () => {
       test('Then return 0', () => {
-        const myArray = new SkylabArray();
+        myArray = new SkylabArray();
+
         expect(myArray.length).toBe(0);
+      });
+    });
+  });
+
+  describe('Given a push method', () => {
+    describe('When is invoked with Skylab string', () => {
+      describe('And the array is empty', () => {
+        beforeEach(() => {
+          myArray = new SkylabArray();
+        });
+
+        test('Then return 1', () => {
+          expect(myArray.push('Skylab')).toBe(1);
+        });
+        test('Then myArray[0]=Skylab', () => {
+          myArray.push('Skylab');
+          expect(myArray[0]).toBe('Skylab');
+        });
       });
     });
   });
