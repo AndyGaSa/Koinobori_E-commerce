5a7067799567a4e6cf5af7a7166bb9ff6b7ab366
Alex <alexmunoz91@gmail.com>
2021-07-16 11:49:22 +0200

    Add length test

--- alex-muñoz/challenges/arrays-methods-gilber/src/skylab-array.test.js
+++ alex-muñoz/challenges/arrays-methods-gilber/src/skylab-array.test.js
@@ -1,5 +1,25 @@
-const SkylabArray = require('./index');// invocando la funcion
+const SkylabArray = require('../index');// invocando la funcion
 // require que devuelve la ruta del fichero que quermos importar.
+
+
+/**
+ * GIVEN
+ * WHEN
+ * THEN
+ * AND
+ * OR
+ */
+
+describe('SkylabArray class'()=>{
+  describe('Given a length property',()=>{
+    describe('When is first created',()=>{
+      test('Then return 0',()=>{
+        const myArray = new SkylabArray();
+        expect(myArray.length).toBe(0);
+      })
+    })
+  })
+})
 
 test('should have a length equals to 0', () => {
   const myArray = new SkylabArray();
