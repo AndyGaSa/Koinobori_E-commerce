1ae9dce332fb99c99a0c16ee03be38443df5d001
Matryoshka.es <eugenia.ulko@gmail.com>
2021-07-16 02:13:16 +0200

    Append new examples of DA

--- eugenia-ulko/lectures/Destructuring-assignment/index.js
+++ eugenia-ulko/lectures/Destructuring-assignment/index.js
@@ -63,4 +63,12 @@ for (let [key, value] of user) {
     alert(`${key}:${value}`); // name:John, затем age:30
     
 
+let [name1, name2, ...rest] = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
 
+alert(name1); // Julius
+alert(name2); // Caesar
+
+// Обратите внимание, что `rest` является массивом.
+alert(rest[0]); // Consul
+alert(rest[1]); // of the Roman Republic
+alert(rest.length); // 2
