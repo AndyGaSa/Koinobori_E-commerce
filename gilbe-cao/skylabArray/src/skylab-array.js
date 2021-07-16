fbd56bd0f87a125774f1b89d79e3541a70c06c13
Gilberto Cao <gilbe.cao@gmail.com>
2021-07-16 16:17:20 +0200

    Add map method

--- gilbe-cao/skylabArray/src/skylab-array.js
+++ gilbe-cao/skylabArray/src/skylab-array.js
@@ -8,6 +8,17 @@ class SkylabArray {
     this.length += 1;
     return this.length;
   }
+
+  map(callback) {
+    const newArray = new SkylabArray();
+
+    for (let index = 0; index < this.length; index += 1) {
+      const newValue = callback(this[index]);
+      newArray.push(newValue);
+    }
+
+    return newArray;
+  }
 }
 
 module.exports = SkylabArray;
--- gilbe-cao/skylabArray/src/skylab-array.test.js
+++ gilbe-cao/skylabArray/src/skylab-array.test.js
@@ -11,11 +11,13 @@ const SkylabArray = require('./skylab-array');
 describe('SkylabArray class', () => {
   let myArray;
 
+  beforeEach(() => {
+    myArray = new SkylabArray();
+  });
+
   describe('Given a length property', () => {
     describe('When the object is first created', () => {
       test('Then return 0', () => {
-        myArray = new SkylabArray();
-
         expect(myArray.length).toBe(0);
       });
     });
@@ -24,16 +26,46 @@ describe('SkylabArray class', () => {
   describe('Given a push method', () => {
     describe('When is invoked with Skylab string', () => {
       describe('And the array is empty', () => {
-        beforeEach(() => {
-          myArray = new SkylabArray();
-        });
-
         test('Then return 1', () => {
           expect(myArray.push('Skylab')).toBe(1);
         });
         test('Then myArray[0]=Skylab', () => {
           myArray.push('Skylab');
           expect(myArray[0]).toBe('Skylab');
+        });
+      });
+    });
+  });
+
+  describe('Given a map method', () => {
+    describe('When is invoked', () => {
+      let newArray;
+
+      describe('And array is empty', () => {
+        // Test scenarios when array is empty
+      });
+
+      describe('And array has at least one element', () => {
+        beforeEach(() => {
+          myArray.push('mola');
+
+          function mapCallback(value) {
+            return `Skylab: ${value}`;
+          }
+
+          newArray = myArray.map(mapCallback);
+        });
+
+        test('Then return a new array', () => {
+          expect(myArray).not.toEqual(newArray);
+        });
+
+        test('Then return a new array with same length', () => {
+          expect(myArray.length).toEqual(newArray.length);
+        });
+
+        test('Then newArray[0] should contain myArray[0]', () => {
+          expect(newArray[0]).toMatch(myArray[0]);
         });
       });
     });
