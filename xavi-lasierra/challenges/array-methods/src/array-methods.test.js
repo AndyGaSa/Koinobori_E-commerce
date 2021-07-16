8f1f03eb3015722f72ea48a73dc0ee0935ffc40b
Xavier Lasierra <xlp1998@gmail.com>
2021-07-16 10:53:56 +0200

    Add GHERKIN structure to the test

--- xavi-lasierra/challenges/array-methods/src/array-methods.test.js
+++ xavi-lasierra/challenges/array-methods/src/array-methods.test.js
@@ -1,6 +1,12 @@
 const SkylabArray = require('./array-methods');
 
-test('should have a length equals to 0', () => {
-  const myArray = new SkylabArray();
-  expect(myArray.length).toBe(0);
+describe('SkylabArray class', () => {
+  describe('Given a length propety', () => {
+    describe('When its created', () => {
+      test('Then return 0', () => {
+        const myArray = new SkylabArray();
+        expect(myArray.length).toBe(0);
+      });
+    });
+  });
 });
