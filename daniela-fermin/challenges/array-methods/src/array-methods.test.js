e4491ed896d70bf5c9066a480f6eb51cec240b82
Daniela Fermin <fermin.daniela@gmail.com>
2021-07-16 19:16:08 +0200

    Add find, filter and map method

--- daniela-fermin/challenges/array-methods/src/array-methods.test.js
+++ daniela-fermin/challenges/array-methods/src/array-methods.test.js
@@ -1,13 +1,12 @@
-const { describe, test, beforeEach } = require('@jest/globals');
 const SkylabArray = require('./array-methods');
 
 let myArray;
 describe('SkylabArray class', () => {
+  beforeEach(() => {
+    myArray = new SkylabArray();
+  });
   describe('Given a length property', () => {
     describe('When is first created', () => {
-      beforeEach(() => {
-        myArray = new SkylabArray();
-      });
       test('Then returns 0', () => {
         expect(myArray.length).toBe(0);
       });
@@ -17,9 +16,6 @@ describe('SkylabArray class', () => {
   describe('Given given a push method', () => {
     describe('When the array is empty', () => {
       describe('And invoked with the string "Daniela"', () => {
-        beforeEach(() => {
-          myArray = new SkylabArray();
-        });
         test('Then the array length should return 1', () => {
           expect(myArray.push('Daniela')).toBe(1);
         });
@@ -28,14 +24,8 @@ describe('SkylabArray class', () => {
           expect(myArray[0]).toBe('Daniela');
         });
       });
-    });
-  });
-  describe('Given given a push method', () => {
-    describe('When the array is empty', () => {
+
       describe('And invoked with two new values', () => {
-        beforeEach(() => {
-          myArray = new SkylabArray();
-        });
         test('Then the array length should return 2', () => {
           expect(myArray.push(1, 2)).toBe(2);
         });
@@ -44,38 +34,58 @@ describe('SkylabArray class', () => {
   });
   describe('Given given a push method', () => {
     describe('When the array lenght in 1', () => {
+      beforeEach(() => {
+        myArray.push(1);
+      });
       describe('And invoked with one new value', () => {
-        beforeEach(() => {
-          myArray = new SkylabArray();
-          myArray.push(1);
-        });
         test('Then the array length should return 3', () => {
           expect(myArray.push(5)).toBe(2);
         });
       });
-    });
-  });
-  describe('Given given a push method', () => {
-    describe('When the array has one value', () => {
       describe('And invoked with two new values', () => {
-        beforeEach(() => {
-          myArray = new SkylabArray();
-          myArray.push(1);
-        });
         test('Then the array length should return 3', () => {
           expect(myArray.push(1, 2)).toBe(3);
         });
       });
     });
   });
+
   describe('Given given a pop method', () => {
     describe('When the array has 2 values', () => {
       beforeEach(() => {
-        myArray = new SkylabArray();
         myArray.push(1, 2);
       });
       test('Then the array length to be 1 ', () => {
         expect(myArray.pop()).toBe(1);
+      });
+    });
+  });
+
+  describe('Given given a some method', () => {
+    describe('When invoked with callback in a object of 5 properties', () => {
+      beforeEach(() => {
+        myArray.push(1, 2, 3, 4, 'Daniela');
+      });
+      test('Then return true', () => {
+        expect(myArray.some((element) => typeof element === 'string')).toBe(true);
+      });
+    });
+  });
+  describe('Given given a find method', () => {
+    describe('When invoked with callback in a object of 5 properties', () => {
+      beforeEach(() => {
+        myArray.push(1, 2, 3, 4, 'Daniela');
+      });
+      test('Then return the value of the first property that returns', () => {
+        expect(myArray.find((element) => typeof element === 'string')).toBe('Daniela');
+      });
+    });
+  });
+  describe('Given given a filter method', () => {
+    describe('When invoked with a callback function', () => {
+      test('Then returns new array', () => {
+        const newArray = myArray.filter((element) => typeof element === 'string');
+        expect(myArray.filter((element) => typeof element === 'string')).toBe(newArray);
       });
     });
   });
