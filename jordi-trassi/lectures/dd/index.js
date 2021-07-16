a8c59988f3e794117e9226b8eb8fbd2a723cb52f
JordiTrassi <j.trassi@telefonica.net>
2021-07-16 10:00:01 +0200

    Config enviroment

---
+++ jordi-trassi/lectures/dd/index.js
@@ -0,0 +1,21 @@
+class SkaylabArray {
+  constructor() {
+    this.valores = 0;
+  }
+
+  push(...rest) {
+    let arr = [];
+    for (let i=0; i<arr.length, i++) {
+      return arr;
+    }
+  }
+
+  suma(a, b) {
+    return a + b;
+  }
+}
+
+const a = new SkaylabArray();
+
+console.log(a.suma(1, 4));
+console.log(a.push(a,b,c));
