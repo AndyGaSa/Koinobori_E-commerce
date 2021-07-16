f13f55901a7589462c6f97c0cd27c2e11e1497c7
Manuel Barra <manuelbarralazo@gmail.com>
2021-07-16 17:25:46 +0200

    lecture

--- manuel-barra/challenges/arraymetods/src/skylab-array.js
+++ manuel-barra/challenges/arraymetods/src/skylab-array.js
@@ -1,7 +1,32 @@
 class SkylabArray {
   constructor() {
     this.length = 0;
-    
   }
+
+  push(...rest) {
+    const arr = [...rest];
+    for (let i = 0; i < arr.length; i += 1) {
+      this[this.length] = arr[i];
+      this.length += 1;
+    }
+    return this.length;
+  }
+
+  pop() {
+    const value = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length -= 1;
+    return value;
+  }
+
+  map(callback) {
+    const newArray = new SkylabArray();
+    for (let index = 0; index < this.length - 1; index += 1) {
+      const element = callback(this[index]);
+      newArray.push(element);
+    }
+    return newArray;
+  }
+}
 
 module.exports = SkylabArray;
--- manuel-barra/challenges/arraymetods/src/skylab-array.test.js
+++ manuel-barra/challenges/arraymetods/src/skylab-array.test.js
@@ -1,6 +1,51 @@
-const SkylabArray = require('./index');
+const SkylabArray = require('./skylab-array');
 
-test('should have a length equals to 0', () => {
+/**
+ * GIVEN
+ * WHEN
+ * THEN
+ * AND
+ * OR
+ */
+describe('SkylabArray class', () => {
   const myArray = new SkylabArray();
-  expect(myArray.length).toBe(0);
+
+  describe('Given a length property', () => {
+    describe('When is first creates', () => {
+      test('should have a length equals to 0', () => {
+      /* expect(myArray.length).toBe(0); */
+      });
+    });
+  });
+
+  describe('Given a push property', () => {
+    describe('When is invoke with one value', () => {
+      test('Then return 0', () => {
+        // expect(myArray.push()).toBe(1);
+      });
+    });
+  });
+
+  describe('Given a pop property', () => {
+    myArray.push(2, 3, 4, 5, 'tomate');
+    describe('When is invoke with one value', () => {
+      test('Then return the value and delete', () => {
+        expect(myArray.pop()).toBe('tomate');
+      });
+    });
+  });
+  describe('Given a map method', () => {
+    let myArray;
+    beforeEach(() => {
+      myArray = new SkylabArray();
+      myArray.push('mola');
+    });
+    describe('When is invoked', () => {
+      test('Then return new array', () => {
+        const mapCallback = (value) => `Skylab: ${value}`;
+        const newArray = myArray.map(mapCallback);
+        expect(myArray).not.toEqual(newArray);
+      });
+    });
+  });
 });
