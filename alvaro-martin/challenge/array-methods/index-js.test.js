8f78a344a62b135b1177168858e9f9b4329f2fc5
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-07-16 18:17:21 +0200

    Add .push and .pop with their tests

--- alvaro-martin/challenge/array-methods/index-js.test.js
+++ alvaro-martin/challenge/array-methods/index-js.test.js
@@ -1,20 +1,43 @@
-class SkyLab {
+class SkylabArray {
   constructor() {
     this.length = 0;
   }
 
-  myPush(x) {
-    this[this.length] = x;
+  push(newValue) {
+    this[this.length] = newValue;
     this.length += 1;
+    return this.length;
   }
 
-  myPop() {
-    if (this.length === 0) {
-      return undefined;
-    }
+  pop() {
+    const value = this[this.length - 1];
+    delete this[this.length - 1];
     this.length -= 1;
-    delete this[this.length];
+    return value;
   }
 }
-const cosa = new SkyLab();
-cosa.myPush('Alvaro');
+const cosa = new SkylabArray();
+cosa.push('luca');
+cosa.push('jordi');
+console.log(cosa);
+const po = cosa.pop();
+console.log(po);
+
+describe('SkylabArray class', () => {
+  const cosa = new SkylabArray();
+  describe('Given a lenght property', () => {
+    describe('when is first created', () => {
+      test('should have a length equal to 0', () => {
+        expect(cosa.length).toBe(0);
+      });
+    });
+  });
+  describe('Given a push property', () => {
+    describe('when is invoked with string', () => {
+      test('should have a length equal to 1', () => {
+        cosa.push('luca');
+        expect(cosa[0]).toBe('luca');
+      });
+    });
+  });
+});
