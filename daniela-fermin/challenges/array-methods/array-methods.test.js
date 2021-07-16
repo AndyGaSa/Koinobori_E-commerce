4b86f24fc78e98d76c0567c390d56e3d0de8ebef
Daniela Fermin <fermin.daniela@gmail.com>
2021-07-16 14:26:35 +0200

    Add functions for the push and pop methods and 6 tests for the push method and 1 test for the pop method

--- daniela-fermin/challenges/array-methods/array-methods.test.js
+++ daniela-fermin/challenges/array-methods/array-methods.test.js
@@ -1,57 +1,82 @@
-class SkylabArray {
-  constructor() {
-    this.myLength = 0;
-  }
+const { describe, test, beforeEach } = require('@jest/globals');
+const SkylabArray = require('./array-methods');
 
-  length() {
-    this.myLength = 0;
-    let property;
-    while (this.object[property] !== undefined) { myLength += 1; }
-    return this.myLength;
-  }
+let myArray;
+describe('SkylabArray class', () => {
+  describe('Given a length property', () => {
+    describe('When is first created', () => {
+      beforeEach(() => {
+        myArray = new SkylabArray();
+      });
+      test('Then returns 0', () => {
+        expect(myArray.length).toBe(0);
+      });
+    });
+  });
 
-  push(newItem) {
-    this.myLength = 0;
-    this[this.myLength] = newItem;
-    this.myLength += 1;
-    return this.myLength;
-  }
-
-  pop() {
-    if (this.myLength !== 0) { delete this[this.myLength - 1]; }
-    this.myLength -= 1;
-    return this.myLength;
-  }
-
-  some(callback) {
-    for (let property = 0; property < this.myLength; property++) {
-      if (callback(this[property])) {
-        return true;
-      }
-    } return false;
-  }
-
-  find(callback) {
-    for (let property = 0; property < this.myLength; property++) {
-      if (callback(this[property])) {
-        return this[property];
-      }
-    } return undefined;
-  }
-
-  filter(callback) {
-    const emptyArray = [];
-    for (let property = 0; property < this.myLength; property++) {
-      if (callback(this[property])) {
-        emptyArray.push(this[property]);
-      }
-    }
-  }
-
-  map(callback) {
-    const emptyArray = [];
-    for (let property = 0; property < this.myLength; property++) {
-      emptyArray.push(callback(this[property]));
-    }
-  }
-}
+  describe('Given given a push method', () => {
+    describe('When the array is empty', () => {
+      describe('And invoked with the string "Daniela"', () => {
+        beforeEach(() => {
+          myArray = new SkylabArray();
+        });
+        test('Then the array length should return 1', () => {
+          expect(myArray.push('Daniela')).toBe(1);
+        });
+        test('Then my array[0] should return Daniela', () => {
+          myArray.push('Daniela');
+          expect(myArray[0]).toBe('Daniela');
+        });
+      });
+    });
+  });
+  describe('Given given a push method', () => {
+    describe('When the array is empty', () => {
+      describe('And invoked with two new values', () => {
+        beforeEach(() => {
+          myArray = new SkylabArray();
+        });
+        test('Then the array length should return 2', () => {
+          expect(myArray.push(1, 2)).toBe(2);
+        });
+      });
+    });
+  });
+  describe('Given given a push method', () => {
+    describe('When the array lenght in 1', () => {
+      describe('And invoked with one new value', () => {
+        beforeEach(() => {
+          myArray = new SkylabArray();
+          myArray.push(1);
+        });
+        test('Then the array length should return 3', () => {
+          expect(myArray.push(5)).toBe(2);
+        });
+      });
+    });
+  });
+  describe('Given given a push method', () => {
+    describe('When the array has one value', () => {
+      describe('And invoked with two new values', () => {
+        beforeEach(() => {
+          myArray = new SkylabArray();
+          myArray.push(1);
+        });
+        test('Then the array length should return 3', () => {
+          expect(myArray.push(1, 2)).toBe(3);
+        });
+      });
+    });
+  });
+  describe('Given given a pop method', () => {
+    describe('When the array has 2 values', () => {
+      beforeEach(() => {
+        myArray = new SkylabArray();
+        myArray.push(1, 2);
+      });
+      test('Then the array length to be 1 ', () => {
+        expect(myArray.pop()).toBe(1);
+      });
+    });
+  });
+});
