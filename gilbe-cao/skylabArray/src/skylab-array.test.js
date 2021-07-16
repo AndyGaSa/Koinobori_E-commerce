98d75ed8e4448e9b5774f83c4c09d159a1234c81
Gilberto Cao <gilbe.cao@gmail.com>
2021-07-16 10:54:46 +0200

    Update test description

--- gilbe-cao/skylabArray/src/skylab-array.test.js
+++ gilbe-cao/skylabArray/src/skylab-array.test.js
@@ -1,6 +1,20 @@
 const SkylabArray = require('./skylab-array');
 
-test('should have a length equals to 0', () => {
-  const myArray = new SkylabArray();
-  expect(myArray.length).toBe(0);
+/**
+ * GIVEN
+ * WHEN
+ * THEN
+ * AND
+ * OR
+ */
+
+describe('SkylabArray class', () => {
+  describe('Given a length property', () => {
+    describe('When the object is first created', () => {
+      test('Then return 0', () => {
+        const myArray = new SkylabArray();
+        expect(myArray.length).toBe(0);
+      });
+    });
+  });
 });
