8ac0c59caa49aea778f26fc3ee7a883c5a25f1c4
Alex <alexmunoz91@gmail.com>
2021-07-16 15:42:41 +0200

    Add push method

--- alex-muñoz/challenges/arrays-methods-gilber/src/skylab-array.js
+++ alex-muñoz/challenges/arrays-methods-gilber/src/skylab-array.js
@@ -4,6 +4,12 @@ class SkylabArray {
   constructor() {
     this.length = 0;
   }
+
+  push(newValue) {
+    this[this.length] = newValue;
+    this.length += 1;
+    return this.length;
+  }
 }
 
 module.exports = SkylabArray;
--- alex-muñoz/challenges/arrays-methods-gilber/src/skylab-array.test.js
+++ alex-muñoz/challenges/arrays-methods-gilber/src/skylab-array.test.js
@@ -1,7 +1,6 @@
 const SkylabArray = require('../index');// invocando la funcion
 // require que devuelve la ruta del fichero que quermos importar.
 
-
 /**
  * GIVEN
  * WHEN
@@ -10,18 +9,37 @@ const SkylabArray = require('../index');// invocando la funcion
  * OR
  */
 
-describe('SkylabArray class'()=>{
-  describe('Given a length property',()=>{
-    describe('When is first created',()=>{
-      test('Then return 0',()=>{
-        const myArray = new SkylabArray();
-        expect(myArray.length).toBe(0);
-      })
-    })
-  })
-})
+describe('SkylabArray class', () => {
+  let myArray = new SkylabArray();
 
-test('should have a length equals to 0', () => {
-  const myArray = new SkylabArray();
-  expect(myArray.length).toBe(0);
+  describe('Given a length property', () => {
+    describe('When is first created', () => {
+      test('Then return 0', () => {
+        myArray = new SkylabArray();
+        expect(myArray.length).toBe(0);
+      });
+    });
+  });
+
+  describe('Given a push property', () => {
+    describe('When is invoked with Skylab string', () => {
+      describe('And the array is empty', () => {
+        beforeEach(() => {
+          myArray = new SkylabArray();
+        });
+
+        test('Then return 1', () => {
+          expect(myArray.push('Skylab')).toBe(1);
+        });
+        test('Then myArray [0] = Skylab', () => {
+          myArray.push('Skylab');
+          expect(myArray[0]).toBe('Skylab');
+        });
+        test('Then return ...new', () => {
+          const x = newValue;
+          expect(myArray.push(newValue[i])).toBe(newValue[i]);
+        });
+      });
+    });
+  });
 });
