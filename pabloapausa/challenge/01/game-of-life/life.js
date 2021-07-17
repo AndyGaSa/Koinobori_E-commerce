4c847d4d22fb8485359ea3700e50d9d4a6508f64
pabloapausa <pabloapausa@outlook.com>
2021-07-17 21:36:25 +0200

    Develop all 'life.js' logic

--- pabloapausa/challenge/01/game-of-life/life.js
+++ pabloapausa/challenge/01/game-of-life/life.js
@@ -1,8 +1,66 @@
 class Life {
-    constructor(array) {
-        //Create grid
+    constructor() {
+        world0 = this.matrix(); 
+        world1 = this.matrix();
     }
+
+    matrix() {
+        let array = [];
+        for (let xAxis = 0; xAxis <= row; xAxis++) {
+            array[xAxis] = [];  
+            for (let yAxis = 0; yAxis <= column; yAxis++) {
+                array[xAxis][yAxis] = 0;
+            }
+        }
+        return array;
+    }
+
+    kill() {
+        let neighbours; 
+        for (let x = 0; x <= row; x++) {
+            for (let y = 0; y <= column; y++) {
+                neighbours = 1;
+                neighbours += ((((x + 1) < row) && ((y - 1) > 0)) && (world1[x + 1][y - 1] === 1)) ? 1 : 0; //Top, left.
+                neighbours += ((((x - 1) > 0) && ((y - 1) > 0)) && (world1[x - 1][y - 1] === 1)) ? 1 : 0; //Top, right.
+                neighbours += ((((x + 1) < row) && ((y + 1) < column)) && (world1[x + 1][y + 1] === 1)) ? 1 : 0; //Bottom, left.
+                neighbours += ((((x - 1) > 0) && ((y + 1) < column)) && (world1[x - 1][y + 1] === 1)) ? 1 : 0; //Bottom, right. 
+                neighbours += (((y - 1) > 0) && (world1[x][y - 1] === 1)) ? 1 : 0; //Top, center.
+                neighbours += (((x - 1) > 0) && (world1[x - 1][y] === 1)) ? 1 : 0; //Center, left.
+                neighbours += (((x + 1) < row) && (world1[x + 1][y] === 1)) ? 1 : 0; //Center, right. 
+                neighbours += (((y + 1) < column) && world1[x][y + 1] === 1) ? 1 : 0; //Bottom, center.
+                world1[x][y] = (neighbours === 3 ||  (neighbours === 2 && world1[x][y] === 1)) ? 1 : 0; 
+                [world0, world1] = [world1, world0];
+            }
+        }
+        console.table(world0);
+        console.table(world1);
+    }
+
+
 }
 
-//Un array con varias coordenadas. 
-const life = new Life(); 
+let world0;
+let world1;
+const [row, column] = [12, 12];
+const life = new Life();
+
+function hello() {
+    life.kill();
+}
+
+hello();
+
+
+
+
+/*
+if ((((x + 1) < row) && ((y - 1) > 0)) && (world[x + 1][y - 1] === 1)) neighbours += 1; //Top, left.
+if ((((x - 1) > 0) && ((y - 1) > 0)) && (world[x - 1][y - 1] === 1)) neighbours += 1; //Top, right.
+if ((((x + 1) < row) && ((y + 1) < column)) && (world[x + 1][y + 1] === 1)) neighbours += 1; //Bottom, left.
+if ((((x - 1) > 0) && ((y + 1) < column)) && (world[x - 1][y + 1] === 1)) neighbours += 1; //Bottom, right. 
+if (((y - 1) > 0) && (world[x][y - 1] === 1)) neighbours += 1; //Top, center.
+if (((x - 1) > 0) && (world[x - 1][y] === 1)) neighbours += 1; //Center, left.
+if (((x + 1) < row) && (world[x + 1][y] === 1)) neighbours += 1; //Center, right. 
+if (((y + 1) < column) && world[x][y + 1] === 1) neighbours += 1; //Bottom, center.
+world[x][y] = (neighbours === 3 ||  (neighbours === 2 && world[x][y] === 1)) ? 1 : 0; 
+*/
--- pabloapausa/challenge/01/skylabArray/src/skylab-array.js
+++ pabloapausa/challenge/01/skylabArray/src/skylab-array.js
@@ -1,22 +1,22 @@
 class SkylabArray {
-  constructor(oldArray) {
-    this.oldArray = oldArray;
+  constructor(array) {
+    this.array = array;
     let i = 0;
-    while (this.oldArray[i] !== undefined) {
+    while (this.array[i] !== undefined) {
       i += 1;
     }
     this.length = i;
   }
 
   push(value) {
-    this.oldArray[this.length] = value;
-    return this.oldArray;
+    this.array[this.length] = value;
+    return this.array;
   }
 
   some(even) {
     this.even = even;
-    for (let i = 0; this.oldArray[i] !== undefined; i += 1) {
-      if (this.even(this.oldArray[i])) {
+    for (let i = 0; this.array[i] !== undefined; i += 1) {
+      if (this.even(this.array[i])) {
         return true;
       }
     }
@@ -25,9 +25,9 @@ class SkylabArray {
 
   find(found) {
     this.found = found;
-    for (let i = 0; this.oldArray[i] !== undefined; i += 1) {
-      if (this.found(this.oldArray[i])) {
-        return this.oldArray[i];
+    for (let i = 0; this.array[i] !== undefined; i += 1) {
+      if (this.found(this.array[i])) {
+        return this.array[i];
       }
     }
     return undefined;
@@ -36,9 +36,9 @@ class SkylabArray {
   filter(result) {
     this.result = result;
     const newArray0 = [];
-    for (let i = 0; this.oldArray[i] !== undefined; i += 1) {
-      if (this.result(this.oldArray[i])) {
-        newArray0.push(this.oldArray[i]);
+    for (let i = 0; this.array[i] !== undefined; i += 1) {
+      if (this.result(this.array[i])) {
+        newArray0.push(this.array[i]);
       }
     }
     return newArray0;
@@ -47,9 +47,9 @@ class SkylabArray {
   map(multiply) {
     this.multiply = multiply;
     const newArray1 = [];
-    for (let i = 0; this.oldArray[i] !== undefined; i += 1) {
-      if (this.multiply(this.oldArray[i])) {
-        newArray1.push(multiply(this.oldArray[i]));
+    for (let i = 0; this.array[i] !== undefined; i += 1) {
+      if (this.multiply(this.array[i])) {
+        newArray1.push(multiply(this.array[i]));
       }
     }
     return newArray1;
