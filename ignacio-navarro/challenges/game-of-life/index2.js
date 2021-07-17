c46d190d0750b5e2a468a77cec800f5a924fa996
ignacioND <ignacio.navarro.vfx@gmail.com>
2021-07-17 19:56:32 +0200

    Fix errors on the functions that broke the chain

--- ignacio-navarro/challenges/game-of-life/index2.js
+++ ignacio-navarro/challenges/game-of-life/index2.js
@@ -1,48 +1,77 @@
-let aliveCells = [33,34,35,42,43,44]
+const { func } = require('prop-types');
 
-function checkNeighbours (arr = []){
-    let cellsAroundAlive = [];
-    arr.forEach(element=>(cellsAroundAlive = cellsAroundAlive.concat(cellsAround(element))))
-    let cellsCountAll = removeDupes(cellsAroundAlive);
-    let newCellsAlive = cellsDepuration(cellsCountAll)
-    return newCellsAlive
+let aliveCells = [];
+aliveCells = [33, 34, 35, 42, 43, 44];
+
+function numToArray(num) {
+  const arrayOfArrays = [];
+  for (let i = 0; i < num.length; i += 1) {
+    const element = num[i].toString(10).replace(/\D/g, '0').split('').map(Number);
+    arrayOfArrays.push(element);
+  }
+  return arrayOfArrays;
 }
-
-function cellsAround (cell){
-    let cellNeighbours = [];
-    cellNeighbours.push(cell-11);
-    cellNeighbours.push(cell-10);
-    cellNeighbours.push(cell-9);
-    cellNeighbours.push(cell-1);
-    cellNeighbours.push(cell);
-    cellNeighbours.push(cell+1);
-    cellNeighbours.push(cell+9);
-    cellNeighbours.push(cell+10);
-    cellNeighbours.push(cell+11);
-    return cellNeighbours
+function arrToNum(arr = []) {
+  const numberArray = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    const element = Number(arr[i].join(''));
+    numberArray.push(element);
+  }
+  return numberArray;
 }
-
-
 function removeDupes(arr) {
-    let cellsCount = {};
-    let i = arr.length;
-    while (i--) {   
-        if (cellsCount.hasOwnProperty(arr[i])) {
-            cellsCount[arr[i]]+=1;
-            arr.splice(i, 1);
-        } else {
-            cellsCount[arr[i]] = 1;
-        }
+  const cellsCount = {};
+  let i = arr.length;
+  while (i--) {
+    if (cellsCount.hasOwnProperty(arr[i])) {
+      cellsCount[arr[i]] += 1;
+      arr.splice(i, 1);
+    } else {
+      cellsCount[arr[i]] = 1;
     }
-    return cellsCount;
-}       
-
-function cellsDepuration (countedCells){
-    let alive = []
-    for (let cell in countedCells) {
-        if (countedCells[cell] === 3){
-            alive.push(parseInt(cell))
-        }
-    }
-    return alive
+  }
+  return cellsCount;
 }
+
+function cellsDepuration(countedCells) {
+  const alive = [];
+  for (const cell in countedCells) {
+    if (countedCells[cell] === 3) {
+      alive.push(parseInt(cell));
+    }
+  }
+  return alive;
+}
+
+function cellsAround(cell) {
+  const cellNeighbours = [];
+  cellNeighbours.push(cell - 11);
+  cellNeighbours.push(cell - 10);
+  cellNeighbours.push(cell - 9);
+  cellNeighbours.push(cell - 1);
+  cellNeighbours.push(cell);
+  cellNeighbours.push(cell + 1);
+  cellNeighbours.push(cell + 9);
+  cellNeighbours.push(cell + 10);
+  cellNeighbours.push(cell + 11);
+
+  return cellNeighbours;
+}
+function touchedCells(arr = []) {
+  let touched = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    touched = [...touched, ...cellsAround(arr[i])];
+  }
+  return touched;
+}
+
+function newPhase(arr = []) {
+  const cellsAroundAlive = touchedCells(arr);
+  const cellsCountAll = removeDupes(cellsAroundAlive);
+  const newCellsAlive = cellsDepuration(cellsCountAll);
+  console.log(`cellsDepuration: ${newCellsAlive}`);
+  // newCellsAlive = numToArray(newCellsAlive);
+  return newCellsAlive;
+}
+
+setInterval(() => newPhase(aliveCells), 1000);
