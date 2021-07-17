de441ccf0d0a424db465cedf9db04e8b9dd80ab5
Xavier Lasierra <xlp1998@gmail.com>
2021-07-17 08:22:13 +0200

    Create LifeGrid methods
    -changeCellState to manipulate cells initial state.
    -aliveNeighbours to check how many alive neighbours a cell have
    -cellCheckNext to calculate what will be the state of a single cell next generation
    -nextGeneration to calculate the state of all cells in the next generation

--- xavi-lasierra/challenges/game-of-life/src/game-of-life.js
+++ xavi-lasierra/challenges/game-of-life/src/game-of-life.js
@@ -1,11 +1,57 @@
 class LifeGrid {
   constructor(size) {
-    const width = 16 * size;
-    const height = 8 * size;
+    this.width = 16 * size;
+    this.height = 8 * size;
 
-    for (let i = 0; i < height; i += 1) {
-      this[height] = new Array(width).fill(0);
+    for (let i = 0; i < this.height; i += 1) {
+      this[this.height] = new Array(this.width).fill(0);
     }
+  }
+
+  changeCellState(x, y) {
+    if (this[y][x] === 0) {
+      this[y][x] = 1;
+    } else {
+      this[y][x] = 0;
+    }
+  }
+
+  aliveNeighbours(x, y) {
+    let result = 0;
+    for (let i = -1; i <= 1; i += 1) {
+      if (y + i < 0 || y + i >= this.height - 1) {
+        break;
+      }
+      for (let j = -1; j <= 1; j += 1) {
+        if (x + j < 0 || x + j >= this.width - 1 || (i === 0 && j === 0)) {
+          break;
+        }
+        if (this[y + i][x + j] === 1) {
+          result += 1;
+        }
+      }
+    }
+    return result;
+  }
+
+  cellCheckNext(x, y) {
+    if (this[y][x] === 0 && this.aliveNeighbours(x, y) === 3) {
+      return 1;
+    }
+    if (this[y][x] === 1 && (this.aliveNeighbours(x, y) < 2 || this.aliveNeighbours(x, y) > 3)) {
+      return 0;
+    }
+    return this[y][x];
+  }
+
+  nextGeneration() {
+    const aux = new LifeGrid();
+    for (let i = 0; i < this.height; i += 1) {
+      for (let j = 0; j < this.width; j += 1) {
+        aux[i][j] = this.cellCheckNext(j, i);
+      }
+    }
+    return aux;
   }
 }
 
