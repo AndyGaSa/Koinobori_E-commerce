9234b18eb3a33fe4686978d8d7416baf4825e6ac
Matryoshka.es <eugenia.ulko@gmail.com>
2021-07-17 12:38:42 +0200

    Edit test conditions description

--- eugenia-ulko/challenges/Array-methods/stash.js
+++ eugenia-ulko/challenges/Array-methods/stash.js
@@ -16,16 +16,19 @@ test('Length should be equal to 0'), () => {
 // testing PUSH:
 describe('GIVEN a push method', () => {
   describe('WHEN is invoked with one element', () => {
-  });
   describe('and the array is empty', () => {
     test('THEN return 1', () => {
-      expect(true).toBe(false);
+      expect(myArray.push("Skylab")).toBe(1);
     });
   });
 
-  describe('and the array length is 2', () => {
+    describe('and the array.length is 2', () => {
+      beforeEach(() => {
+        myArray.push("true");
+        myArray.push("true");
+    }
     test('THEN return 3', () => {
-      expect(true).toBe(false);
+      expect(myArray.push()).toBe(3);
     });
   });
 });
