2861c94368deab192b08b09269de83949316f1cb
Pau Gallardo <paugallrdocordon@gmail.com>
2021-07-17 18:10:20 +0200

    Solve nextGeneration bugs
    - Change how id str is called to solve bug

--- pau-gallardo/challanges/game-of-life/index.js
+++ pau-gallardo/challanges/game-of-life/index.js
@@ -1,5 +1,40 @@
 const tableFirst = [];
 const tableNext = [];
+
+// Function that swaps alive to dead and viceversa in html and the array
+function whatsMyId() {
+  const id = this.id.split('_');
+  const i = Number(id[0]);
+  const j = Number(id[1]);
+  if (this.className === 'alive') {
+    this.setAttribute('class', 'dead');
+    tableFirst[i][j] = 0;
+  } else {
+    this.setAttribute('class', 'alive');
+    tableFirst[i][j] = 1;
+  }
+}
+
+// Function that creates the initial table given the number of rows and columns
+function createTable(rows, columns) {
+  let rowArray = [];
+  const tableHTML = document.querySelector('.mainTable');
+  for (let i = 0; i < rows; i += 1) {
+    const tr = document.createElement('tr');
+    for (let j = 0; j < columns; j += 1) {
+      rowArray.push(0);
+      const td = document.createElement('td');
+      td.setAttribute('id', `${i}_${j}`);
+      td.addEventListener('click', whatsMyId);
+      td.setAttribute('class', 'dead');
+      tr.appendChild(td);
+    }
+    tableFirst.push(rowArray);
+    rowArray = [];
+    tableHTML.appendChild(tr);
+  }
+}
+
 // Funcition that counts alive neighbours
 function neighbourCount(arr, i, j) {
   let count = 0;
@@ -85,52 +120,21 @@ function amAlive(arr, i, j) {
   return lifeState;
 }
 
-// Function that creates a new array with de new cell status
-function nextGeneration(arr) {
+// Function that creates a new array with de new cell status in tableNext variable
+function nextGeneration() {
   let rowArray = [];
-  for (let i = 0; i < arr[0].length; i += 1) {
-    for (let j = 0; j < arr.length; j += 1) {
-      rowArray.push(amAlive(arr, i, j));
+  let id = '';
+  for (let i = 0; i < tableFirst[0].length; i += 1) {
+    for (let j = 0; j < tableFirst.length; j += 1) {
+      id = `${i}_${j}`;
+      if (amAlive(tableFirst, i, j) === 1) {
+        document.getElementById(id).setAttribute('class', 'alive');
+      } else {
+        document.getElementById(id).setAttribute('class', 'dead');
+      }
+      rowArray.push(amAlive(tableFirst, i, j));
     }
     tableNext.push(rowArray);
     rowArray = [];
   }
-}
-
-// Function that swaps alive to dead and viceversa in html and the array
-function whatsMyId() {
-  const id = this.id.split('_');
-  const i = Number(id[0]);
-  const j = Number(id[1]);
-  if (this.className === 'alive') {
-    this.setAttribute('class', 'dead');
-    tableFirst[i][j] = 0;
-  } else {
-    this.setAttribute('class', 'alive');
-    tableFirst[i][j] = 1;
-  }
-}
-
-// Function that creates the initial table given the number of rows and columns
-function createTable(rows, columns) {
-  let rowArray = [];
-  const tableHTML = document.querySelector('.mainTable');
-  for (let i = 0; i < rows; i += 1) {
-    const tr = document.createElement('tr');
-    for (let j = 0; j < columns; j += 1) {
-      rowArray.push(0);
-      const td = document.createElement('td');
-      td.setAttribute('id', `${i}_${j}`);
-      td.addEventListener('click', whatsMyId);
-      td.setAttribute('class', 'dead');
-      tr.appendChild(td);
-    }
-    tableFirst.push(rowArray);
-    rowArray = [];
-    tableHTML.appendChild(tr);
-  }
-}
-
-function play() {
-
 }
