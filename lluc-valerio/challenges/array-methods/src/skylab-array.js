367b4803885c6b1dd5ca6db671dd9cec52af9150
Lluc Valério <lluc.valerio@gmail.com>
2021-07-17 12:20:39 +0200

    Add methods to skylabArray class
     - push with one parameter
     - pop

--- lluc-valerio/challenges/array-methods/src/skylab-array.js
+++ lluc-valerio/challenges/array-methods/src/skylab-array.js
@@ -1,7 +1,43 @@
+/*
+Implementar una clase SkylabArray que simule el comportamiento de un Array.
+- some
+- find
+- filter
+- map
+*/
 class SkylabArray {
   constructor() {
     this.length = 0;
   }
-}
 
+  push(newValue, ...moreValues) {
+    this[this.length] = newValue;
+    this.length += 1;
+    // for (let i = 0; i < this.length - 1; i += 1) {
+    //   this[this.length] = newValue;
+    //   this.length += 1;
+    // }
+    return this.length;
+  }
+
+  pop() {
+    if (this.length <= 0) {
+      return 0;
+    }
+    delete this[this.length - 1];
+    this.length -= 1;
+    return this.length;
+  }
+
+  map(callback) {
+    const newArray = new SkylabArray();
+
+    for (let index = 0; index < this.length; index += 1) {
+      const newValue = callback(this[index]);
+      newArray.push(newValue);
+    }
+
+    return newArray;
+  }
+}
 module.exports = SkylabArray;
--- lluc-valerio/challenges/array-methods/src/skylab-array.test.js
+++ lluc-valerio/challenges/array-methods/src/skylab-array.test.js
@@ -1,6 +1,88 @@
 const SkylabArray = require('./skylab-array');
-
-test('should have a length equals to 0', () => {
+//
+/*
+ * GIVEN
+ * WHEN
+ * THEN
+ * AND
+ * OR
+ */
+describe('SkylabArray class', () => {
   const myArray = new SkylabArray();
-  expect(myArray.length).toBe(0);
+
+  describe('Given a length property', () => {
+    describe('When is first created', () => {
+      test('Then return 0', () => {
+        expect(myArray.length).toBe(0);
+      });
+    });
+  });
+
+  describe('Given a push method', () => {
+    describe('When is invoked with one value', () => {
+      describe('And the array is empty', () => {
+        test('Then return 1', () => {
+          expect(myArray.push('first element')).toBe(1);
+        });
+      });
+      describe('And the array has 1 value', () => {
+        test('Then myArray[1] = Skylab', () => {
+          myArray.push('Skylab');
+          expect(myArray[1]).toBe('Skylab');
+        });
+      });
+    });
+  });
+  describe('When is invoked with 3 values', () => {
+    describe('And the array has 2 elements', () => {
+      test('Then return 5', () => {
+        expect(myArray.push('Friday', 'Pizza', 'Ñam')).toBe(5);
+      });
+    });
+    describe('And the array has 5 elements', () => {
+      test('Then myArray[3]', () => {
+        expect(myArray[3]).toBe('Pizza');
+      });
+    });
+  });
+
+  // describe('Given a pop method', () => {
+  //   describe('When is invoked', () => {
+  //     describe('And the array has 2 elements', () => {
+  //       test('Then return 1', () => {
+  //         expect(myArray.pop()).toBe(1);
+  //       });
+  //     });
+  //     describe('And the array is Empty', () => {
+  //       myArray.pop();
+  //       test('Then return 0', () => {
+  //         expect(myArray.pop()).toBe(0);
+  //       });
+  //     });
+  //   });
+  // });
+
+  describe('Given a map method', () => {
+    describe('When is invoked', () => {
+      let newArray;
+      beforeEach(() => {
+        myArray.push('mola');
+
+        const mapCallback = (value) => `Skylab: ${value}`;
+        newArray = myArray.map(mapCallback);
+      });
+
+      test('Then return a new array', () => {
+        expect(myArray).not.toEqual(newArray);
+      });
+
+      test('Then return a new array with same length', () => {
+        expect(myArray.length).toEqual(newArray.length);
+      });
+
+      test('Then newArray[0] should contain myArray[0]', () => {
+        expect(myArray[0]).toMatch(myArray[0]);
+      });
+    });
+  });
 });
