40514a02bffb3788d87c7f31e8364cff15fdcfae
Xavier Lasierra <xlp1998@gmail.com>
2021-07-17 08:58:40 +0200

    Fix life grid class
    -Add a grid property to include all the board
    -Adapt the methos to support the new property
    -Fix aliveNeighbor attribute to not use break statements

--- xavi-lasierra/challenges/game-of-life/src/game-of-life.js
+++ xavi-lasierra/challenges/game-of-life/src/game-of-life.js
@@ -3,31 +3,30 @@ class LifeGrid {
     this.width = 16 * size;
     this.height = 8 * size;
 
+    this.grid = new Array(this.height);
     for (let i = 0; i < this.height; i += 1) {
-      this[this.height] = new Array(this.width).fill(0);
+      this.grid[i] = new Array(this.width).fill(0);
     }
   }
 
   changeCellState(x, y) {
-    if (this[y][x] === 0) {
-      this[y][x] = 1;
+    if (this.grid[y][x] === 0) {
+      this.grid[y][x] = 1;
     } else {
-      this[y][x] = 0;
+      this.grid[y][x] = 0;
     }
   }
 
-  aliveNeighbours(x, y) {
+  aliveNeighbors(x, y) {
     let result = 0;
     for (let i = -1; i <= 1; i += 1) {
-      if (y + i < 0 || y + i >= this.height - 1) {
-        break;
-      }
-      for (let j = -1; j <= 1; j += 1) {
-        if (x + j < 0 || x + j >= this.width - 1 || (i === 0 && j === 0)) {
-          break;
-        }
-        if (this[y + i][x + j] === 1) {
-          result += 1;
+      if (y + i >= 0 && y + i <= this.height - 1) {
+        for (let j = -1; j <= 1; j += 1) {
+          if (x + j >= 0 && x + j <= this.width - 1 && !(i === 0 && j === 0)) {
+            if (this.grid[y + i][x + j] === 1) {
+              result += 1;
+            }
+          }
         }
       }
     }
@@ -35,20 +34,20 @@ class LifeGrid {
   }
 
   cellCheckNext(x, y) {
-    if (this[y][x] === 0 && this.aliveNeighbours(x, y) === 3) {
+    if (this.grid[y][x] === 0 && this.aliveNeighbors(x, y) === 3) {
       return 1;
     }
-    if (this[y][x] === 1 && (this.aliveNeighbours(x, y) < 2 || this.aliveNeighbours(x, y) > 3)) {
+    if (this.grid[y][x] === 1 && (this.aliveNeighbors(x, y) < 2 || this.aliveNeighbors(x, y) > 3)) {
       return 0;
     }
-    return this[y][x];
+    return this.grid[y][x];
   }
 
   nextGeneration() {
-    const aux = new LifeGrid();
+    const aux = new LifeGrid(this.height / 8);
     for (let i = 0; i < this.height; i += 1) {
       for (let j = 0; j < this.width; j += 1) {
-        aux[i][j] = this.cellCheckNext(j, i);
+        aux.grid[i][j] = this.cellCheckNext(j, i);
       }
     }
     return aux;
