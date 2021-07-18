1d0c659f4c0769cfe704492b216b7a0d772b6eb9
Alex <alexmunoz91@gmail.com>
2021-07-18 16:51:57 +0200

    Add principal js program

--- alex-muñoz/challenges/game-of-life/src/game-of-life.js
+++ alex-muñoz/challenges/game-of-life/src/game-of-life.js
@@ -1,16 +1,51 @@
-const genOne = [[0, 0, 0], [0, 0, 0], [0, 0, 0,]];
-const genTwo = [];
-const flat = genOne.flat();
-const reducer = (accumulator, currentValue) => accumulator + currentValue;
-function mainLoop() {
-  for (let rows = 0; rows < genOne.length; rows += 1) {
-    for (let columns = 0; columns < genOne[rows].length; columns += 1) {
-      if (genOne[rows][columns] === 0 && flat.reduce(reducer){
-          return true
+let genOne = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0]
+];
+
+function gameOfLife() {
+  const genOneClone = genOne.map((outer) => outer.map((inner) => inner));
+  for (let rows = 1; rows < genOne.length - 1; rows += 1) {
+    for (let columns = 1; columns < genOne[rows].length - 1; columns += 1) {
+      const neighbourArr = [
+        [genOne[rows - 1][columns - 1]],
+        [genOne[rows - 1][columns]],
+        [genOne[rows - 1][columns + 1]],
+        [genOne[rows][columns - 1]],
+        [genOne[rows][columns + 1]],
+        [genOne[rows + 1][columns - 1]],
+        [genOne[rows + 1][columns]],
+        [genOne[rows + 1][columns + 1]]
+      ];
+
+      const neighbourFlat = neighbourArr.flat();
+
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+      if (genOne[rows][columns] === 0 && neighbourFlat.reduce(reducer) === 3) {
+        genOneClone[rows][columns] = 1;
+      } else if (genOne[rows][columns] === 1 && (neighbourFlat.reduce(reducer) < 2
+        || neighbourFlat.reduce(reducer) > 3)) {
+        genOneClone[rows][columns] = 0;
+      } else {
+        genOne[rows][columns] = genOneClone[rows][columns];
       }
     }
   }
-  console.log(reducer);
-  console.log(genOne);
+  [genOne] = [genOneClone];
 }
-mainLoop();
+
+gameOfLife();
+
+/* function startGame() {
+    const start = setInterval(() => { gameOfLife(); }, 5000);
+  }
+
+  function stopGame() {
+    clearInterval(start);
+  } */
