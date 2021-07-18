5daf29d85b1270348de599b1c79ee45933734150
pabloapausa <pabloapausa@outlook.com>
2021-07-18 14:02:34 +0200

    Terminal friendly 'life.js'

--- pabloapausa/challenge/01/game-of-life/life.js
+++ pabloapausa/challenge/01/game-of-life/life.js
@@ -1,7 +1,7 @@
 class Life {
     constructor() {
-        world0 = this.matrix(); 
-        world1 = this.matrix();
+        cell0 = this.matrix();
+        cell1 = this.matrix();
     }
 
     matrix() {
@@ -15,52 +15,105 @@ class Life {
         return array;
     }
 
-    kill() {
+    execute() {
         let neighbours; 
-        for (let x = 0; x <= row; x++) {
-            for (let y = 0; y <= column; y++) {
-                neighbours = 1;
-                neighbours += ((((x + 1) < row) && ((y - 1) > 0)) && (world1[x + 1][y - 1] === 1)) ? 1 : 0; //Top, left.
-                neighbours += ((((x - 1) > 0) && ((y - 1) > 0)) && (world1[x - 1][y - 1] === 1)) ? 1 : 0; //Top, right.
-                neighbours += ((((x + 1) < row) && ((y + 1) < column)) && (world1[x + 1][y + 1] === 1)) ? 1 : 0; //Bottom, left.
-                neighbours += ((((x - 1) > 0) && ((y + 1) < column)) && (world1[x - 1][y + 1] === 1)) ? 1 : 0; //Bottom, right. 
-                neighbours += (((y - 1) > 0) && (world1[x][y - 1] === 1)) ? 1 : 0; //Top, center.
-                neighbours += (((x - 1) > 0) && (world1[x - 1][y] === 1)) ? 1 : 0; //Center, left.
-                neighbours += (((x + 1) < row) && (world1[x + 1][y] === 1)) ? 1 : 0; //Center, right. 
-                neighbours += (((y + 1) < column) && world1[x][y + 1] === 1) ? 1 : 0; //Bottom, center.
-                world1[x][y] = (neighbours === 3 ||  (neighbours === 2 && world1[x][y] === 1)) ? 1 : 0; 
-                [world0, world1] = [world1, world0];
+        for (let x = 1; x < row; x++) {
+            for (let y = 1; y < column; y++) {
+                neighbours = 0;
+                neighbours += (cell0[x - 1][y - 1] === 1) ? 1 : 0; // Top left position.
+                neighbours += (cell0[x][y - 1] === 1) ? 1 : 0; // Top center position.
+                neighbours += (cell0[x + 1][y - 1] === 1) ? 1 : 0; // Top right position.
+                neighbours += (cell0[x - 1][y] === 1) ? 1 : 0; // Center left position.
+                neighbours += (cell0[x + 1][y] === 1) ? 1 : 0; // Center right position. 
+                neighbours += (cell0[x - 1][y + 1] === 1) ? 1 : 0; // Bottom left position. 
+                neighbours += (cell0[x][y + 1] === 1) ? 1 : 0; // Bottom center position.
+                neighbours += (cell0[x + 1][y + 1] === 1) ? 1 : 0; // Bottom right position. 
+                cell1[x][y] = ((neighbours === 3) || (neighbours === 2 && cell0[x][y] === 1)) ? 1 : 0; 
             }
         }
-        console.table(world0);
-        console.table(world1);
+        return cell1;
     }
 
 
 }
 
-let world0;
-let world1;
+let cell0;
+let cell1;
 const [row, column] = [12, 12];
 const life = new Life();
 
 function hello() {
-    life.kill();
+    cell0[6][5] = 1; 
+    cell0[6][6] = 1; 
+    cell0[6][7] = 1; 
+    console.table(cell0);
+    for (let i = 0; i < 5; i++) {
+        life.execute(cell0);
+        console.table(cell1);
+        [cell0, cell1] = [cell1, cell0];  
+    }
+
 }
 
 hello();
 
+/* 
+    neighbours = 1;
+    neighbours += ((((x - 1) > 0) && ((y - 1) > 0)) && (cell0[x - 1][y - 1] === 1)) ? 1 : 0; // Top left position.
+    neighbours += (((y - 1) > 0) && (cell0[x][y - 1] === 1)) ? 1 : 0; // Top center position.
+    neighbours += ((((x + 1) < row) && ((y - 1) > 0)) && (cell0[x + 1][y - 1] === 1)) ? 1 : 0; // Top right position.
+    neighbours += (((x - 1) > 0) && (cell0[x - 1][y] === 1)) ? 1 : 0; // Center left position.
+    neighbours += (((x + 1) < row) && (cell0[x + 1][y] === 1)) ? 1 : 0; // Center right position. 
+    neighbours += ((((x - 1) > 0) && ((y + 1) < column)) && (cell0[x - 1][y + 1] === 1)) ? 1 : 0; // Bottom left position. 
+    neighbours += (((y + 1) < column) && cell0[x][y + 1] === 1) ? 1 : 0; // Bottom center position.
+    neighbours += ((((x + 1) < row) && ((y + 1) < column)) && (cell0[x + 1][y + 1] === 1)) ? 1 : 0; // Bottom right position. 
+    cell0[x][y] = ((neighbours === 3) || (neighbours === 2 && cell0[x][y] === 1)) ? 1 : 0; // Chooses which cells live and which ones die.
 
 
+                    if (cell0[x - 1][y - 1] === 1) neighbours += 1; // Top left position.
+                if (cell0[x][y - 1] === 1) neighbours += 1; // Top center position.
+                if (cell0[x + 1][y - 1] === 1) neighbours += 1; // Top right position.
+                if (cell0[x - 1][y] === 1) neighbours += 1; // Center left position.
+                if (cell0[x + 1][y] === 1) neighbours += 1; // Center right position. 
+                if (cell0[x - 1][y + 1] === 1) neighbours += 1; // Bottom left position. 
+                if (cell0[x][y + 1] === 1) neighbours += 1; // Bottom center position.
+                if (cell0[x + 1][y + 1] === 1) neighbours += 1; // Bottom right position. 
+
+*/
+
+
+
+
+//Genera dos arrays. 
+//Los cambios los comprueba en el primero. 
+//Los cambios se aplican en el segundo. 
 
 /*
-if ((((x + 1) < row) && ((y - 1) > 0)) && (world[x + 1][y - 1] === 1)) neighbours += 1; //Top, left.
-if ((((x - 1) > 0) && ((y - 1) > 0)) && (world[x - 1][y - 1] === 1)) neighbours += 1; //Top, right.
-if ((((x + 1) < row) && ((y + 1) < column)) && (world[x + 1][y + 1] === 1)) neighbours += 1; //Bottom, left.
-if ((((x - 1) > 0) && ((y + 1) < column)) && (world[x - 1][y + 1] === 1)) neighbours += 1; //Bottom, right. 
-if (((y - 1) > 0) && (world[x][y - 1] === 1)) neighbours += 1; //Top, center.
-if (((x - 1) > 0) && (world[x - 1][y] === 1)) neighbours += 1; //Center, left.
-if (((x + 1) < row) && (world[x + 1][y] === 1)) neighbours += 1; //Center, right. 
-if (((y + 1) < column) && world[x][y + 1] === 1) neighbours += 1; //Bottom, center.
-world[x][y] = (neighbours === 3 ||  (neighbours === 2 && world[x][y] === 1)) ? 1 : 0; 
+function searchLife(arr) {
+    initMatrix(life2);
+    for (let i = 1; i < arr.length - 1; i += 1) {
+      for (let j = 1; j < arr.length - 1; j += 1) {
+        const arrayLife = [arr[i - 1][j - 1],
+          arr[i - 1][j], arr[i - 1][j + 1],
+          arr[i][j - 1], arr[i][j + 1],
+          arr[i + 1][j - 1], arr[i + 1][j],
+          arr[i + 1][j + 1]];
+        const sumNeighbours = arrayLife.reduce((i, j) => i + j, 0);
+        if (arr[i][j] === 1) {
+          life2[i][j] = (sumNeighbours === 2 || sumNeighbours === 3) ? 1 : 0;
+        } else if (life2[i][j] === 0) {
+          if (sumNeighbours === 3) life2[i][j] = 1;
+        }
+      }
+    }
+    return life2;
+  }
+  let s = 0;
+  addGrid();
+  while (s < 5) {
+    searchLife(life);
+    console.table(life2);
+    [life, life2] = [life2, life];
+    s += 1;
+  }
 */
