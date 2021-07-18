20a3a884f42630f408e94483b69df5d86c8e9a12
ignacioND <ignacio.navarro.vfx@gmail.com>
2021-07-18 20:28:05 +0200

    Fix more function problems
    - Fix problems with eslint
    - Add css styles for sliders and some elements
    - Change html layout

--- ignacio-navarro/challenges/game-of-life/draw/draw.js
+++ ignacio-navarro/challenges/game-of-life/draw/draw.js
@@ -1,109 +1,61 @@
-let canvas = document.getElementById("canvas-container");
-let ctx = canvas.getContext("2d");  
-let startButton = document.getElementById("start-button");
-let stopButton = document.getElementById("stop-button");
-let resetButton = document.getElementById("reset-button");
-let clearButton = document.getElementById("clear-button");
-let speedSlider = document.getElementById("speed-slider")
+const canvas = document.getElementById('canvas-container');
+const ctx = canvas.getContext('2d');
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+const resetButton = document.getElementById('reset-button');
+const clearButton = document.getElementById('clear-button');
+const speedSlider = document.getElementById('speed-slider');
 let wantedSpeed = 50;
-let startTime =0;
+let startTime = 0;
 let paintMode = false;
 
 let aliveCells = [];
-let cellsReservoir = []
+let cellsReservoir = [];
 aliveCells = [];
-let arraySize = 400
-let parcelSize = 2
+const arraySize = 400;
+const parcelSize = 2;
 
-startButton.addEventListener("click",start)
-stopButton.addEventListener("click",stop)
-resetButton.addEventListener('click',reset)
-clearButton.addEventListener('click',clear)
-speedSlider.addEventListener("change", speedChange)
-canvas.addEventListener("mousedown",()=>{paintMode = true})
-canvas.addEventListener("mouseup",()=>{paintMode = false})
-canvas.addEventListener("mouseleave",()=>{paintMode = false})
-canvas.addEventListener("mousemove",updateCellsArray)
-
-function clear (){
-  aliveCells =[]
-  paintArray(aliveCells)
-}
-function reset (){
-  aliveCells = cellsReservoir
-  paintArray(aliveCells)
-}
-
-function speedChange(){
-  if (startTime >= 1){
-    stop()
-    if (speedSlider.value === "0"){
-      wantedSpeed = 1;
-    } else {
-      wantedSpeed = speedSlider.value;
-    }
-    start()
-  } else {
-    if (speedSlider.value === "0"){
-      wantedSpeed = 1;
-    } else {
-      wantedSpeed = speedSlider.value;
+function newArray(width, height) {
+  const arr = [];
+  for (let i = 0; i < height; i += 1) {
+    arr.push([]);
+    for (let j = 0; j < width; j += 1) {
+      arr[i].push(0);
     }
   }
-  
+  return arr;
 }
-
-function newCell (e){
-  let windowWidth = e.srcElement.clientWidth;
-  let blockSize = windowWidth/arraySize;
-  let clickedCell = [Math.round(e.offsetY / blockSize)+parcelSize, Math.round(e.offsetX / blockSize)]
-  return clickedCell
-}
-function updateCellsArray (e) {
-  if (newCell(e)!==undefined){
-    if (paintMode){
-      aliveCells.push(newCell(e))
-      paintArray(aliveCells)
-      cellsReservoir = aliveCells
-    }
-  }
-  
-  
-}
-function paintArray(arr=[]){
-  let printedArray = newArray(arraySize,arraySize/2)
+function paintArray(arr = []) {
+  const printedArray = newArray(arraySize, arraySize / 2);
   ctx.clearRect(0, 0, 800, 400);
-  for (let i = 0; i < arr.length;i += 1){
+  for (let i = 0; i < arr.length; i += 1) {
     printedArray[arr[i][0]][arr[i][1]] = 1;
-    ctx.fillStyle = 'orange'; 
-    ctx.fillRect(arr[i][1]*parcelSize, arr[i][0]*parcelSize, parcelSize, parcelSize)
+    ctx.fillStyle = 'orange';
+    ctx.fillRect((arr[i][1] * parcelSize), arr[i][0] * parcelSize, parcelSize, parcelSize);
   }
-  return printedArray
+  return printedArray;
 }
-function numToArray(num) {
-  const arrayOfArrays = [];
-  for (let i = 0; i < num.length; i += 1) {
-    let element = num[i].split(",");
-    element[0] = parseInt(element[0])
-    element[1] = parseInt(element[1])
-    arrayOfArrays.push(element);
-  }
-  return arrayOfArrays;
+function clear() {
+  aliveCells = [];
+  paintArray(aliveCells);
+}
+function reset() {
+  aliveCells = cellsReservoir;
+  paintArray(aliveCells);
 }
 function arrToNum(arr = []) {
   const numberArray = [];
   for (let i = 0; i < arr.length; i += 1) {
-    const element = arr[i]
-    let elementArray = element.toString();
+    const element = arr[i];
+    const elementArray = element.toString();
     numberArray.push(elementArray);
   }
   return numberArray;
 }
 function removeDupes(arr) {
   const cellsCount = {};
-  let i = arr.length;
-  while (i--) {
-    if (cellsCount.hasOwnProperty(arr[i])) {
+  for (let i = arr.length; i >= 0; i -= 1) {
+    if (Object.prototype.hasOwnProperty.call(cellsCount, arr[i])) {
       cellsCount[arr[i]] += 1;
       arr.splice(i, 1);
     } else {
@@ -112,50 +64,35 @@ function removeDupes(arr) {
   }
   return cellsCount;
 }
-function cellsDepuration(countedCells) {
-  let stringAlive = arrToNum(aliveCells);
-  const alive = [];
-  for (const cell in countedCells) {
-    if(stringAlive.includes(cell)){
-      if (countedCells[cell] === 3 || countedCells[cell] === 4){
-        alive.push(cell);
-      }
-    } else if (countedCells[cell] === 3) {
-      alive.push(cell);
-    }
+function cellsAround(cell) {
+  const cellNeighbours = [];
+  if ((cell[1] !== 0) && (cell[0] !== 0)) {
+    cellNeighbours.push([cell[0] - 1, cell[1] - 1]);
   }
-  return alive;
+  if ((cell[1] !== arraySize) && (cell[0] !== 0)) {
+    cellNeighbours.push([cell[0] - 1, cell[1] + 1]);
+  }
+  if (cell[0] !== 0) {
+    cellNeighbours.push([cell[0] - 1, cell[1]]);
+  }
+  if (cell[1] !== 0) {
+    cellNeighbours.push([cell[0], cell[1] - 1]);
+  }
+  if (cell[1] !== arraySize) {
+    cellNeighbours.push([cell[0], cell[1] + 1]);
+  }
+  if ((cell[0] !== arraySize / 2 - 1) && (cell[1] !== 0)) {
+    cellNeighbours.push([cell[0] + 1, cell[1] - 1]);
+  }
+  if ((cell[0] !== arraySize / 2 - 1) && (cell[1] !== arraySize)) {
+    cellNeighbours.push([cell[0] + 1, cell[1] + 1]);
+  }
+  if (cell[0] !== arraySize / 2 - 1) {
+    cellNeighbours.push([cell[0] + 1, cell[1]]);
+  }
+  cellNeighbours.push([cell[0], cell[1]]);
+  return cellNeighbours;
 }
-
-  function cellsAround(cell) {
-    const cellNeighbours = [];
-    if ((cell[1] !== 0) && (cell[0] !== 0)){
-      cellNeighbours.push([cell[0] - 1, cell[1] - 1]);
-    }
-    if ((cell[1] !== arraySize) && (cell[0] !== 0)){
-      cellNeighbours.push([cell[0] - 1, cell[1] + 1]);
-    }
-    if (cell[0] !== 0){
-      cellNeighbours.push([cell[0] - 1, cell[1]]);
-    }
-    if (cell[1] !== 0){
-      cellNeighbours.push([cell[0], cell[1]-1]);
-    }
-    if (cell[1] !== arraySize){
-      cellNeighbours.push([cell[0], cell[1]+1]);
-    }
-    if ((cell[0] !== arraySize/2-1) && (cell[1] !== 0)){
-      cellNeighbours.push([cell[0] + 1, cell[1] - 1]);
-    }
-    if ((cell[0] !== arraySize/2-1) && (cell[1] !== arraySize)){
-      cellNeighbours.push([cell[0] + 1, cell[1] + 1]);
-    }
-    if (cell[0] !== arraySize/2-1){
-      cellNeighbours.push([cell[0]+1, cell[1]]);
-    }
-    cellNeighbours.push([cell[0], cell[1]]);
-    return cellNeighbours;
-  }
 function touchedCells(arr = []) {
   let touched = [];
   for (let i = 0; i < arr.length; i += 1) {
@@ -163,15 +100,32 @@ function touchedCells(arr = []) {
   }
   return touched;
 }
-function newArray(width,height){
-  let arr = [];
-  for (let i = 0; i < height; i+=1){
-      arr.push([])
-      for (let j = 0; j < width; j+=1){
-          arr[i].push(0)
+function cellsDepuration(countedCells) {
+  const stringAlive = arrToNum(aliveCells);
+  const alive = [];
+  const countedCellsArray = Object.keys(countedCells);
+  for (let cell = 0; cell < countedCellsArray.length; cell += 1) {
+    if (stringAlive.includes(countedCellsArray[cell])) {
+      if (countedCells[countedCellsArray[cell]] === 3
+        || countedCells[countedCellsArray[cell]] === 4) {
+        alive.push(countedCellsArray[cell]);
       }
+    } else if (countedCells[countedCellsArray[cell]] === 3) {
+      alive.push(countedCellsArray[cell]);
+    }
   }
-  return arr
+
+  return alive;
+}
+function numToArray(num) {
+  const arrayOfArrays = [];
+  for (let i = 0; i < num.length; i += 1) {
+    const element = num[i].split(',');
+    element[0] = Number(element[0]);
+    element[1] = Number(element[1]);
+    arrayOfArrays.push(element);
+  }
+  return arrayOfArrays;
 }
 function newPhase(arr = []) {
   const cellsAroundAlive = touchedCells(arr);
@@ -179,15 +133,58 @@ function newPhase(arr = []) {
   const cellsCountAll = removeDupes(cellsToNumbers);
   const newCellsAlive = cellsDepuration(cellsCountAll);
   const cellsToArray = numToArray(newCellsAlive);
-  paintArray(cellsToArray)
+  paintArray(cellsToArray);
   return cellsToArray;
 }
-
-function start (){
-  startTime = setInterval(() => aliveCells = newPhase(aliveCells), (5000/wantedSpeed));
+function start() {
+  startTime = setInterval(() => { aliveCells = newPhase(aliveCells); }, (5000 / wantedSpeed));
 }
 
-function stop (){
-  clearInterval(startTime)
+function stop() {
+  clearInterval(startTime);
   startTime = 0;
 }
+
+function speedChange() {
+  if (startTime >= 1) {
+    stop();
+    if (speedSlider.value === '0') {
+      wantedSpeed = 1;
+    } else {
+      wantedSpeed = speedSlider.value;
+    }
+    start();
+  } else if (speedSlider.value === '0') {
+    wantedSpeed = 1;
+  } else {
+    wantedSpeed = speedSlider.value;
+  }
+}
+
+function newCell(e) {
+  const windowWidth = e.srcElement.clientWidth;
+  const blockSize = windowWidth / arraySize;
+  const clickedCell = [Math.round(e.offsetY / blockSize) + parcelSize,
+    Math.round(e.offsetX / blockSize)];
+  return clickedCell;
+}
+
+function updateCellsArray(e) {
+  if (newCell(e) !== undefined) {
+    if (paintMode) {
+      aliveCells.push(newCell(e));
+      paintArray(aliveCells);
+      cellsReservoir = aliveCells;
+    }
+  }
+}
+
+startButton.addEventListener('click', start);
+stopButton.addEventListener('click', stop);
+resetButton.addEventListener('click', reset);
+clearButton.addEventListener('click', clear);
+speedSlider.addEventListener('change', speedChange);
+canvas.addEventListener('mousedown', () => { paintMode = true; });
+canvas.addEventListener('mouseup', () => { paintMode = false; });
+canvas.addEventListener('mouseleave', () => { paintMode = false; });
+canvas.addEventListener('mousemove', updateCellsArray);
--- ignacio-navarro/challenges/game-of-life/index.js
+++ ignacio-navarro/challenges/game-of-life/index.js
@@ -1,104 +1,99 @@
-let canvas = document.getElementById("canvas-container");
-let ctx = canvas.getContext("2d");  
-let startButton = document.getElementById("start-button");
-let stopButton = document.getElementById("stop-button");
-let speedSlider = document.getElementById("speed-slider")
-let resetButton = document.getElementById("reset-button");
-let clearButton = document.getElementById("clear-button");
+const canvas = document.getElementById('canvas-container');
+const ctx = canvas.getContext('2d');
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+const speedSlider = document.getElementById('speed-slider');
+const resetButton = document.getElementById('reset-button');
+const clearButton = document.getElementById('clear-button');
 let wantedSpeed = 50;
-let startTime =0;
+let startTime = 0;
+let cellsReservoir;
 
 let aliveCells = [];
 aliveCells = [];
-let arraySize = 80
-let parcelSize = 10
+const arraySize = 80;
+const parcelSize = 10;
 
-
-startButton.addEventListener("click",start)
-stopButton.addEventListener("click",stop)
-resetButton.addEventListener('click',reset)
-clearButton.addEventListener('click',clear)
-canvas.addEventListener("click",updateCellsArray)
-speedSlider.addEventListener("change", speedChange)
-
-function speedChange(){
-  if (startTime >= 1){
-    stop()
-    if (speedSlider.value === "0"){
-      wantedSpeed = 1;
-    } else {
-      wantedSpeed = speedSlider.value;
-    }
-    start()
-  } else {
-    if (speedSlider.value === "0"){
-      wantedSpeed = 1;
-    } else {
-      wantedSpeed = speedSlider.value;
+function newArray(width, height) {
+  const arr = [];
+  for (let i = 0; i < height; i += 1) {
+    arr.push([]);
+    for (let j = 0; j < width; j += 1) {
+      arr[i].push(0);
     }
   }
-  
+  return arr;
 }
-function clear (){
-  aliveCells =[]
-  paintArray(aliveCells)
-}
-function reset (){
-  aliveCells = cellsReservoir
-  paintArray(aliveCells)
-}
-function newCell (e){
-  let windowWidth = e.srcElement.clientWidth;
-  let blockSize = windowWidth/arraySize;
-  let clickedCell = [Math.round((e.offsetY-(parcelSize)) / blockSize), Math.round((e.offsetX-(parcelSize)) / blockSize)]
-  return clickedCell
-}
-function updateCellsArray (e) {
-  let stringAlive = arrToNum(aliveCells);
-  let newCellString = newCell(e).toString()
-  if (stringAlive.includes(newCellString)){
-    aliveCells.splice(stringAlive.indexOf(newCellString),1)
-  } else {
-    aliveCells.push(newCell(e))
-  }
-  cellsReservoir = aliveCells
-  paintArray(aliveCells)
-}
-
-function paintArray(arr=[]){
-  let printedArray = newArray(arraySize,arraySize/2)
+function paintArray(arr = []) {
+  const printedArray = newArray(arraySize, arraySize / 2);
   ctx.clearRect(0, 0, 800, 400);
-  for (let i = 0; i < arr.length;i += 1){
+  for (let i = 0; i < arr.length; i += 1) {
     printedArray[arr[i][0]][arr[i][1]] = 1;
-    ctx.fillStyle = 'orange'; 
-    ctx.fillRect(arr[i][1]*parcelSize, arr[i][0]*parcelSize, parcelSize, parcelSize)
+    ctx.fillStyle = 'orange';
+    ctx.fillRect((arr[i][1] * parcelSize) + 0.5,
+      (arr[i][0] * parcelSize) + 0.5, parcelSize - 1, parcelSize - 1);
   }
-  return printedArray
+  return printedArray;
 }
-function numToArray(num) {
-  const arrayOfArrays = [];
-  for (let i = 0; i < num.length; i += 1) {
-    let element = num[i].split(",");
-    element[0] = parseInt(element[0])
-    element[1] = parseInt(element[1])
-    arrayOfArrays.push(element);
+function clear() {
+  aliveCells = [];
+  paintArray(aliveCells);
+}
+function reset() {
+  aliveCells = cellsReservoir;
+  paintArray(aliveCells);
+}
+function cellsAround(cell) {
+  const cellNeighbours = [];
+  if ((cell[1] !== 0) && (cell[0] !== 0)) {
+    cellNeighbours.push([cell[0] - 1, cell[1] - 1]);
   }
-  return arrayOfArrays;
+  if ((cell[1] !== arraySize) && (cell[0] !== 0)) {
+    cellNeighbours.push([cell[0] - 1, cell[1] + 1]);
+  }
+  if (cell[0] !== 0) {
+    cellNeighbours.push([cell[0] - 1, cell[1]]);
+  }
+  if (cell[1] !== 0) {
+    cellNeighbours.push([cell[0], cell[1] - 1]);
+  }
+  if (cell[1] !== arraySize) {
+    cellNeighbours.push([cell[0], cell[1] + 1]);
+  }
+  if ((cell[0] !== arraySize / 2 - 1) && (cell[1] !== 0)) {
+    cellNeighbours.push([cell[0] + 1, cell[1] - 1]);
+  }
+  if ((cell[0] !== arraySize / 2 - 1) && (cell[1] !== arraySize)) {
+    cellNeighbours.push([cell[0] + 1, cell[1] + 1]);
+  }
+  if (cell[0] !== arraySize / 2 - 1) {
+    cellNeighbours.push([cell[0] + 1, cell[1]]);
+  }
+
+  cellNeighbours.push([cell[0], cell[1]]);
+
+  return cellNeighbours;
 }
 function arrToNum(arr = []) {
   const numberArray = [];
   for (let i = 0; i < arr.length; i += 1) {
-    const element = arr[i]
-    let elementArray = element.toString();
+    const element = arr[i];
+    const elementArray = element.toString();
     numberArray.push(elementArray);
   }
   return numberArray;
 }
+function touchedCells(arr = []) {
+  let touched = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    touched = touched.concat(cellsAround(arr[i]));
+  }
+  return touched;
+}
 function removeDupes(arr) {
   const cellsCount = {};
-  let i = arr.length;
-  while (i--) {
-    if (cellsCount.hasOwnProperty(arr[i])) {
+  for (let i = arr.length; i >= 0; i -= 1) {
+    if (Object.prototype.hasOwnProperty.call(cellsCount, arr[i])) {
       cellsCount[arr[i]] += 1;
       arr.splice(i, 1);
     } else {
@@ -108,68 +103,31 @@ function removeDupes(arr) {
   return cellsCount;
 }
 function cellsDepuration(countedCells) {
-  let stringAlive = arrToNum(aliveCells);
+  const stringAlive = arrToNum(aliveCells);
   const alive = [];
-  for (const cell in countedCells) {
-    if(stringAlive.includes(cell)){
-      if (countedCells[cell] === 3 || countedCells[cell] === 4){
-        alive.push(cell);
+  const countedCellsArray = Object.keys(countedCells);
+  for (let cell = 0; cell < countedCellsArray.length; cell += 1) {
+    if (stringAlive.includes(countedCellsArray[cell])) {
+      if (countedCells[countedCellsArray[cell]] === 3
+        || countedCells[countedCellsArray[cell]] === 4) {
+        alive.push(countedCellsArray[cell]);
       }
-    } else if (countedCells[cell] === 3) {
-      alive.push(cell);
+    } else if (countedCells[countedCellsArray[cell]] === 3) {
+      alive.push(countedCellsArray[cell]);
     }
   }
+
   return alive;
 }
-function cellsAround(cell) {
-  const cellNeighbours = [];
-  if ((cell[1] !== 0) && (cell[0] !== 0)){
-    cellNeighbours.push([cell[0] - 1, cell[1] - 1]);
+function numToArray(num) {
+  const arrayOfArrays = [];
+  for (let i = 0; i < num.length; i += 1) {
+    const element = num[i].split(',');
+    element[0] = Number(element[0]);
+    element[1] = Number(element[1]);
+    arrayOfArrays.push(element);
   }
-  if ((cell[1] !== arraySize) && (cell[0] !== 0)){
-    cellNeighbours.push([cell[0] - 1, cell[1] + 1]);
-  }
-  if (cell[0] !== 0){
-    cellNeighbours.push([cell[0] - 1, cell[1]]);
-  }
-  if (cell[1] !== 0){
-    cellNeighbours.push([cell[0], cell[1]-1]);
-  }
-  if (cell[1] !== arraySize){
-    cellNeighbours.push([cell[0], cell[1]+1]);
-  }
-  if ((cell[0] !== arraySize/2-1) && (cell[1] !== 0)){
-    cellNeighbours.push([cell[0] + 1, cell[1] - 1]);
-  }
-  if ((cell[0] !== arraySize/2-1) && (cell[1] !== arraySize)){
-    cellNeighbours.push([cell[0] + 1, cell[1] + 1]);
-  }
-  if (cell[0] !== arraySize/2-1){
-    cellNeighbours.push([cell[0]+1, cell[1]]);
-  }
-
-  cellNeighbours.push([cell[0], cell[1]]);
-  
-  
-
-  return cellNeighbours;
-}
-function touchedCells(arr = []) {
-  let touched = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    touched = touched.concat(cellsAround(arr[i]));
-  }
-  return touched;
-}
-function newArray(width,height){
-  let arr = [];
-  for (let i = 0; i < height; i+=1){
-      arr.push([])
-      for (let j = 0; j < width; j+=1){
-          arr[i].push(0)
-      }
-  }
-  return arr
+  return arrayOfArrays;
 }
 function newPhase(arr = []) {
   const cellsAroundAlive = touchedCells(arr);
@@ -177,15 +135,52 @@ function newPhase(arr = []) {
   const cellsCountAll = removeDupes(cellsToNumbers);
   const newCellsAlive = cellsDepuration(cellsCountAll);
   const cellsToArray = numToArray(newCellsAlive);
-  paintArray(cellsToArray)
+  paintArray(cellsToArray);
   return cellsToArray;
 }
-
-function start (){
-  startTime = setInterval(() => aliveCells = newPhase(aliveCells), (5000/wantedSpeed));
+function start() {
+  startTime = setInterval(() => { aliveCells = newPhase(aliveCells); }, (5000 / wantedSpeed));
 }
-
-function stop (){
-  clearInterval(startTime)
+function stop() {
+  clearInterval(startTime);
   startTime = 0;
 }
+function newCell(e) {
+  const windowWidth = e.srcElement.clientWidth;
+  const blockSize = windowWidth / arraySize;
+  const clickedCell = [Math.round((e.offsetY - (parcelSize)) / blockSize),
+    Math.round((e.offsetX - (parcelSize)) / blockSize)];
+  return clickedCell;
+}
+function updateCellsArray(e) {
+  const stringAlive = arrToNum(aliveCells);
+  const newCellString = newCell(e).toString();
+  if (stringAlive.includes(newCellString)) {
+    aliveCells.splice(stringAlive.indexOf(newCellString), 1);
+  } else {
+    aliveCells.push(newCell(e));
+  }
+  cellsReservoir = aliveCells;
+  paintArray(aliveCells);
+}
+function speedChange() {
+  if (startTime >= 1) {
+    stop();
+    if (speedSlider.value === '0') {
+      wantedSpeed = 1;
+    } else {
+      wantedSpeed = speedSlider.value;
+    }
+    start();
+  } else if (speedSlider.value === '0') {
+    wantedSpeed = 1;
+  } else {
+    wantedSpeed = speedSlider.value;
+  }
+}
+startButton.addEventListener('click', start);
+stopButton.addEventListener('click', stop);
+resetButton.addEventListener('click', reset);
+clearButton.addEventListener('click', clear);
+canvas.addEventListener('click', updateCellsArray);
+speedSlider.addEventListener('change', speedChange);
