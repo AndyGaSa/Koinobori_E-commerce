f02ccdb7118fec939dd8b1614f7976783075f339
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-18 19:18:43 +0200

    Add method playRound.
    Add function howManyAreAlive to the method.

--- laura-arcidiacono/challenges/game-of-life-laura/src/game-of-life.js
+++ laura-arcidiacono/challenges/game-of-life-laura/src/game-of-life.js
@@ -1,21 +1,79 @@
-class GameOfLife {
+class GameOfLife extends Array { /* extends: toma las propiedades del proto de array. */
   constructor() {
+    super(); /* Invoca al constructor de Array,
+                necesario porque class GameOfLife extends of class Array */
     this.gridSize = 5;
   }
 
   gridArrayGenerator(newGridSize) {
+    let definitiveGridSize;
     if (newGridSize === undefined) {
-      newGridSize = this.gridSize;
+      definitiveGridSize = this.gridSize + 2;
+    } else {
+      definitiveGridSize = newGridSize + 2;
+      this.gridSize = newGridSize;
     }
-    const myGridArray = [];
-    for (let i = 0; i < newGridSize; i += 1) {
+    const myGridArray = new GameOfLife();
+    myGridArray.gridSize = this.gridSize;
+    for (let i = 0; i < definitiveGridSize; i += 1) {
       const temporalArray = [];
-      for (let j = 0; j < newGridSize; j += 1) {
+      for (let j = 0; j < definitiveGridSize; j += 1) {
         temporalArray.push(0);
       }
       myGridArray.push(temporalArray);
     }
     return myGridArray;
+  }
+
+  playRound() {
+    function howManyAreAlived(i, j, myGridArray) {
+      let cellsAlive = 0;
+
+      if (myGridArray[i - 1][j - 1] === 1) {
+        cellsAlive += 1;
+      }
+      if (myGridArray[i - 1][j] === 1) {
+        cellsAlive += 1;
+      }
+      if (myGridArray[i - 1][j + 1] === 1) {
+        cellsAlive += 1;
+      }
+      if (myGridArray[i][j - 1] === 1) {
+        cellsAlive += 1;
+      }
+      if (myGridArray[i][j + 1] === 1) {
+        cellsAlive += 1;
+      }
+      if (myGridArray[i + 1][j - 1] === 1) {
+        cellsAlive += 1;
+      }
+      if (myGridArray[i + 1][j] === 1) {
+        cellsAlive += 1;
+      }
+      if (myGridArray[i + 1][j + 1] === 1) {
+        cellsAlive += 1;
+      }
+      return cellsAlive;
+    }
+    const newGrid = new GameOfLife();
+    const changedGrid = newGrid.gridArrayGenerator(this.gridSize);
+    for (let i = 1; i < this.gridSize + 1; i += 1) {
+      for (let j = 1; j < this.gridSize + 1; j += 1) {
+        if (this[i][j] === 0) {
+          if (howManyAreAlived(i, j, this) === 3) {
+            changedGrid[i][j] = 1;
+          }
+        }
+        if (this[i][j] === 1) {
+          if (howManyAreAlived(i, j, this) < 2 || howManyAreAlived(i, j, this) > 3) {
+            changedGrid[i][j] = 0;
+          } else {
+            changedGrid[i][j] = 1;
+          }
+        }
+      }
+    }
+    return changedGrid;
   }
 }
 
