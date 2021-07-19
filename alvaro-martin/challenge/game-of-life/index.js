de4c484ef07b887e3b74b77bf06ffbddb3b3cf3f
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-07-19 15:21:56 +0200

    Add final version game of life

--- alvaro-martin/challenge/game-of-life/index.js
+++ alvaro-martin/challenge/game-of-life/index.js
@@ -1,9 +1,3 @@
-class Button {
-  constructor() {
-    this.status = 0;
-    this.element = element;
-  }
-}
 // Create a multidimensional array
 let life2 = new Array(10);
 for (let i = 0; i < life2.length; i += 1) { life2[i] = new Array(10); }
@@ -11,8 +5,7 @@ for (let i = 0; i < life2.length; i += 1) { life2[i] = new Array(10); }
 function initMatrix() {
   for (let row = 0; row < life2.length; row += 1) {
     for (let col = 0; col < life2.length; col += 1) {
-      const box = new Button();
-      life2[row][col] = box;
+      life2[row][col] = 0;
     }
   }
 }
@@ -22,29 +15,29 @@ let life = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
-function searchLife(arr) {
+function searchLife() {
   initMatrix(life2);
-  for (let i = 1; i < arr.length - 1; i += 1) {
-    for (let j = 1; j < arr.length - 1; j += 1) {
-      const arrayLife = [arr[i - 1][j - 1],
-        arr[i - 1][j], arr[i - 1][j + 1],
-        arr[i][j - 1], arr[i][j + 1],
-        arr[i + 1][j - 1], arr[i + 1][j],
-        arr[i + 1][j + 1]];
+  for (let i = 1; i < life.length - 1; i += 1) {
+    for (let j = 1; j < life[i].length - 1; j += 1) {
+      const arrayLife = [life[i - 1][j - 1],
+        life[i - 1][j], life[i - 1][j + 1],
+        life[i][j - 1], life[i][j + 1],
+        life[i + 1][j - 1], life[i + 1][j],
+        life[i + 1][j + 1]];
       const sumNeighbours = arrayLife.reduce((i, j) => i + j, 0);
-      if (arr[i][j] === 1) {
+      if (life[i][j] === 1) {
         if (sumNeighbours === 2 || sumNeighbours === 3) {
           life2[i][j] = 1;
         } else {
           life2[i][j] = 0;
         }
-      } else if (life2[i][j] === 0) {
+      } else if (life[i][j] === 0) {
         if (sumNeighbours === 3) {
           life2[i][j] = 1;
         }
