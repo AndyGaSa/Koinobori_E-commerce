57e4444ed50afe7c6ba99e492201b725826a9b17
Gilberto Cao <gilbe.cao@gmail.com>
2021-07-19 16:56:38 +0200

    Add tests

--- gilbe-cao/tour-of-heroes-vanilla/hero.class.js
+++
@@ -1,6 +0,0 @@
-class Hero {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-    }
-}
--- gilbe-cao/tour-of-heroes-vanilla/src/pages/Details/details.js
+++ gilbe-cao/tour-of-heroes-vanilla/src/pages/Details/details.js
@@ -9,19 +9,6 @@
 })()
 */
 
-class DetailsPage {
-    constructor(id, heroes) {
-        this.id = id;
-        this.hero = heroes.find((hero) => hero.id === id)
-    }
-
-    setView() {
-        document.getElementById('hero__title').innerHTML = this.hero.name;
-        document.getElementById('hero__id').innerHTML = this.hero.id;
-        document.getElementById('hero__name').value = this.hero.name;
-    }
-}
-
 const heroId = 12;
 const details = new DetailsPage(heroId, heroes);
 details.setView();
