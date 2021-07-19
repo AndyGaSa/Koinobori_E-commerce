755ca3f5bc54ea61ddfe7583c0b9bf4f5bee88ab
Lola Rufino <lola.ruf.ar@gmail.com>
2021-07-19 19:52:48 +0200

    Configure javascript code
    -Create array
    -Add for loop

---
+++ lola-rufino/challenges/tourofheroes/src/pages/heroes/heroes.js
@@ -0,0 +1,8 @@
+const heroesList = document.getElementById('heroes__list');
+
+for (let index = 0; index < heroes.length; index += 1) {
+  const heroLink = document.createElement('a');
+  heroesList.appendChild(heroLink);
+  heroLink.innerHTML = heroes[index].superhero;
+  heroLink.setAttribute('href', `../details/details.html?${heroes[index].id}`);
+}
