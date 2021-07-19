3fee751d4c3834465c718871e99170075ecec0d3
Xavier Lasierra <xlp1998@gmail.com>
2021-07-19 16:25:53 +0200

    Add details basic structure html and js

---
+++ xavi-lasierra/challenges/tour-of-heroes-vanilla/details/details.js
@@ -0,0 +1,18 @@
+class DetailsPage {
+    constructor(id, heroes) {
+        this.id = id;
+        this.hero = heroes.find((hero) => hero.id === id);
+    }
+
+    setView(){
+        document.getElementById('hero__title').innerHTML = this.hero.name;
+        document.getElementById('hero__id').innerHTML = this.hero.id;
+        document.getElementById('hero__name').value = this.hero.name;
+    }
+}
+
+const id = 12; //create getId(location.search)
+const detail = new DetailsPage(id, heroes);
+detail.setView();
+
+
