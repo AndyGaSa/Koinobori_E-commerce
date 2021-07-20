4741df8de02c324e4ef6b500605facdd2a938d71
Nacho Martín <martin.nacho.31@gmail.com>
2021-07-21 00:46:55 +0200

    Capitalize class name

--- nacho-martin/challenges/tour-of-heroes/src/Details/details.test.js
+++ nacho-martin/challenges/tour-of-heroes/src/Details/details.test.js
@@ -1,14 +1,14 @@
-const detailsPage = require('./details.class');
+const DetailsPage = require('./details.class');
 
 describe('Given a details class', () => {
   describe('When instanced with a valid id and an array', () => {
     test('Then this.id should be equal to id', () => {
-      const page = new detailsPage(5, []);
+      const page = new DetailsPage(5, []);
       expect(page.id === 5).toBe(true);
     });
 
     test('Then this.hero should be an element from the array with a property id equal to id', () => {
-      const page = new detailsPage(6, [{ id: 6, name: 'John' }, {}]);
+      const page = new DetailsPage(6, [{ id: 6, name: 'John' }, {}]);
       expect(page.hero.id === 6).toBe(true);
     });
   });
