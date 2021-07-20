aaafec5c2a05311d41c36553867310882e01d85d
CRISTINA_MARTINEZ <cristina.martinez.hald14@gmail.com>
2021-07-20 06:53:32 +0200

    Add Tour of Heroes files

---
+++ cristina-martinez/Challenges/tourofheroes/myheroes/heroes/hero1.class.js
@@ -0,0 +1,10 @@
+class Hero {
+    constructor(id, name, publisher, alter_ego, first_appearance, characters) {
+      this.id = id;
+      this.name = name;
+      this.publisher = publisher;
+      this.alter_ego = alter_ego;
+      this.first_appearance = first_appearance;
+      this.characters = characters;
+    }
+  }
