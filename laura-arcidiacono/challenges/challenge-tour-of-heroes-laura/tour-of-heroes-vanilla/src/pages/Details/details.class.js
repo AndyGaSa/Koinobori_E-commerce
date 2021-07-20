9e8f59a498b5de663b57ca35e77793087f8ce3d6
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-20 17:30:03 +0200

    Add items to setView.

--- laura-arcidiacono/challenges/challenge-tour-of-heroes-laura/tour-of-heroes-vanilla/src/pages/Details/details.class.js
+++ laura-arcidiacono/challenges/challenge-tour-of-heroes-laura/tour-of-heroes-vanilla/src/pages/Details/details.class.js
@@ -8,6 +8,14 @@ class DetailsPage {
     document.getElementById('hero__title').innerHTML = this.hero.name;
     document.getElementById('hero__id').innerHTML = this.hero.id;
     document.getElementById('hero__name').value = this.hero.name;
+    document.getElementById('hero__slug').value = this.hero.slug;
+    document.getElementById('hero__appearance').value = this.hero.appearence;
+    document.getElementById('hero__powerstats').value = this.hero.powerstats;
+    document.getElementById('hero__biography').value = this.hero.biography;
+    document.getElementById('hero__work').value = this.hero.work;
+    document.getElementById('hero__connections').value = this.hero.connections;
+    document.getElementById('hero__images').value = this.hero.images;
+    
   }
 }
 
