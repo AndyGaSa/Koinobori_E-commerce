49402a3560f68a0f4658a88ed622e7008708d3a2
Pere08 <pripoll91@gmail.com>
2021-07-20 16:18:55 +0200

    a

--- pere-ripoll-cortes/challenges/toh/src/pages/Details/details.class.js
+++ pere-ripoll-cortes/challenges/toh/src/pages/Details/details.class.js
@@ -1,18 +1,14 @@
 class DetailsPage {
   constructor(id, heroes) {
     this.id = id;
-    this.hero = heroes?.find((hero) => hero.id === id);
+    this.hero = heroes.find((hero) => hero.id === id);
   }
 
   setView() {
-    document.getElementById('hero__title').innerHTML = this.hero.name;
-    document.getElementById('hero__id').innerHTML = this.hero.id;
-    document.getElementById('hero__name').value = this.hero.name;
+    document.getElementById('hero__title').innerHTML = this.hero;
+    document.getElementById('hero__id').innerHTML = this.id;
+    document.getElementById('hero__name').value = this.hero;
   }
 }
-
-const id = location.search.split('?')[1];
-const a = new DetailsPage(id);
-window.onload(a);
 
 module.exports = DetailsPage;
