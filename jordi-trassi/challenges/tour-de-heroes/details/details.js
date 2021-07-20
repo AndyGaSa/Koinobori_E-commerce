22993daa7956e70839bb56de48e23719d4b19a36
JordiTrassi <j.trassi@telefonica.net>
2021-07-20 08:52:20 +0200

    Install test enviroment

---
+++ jordi-trassi/challenges/tour-de-heroes/details/details.js
@@ -0,0 +1,38 @@
+// acceder a los heroes
+
+
+// (function details() {
+//     const id = 11;
+//     const currentHero = heroes.find((hero) => hero.id === id);
+    
+//     document.getElementById('hero__title').innerHTML = currentHero.name;
+//     document.getElementById('hero__id').innerHTML = currentHero.id;
+//     document.getElementById('hero__name').value = currentHero.name;
+// })()
+
+// buscar el que tiene el id 5
+// const hero = new Hero(heroes.find(15));
+
+// pintar el nombre y el id
+
+
+// selecciono donde pintar el id y pinto
+
+// selecciono donde pinter el nombre y pinto
+
+class DetailsPage {
+    constructor(id, heroes) {
+        this.id = id;
+        this.hero = heroes.find((hero) => hero.id === id)
+    }
+
+    setView(){
+        document.getElementById('hero__title').innerHTML = currentHero.name;
+        document.getElementById('hero__id').innerHTML = currentHero.id;
+        document.getElementById('hero__name').value = currentHero.name;
+    }
+}
+
+const heroId = 11;
+const details = new DetailsPage(heroId, heroes);
+details.setView();
