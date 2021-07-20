d86e0ab4bd74d62b97f7b89bc97c297a60394b99
Daniela Fermin <fermin.daniela@gmail.com>
2021-07-20 17:54:08 +0200

    add method to details class to search for id and slug

--- daniela-fermin/challenges/super-hero/src/pages/details/detailspage.class.js
+++ daniela-fermin/challenges/super-hero/src/pages/details/detailspage.class.js
@@ -3,6 +3,14 @@ class DetailsPage {
     this.id = id;
     this.slug = slug;
   }
+
+  findHero(superHeroes) {
+    if (this.id) {
+      this.hero = superHeroes.find((hero) => hero.id === this.id);
+    } else if (this.slug) {
+      this.hero = superHeroes.find((hero) => hero.slug === this.slug);
+    }
+  }
 }
 
 module.exports = DetailsPage;
