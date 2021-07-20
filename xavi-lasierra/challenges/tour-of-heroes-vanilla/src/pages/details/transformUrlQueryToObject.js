3592be26bdc008f98b7e09f452a22619cd8438ee
Xavier Lasierra <xlp1998@gmail.com>
2021-07-20 13:27:06 +0200

    Create transformUrlQueryToObject function and test

--- xavi-lasierra/challenges/tour-of-heroes-vanilla/src/pages/details/transformUrlQueryToObject.js
+++ xavi-lasierra/challenges/tour-of-heroes-vanilla/src/pages/details/transformUrlQueryToObject.js
@@ -1,5 +1,15 @@
 function transformUrlQueryToObject(searchString) {
-  return searchString.split('=')[1];
+  const response = {};
+
+  if (searchString) {
+    const queryWithoutQuestionMark = searchString.slice(1, searchString.length);
+    const entries = queryWithoutQuestionMark.split('&');
+    entries.forEach((entry) => {
+      const [key, value] = entry.split('=');
+      response[key] = value;
+    });
+  }
+  return response;
 }
 
 module.exports = transformUrlQueryToObject;
--- xavi-lasierra/challenges/tour-of-heroes-vanilla/src/pages/details/transformUrlQueryToObject.test.js
+++ xavi-lasierra/challenges/tour-of-heroes-vanilla/src/pages/details/transformUrlQueryToObject.test.js
@@ -1,36 +1,48 @@
-const transformUrlQueryToObject = require('./id-getter');
+const transformUrlQueryToObject = require('./transformUrlQueryToObject');
 
 describe('Given a transformUrlQueryToObject function', () => {
-  describe('When is invoked with a valid search string', () => {
+  let query;
+  let response;
+  describe('When is invoked', () => {
     test('Then return an object', () => {
-
-    });
-  });
-
-  describe('When is invoked with an invalid search string', () => {
-    test('Then return an empty object', () => {
-
+      response = transformUrlQueryToObject();
+      expect(typeof response).toBe('object');
     });
   });
 
   describe('When is invoked with id=dc-batman', () => {
-    test('Then id should be a key in the returned object', () => {
-
+    beforeEach(() => {
+      query = '?id=dc-batman';
+      response = transformUrlQueryToObject(query);
     });
-    test('Then id should point to dc-batman', () => {
 
+    test('Then id should be a key in the returned object', () => {
+      const keys = Object.keys(response);
+      expect(keys.some((key) => key === 'id')).toBe(true);
+    });
+
+    test('Then id should point to dc-batman', () => {
+      expect(response.id).toBe('dc-batman');
     });
   });
 
   describe('When is invoked with id=dc-batman and superhero=Batman', () => {
+    beforeEach(() => {
+      query = '?id=dc-batman&superhero=Batman';
+      response = transformUrlQueryToObject(query);
+    });
     test('Then id should be a key in the returned object', () => {
-
+      const keys = Object.keys(response);
+      expect(keys.some((key) => key === 'id')).toBe(true);
     });
+
     test('Then superhero should be a key in the returned object', () => {
-
+      const keys = Object.keys(response);
+      expect(keys.some((key) => key === 'superhero')).toBe(true);
     });
-    test('Then superhero should point to Batman', () => {
 
+    test('Then superhero should point to Batman', () => {
+      expect(response.superhero).toBe('Batman');
     });
   });
 });
