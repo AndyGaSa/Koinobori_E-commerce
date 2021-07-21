5a7e4dfd073ca9c15f0021db8c9e508d83dbe964
Xavier Lasierra <xlp1998@gmail.com>
2021-07-21 17:44:31 +0200

    Create property in local storage to store the maxId

--- xavi-lasierra/challenges/tour-of-heroes-vanilla/src/heroes-local-storage.js
+++ xavi-lasierra/challenges/tour-of-heroes-vanilla/src/heroes-local-storage.js
@@ -1,8 +1,17 @@
 let heroes;
 
+function getMaxId(heroesObject) {
+  return Math.max(...heroesPredefined.map((hero) => +hero.id));
+}
+
 (function heroesLocalStorage() {
   if (localStorage.getItem('heroes') === null) {
     localStorage.setItem('heroes', JSON.stringify(heroesPredefined));
+    localStorage.setItem('heroesMaxId', JSON.stringify(getMaxId(heroesPredefined)));
   }
   heroes = JSON.parse(localStorage.getItem('heroes'));
 }());
+
+function createNewHero() {
+
+}
