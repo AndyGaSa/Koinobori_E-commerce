8d1a3da8f06d37a4bb7b389db6b7bae3a173f10f
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-21 18:16:46 +0200

    Add condition to setItem to localStorage.

--- laura-arcidiacono/challenges/challenge-tour-of-heroes-laura/tour-of-heroes-vanilla/src/pages/Dashboard/dashboard.js
+++ laura-arcidiacono/challenges/challenge-tour-of-heroes-laura/tour-of-heroes-vanilla/src/pages/Dashboard/dashboard.js
@@ -1,4 +1,7 @@
 (function dashboardPageModel() {
+  if (localStorage.setItem('heroes' === null) {
+    localStorage.setItem('heroes', JSON.stringify(heroes));
+  };
   const dashboardPage = new DashboardPage(heroes.slice(1, 5));
   dashboardPage.setView();
 }());
