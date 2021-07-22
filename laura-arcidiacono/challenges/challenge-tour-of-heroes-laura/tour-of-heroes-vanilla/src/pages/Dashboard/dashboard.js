d5e290559136b97c4099f26638a9f131894698fb
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-22 23:35:10 +0200

    Add new const heroes.
    -Refere to localSotrage.

--- laura-arcidiacono/challenges/challenge-tour-of-heroes-laura/tour-of-heroes-vanilla/src/pages/Dashboard/dashboard.js
+++ laura-arcidiacono/challenges/challenge-tour-of-heroes-laura/tour-of-heroes-vanilla/src/pages/Dashboard/dashboard.js
@@ -1,7 +1,5 @@
 (function dashboardPageModel() {
-  if (localStorage.setItem('heroes' === null) {
-    localStorage.setItem('heroes', JSON.stringify(heroes));
-  };
+  const heroes = JSON.parse(localStorage.getItem('heroes'));
   const dashboardPage = new DashboardPage(heroes.slice(1, 5));
   dashboardPage.setView();
 }());
