b606d3063d2d2820d14ce85d7c563a8d92affe9e
Xavier Lasierra <xlp1998@gmail.com>
2021-07-24 09:12:04 +0200

    Add functionality to input to change between pages

--- xavi-lasierra/challenges/pokemon-api/src/pages/pokemon-list/pokemon-list.js
+++ xavi-lasierra/challenges/pokemon-api/src/pages/pokemon-list/pokemon-list.js
@@ -1,18 +1,50 @@
 let pokemonList;
 let currentPage;
-let numberOfPokemons;
+let pokemonsPerPage;
 const NUMOFPOKE = 150;
 
-function printPage(numberOfPokemons, currentPage) {
-  pokemonList.clearView();
-  pokemonList.setView(numberOfPokemons, currentPage);
+function setButtons(page, lastPage) {
+  document.querySelectorAll('.navigation__previous').forEach((button) => {
+    button.classList = 'navigation__previous';
+  });
+  document.querySelectorAll('.navigation__next').forEach((button) => {
+    button.classList = 'navigation__next';
+  });
+
+  if (page === 0) {
+    document.querySelectorAll('.navigation__previous').forEach((button) => {
+      button.classList = 'navigation__previous hide';
+    });
+  } else if (page === lastPage) {
+    document.querySelectorAll('.navigation__next').forEach((button) => {
+      button.classList = 'navigation__next hide';
+    });
+  }
 }
 
-(function startHeroList() {
-  currentPage = 0;
-  numberOfPokemons = 50;
+function setInput(page, lastPage) {
+  const navigationInput = document.getElementById('navigation__input');
+  navigationInput.value = currentPage + 1;
+  navigationInput.max = (lastPage + 1).toString();
+  document.getElementById('navigation__input__max').innerText = lastPage + 1;
+}
+
+function setNavigator(page, lastPage) {
+  setInput(page, lastPage);
+  setButtons(page, lastPage);
+}
+
+function printPage(numberOfPokemons, page) {
+  pokemonList.clearView();
+  setNavigator(page, Math.floor(NUMOFPOKE / numberOfPokemons));
+  pokemonList.setView(numberOfPokemons, page);
+}
+
+(function startPokemonList() {
   pokemonList = new PokemonList();
-  printPage(numberOfPokemons, currentPage);
+  pokemonsPerPage = 20;
+  currentPage = 0;
+  printPage(pokemonsPerPage, currentPage);
 }());
 
 function changePage(option) {
@@ -27,11 +59,34 @@ function changePage(option) {
       currentPage += 1;
       break;
     case 'last':
-      currentPage = NUMOFPOKE / numberOfPokemons;
+      currentPage = Math.floor(NUMOFPOKE / pokemonsPerPage);
       break;
     default:
       currentPage = 0;
       break;
   }
-  printPage(numberOfPokemons, currentPage);
+  printPage(pokemonsPerPage, currentPage);
+}
+
+function changeNumberOfPokemons() {
+  const numberOfPokemons = document.getElementById('navigation__pokemon-number').value;
+  currentPage = Math.floor((currentPage * pokemonsPerPage) / numberOfPokemons);
+  pokemonsPerPage = numberOfPokemons;
+  printPage(pokemonsPerPage, currentPage);
+}
+
+function changePageByNumber() {
+  const page = Math.floor(document.getElementById('navigation__input').value);
+  const { max } = document.getElementById('navigation__input');
+
+  if (page) {
+    if (page < 1) {
+      currentPage = 0;
+    } else if (page > +max) {
+      currentPage = +max - 1;
+    } else {
+      currentPage = page - 1;
+    }
+    printPage(pokemonsPerPage, currentPage);
+  }
 }
