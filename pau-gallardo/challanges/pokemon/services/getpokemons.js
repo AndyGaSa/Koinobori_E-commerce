42e822168e46b5e4f1371fb0a69e13aa2a95ab7b
Pau Gallardo <paugallrdocordon@gmail.com>
2021-07-24 13:18:24 +0200

    Add getPokemons function
    - Add function that creates a custom pokemonObj with the desired properties

--- pau-gallardo/challanges/pokemon/services/getpokemons.js
+++ pau-gallardo/challanges/pokemon/services/getpokemons.js
@@ -1,37 +1,26 @@
-let pokemonsList;
+let pokemonsList = [];
 function getPokemons(howManyPokemons, howManyPokemons) {
-    
     fetch(`https://pokeapi.co/api/v2/pokemon?limit=${howManyPokemons}&offset=${howManyPokemons}`)
       .then((response) => response.json())
       .then((pokemons) => pokemons)
         .then(({results}) => 
-        Promise.all(results.map(({url}) => fetch(`${url}`)
+        Promise.all(results.map(({url}) => 
+        fetch(`${url}`)
             .then((response2) => response2.json())
-            .then((allPokemonStuff) => allPokemonStuff.map(({
-                id,name,sprites}) => {
-                    //let pokemon = {
-                    //    id,
-                    //    name,
-                    //    sprite: sprites.front_default,
-                    //    //types: types.type.name,
-                    //};
-                    //return pokemon;
-                 }))))
-        )
-            
-        
-
-        //console.log(response.json())
-        // .then(pokemons.map(({id, name, sprites, types}) => {
-        //     let singlePokemon = {
-        //         id,
-        //         name,
-        //         sprite: sprites.front_default,
-        //         type: types.type.name,
-        //     }
-        //     return singlePokemon;
-        //}));
-      //})
+            .then((allPokemonStuff) => allPokemonStuff))))
+        .then((pokemons2) => {
+            pokemonsList = pokemons2.map(({
+                id, name, sprites, types
+            }) => {
+                const pokemonObj = {
+                    id,
+                    name,
+                    sprites: sprites.front_default,
+                    types: types[0].type.name,
+                }
+                return pokemonObj;
+            })
+        })
     }
 
     pokemons = getPokemons(100,100);
