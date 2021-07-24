49491b85409be839f8eb7366d35639c0938a9802
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-24 18:27:42 +0200

    Add functions
    -Add function createElementWithClas.
    -Add function createPokeCArd.

---
+++ laura-arcidiacono/challenges/poke-api-laura/src/List/paint-card.js
@@ -0,0 +1,23 @@
+function createElementWithClass(type, className) {
+  const element = document.createElement(type);
+  element.className = className;
+  return element;
+}
+
+function createPokeCard(maxCards) {
+  for (let i = 0; i < maxCards; i += 1) {
+    const sectionList = document.getElementById('section__list');
+    const pokeCard = createElementWithClass('a', 'list__poke-card');
+    const pokeName = createElementWithClass('h3', 'list__poke-name');
+    const pokeId = createElementWithClass('h4', 'list__poke-id');
+    const pokeImg = createElementWithClass('img', 'list__poke-img');
+    sectionList.appendChild(pokeCard);
+    pokeCard.href = '#';
+    pokeCard.appendChild(pokeName);
+    pokeName.innerHTML = `${pokeArray[i].name}`;
+    pokeCard.appendChild(pokeId);
+    pokeId.innerHTML = `${pokeArray[i].id}`;
+    pokeCard.appendChild(pokeImg);
+    pokeImg.src = `${pokeArray[i].sprites.front_default}`;
+  }
+}
