c3821e68abe0d8b615872e3d4458bb96bdb7080a
Pau Gallardo <paugallrdocordon@gmail.com>
2021-07-25 10:22:57 +0200

    Fix syncronism bug
    - Include in the getPokemon promise html renderin

--- pau-gallardo/challanges/pokemon/scr/pages/pokedex/pokedexPage.class.js
+++ pau-gallardo/challanges/pokemon/scr/pages/pokedex/pokedexPage.class.js
@@ -11,25 +11,27 @@ class pokedexPage{
             localStorage.setItem('pokemons',JSON.stringify(pokeArray));
             localStorage.setItem('pokemonsShown',JSON.stringify(this.pokemonsShown));
             localStorage.setItem('pokemonsPage',JSON.stringify(this.pokemonsPage));
+            return pokeArray;
         })
-        this.pokemonsList = JSON.parse(localStorage.getItem('pokemons'));
-        
-        this.pokemonsList.forEach((pokemon) => {
-            const element = 
-            `<a class="pokecontainer__card" href="../Details/details.html?pokemonId=${pokemon.id}">
-                <img src="${pokemon.sprites}" class="pokecontainer__card__img"alt="pokemon ${pokemon.name}"></img>
-                <div class="pokecontainer__card__stats">
-                    <div class="pokecontainer__card__stats__text">
-                        <span class="pokecontainer__card__stats__text__name">${pokemon.name}</span>
-                        <span class="pokecontainer__card__stats__text__id">${pokemon.id}</span>
+        .then((pokeArray) => {
+            this.pokemonsList = pokeArray; 
+            this.pokemonsList.forEach((pokemon) => {
+                const element = 
+                `<a class="pokecontainer__card" href="../Details/details.html?pokemonId=${pokemon.id}">
+                    <img src="${pokemon.sprites}" class="pokecontainer__card__img"alt="pokemon ${pokemon.name}"></img>
+                    <div class="pokecontainer__card__stats">
+                        <div class="pokecontainer__card__stats__text">
+                            <span class="pokecontainer__card__stats__text__name">${pokemon.name}</span>
+                            <span class="pokecontainer__card__stats__text__id">${pokemon.id}</span>
+                        </div>
+                        <div class="pokecontainer__card__stats__type-container">
+                            <span class="pokecontainer__card__stats__type-container__type">${pokemon.types}</span>
+                        </div>
                     </div>
-                    <div class="pokecontainer__card__stats__type-container">
-                        <span class="pokecontainer__card__stats__type-container__type">${pokemon.types}</span>
-                    </div>
-                </div>
-            </a>`;
-            let pokemonContainer = document.querySelector('.main__nav__pokemons-container');
-            pokemonContainer.innerHTML += element;
-        });
+                </a>`;
+                let pokemonContainer = document.querySelector('.main__nav__pokemons-container');
+                pokemonContainer.innerHTML += element;
+            });
+        })
     }
 }
