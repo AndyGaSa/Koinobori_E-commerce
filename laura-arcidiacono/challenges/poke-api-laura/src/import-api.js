0f4c6641d4c4f0b93d8566ac27f802d16265a9f3
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-25 14:56:39 +0200

    Add funtions
    -Add createElementWithClass(type, className)
    -Add createPokeCard(limit, offset)
    -Add  nextPage()
    -AddpreviousPage()
    -Add addPokeToArray(data).

--- laura-arcidiacono/challenges/poke-api-laura/src/import-api.js
+++ laura-arcidiacono/challenges/poke-api-laura/src/import-api.js
@@ -1,13 +1,76 @@
 const pokeArray = [];
+const firstPokesToShow = 5;
+const maxPokesInMyArray = 350;
+let currentPage = 1;
+const numberOfCardsToShowPerPage = 5;
+
+function createElementWithClass(type, className) {
+  const element = document.createElement(type);
+  element.className = className;
+  return element;
+}
+
+function createPokeCard(limit, offset) {
+  document.getElementById('section__list').innerHTML = '';
+  /* TODO, si limit +offset > length, no ejecutar asi. */
+  for (let item = offset; item < limit + offset; item += 1) {
+    const sectionList = document.getElementById('section__list');
+
+    const pokeCard = createElementWithClass('div', 'list__poke-card');
+    sectionList.appendChild(pokeCard);
+
+    const pokeAnchor = createElementWithClass('a', 'list__poke-anchor');
+    pokeCard.appendChild(pokeAnchor);
+    pokeAnchor.href = '#';
+
+    const pokeName = createElementWithClass('h3', 'list__poke-name');
+    pokeName.innerHTML = `${pokeArray[item].name}`;
+    pokeCard.appendChild(pokeName);
+
+    const pokeImg = createElementWithClass('img', 'list__poke-img');
+    pokeImg.src = `${pokeArray[item].sprites.front_default}`;
+    pokeImg.alt = `Imagen de ${pokeArray[item].name}`;
+    pokeCard.appendChild(pokeImg);
+
+    const pokeId = createElementWithClass('h4', 'list__poke-id');
+    pokeId.innerHTML = `Id: ${pokeArray[item].id}`;
+    pokeCard.appendChild(pokeId);
+  }
+}
+/* funcion que reciba el 5, 10, 20, 30, setee como nuevo limite de cartas porpagina y que despues llame a pokecard
+
+*/
+
+function limitOfCardsPerPage(maxCards) {
+
+}
+
+function nextPage() {
+  createPokeCard(numberOfCardsToShowPerPage, numberOfCardsToShowPerPage * currentPage);
+  currentPage += 1;
+  document.getElementById('currentPage').innerHTML = currentPage;
+}
+function previousPage() {
+  createPokeCard(numberOfCardsToShowPerPage, numberOfCardsToShowPerPage * (currentPage - 1));
+  currentPage -= 1;
+  document.getElementById('currentPage').innerHTML = currentPage;
+}
+
+function addPokeToArray(data) {
+  pokeArray.push(data);
+  if (pokeArray.length === firstPokesToShow) {
+    createPokeCard(firstPokesToShow, 0);
+  }
+}
 
 function getPokesFromApi() {
   if (pokeArray.length === 0) {
-    for (let i = 1; i < 350; i += 1) {
-      fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    for (let item = 1; item <= maxPokesInMyArray; item += 1) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${item}`)
         .then((response) => response.json())
-        .then((data) => pokeArray.push(data));
+        .then((data) => addPokeToArray(data));
     }
-    console.log(pokeArray);// TODO erase console.log()
+    // TODO crear catch error
   }
 }
 getPokesFromApi();
