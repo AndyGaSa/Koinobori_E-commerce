3d7fd9cccfe7fa280e64e9c5fad1a391b29dd493
Lola Rufino <lola.ruf.ar@gmail.com>
2021-07-25 15:44:22 +0200

    Add limit and offset function

--- lola-rufino/challenges/pokemon/src/pages/Pokemons/pokemons.js
+++ lola-rufino/challenges/pokemon/src/pages/Pokemons/pokemons.js
@@ -24,22 +24,28 @@ for (let poke = 1; poke < 51; poke += 1) {
 const parentElement = document.getElementById('main__pokelist');
 const url = 'https://pokeapi.co/api/v2/pokemon/';
 
-for (let poke = 1; poke < 51; poke += 1) {
-  fetch(`${url}${poke}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const newElementAnchor = document.createElement('a');
-      const newElementName = document.createElement('h3');
-      const newElementImage = document.createElement('img');
-      newElementAnchor.className = 'main__poke';
-      newElementName.className = 'main__poke-name';
-      newElementImage.className = 'main__poke-img';
-      newElementName.innerHTML = `${data.name}`;
-      newElementImage.src = `${data.sprites.other.dream_world.front_default}`;
-      newElementAnchor.href = `../Details/details.html?pokemonId=${data.id}`;
-      parentElement.appendChild(newElementAnchor);
-      newElementAnchor.appendChild(newElementName);
-      newElementAnchor.appendChild(newElementImage);
-    })
-    .catch(new Error('Something went wrong'));
+// https://pokeapi.co/api/v2/pokemon?limit=50&offset=0
+
+function setPaginationAndPaint(limit, offset) {
+  for (let poke = offset; poke < limit; poke += offset) {
+    fetch(`${url}${poke}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const newElementAnchor = document.createElement('a');
+        const newElementName = document.createElement('h3');
+        const newElementImage = document.createElement('img');
+        newElementAnchor.className = 'main__poke';
+        newElementName.className = 'main__poke-name';
+        newElementImage.className = 'main__poke-img';
+        newElementName.innerHTML = `${data.name}`;
+        newElementImage.src = `${data.sprites.other.dream_world.front_default}`;
+        newElementAnchor.href = `../Details/details.html?pokemonId=${data.id}`;
+        parentElement.appendChild(newElementAnchor);
+        newElementAnchor.appendChild(newElementName);
+        newElementAnchor.appendChild(newElementImage);
+      })
+      .catch(new Error('Something went wrong'));
+  }
 }
+
+setPaginationAndPaint(50, 1);
