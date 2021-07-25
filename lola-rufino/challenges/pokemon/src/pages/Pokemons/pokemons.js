46dbd48ac1551fd1c8463303af0b19cce845cbce
Lola Rufino <lola.ruf.ar@gmail.com>
2021-07-25 18:55:56 +0200

    Change bar progress color

--- lola-rufino/challenges/pokemon/src/pages/Pokemons/pokemons.js
+++ lola-rufino/challenges/pokemon/src/pages/Pokemons/pokemons.js
@@ -1,32 +1,9 @@
-/* const parentElement = document.getElementById('main__pokelist');
-const url = 'https://pokeapi.co/api/v2/pokemon/';
-
-for (let poke = 1; poke < 51; poke += 1) {
-  fetch(`${url}${poke}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const newElementAnchor = document.createElement('a');
-      const newElementName = document.createElement('h3');
-      const newElementImage = document.createElement('img');
-      newElementAnchor.className = 'main__poke';
-      newElementName.className = 'main__poke-name';
-      newElementImage.className = 'main__poke-img';
-      newElementName.innerHTML = `${data.name}`;
-      newElementImage.src = `${data.sprites.other.dream_world.front_default}`;
-      newElementAnchor.href = `../Details/details.html?pokemonId=${data.id}`;
-      parentElement.appendChild(newElementAnchor);
-      newElementAnchor.appendChild(newElementName);
-      newElementAnchor.appendChild(newElementImage);
-    })
-    .catch(new Error('Something went wrong'));
-} */
-
 const parentElement = document.getElementById('main__pokelist');
 const url = 'https://pokeapi.co/api/v2/pokemon/';
-
-// https://pokeapi.co/api/v2/pokemon?limit=50&offset=0
+let limitSelected;
 
 function setPaginationAndPaint(limit, offset) {
+  parentElement.innerHTML = '';
   for (let poke = offset; poke < limit; poke += offset) {
     fetch(`${url}${poke}`)
       .then((response) => response.json())
@@ -48,4 +25,17 @@ function setPaginationAndPaint(limit, offset) {
   }
 }
 
-setPaginationAndPaint(50, 1);
+setPaginationAndPaint(5, 1);
+
+function getValue() {
+  limitSelected = document.getElementById('viewNum').value;
+  setPaginationAndPaint(limitSelected, 1);
+}
+
+/* function beforeFn() {
+   console.log('anterior');
+}
+
+function nextFn() {
+
+} */
