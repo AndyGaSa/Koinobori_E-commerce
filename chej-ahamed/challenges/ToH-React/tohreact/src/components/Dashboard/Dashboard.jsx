22af7cad1483183ef878e3cef5a432fa4bd00dcc
“Chej <chejahamed@gmail.com>
2021-07-26 21:28:51 +0200

    Add top heroes list

--- chej-ahamed/challenges/ToH-React/tohreact/src/components/Dashboard/Dashboard.jsx
+++ chej-ahamed/challenges/ToH-React/tohreact/src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import Header from '../Header/Header';
+import './Dashboard.css';
+import '../Styles.css';
 
 export default function Dashboard() {
   return (
-    <Header />
+    <>
+      <h2>Top Heroes</h2>
+      <div>Narco</div>
+      <div>Bombasto</div>
+      <div>Celeritas</div>
+      <div>Magneta</div>
+    </>
   );
 }
