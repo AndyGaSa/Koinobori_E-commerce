454c7a3b78d9391af6cab0ab6612993dbb01de50
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-26 21:29:49 +0200

    Import styles and add elements.

--- laura-arcidiacono/challenges/toh-react-laura/toh-react-laura/src/components/Heroes/Heroes.jsx
+++ laura-arcidiacono/challenges/toh-react-laura/toh-react-laura/src/components/Heroes/Heroes.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import Header from '../Header/Header';
+import './Heroes.css';
+import '../Styles.css';
 
 export default function Heroes() {
   return (
-    <Header />
+    <>
+      <h2>My Heroes</h2>
+      <ul>
+        <li>Mr. Nice</li>
+        <li>Narco</li>
+        <li>Bombasto</li>
+        <li>Magneta</li>
+        <li>RubberMan</li>
+        <li>Dynama</li>
+        <li>Dr. IQ</li>
+        <li>Magma</li>
+        <li>Tornado</li>
+      </ul>
+    </>
   );
 }
