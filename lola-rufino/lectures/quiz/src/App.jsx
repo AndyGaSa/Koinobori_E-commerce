1094149b2951053fa54eeef577af0dbf1f7424a4
Lola Rufino <lola.ruf.ar@gmail.com>
2021-07-26 17:41:08 +0200

    Create pages and imports

--- lola-rufino/lectures/quiz/src/App.jsx
+++ lola-rufino/lectures/quiz/src/App.jsx
@@ -1,30 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AuthorQuiz from './pages/AuthorQuiz';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthorQuiz />
   );
 }
-
-export default App;
