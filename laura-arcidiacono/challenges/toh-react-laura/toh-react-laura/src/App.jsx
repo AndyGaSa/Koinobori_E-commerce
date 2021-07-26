231aa84b93acd35a88913f46aaae7f8ef3623c05
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-26 21:27:34 +0200

    Import components.

--- laura-arcidiacono/challenges/toh-react-laura/toh-react-laura/src/App.jsx
+++ laura-arcidiacono/challenges/toh-react-laura/toh-react-laura/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Heroes from './components/Heroes/Heroes';
+import Dashboard from './components/Dashboard/Dashboard';
+import Details from './components/Details/Details';
+import './components/Styles.css';
 
 function App() {
   return (
     <>
       <Header />
+      <Heroes />
+      <Dashboard />
+      <Details />
     </>
   );
 }
