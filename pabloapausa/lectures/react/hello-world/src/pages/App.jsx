50c50d6b3ca45c47b5b73138e830e332178cb375
pabloapausa <pabloapausa@outlook.com>
2021-07-26 18:32:01 +0200

    End 'quiz' on React

---
+++ pabloapausa/lectures/react/hello-world/src/pages/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header/Header';
+import Main from '../components/main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
