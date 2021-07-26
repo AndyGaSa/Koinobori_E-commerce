34be4e44554e543a91badc4a021da6f20fb142d6
Pere08 <pripoll91@gmail.com>
2021-07-26 18:06:15 +0200

    Quiz lecture

--- pere-ripoll-cortes/lectures/lecture-react/mi-app/src/index.js
+++ pere-ripoll-cortes/lectures/lecture-react/mi-app/src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  // eslint-disable-next-line react/jsx-filename-extension
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
