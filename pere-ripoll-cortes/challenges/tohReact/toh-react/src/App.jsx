bb913024e2baa8d1de0b6e34e61886a228341b06
Pere08 <pripoll91@gmail.com>
2021-07-26 18:28:05 +0200

    Set new env to work

---
+++ pere-ripoll-cortes/challenges/tohReact/toh-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Dashboard from './pages/DashBoard/Dashboard';
+import Details from './pages/Details/Details';
+import Heroes from './pages/Heroes/Heroes';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+      <Details />
+      <Heroes />
+    </div>
+  );
+}
+
+export default App;
