f3ba1b6d319674edac2e88e1cac717548bee325a
Daniela Fermin <fermin.daniela@gmail.com>
2021-07-26 19:45:41 +0200

    Add the buttons on the header component

---
+++ daniela-fermin/challenges/toh-react/toh/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+return (
+    <div>
+        <button type = button>Dashboard</button>
+        <button type = button>Heroes</button>
+    </div>
+)
+}
