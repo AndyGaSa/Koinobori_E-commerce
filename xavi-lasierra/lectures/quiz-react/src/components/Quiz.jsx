ec21508892ac2b960d3b8f506bf82fd23878cd4c
Xavier Lasierra <xlp1998@gmail.com>
2021-07-27 00:21:15 +0200

    Add button when answer right

--- xavi-lasierra/lectures/quiz-react/src/components/Quiz.jsx
+++ xavi-lasierra/lectures/quiz-react/src/components/Quiz.jsx
@@ -5,23 +5,33 @@ import './Quiz.css';
 
 function Quiz() {
   const [userAnswer, setAnswer] = useState(null);
+  const [button, showButton] = useState(false);
 
-  const [quiz] = useState({
+  const quiz = {
     books: ['The Shining', 'The Adventures of Huckeleberry Finn', 'Macbeth', 'IT'],
     rightAnswer: 'The Adventures of Huckeleberry Finn',
     authorImage: 'https://upload.wikimedia.org/wikipedia/commons/2/25/Mark_Twain,_Brady-Handy_photo_portrait,_Feb_7,_1871,_cropped.jpg',
     authorName: 'Mark Twain'
-  });
+  };
 
   function isTheRightAnswer(book) {
-    const userAnswerClass = quiz.rightAnswer.toLowerCase() === book.toLowerCase() ? 'quiz--success' : 'quiz--error';
+    const answer = quiz.rightAnswer.toLowerCase() === book.toLowerCase();
+    const userAnswerClass = answer ? 'quiz--success' : 'quiz--error';
     setAnswer(userAnswerClass);
+    showButton(answer);
   }
 
   return (
-    <main className={`quiz d-flex align-items-center container p-3 ${userAnswer}`}>
-      <img className="img-thumbnail" src={quiz.authorImage} alt={quiz.authorName} />
-      <Books books={quiz.books} validate={isTheRightAnswer} />
+    <main className="container">
+      <section className={`quiz d-flex align-items-center p-3 ${userAnswer}`}>
+        <img className="img-thumbnail" src={quiz.authorImage} alt={quiz.authorName} />
+        <Books books={quiz.books} validate={isTheRightAnswer} />
+      </section>
+      {button && (
+        <div className="d-flex justify-content-end">
+          <button type="button" className="btn btn-primary mt-4">Next Quiz</button>
+        </div>
+      )}
     </main>
   );
 }
