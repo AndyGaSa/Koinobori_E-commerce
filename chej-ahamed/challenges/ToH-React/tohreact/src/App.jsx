69e684b1c925cda77b6b8c8c2e6f1bb0a89455c3
“Chej <chejahamed@gmail.com>
2021-07-26 21:27:29 +0200

    Import App.jsx Styles

--- chej-ahamed/challenges/ToH-React/tohreact/src/App.jsx
+++ chej-ahamed/challenges/ToH-React/tohreact/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import Header from './components/Header/Header';
+import Heroes from './components/Heroes/Heroes';
+import Dashboard from './components/Dashboard/Dashboard';
+import Details from './components/Details/Details';
 import './App.css';
+import './components/Styles.css';
 
 function App() {
   return (
     <>
       <Header />
+      <Heroes />
+      <Dashboard />
+      <Details />
+
     </>
   );
 }
