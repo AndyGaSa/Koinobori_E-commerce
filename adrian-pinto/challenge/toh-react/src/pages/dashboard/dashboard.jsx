3d44101d95b799526362cba3f92eb9f593748ecd
Adrian Pinto <adripintogonzalez@gmail.com>
2021-07-27 06:34:23 +0200

    Implement callHero fetch to dashboard

--- adrian-pinto/challenge/toh-react/src/pages/dashboard/dashboard.jsx
+++ adrian-pinto/challenge/toh-react/src/pages/dashboard/dashboard.jsx
@@ -4,19 +4,22 @@ import callHeroe from '../../modules/fetch-api';
 
 const dashboard = ({ test }) => {
   const { status, heroe } = callHeroe('all');
-  console.log(status);
-  console.log(heroe);
-
+  const first = status === 'fetched' ? heroe[0] : null;
+  const second = status === 'fetched' ? heroe[1] : null;
+  const third = status === 'fetched' ? heroe[2] : null;
+  const four = status === 'fetched' ? heroe[3] : null;
   return (
     <>
       <h1>Hero Dashboard</h1>
       <p>{test}</p>
+      {status === 'fetched' && (
       <ul>
-        <li>{heroe[0].name}</li>
-        <li>Hero 2</li>
-        <li>Hero 3</li>
-        <li>Hero 4</li>
+        <li>{first.name}</li>
+        <li>{second.name}</li>
+        <li>{third.name}</li>
+        <li>{four.name}</li>
       </ul>
+      )}
     </>
   );
 };
