df8a56b8028f70a69d8d5c3d2304786eace3e234
Lola Rufino <lola.ruf.ar@gmail.com>
2021-07-27 11:38:44 +0200

    Add map method to paint all heroes in page Heroes

--- lola-rufino/challenges/tourofheroes-react/src/App.jsx
+++ lola-rufino/challenges/tourofheroes-react/src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Heroes from './components/Heroes';
 import Details from './components/Details';
+import Header from './components/Header';
+import Nav from './components/Nav';
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [currentPage, setCurrentPage] = useState('dashboard');
+  const pages = {
+    dashboard: <Dashboard />,
+    heroes: <Heroes />,
+    details: <Details />
+  };
+
   return (
     <>
-      <Dashboard />
-      <Heroes />
-      <Details />
+      <Header />
+      <Nav setCurrentView={setCurrentPage} />
+      {
+        pages[currentPage]
+      }
     </>
   );
 }
--- lola-rufino/challenges/tourofheroes-react/src/components/Dashboard.jsx
+++ lola-rufino/challenges/tourofheroes-react/src/components/Dashboard.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import '../styles.css';
 import './dashboard.css';
-import Header from './Header';
-import Nav from './Nav';
 
 export default function Dashboard() {
   const url = '';
   return (
     <>
-      <Header />
-      <Nav />
       <h2>Top Heroes</h2>
       <a href={url}>Abe Sapien</a>
       <a href={url}>Abin Sur</a>
