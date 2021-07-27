4654dd072cb274365d0d4ebadb3ceb74fcaecb68
Gilberto Cao <gilbe.cao@gmail.com>
2021-07-27 11:50:11 +0200

    Add dashboard and heroes template

--- gilbe-cao/toh-react/src/pages/Dashboard.jsx
+++ gilbe-cao/toh-react/src/pages/Dashboard.jsx
@@ -4,7 +4,29 @@ import './Dashboard.css';
 export default function Dashboard() {
   return (
     <>
-      <p>Dashboard works</p>
+      <h2>Top Heroes</h2>
+      <div className="heroes-menu">
+        <a
+          href="/detail/11"
+        >
+          Dr Nice
+        </a>
+        <a
+          href="/detail/11"
+        >
+          Dr Nice
+        </a>
+        <a
+          href="/detail/11"
+        >
+          Dr Nice
+        </a>
+        <a
+          href="/detail/11"
+        >
+          Dr Nice
+        </a>
+      </div>
     </>
   );
 }
--- gilbe-cao/toh-react/src/pages/Heroes.jsx
+++ gilbe-cao/toh-react/src/pages/Heroes.jsx
@@ -4,7 +4,36 @@ import './Heroes.css';
 export default function Heroes() {
   return (
     <>
-      <p>Heroes works</p>
+      <h2>My Heroes</h2>
+
+      <div>
+        <label htmlFor="new-hero">
+          Hero name:
+          <input id="new-hero" />
+        </label>
+
+        <button className="add-button" type="button">
+          Add hero
+        </button>
+      </div>
+
+      <ul className="heroes">
+        <li>
+          <a href="/detail/11">
+            <span className="badge">11</span>
+            {' '}
+            Dr Nice
+          </a>
+          <button
+            className="delete"
+            title="delete hero"
+            type="button"
+          >
+            x
+
+          </button>
+        </li>
+      </ul>
     </>
   );
 }
