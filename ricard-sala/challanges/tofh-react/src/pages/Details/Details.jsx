d7fcebf44298dcb6d651212a6a1f002793328841
ricardSala <riki_sala@icloud.com>
2021-07-27 22:45:20 +0200

    Import files, create funtion Details

--- ricard-sala/challanges/tofh-react/src/pages/Details/Details.jsx
+++ ricard-sala/challanges/tofh-react/src/pages/Details/Details.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
-import DetailsContainer from '../../components/DetailsContainer/DetailsContainer';
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import './details.css';
 
+import heroesList from '../../assets/Heroes.const';
+
 export default function Details() {
+  const { heroId } = useParams();
+  const [hero, setHero] = useState();
+  useEffect(() => {
+    if (heroId) {
+      localStorage.setItem('heroId', heroId);
+      const foundHero = heroesList.find((currentHero) => currentHero.id === +heroId);
+      setHero(foundHero);
+    }
+  }, [heroId]);
+
   return (
     <>
-      <DetailsContainer />
+      <h2>
+        {hero?.name}
+        {' '}
+        details!
+      </h2>
+      <p>
+        Id:
+        {heroId}
+
+      </p>
+      <p>
+        Name:
+        {' '}
+        <input type="text" value={hero?.name} />
+      </p>
+      <nav>
+        <Link to="/">go back</Link>
+      </nav>
     </>
   );
 }
