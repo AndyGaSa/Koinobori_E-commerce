a67e9972de74d85257bd717b9b0d3f79d4eccdfc
“Chej <chejahamed@gmail.com>
2021-07-27 11:33:32 +0200

    Add header structure

--- chej-ahamed/challenges/ToH-React/tohreact/src/components/Header/Header.jsx
+++ chej-ahamed/challenges/ToH-React/tohreact/src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import '../Styles.css';
 
-export default function Header() {
+export default function Header({ setCurrentView }) {
   return (
     <header>
+
       <h1>Tour of Heroes</h1>
-      <button type="button">Dashboard</button>
-      <button type="button">Heroes</button>
+      <button type="button" onClick={() => setCurrentView('dashboard')}>Dashboard</button>
+      <button type="button" onClick={() => setCurrentView('heroes')}>Heroes</button>
     </header>
   );
 }
