f806a02480aa42a7dc319d7687bba4f6ac425042
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-27 15:13:46 +0200

    Add html structure and "use".
    -Add useStates.

--- laura-arcidiacono/challenges/toh-react-laura/toh-react-laura/src/components/Details/Details.jsx
+++ laura-arcidiacono/challenges/toh-react-laura/toh-react-laura/src/components/Details/Details.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import './Details.css';
-import '../Styles.css';
 
-export default function Details() {
+export default function Details({ match }) {
+  // const [{ heroId }] = useState(match.params.heroId);
+
+  const heroes = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
   return (
-    <>
-      <h3>Magneta details</h3>
-      <p>id: 15</p>
-      <p>Magneta</p>
-    </>
+    <div>
+      <h2>name Details</h2>
+      <div>
+        <span>
+          id:
+        </span>
+        {hero?.id}
+      </div>
+      <div>
+        <label htmlFor="hero-name">Hero name: </label>
+        <input
+          id="hero-name"
+          placeholder="Hero name"
+          value={hero?.name}
+        />
+      </div>
+      <button type="button">go back</button>
+      <button type="button">save</button>
+    </div>
   );
 }
