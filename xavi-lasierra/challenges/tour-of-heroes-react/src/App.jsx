e4931990cc2b4fd7fdfac31cdca712afdea6fec5
Xavier Lasierra <xlp1998@gmail.com>
2021-07-27 10:09:49 +0200

    Change hero-const to a separate file

--- xavi-lasierra/challenges/tour-of-heroes-react/src/App.jsx
+++ xavi-lasierra/challenges/tour-of-heroes-react/src/App.jsx
@@ -4,21 +4,9 @@ import Dashboard from './components/Dashboard';
 import List from './components/List';
 import Detail from './components/Detail';
 import './styles.css';
+import heroes from './heroes-const';
 
 function App() {
-  const heroes = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
-
   const [currentDetail, changeDetailPage] = useState(heroes[0]);
   const [currentPage, setCurrentPage] = useState('dashboard');
 
