e4aef586d8a305ebafc065a727633726ec3f8c2d
Lola Rufino <lola.ruf.ar@gmail.com>
2021-07-27 12:23:32 +0200

    Add navigation with Router

--- lola-rufino/challenges/tourofheroes-react/src/App.jsx
+++ lola-rufino/challenges/tourofheroes-react/src/App.jsx
@@ -1,27 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Heroes from './components/Heroes';
-import Details from './components/Details';
 import Header from './components/Header';
 import Nav from './components/Nav';
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-  const [currentPage, setCurrentPage] = useState('dashboard');
-  const pages = {
-    dashboard: <Dashboard />,
-    heroes: <Heroes />,
-    details: <Details />
-  };
-
   return (
     <>
       <Header />
-      <Nav setCurrentView={setCurrentPage} />
-      {
-        pages[currentPage]
-      }
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/heroes" component={Heroes} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
