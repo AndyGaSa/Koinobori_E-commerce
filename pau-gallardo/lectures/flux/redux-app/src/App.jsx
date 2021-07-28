0515dd87ad6626bbef66476eda3e7b46796865a4
Pau Gallardo <paugallrdocordon@gmail.com>
2021-07-28 16:26:52 +0200

    Add basic structure
    - Add header
    - Add list
    - Add Form
    - Add button

--- pau-gallardo/lectures/flux/redux-app/src/App.jsx
+++ pau-gallardo/lectures/flux/redux-app/src/App.jsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Input from './components/Input/Input';
+import List from './components/List/List';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Input />
+      <List />
+      <button className="btn btn-primary" type="button">This is a button</button>
+    </>
   );
 }
 
---
+++ pau-gallardo/lectures/flux/redux-app/src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <h1>This is a class challenge</h1>
+  );
+}
