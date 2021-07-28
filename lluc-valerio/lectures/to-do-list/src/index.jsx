42db05ac957736a8e688573ed9d0a220f5a57abd
Lluc Valério <lluc.valerio@gmail.com>
2021-07-28 17:49:12 +0200

    Add component

--- lluc-valerio/lectures/to-do-list/src/index.jsx
+++ lluc-valerio/lectures/to-do-list/src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import FormToDoList from './page/FormToDoList';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FormToDoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ lluc-valerio/lectures/to-do-list/src/page/FormToDoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function FromToDoList() {
+  return (
+    <>
+      <h1>To Do List</h1>
+      <br />
+      <input type="text" name="todo" />
+      <button type="button">Save</button>
+      <ul />
+    </>
+  );
+}
