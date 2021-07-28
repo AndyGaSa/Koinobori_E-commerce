5d5a364fc2a534f8e14719fe57f7060f6ab4226d
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-28 19:47:01 +0200

    Add Provider.

--- laura-arcidiacono/challenges/todo-list-laura/src/index.js
+++ laura-arcidiacono/challenges/todo-list-laura/src/index.js
@@ -1,13 +1,15 @@
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import ToDo from './component/ToDo';
+import configureStore from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStore()}>
+      <ToDo />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
