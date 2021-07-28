8aeb553a4adce7e0d8f4ab6ca35ce44d2ff230ed
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-28 15:18:05 +0200

    Add function heroChange

--- laura-arcidiacono/challenges/toh-react-laura/toh-react-laura/src/pages/Details/Details.jsx
+++ laura-arcidiacono/challenges/toh-react-laura/toh-react-laura/src/pages/Details/Details.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/prop-types */
-import React, { useEffect, useRef, useState } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import './Details.css';
 
 import heroes from '../../components/constants/HeroesConst';
+import HeroForm from '../../components/HeroForm/HeroForm';
 
 export default function Details() {
   const { heroId } = useParams();
@@ -18,6 +19,13 @@ export default function Details() {
     }
   }, [heroId]);
 
+  function heroChange(event) {
+    setHero({
+      ...hero,
+      [event.target.name]: event.target.value
+    });
+  }
+
   return (
     <div className="details">
       <h2>
@@ -26,15 +34,14 @@ export default function Details() {
         Details!
       </h2>
       <div>
-        id:
-
         <span>
-          {hero?.id}
-          {' '}
+          id:
         </span>
+        {hero?.id}
       </div>
-      <button type="button" onClick={useHistory().goBack}>go back</button>
-      <button type="button" onClick={() => updateHero(updatedHeroName)}>save</button>
+      <HeroForm hero={hero} heroChange={heroChange} />
+      <button type="button">go back</button>
+      <button type="button">save</button>
     </div>
   );
 }
