61833f93cabce14af83277163d7dbe0006a9e2f7
Pere08 <pripoll91@gmail.com>
2021-07-28 17:56:24 +0200

    firts commit

---
+++ pere-ripoll-cortes/lectures/redux-todoList/todo-list/src/component/ToDo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ToDo() {
+  return (
+    <>
+      <h1>ToDo List</h1>
+      <input type="text" name="todo" />
+      <button type="button"> Save </button>
+      <ul />
+    </>
+  );
+}
