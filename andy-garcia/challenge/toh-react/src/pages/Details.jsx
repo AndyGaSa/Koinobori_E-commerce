d6123243607e176d73ef8d3ac0a98ab7d279b2a5
“Andy <garcia.andy.tecnologies10@gmail.com>
2021-07-28 15:17:56 +0200

    Add redux files

--- andy-garcia/challenge/toh-react/src/pages/Details.jsx
+++ andy-garcia/challenge/toh-react/src/pages/Details.jsx
@@ -2,9 +2,8 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import './Details.css';
-import { Link } from 'react-router-dom';
-import { update } from '../assets/utils';
 import heroesArray from '../const.hero';
+import Form from '../components/Form';
 
 let heroes;
 function updateLocalStorage() {
@@ -14,37 +13,21 @@ updateLocalStorage();
 if (heroes === null) {
   localStorage.setItem('heroes', JSON.stringify(heroesArray));
 }
+
 function Details({ match }) {
   const [heroId] = useState(match.params.heroId);
   const [hero, setHero] = useState(heroes.find(({ id }) => id === +heroId));
+
+  function heroChange(event) {
+    setHero({
+      ...hero,
+      [event.target.name]: event.target.value,
+    });
+  }
+
   return (
     <>
-      <h2>
-        {hero?.name}
-        {' '}
-        details!
-      </h2>
-      <span className="hero-id">
-        id:
-        {hero?.id}
-      </span>
-      <div>
-        <label>
-          name:
-          <input
-            type="text"
-            placeholder="name"
-            value={hero?.name}
-            onChange={(event) => setHero({
-              ...hero,
-              name: event.target.value,
-            })}
-            className="hero-name"
-          />
-        </label>
-      </div>
-      <Link to="/heroes" onClick={() => window.location.reload()} className="goback">go back</Link>
-      <button onClick={() => update(heroId, hero.name)} type="button" className="save">save</button>
+      <Form heroChange={heroChange} hero={hero} />
     </>
   );
 }
