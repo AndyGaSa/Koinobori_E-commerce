a9d851d8d05901db52580e4211e45472682da223
pabloapausa <pabloapausa@outlook.com>
2021-07-29 15:29:58 +0200

    Start new 'toh' project on React

---
+++ pabloapausa/challenge/03/toh-react-29/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter, Route, Redirect, Switch,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Dashboard from './pages/Dashboard';
+import Heroes from './pages/Heroes';
+import Header from './components/Header';
+import Details from './pages/Details';
+import configureStore from './redux/store';
+
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Provider store={configureStore()}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Redirect path="/dashboard" to="/" />
+          <Route path="/heroes" component={Heroes} />
+          <Route path="/details/:heroId" component={Details} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
