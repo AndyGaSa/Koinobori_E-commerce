cd91c7df5d118188e1311d7aaf62d77b5c604e10
Pau Gallardo <paugallrdocordon@gmail.com>
2021-07-29 11:43:50 +0200

    Last commit

--- pau-gallardo/lectures/flux/redux-app/src/redux/reducers/todosReducer.js
+++ pau-gallardo/lectures/flux/redux-app/src/redux/reducers/todosReducer.js
@@ -1,5 +1,5 @@
 import actionTypes from '../actions/action.types';
-
+// Preguntar toDoList donde se almacena
 export default function todosReducer(toDoList = [], action) {
   let newTodoList = toDoList;
   switch (action.type) {
