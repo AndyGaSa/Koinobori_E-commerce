17af77f453cd878c24661753736411e59555b79f
Daniela Fermin <fermin.daniela@gmail.com>
2021-07-29 13:13:05 +0200

    Setup redux and store for the project except the CRUD switch

--- daniela-fermin/lectures/user-list/user-list/src/pages/UserList.jsx
+++ daniela-fermin/lectures/user-list/user-list/src/pages/UserList.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter, Route, Redirect, Switch
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import Header from '../components/Header';
+import ListOfUsers from '../components/ListOfUsers';
+import Details from '../components/Details';
 
 export default function UserList() {
+  return (
+    <Provider store={configureStore()}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ListOfUsers} />
+          <Redirect path={ListOfUsers} to="/" />
+          <Route path="../components/Details" component={Details} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
 
+  );
 }
---
+++ daniela-fermin/lectures/user-list/user-list/src/redux/reducers/userReducer.js
@@ -0,0 +1,3 @@
+export default function usersReducer(users = [], action) {
+  let newUsersList;
+}
