3f1c667c7b574f8540cc1dcc5e899a6d849b9e4d
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-29 13:52:28 +0200

    Add components
    -Add Header.
    -Add UserList.

--- laura-arcidiacono/lectures/user-list-laura/user-list-laura/src/App.jsx
+++ laura-arcidiacono/lectures/user-list-laura/user-list-laura/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './component/Header/Header';
 import './App.css';
+import UsersList from './component/UsersList/UsersList';
 
 function App() {
   return (
-    
+    <>
+      <Header />
+      <UsersList />
+    </>
   );
 }
 
