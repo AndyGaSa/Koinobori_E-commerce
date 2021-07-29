193eb5925d528190f116cef7b397782bec9afda4
Xavier Lasierra <xlp1998@gmail.com>
2021-07-29 18:39:27 +0200

    Fix find maxId when creating a new user

--- xavi-lasierra/lectures/user-list/src/redux/reducers/usersReducer.js
+++ xavi-lasierra/lectures/user-list/src/redux/reducers/usersReducer.js
@@ -5,7 +5,10 @@ function usersReducer(users = usersMock, action) {
   let newUsers = users;
   switch (action.type) {
     case actionTypes.CREATE_USER:
-      newUsers = [...newUsers, { username: action.newUsername.trim(), id: newUsers.length + 1 }];
+      newUsers = [...newUsers, {
+        username: action.newUsername.trim(),
+        id: Math.max.apply(this, newUsers.map(({ id }) => +id)) + 1
+      }];
       break;
     case actionTypes.DELETE_USER:
       newUsers = newUsers.filter(({ id }) => id !== action.userId);
