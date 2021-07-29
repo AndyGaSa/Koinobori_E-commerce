9b80af504389cd9a7a0e11f45f01ba64ca5a4a70
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-29 21:17:22 +0200

    Add imports and configure functions.

--- laura-arcidiacono/lectures/user-list-laura/user-list-laura/src/App.jsx
+++ laura-arcidiacono/lectures/user-list-laura/user-list-laura/src/App.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter, Route, Redirect, Switch
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store';
+
 import Header from './component/Header/Header';
+import UsersList from './pages/UsersList/UsersList';
+import UsersDetails from './pages/details/UsersDetails';
+import NotFound from './pages/NotFound/NotFound';
+
 import './App.css';
-import UsersList from './component/UsersList/UsersList';
 
 function App() {
   return (
-    <>
-      <Header />
-      <UsersList />
-    </>
+    <Provider store={configureStore()}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+
+          <Route path="/" exact component={UsersList} />
+          <Redirect path="/userList" to="/" />
+          <Route path="/details/:userId" component={UsersDetails} />
+          <Route component={NotFound} />
+
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
