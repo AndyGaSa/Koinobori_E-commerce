ecc891f996af12d501a88da0c8b0b45b5d882a60
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-07-29 13:57:17 +0200

    Import usersMock and start to configure cases.

--- laura-arcidiacono/lectures/user-list-laura/user-list-laura/src/redux/reducers/userListReducers.js
+++ laura-arcidiacono/lectures/user-list-laura/user-list-laura/src/redux/reducers/userListReducers.js
@@ -1,26 +1,24 @@
 import actionTypes from '../actions/action.types';
+import usersMock from '../../component/constantUsersList.js/user.mock';
 
-export default function todosReducer(toDoList = [], action) {
-  let newToDoList = toDoList;
+export default function userListReducer(usersList = usersMock, action) {
+  let newUsersList = usersList;
   switch (action.type) {
-    case actionTypes.CREATE_TODO:
-      newToDoList = [...toDoList, action.toDo];
+    case actionTypes.CREATE_USER:
+      newUsersList = [...usersList, action.user];
       break;
 
-    case actionTypes.UPDATE_TODO:
-      newToDoList[action.index] = action.toDo;
-      newToDoList = [
-        ...newToDoList
-      ];
+      /* case actionTypes.UPDATE_USER:
+      if(newUsersList.id === )
       break;
 
-    case actionTypes.DELETE_TODO:
-      newToDoList = toDoList.filter((toDo) => toDo !== action.toDo);
-      break;
+    case actionTypes.DELETE_USER:
+      newUsersList = usersList.filter((user) => user !== action.user);
+      break; */
 
     default:
       break;
   }
 
-  return newToDoList;
+  return newUsersList;
 }
