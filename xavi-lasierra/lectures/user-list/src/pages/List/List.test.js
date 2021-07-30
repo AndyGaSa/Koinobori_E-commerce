614261b34173a73ffa065edb6af806e620ed0bb6
Xavier Lasierra <xlp1998@gmail.com>
2021-07-30 08:05:55 +0200

    Add list testing

--- xavi-lasierra/lectures/user-list/src/pages/List/List.test.js
+++ xavi-lasierra/lectures/user-list/src/pages/List/List.test.js
@@ -1,6 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { fireEvent, screen } from '@testing-library/dom';
+import * as reactRedux from 'react-redux';
+
 import { render } from '../../utils/test-utils';
 import List from './List';
 
@@ -38,6 +40,35 @@ describe('Given a List component', () => {
       fireEvent.change(screen.getByTestId('create-input'), { target: { value: 'Pepe' } });
       fireEvent.click(screen.getByTestId('create-button'));
       expect(screen.getByTestId('create-input').value).toBe('');
+    });
+
+    test('Then dispatch should be called with parameters {type: CREATE_USER, newUsername: Pepe}', () => {
+      const dispatch = jest.fn();
+      const useDispatch = jest.spyOn(reactRedux, 'useDispatch');
+      useDispatch.mockReturnValue(dispatch);
+      render(
+        <List />, {
+          initialState: { users }
+        }
+      );
+
+      fireEvent.change(screen.getByTestId('create-input'), { target: { value: 'Pepe' } });
+      fireEvent.click(screen.getByTestId('create-button'));
+      expect(dispatch).toHaveBeenCalledWith({ type: actionTypes.CREATE_USER, newUsername: 'Pepe' });
+    });
+  });
+  describe('When create user button is clicked with empty userName input', () => {
+    test('Then dispatch should be called with parameters {type: CREATE_USER, newUsername: Pepe}', () => {
+      const dispatch = jest.fn();
+      const useDispatch = jest.spyOn(reactRedux, 'useDispatch');
+      useDispatch.mockReturnValue(dispatch);
+      render(
+        <List />, {
+          initialState: { users }
+        }
+      );
+      fireEvent.click(screen.getByTestId('create-button'));
+      expect(dispatch).not.toHaveBeenCalled();
     });
   });
 });
