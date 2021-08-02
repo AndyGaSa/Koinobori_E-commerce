04a81c8d3e8530a9e354dc9dedcc1a4ef177c4c7
Alex <alexmunoz91@gmail.com>
2021-08-02 20:46:07 +0200

    Add first footer styles

--- 00-TEAMS/06-TEAM/panoramic/src/components/Footer/index.jsx
+++ 00-TEAMS/06-TEAM/panoramic/src/components/Footer/index.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
+import styles from './styles.css';
 
 export default function Footer() {
   return (
-    <h1>Footer</h1>
+
+    <>
+      <div className="main-container">
+        <nav className="main-container__socialmedia">
+          <ul>
+            <li><Link to="facebook.com">F</Link></li>
+            <li><Link to="facebook.com">F</Link></li>
+            <li><Link to="facebook.com">F</Link></li>
+            <li><Link to="facebook.com">F</Link></li>
+            <li><Link to="facebook.com">F</Link></li>
+            <li><a href="www.facebook.com">A</a></li>
+          </ul>
+        </nav>
+        <div className="main-container__contact-form">
+          <h1>Aqui va el formulario</h1>
+        </div>
+
+      </div>
+      <div className="bot-container">
+        <span>Panoramic 2021 © Made with ♡ by Manu, Pau and Alex</span>
+      </div>
+    </>
   );
 }
