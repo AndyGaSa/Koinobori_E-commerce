2a4564b938de203c044b12b662cc41ef72a3128c
“Chej <chejahamed@gmail.com>
2021-08-02 21:33:26 +0200

    Add Header css Add moving letters Structure y configure header

--- koinobori/src/components/Header.jsx
+++ koinobori/src/components/Header.jsx
@@ -1,44 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import './Header.css';
 
 export default function Header() {
   const stockList = useSelector((store) => store.stock);
-  console.log(stockList);
+
+  console.log(stockList?.resources?.logo);
   return (
-    <Header>
+    <header>
       <div className="header__upper">
-        <div className="header__lower">
-          <nav className="header__lower__nav-icons">
+        <p className="discount">FREE WORLDWIDE SHIPPING ON ORDERS OVER 90€</p>
+      </div>
+      <div className="header__lower">
+        <nav className="header__lower__nav-icons">
 
-            <img src="" alt="" />
-
-            <Link to="/details/tshirts">
-              <img src={stockList?.resources?.logo} alt="tshirts" />
+          <Link to="/category/tShirts">
+            <img className="logo" src={stockList?.resources?.logo} alt="tshirts" />
+          </Link>
+          <div className="menu">
+            <Link to="/category/shirts">
+              <img className="icons" src={stockList?.resources?.shirtsIcon} alt="jumpers" />
             </Link>
 
-            <Link to="/details/jumpers">
-              <img src="" alt="jumpers" />
+            <Link to="/category/hoodies">
+              <img className="icons" src={stockList?.resources?.hoodiesIcon} alt="hoodies" />
             </Link>
 
-            <Link to="/details/shorts">
-              <img src="" alt="shorts" />
+            <Link to="/category/trousers">
+              <img className="icons" src={stockList?.resources?.trousersIcon} alt="trousers" />
             </Link>
 
-            <Link to="/details/hats">
-              <img src="" alt="hats" />
+            <Link to="/category/hats">
+              <img className="icons" src={stockList?.resources?.accIcon} alt="hats" />
             </Link>
+          </div>
+          <Link to="/cart">
+            <img className="cart" src={stockList?.resources?.cartIconPurple} alt="cart" />
+          </Link>
 
-            <Link to="/cart">
-              <img src="" alt="cart" />
-            </Link>
-
-          </nav>
-        </div>
+        </nav>
       </div>
 
       <p>{stockList?.clothes?.accessories[0]?.name || 'hola'}</p>
       <p>Hola soy el headers</p>
-    </Header>
+    </header>
   );
 }
