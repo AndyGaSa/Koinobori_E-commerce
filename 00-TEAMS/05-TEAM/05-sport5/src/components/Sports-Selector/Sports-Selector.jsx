efea2f20461ff2e70b975918c99b0133984c1f04
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-02 17:39:50 +0200

    Improve indentation and spacing

--- 00-TEAMS/05-TEAM/05-sport5/src/components/Sports-Selector/Sports-Selector.jsx
+++ 00-TEAMS/05-TEAM/05-sport5/src/components/Sports-Selector/Sports-Selector.jsx
@@ -5,21 +5,27 @@ export default function SportSelector() {
   return (
 
     <nav className="sportSelector">
+
       <button type="button" className="sport-selector__navigation-menu">
         <i className="fas fa-futbol" />
       </button>
+
       <button type="button" className="sport-selector__navigation-menu">
         <i className="fas fa-basketball-ball" />
       </button>
+
       <button type="button" className="sport-selector__navigation-menu">
         <i className="fas fa-baseball-ball" />
       </button>
+
       <button type="button" className="sport-selector__navigation-menu">
         <i className="fas fa-football-ball" />
       </button>
+
       <button type="button" className="sport-selector__navigation-menu">
         <i className="far fa-futbol" />
       </button>
+
       <button type="button" className="sport-selector__navigation-menu">
         <i className="fas fa-hockey-puck" />
       </button>
