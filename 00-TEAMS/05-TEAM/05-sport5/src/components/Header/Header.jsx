070bed6e0669ecf097157972535099510ef1c788
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-02 13:03:23 +0200

    Add script on html with menu icon:
    add title and navigation button
    add header.scss

--- 00-TEAMS/05-TEAM/05-sport5/src/components/Header/Header.jsx
+++ 00-TEAMS/05-TEAM/05-sport5/src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Header.css';
 
 export default function Header() {
   return (
     <>
-      <h1>Hello world</h1>
+      <nav>
+        <button type="button" className="header__navigation-menu">
+          {' '}
+          <i className="fas fa-bars" />
+          {' '}
+        </button>
+        <h1 className="header__main-title">Sport5</h1>
+      </nav>
     </>
   );
 }
