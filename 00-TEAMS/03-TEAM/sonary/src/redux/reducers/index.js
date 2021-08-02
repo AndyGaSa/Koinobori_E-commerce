678389bc3ea1099ae3bf6e9f884aa368df06dba6
Bryan Lc <bryanlc9808@gmail.com>
2021-08-02 13:51:21 +0200

    Add folder redux

---
+++ 00-TEAMS/03-TEAM/sonary/src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars */
+import { combineReducers } from 'redux';
+import todosReducer from './todos.reducer';
+
+export default combineReducers({
+});
