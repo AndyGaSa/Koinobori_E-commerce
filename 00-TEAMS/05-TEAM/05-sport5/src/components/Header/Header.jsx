1c513a5f53d3dea4b0bef941eb1623225bceed3a
Xavier Lasierra <xlp1998@gmail.com>
2021-08-02 16:15:59 +0200

    Add side-menu functionality
    -Create open close button.
    -Add Links to favorites elements.
    -Refine overall header and side-menu style

--- 00-TEAMS/05-TEAM/05-sport5/src/components/Header/Header.jsx
+++ 00-TEAMS/05-TEAM/05-sport5/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SideMenu from '../SideMenu/SideMenu';
 
 import './Header.css';
@@ -6,17 +6,25 @@ import './Header.css';
 import favourites from '../../mock/favourites.mock';
 
 export default function Header() {
+  const [navClass, setNavClass] = useState('side-menu__container');
+
+  function changeNavState() {
+    if (navClass === 'side-menu__container') {
+      setNavClass('side-menu__container side-menu__container--active');
+    } else {
+      setNavClass('side-menu__container');
+    }
+  }
+
   return (
     <header>
       <nav className="header">
-        <button type="button" className="header__navigation-menu">
-          {' '}
+        <button type="button" className="header__navigation-menu" onClick={changeNavState}>
           <i className="fas fa-bars" />
-          {' '}
         </button>
         <h1 className="header__main-title">Sport5</h1>
       </nav>
-      <SideMenu favourites={favourites} />
+      <SideMenu favourites={favourites} navClass={navClass} />
     </header>
   );
 }
--- 00-TEAMS/05-TEAM/05-sport5/src/components/SideMenu/SideMenu.jsx
+++ 00-TEAMS/05-TEAM/05-sport5/src/components/SideMenu/SideMenu.jsx
@@ -1,39 +1,46 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SideMenu.css';
 
-function SideMenu({ favourites }) {
+function SideMenu({ favourites, navClass }) {
   return (
-    <nav className="side-menu">
-      <div>
-        <h3 className="side-menu__favourite-title">
-          <i className="fas fa-star" />
-          Favourite Leagues
-        </h3>
-        <ul>
-          {favourites.favouriteLeagues.map((favouriteLeague) => (
-            <li key={`favourite-league-${favouriteLeague.id}`} className="side-menu__favourite">
-              {favouriteLeague.name}
-              <button className="favourite__delete" type="button">x</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h3 className="side-menu__favourite-title">
-          <i className="fas fa-star" />
-          Favourite Teams
-        </h3>
-        <ul>
-          {favourites.favouriteTeams.map((favouriteTeam) => (
-            <li key={`favourite-team-${favouriteTeam.id}`} className="side-menu__favourite">
-              {favouriteTeam.name}
-              <button className="favourite__delete" type="button">x</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
+    <aside className={navClass}>
+      <nav className="side-menu">
+        <div>
+          <h3 className="side-menu__favourite-title">
+            <i className="fas fa-star" />
+            Favourite Leagues
+          </h3>
+          <ul>
+            {favourites.favouriteLeagues.map((favouriteLeague) => (
+              <li key={`favourite-league-${favouriteLeague.id}`} className="side-menu__favourite">
+                <Link to={`/league/${favouriteLeague.id}`}>
+                  {favouriteLeague.name}
+                </Link>
+                <button className="favourite__delete" type="button">x</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h3 className="side-menu__favourite-title">
+            <i className="fas fa-star" />
+            Favourite Teams
+          </h3>
+          <ul>
+            {favourites.favouriteTeams.map((favouriteTeam) => (
+              <li key={`favourite-team-${favouriteTeam.id}`} className="side-menu__favourite">
+                <Link to={`/team/${favouriteTeam.id}`}>
+                  {favouriteTeam.name}
+                </Link>
+                <button className="favourite__delete" type="button">x</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </aside>
   );
 }
 
