6baf3a58ad7e5cc8c6f7796e02f78f88a44d0b8c
Lola Rufino <lola.ruf.ar@gmail.com>
2021-08-02 20:27:26 +0200

    Add logic for Category and Details

--- 00-TEAMS/01-TEAM/koinobori/src/components/Details.jsx
+++ 00-TEAMS/01-TEAM/koinobori/src/components/Details.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Details() {
+  const stockList = useSelector((store) => store.stock);
+
+  const { stockId } = useParams();
+  const { category } = useParams();
+  const [stock, setStock] = useState();
+
+  useEffect(() => {
+    if (stockId) {
+      const foundStock = stockList.clothes[category].find((stockNow) => stockNow.id === +stockId);
+      setStock(foundStock);
+    }
+  }, [stockId]);
+
+  console.log(stock);
+
   return (
-    <p>Hola soy el Details</p>
+    <main>
+      <h2>{stock?.name}</h2>
+      <section>
+        <section>
+          <figure>
+            <img src={`${stock?.imageFront}`} alt="" />
+            <img src={`${stock?.imageBack}`} alt="" />
+          </figure>
+        </section>
+        <section>
+          <section>{stock?.price}</section>
+          <ul>
+            {
+          stock.description.map((item) => (
+            <li>{`${item}`}</li>
+          ))
+          }
+          </ul>
+          <span>Tax included. Shipping calculated at checkout.</span>
+          <button type="button">ADD TO CART</button>
+        </section>
+      </section>
+    </main>
   );
 }
