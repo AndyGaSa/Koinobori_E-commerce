2e9462ac6f967364f0767c97a1be2c0f16861461
Xavier Lasierra <xlp1998@gmail.com>
2021-08-02 17:24:54 +0200

    Change leagues list style

--- 00-TEAMS/05-TEAM/05-sport5/src/pages/LeaguesList/LeaguesList.jsx
+++ 00-TEAMS/05-TEAM/05-sport5/src/pages/LeaguesList/LeaguesList.jsx
@@ -27,9 +27,10 @@ export default function LeaguesList() {
             <li className="leagues__country">{country[0].toUpperCase()}</li>
             <ul className="leagues__all-leagues">
               {country[1].map((league) => (
-                <li className="leagues__name">
+                <li className="leagues__league">
                   <img src={league.badge} alt={league.name} className="leagues__badge" />
-                  {league.name}
+                  <span className="leagues__name">{league.name}</span>
+                  <i className="fas fa-star" />
                 </li>
               ))}
             </ul>
