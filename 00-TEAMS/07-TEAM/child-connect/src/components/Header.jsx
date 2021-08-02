b66ce87c013dd9825f53d792539cb85f27996732
JordiTrassi <j.trassi@telefonica.net>
2021-08-02 17:48:49 +0200

    Create header files and modifu link in Event file

--- 00-TEAMS/07-TEAM/child-connect/src/components/Header.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/components/Header.jsx
@@ -1,1 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Header.css';
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <Link to="/">
+            <img src="https://drive.google.com/file/d/1hErhyLSEOwzR_QepMIxlAGwUGsa-EG6v/view?usp=sharing" alt="Child Connect Header Logo" />
+          </Link>
+          <section className="header__buttons">
+            <Link to="#" className="buttons__crear-evento">Crear Evento</Link>
+            <div>
+              <Link to="#">Perfil</Link>
+              <Link to="#">Log Out</Link>
+            </div>
+          </section>
+        </nav>
+        <div>
+          <h1>CHILD CONNECT, LA RED SOCIAL PARA LOS MÁS PEQUEÑOS.</h1>
+        </div>
+      </header>
+    </>
+
+  );
+}
