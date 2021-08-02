2c774570d645dd77f33174b0a28382e85389f62d
Xavier Lasierra <xlp1998@gmail.com>
2021-08-02 14:09:43 +0200

    Add side-menu styles

--- 00-TEAMS/05-TEAM/05-sport5/src/components/SideMenu/SideMenu.jsx
+++ 00-TEAMS/05-TEAM/05-sport5/src/components/SideMenu/SideMenu.jsx
@@ -4,17 +4,33 @@ import './SideMenu.css';
 
 function SideMenu({ favourites }) {
   return (
-    <nav>
+    <nav className="side-menu">
       <div>
-        <h3>Favourite Leagues</h3>
+        <h3 className="side-menu__favourite-title">
+          <i className="fas fa-star" />
+          Favourite Leagues
+        </h3>
         <ul>
-          {favourites.favouriteLeagues.map((favouriteLeague) => <li>{favouriteLeague.name}</li>)}
+          {favourites.favouriteLeagues.map((favouriteLeague) => (
+            <li key={`favourite-league-${favouriteLeague.id}`} className="side-menu__favourite">
+              {favouriteLeague.name}
+              <button className="favourite__delete" type="button">x</button>
+            </li>
+          ))}
         </ul>
       </div>
       <div>
-        <h3>Favourite Teams</h3>
+        <h3 className="side-menu__favourite-title">
+          <i className="fas fa-star" />
+          Favourite Teams
+        </h3>
         <ul>
-          {favourites.favouriteTeams.map((favouriteTeam) => <li>{favouriteTeam.name}</li>)}
+          {favourites.favouriteTeams.map((favouriteTeam) => (
+            <li key={`favourite-team-${favouriteTeam.id}`} className="side-menu__favourite">
+              {favouriteTeam.name}
+              <button className="favourite__delete" type="button">x</button>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
