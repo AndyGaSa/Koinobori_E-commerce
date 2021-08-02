1e81c7deb2bb62822b3c766f8579c4ff759f6143
Alex <alexmunoz91@gmail.com>
2021-08-02 14:25:36 +0200

    Add map function in homepage

--- 00-TEAMS/06-TEAM/panoramic/src/pages/Home/Home.jsx
+++ 00-TEAMS/06-TEAM/panoramic/src/pages/Home/Home.jsx
@@ -5,13 +5,17 @@ import { loadArtist } from '../../redux/actions/actionCreators';
 export default function Home() {
   const artist = useSelector((store) => store.artist);
   const dispatch = useDispatch();
-  console.log(artist);
 
   useEffect(() => {
     dispatch(loadArtist());
   }, []);
 
   return (
-    <h1>Home Page</h1>
+    <>
+      <h1>Home Page</h1>
+      <ul>
+        {artist.map((element) => <li>{`${element}`}</li>)}
+      </ul>
+    </>
   );
 }
