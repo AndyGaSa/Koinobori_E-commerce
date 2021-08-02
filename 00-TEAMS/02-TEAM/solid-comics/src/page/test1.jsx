ca087473f184fa9ae7a427132a422c4969e10bc7
Lluc Valério <lluc.valerio@gmail.com>
2021-08-02 17:05:05 +0200

    Connect to marvel api and show info on test component

--- 00-TEAMS/02-TEAM/solid-comics/src/page/test1.jsx
+++ 00-TEAMS/02-TEAM/solid-comics/src/page/test1.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-// import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import loadComics from '../redux/actions/comics.creators';
 
 export default function Test01() {
-//   const comics = useSelector(loadComics());
+  const comics = useSelector((store) => store.comics);
   const dispatch = useDispatch();
-
   dispatch(loadComics());
 
-  // eslint-disable-next-line no-debugger
-  //   debugger;
-  //   useEffect(() => {
-  //     dispatch(loadComics());
-  //   }, []);
   return (
-    <p>helloWorld</p>
+    <div>
+      helloWorld
+      <br />
+      <div>
+        {
+        comics.map((comic) => (<p>{comic.title}</p>))
+      }
+      </div>
+    </div>
   );
 }
