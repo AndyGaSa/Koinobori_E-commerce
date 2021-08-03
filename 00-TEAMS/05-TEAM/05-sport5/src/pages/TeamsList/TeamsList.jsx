fdc62de279f5f129fac07ca412268ae1839d0558
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-03 16:58:01 +0200

    Add teamlist page structure

--- 00-TEAMS/05-TEAM/05-sport5/src/pages/TeamsList/TeamsList.jsx
+++ 00-TEAMS/05-TEAM/05-sport5/src/pages/TeamsList/TeamsList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './TeamsList.scss';
 import getTeams from '../../redux/actions/teams.creator';
 
@@ -8,13 +9,20 @@ export default function TeamsList() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getTeams('Soccer'));
+    dispatch(getTeams('4328'));
   }, []);
 
   return (
     <>
       <ul>
-        {allTeamsPerLeague.map((team) => <li>{team.name}</li>)}
+        {allTeamsPerLeague.map((team) => (
+          <li>
+            <Link to={`/team/${team.id}`}>
+              <img src={team.badge} alt={team.name} />
+              <span>{`${team.name} (team?.shortName)`}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
 
