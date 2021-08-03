98625526e36fd6b9aab74b215fff7312c23d695a
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-03 16:41:58 +0200

    Add 3 new suggested artist
    - Add 3 new suggested artists

--- 00-TEAMS/06-TEAM/panoramic/src/redux/actions/actionCreators.js
+++ 00-TEAMS/06-TEAM/panoramic/src/redux/actions/actionCreators.js
@@ -27,7 +27,7 @@ export function loadAxiosSuggestedArtists() {
     const token = JSON.parse(localStorage.getItem('token'));
     const { data } = await axios({
       method: 'GET',
-      url: 'https://api.spotify.com/v1/artists?ids=5Hsv8dUHHOdnn72q4XIVz7,375zxMmh2cSgUzFFnva0O7,3hfczsQRbhO3io1UwQFM1y',
+      url: 'https://api.spotify.com/v1/artists?ids=5Hsv8dUHHOdnn72q4XIVz7,375zxMmh2cSgUzFFnva0O7,3hfczsQRbhO3io1UwQFM1y,4QVBYiagIaa6ZGSPMbybpy,28gNT5KBp7IjEOQoevXf9N',
       headers: { Authorization: `Bearer ${token}` }
     });
     dispatch({
