22f157f35098dd2d0b90b49bf15375c906136b88
Lluc Valério <lluc.valerio@gmail.com>
2021-08-03 20:12:46 +0200

    Add header
     - Add mobile version header
     - Add desktop version header
     - Add media queries

--- 00-TEAMS/02-TEAM/solid-comics/src/redux/actions/comics.creators.js
+++ 00-TEAMS/02-TEAM/solid-comics/src/redux/actions/comics.creators.js
@@ -3,7 +3,8 @@ import actionTypes from './comics.types';
 
 export default function loadComics() {
   return async (dispatch) => {
-    const { data } = await axios('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=d94eaf06538ee1fbb8dd6605e28f9bd0&hash=769e38a7605236ad957ed87b13ed64a5');
+    // const { data } = await axios('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=d94eaf06538ee1fbb8dd6605e28f9bd0&hash=769e38a7605236ad957ed87b13ed64a5');
+    const { data } = await axios('');
 
     dispatch({
       type: actionTypes.LOAD_COMICS,
