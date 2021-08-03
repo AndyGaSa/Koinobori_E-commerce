fef80416813dc31b9a615dd1e6915bd67b874ea4
Matryoshka.es <eugenia.ulko@gmail.com>
2021-08-03 15:19:18 +0200

    Add Footer component

---
+++ 00-TEAMS/07-TEAM/child-connect/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="footer-terms-list">
+          <ul>
+            <li className="footer-terms-list_item" />
+            <li className="footer-terms-list_item" />
+            <li className="footer-terms-list_item" />
+            <li className="footer-terms-list_item" />
+            <li className="footer-terms-list_item" />
+          </ul>
+        </div>
+      </footer>
+    </>
+
+  );
+}
