765c6c12580038aaffc6e179691d535e2cfcee27
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-03 17:16:23 +0200

    Add dispatch to teamslist and fav button

--- 00-TEAMS/05-TEAM/05-sport5/src/pages/TeamsList/TeamsList.jsx
+++ 00-TEAMS/05-TEAM/05-sport5/src/pages/TeamsList/TeamsList.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './TeamsList.scss';
 import getTeams from '../../redux/actions/teams.creator';
 
 export default function TeamsList() {
   const allTeamsPerLeague = useSelector((store) => store.countriesLeagues.teams);
   const dispatch = useDispatch();
+  const { leagueId } = useParams();
 
   useEffect(() => {
-    dispatch(getTeams('4328'));
-  }, []);
+    dispatch(getTeams(leagueId));
+  }, [leagueId]);
 
   return (
     <>
@@ -21,6 +22,7 @@ export default function TeamsList() {
               <img src={team.badge} alt={team.name} />
               <span>{`${team.name} (team?.shortName)`}</span>
             </Link>
+            <button type="button" aria-label="Add to favourites"><i className="fas fa-star" /></button>
           </li>
         ))}
       </ul>
