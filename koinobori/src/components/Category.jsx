9c617083d8025b128e19c05c801aa617d28f0068
Lola Rufino <lola.ruf.ar@gmail.com>
2021-08-03 16:42:09 +0200

    Add SASS for footer

--- koinobori/src/components/Category.jsx
+++ koinobori/src/components/Category.jsx
@@ -1,7 +1,7 @@
-/* eslint-disable no-debugger */
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import './Trending.scss';
 
 export default function Category() {
   const stockList = useSelector((store) => store.stock);
@@ -11,16 +11,20 @@ export default function Category() {
 
       <section className="main__container">
         <h2 className="main__title">{category}</h2>
-        {stockList.clothes
-      && stockList.clothes[category].map((item) => (
-        <figure className="product__container">
-          <Link to={`/details/${category}/${item.id}`}>
-            <img className="product__image" src={`${item.imageFront}`} alt={`${category}`} />
-            <figcaption>{`${item.name}`}</figcaption>
-            <figcaption>{`${item.price}`}</figcaption>
-          </Link>
-        </figure>
-      ))}
+        <section className="main__products">
+          {stockList.clothes
+          && stockList.clothes[category].map((item) => (
+            <figure className="product__container">
+              <Link className="product__link" to={`/details/${category}/${item.id}`}>
+                <img className="product__image" src={`${item.imageFront}`} alt={`${category}`} />
+                <section className="product__caption">
+                  <figcaption>{`${item.name}`}</figcaption>
+                  <figcaption>{`${item.price}`}</figcaption>
+                </section>
+              </Link>
+            </figure>
+          ))}
+        </section>
       </section>
 
     </main>
