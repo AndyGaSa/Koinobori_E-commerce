9784683e361011b627e1ef5449907d2d75aa6eb8
“Andy <garcia.andy.tecnologies10@gmail.com>
2021-08-03 16:03:07 +0200

    Add flags javascript

--- 00-TEAMS/01-TEAM/koinobori/src/components/Header.jsx
+++ 00-TEAMS/01-TEAM/koinobori/src/components/Header.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './Header.css';
 
-console.log('dddd');
 export default function Header() {
   const stockList = useSelector((store) => store.stock);
   return (
@@ -42,6 +43,34 @@ export default function Header() {
           <Link to="/cart">
             <img className="cart" src={stockList?.resources?.cartIconPurple} alt="cart" />
           </Link>
+
+          <div id="select-container">
+            <ul>
+              <li lang-selection="FR" tooltip="FR" flow="down">
+                <img src="https://cdn.countryflags.com/thumbs/france/flag-800.png" alt="france flag" />
+              </li>
+
+              <li lang-selection="EN" tooltip="EN" flow="down" onClick="onSelect(this)">
+                <img src="https://cdn.countryflags.com/thumbs/united-states-of-america/flag-800.png" alt="america flag" />
+              </li>
+
+              <li lang-selection="ES" tooltip="ES" flow="down" onClick="onSelect(this)">
+                <img src="https://cdn.countryflags.com/thumbs/spain/flag-800.png" alt="spain flag" />
+              </li>
+
+              <li lang-selection="PT" tooltip="PT" flow="down" onClick="onSelect(this)">
+                <img src="https://cdn.countryflags.com/thumbs/portugal/flag-800.png" alt="portugal flag" />
+              </li>
+
+              <li lang-selection="FR" tooltip="FR" flow="up" onClick="onSelect(this)">
+                <img src="https://cdn.countryflags.com/thumbs/france/flag-800.png" alt="france flag" />
+              </li>
+
+              <li lang-selection="DE" tooltip="DE" flow="up" onClick="onSelect(this)">
+                <img src="https://cdn.countryflags.com/thumbs/germany/flag-800.png" alt="germany flag" />
+              </li>
+            </ul>
+          </div>
 
         </nav>
       </div>
