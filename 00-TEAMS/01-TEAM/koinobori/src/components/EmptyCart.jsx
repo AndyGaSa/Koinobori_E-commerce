f2d36428a04ed1a14dc3aef5b867fbdb805601df
Lola Rufino <lola.ruf.ar@gmail.com>
2021-08-04 14:57:15 +0200

    Add media queries for footer

--- 00-TEAMS/01-TEAM/koinobori/src/components/EmptyCart.jsx
+++ 00-TEAMS/01-TEAM/koinobori/src/components/EmptyCart.jsx
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 
 export default function EmptyCart() {
   return (
-    <section className="emptyCartSection">
-      <h1>SHOPPING CART</h1>
-      <figure className="emptyCart">
-        <img className="sadFace" src="https://s3-app.secretbakery.io/clawgame/www-ssstufff-com.myshopify.com/80/caras_filtro-04.png" alt="sadFace" />
-        <figcaption className="caption">Your cart is empty! That’s not cool...</figcaption>
-      </figure>
-      <Link to="/"><button className="backButton" type="button">Go back shopping</button></Link>
-    </section>
+    <main>
+      <section className="emptyCartSection">
+        <h1>SHOPPING CART</h1>
+        <figure className="emptyCart">
+          <img className="sadFace" src="https://s3-app.secretbakery.io/clawgame/www-ssstufff-com.myshopify.com/80/caras_filtro-04.png" alt="sadFace" />
+          <figcaption className="caption">Your cart is empty! That’s not cool...</figcaption>
+        </figure>
+        <Link to="/"><button className="backButton" type="button">Go back shopping</button></Link>
+      </section>
+    </main>
   );
 }
