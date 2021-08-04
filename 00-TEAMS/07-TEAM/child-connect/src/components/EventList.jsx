d29a7d6aca7535e01774b266cc0543047d97b083
Adrian Pinto <adripintogonzalez@gmail.com>
2021-08-04 17:58:47 +0200

    Add EventList component  - Add EventList component  - Add Event component  - Add basic CSS file for Event and EventList  - Edit Event[0] description  - Delete console log en actions/index.js

--- 00-TEAMS/07-TEAM/child-connect/src/components/EventList.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/components/EventList.jsx
@@ -2,23 +2,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Event from './Event';
+import './EventList.css';
 
 const EventList = () => {
-  const currentEvents = useSelector(({ events }) => events);
-  console.log(currentEvents);
+  let currentEvents = useSelector(({ events }) => events);
+  currentEvents = currentEvents.slice(0, 6);
 
   return (
     <>
-      {currentEvents && (
-        <>
-          <h2>Lista de Eventos en tu barrio</h2>
-          <Event />
-        </>
-      )}
-      <section />
-      <nav>
-        <button type="button">Siguiente</button>
-        <button type="button">Anterior</button>
+      <h2>Lista de Eventos en tu barrio</h2>
+      <section className="eventList">
+        {currentEvents && (
+          <>
+            {
+              currentEvents.map((event) => <Event key={event.id} data={event} />)
+            }
+          </>
+        )}
+      </section>
+      <nav className="event-pagination">
+        <button className="event-pagination__siguiente" type="button">Siguiente</button>
+        <button className="event-pagination__anterior" type="button">Anterior</button>
       </nav>
     </>
   );
