abb00dc86f2fdc913211ed07ea8ee2db95947674
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-08-04 10:55:12 +0200

    Link router dom

--- 00-TEAMS/04-TEAM/f1-standings/src/index.jsx
+++ 00-TEAMS/04-TEAM/f1-standings/src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter, Route, Redirect, Switch
+} from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import configureStore from './redux/store';
 import GetDrivers from './pages/Drivers';
-// import DetailsConstructor from './components/Details-constructor/Details.constructor';
+import DetailsConstructor from './components/Details-constructor/Details.constructor';
+import DetailsDriver from './components/Details-drivers/Details.drivers';
 import ListDriver from './components/List/List-driver';
 import ListConstructor from './components/List/List-constructor';
 
@@ -13,8 +17,15 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={configureStore()}>
       <GetDrivers />
-      <ListDriver />
-      <ListConstructor />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={ListDriver} />
+          <Redirect path="/listdriver" to="/" />
+          <Route path="/detailsDriver/:driverId" component={DetailsDriver} />
+          <Route path="/detailsConstructor/:constructorId" component={DetailsConstructor} />
+          <ListConstructor />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
