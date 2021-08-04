ec18a339a38c35c294a075dbf4e290562be508db
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-04 16:22:47 +0200

    Add changes from Pablo-2

--- 00-TEAMS/03-TEAM/sonary/src/components/header/Header.jsx
+++ 00-TEAMS/03-TEAM/sonary/src/components/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './header.input.scss';
+import logo from './sonary.svg';
 
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="header">
+      <nav className="header__top">
+        <figure clasName="header__sonary">
+          <a className="sonary__icon" href="/dashboard">
+            <img className="sonary__icon" alt="sonary icon" src={logo} />
+          </a>
+        </figure>
+        <figure className="header__profile">
+          <a className="profile__anchor" href="/profile">
+            <img className="profile__icon" alt="profile icon" />
+          </a>
+        </figure>
+      </nav>
+      <div className="header__bottom" />
     </header>
   );
 }
--- 00-TEAMS/03-TEAM/sonary/src/redux/actions/information.creator.js
+++ 00-TEAMS/03-TEAM/sonary/src/redux/actions/information.creator.js
@@ -3,11 +3,10 @@ import actionTypes from './actionTypes';
 
 export default function loadInformation() {
   return async (dispatch) => {
-    const response = await fetch('https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=callback&page_size=10&country=us&f_has_lyrics=f_has_lyrics&apikey=b6147311dbfaffd8721603c89d845ed8');
+    const response = await fetch('https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=callback&page_size=5&country=us&f_has_lyrics=f_has_lyrics&apikey=b6147311dbfaffd8721603c89d845ed8');
     let mod = await response.text();
     mod = await mod.substring(9, (mod.length - 2));
     const { message: { body: { track_list } } } = await JSON.parse(mod);
-    console.log(track_list);
     dispatch({
       type: actionTypes.LOAD_INFORMATION,
       dashboard: track_list
