009c1393f312f01e86666897b4e0c6e1408f4837
Pere08 <pripoll91@gmail.com>
2021-08-04 17:14:01 +0200

    Add style to Header and Footer

--- 00-TEAMS/04-TEAM/f1-standings/src/components/List-constructor/List-constructor.jsx
+++ 00-TEAMS/04-TEAM/f1-standings/src/components/List-constructor/List-constructor.jsx
@@ -2,37 +2,42 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import '../List/style.scss';
 import { Link } from 'react-router-dom';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 export default function ListConstructor() {
   const data = useSelector((store) => store.constructor);
   return (
-    <section className="constructor-list">
+    <>
+      <Header />
+      <section className="constructor-list">
 
-      {data[0]?.map((constructor) => (
+        {data[0]?.map((constructor) => (
         // eslint-disable-next-line prefer-template
-        <Link to={'/constructorDetails/' + constructor.Constructor.constructorId}>
-          <button className="constructor" type="button">
-            <section className="constructor-data">
-              <section className="data__main">
-                <span className="data__main-number">{constructor.position}</span>
-                <div className={constructor.Constructor.constructorId} />
-                <p className="data__main-name">{constructor.Constructor.name}</p>
+          <Link to={'/constructorDetails/' + constructor.Constructor.constructorId}>
+            <button className="constructor" type="button">
+              <section className="constructor-data">
+                <section className="data__main">
+                  <span className="data__main-number">{constructor.position}</span>
+                  <div className={constructor.Constructor.constructorId} />
+                  <p className="data__main-name">{constructor.Constructor.name}</p>
+                </section>
+                <section className="data__pts">
+                  <div className="pts__number">
+                    <p className="number">
+                      {constructor.points}
+                    </p>
+                    <p>
+                      PTS
+                    </p>
+                  </div>
+                </section>
               </section>
-              <section className="data__pts">
-                <div className="pts__number">
-                  <p className="number">
-                    {constructor.points}
-                  </p>
-                  <p>
-                    PTS
-                  </p>
-                </div>
-              </section>
-            </section>
-          </button>
-        </Link>
-      ))}
-
-    </section>
+            </button>
+          </Link>
+        ))}
+        <Footer />
+      </section>
+    </>
   );
 }
