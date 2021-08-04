479d3ffcd9dad3ea036a8a51b8498d551c76bb76
ignacioND <ignacio.navarro.vfx@gmail.com>
2021-08-04 13:11:16 +0200

    Redo pull request

--- 00-TEAMS/02-TEAM/solid-comics/src/App.jsx
+++ 00-TEAMS/02-TEAM/solid-comics/src/App.jsx
@@ -1,5 +1,6 @@
 // import React, { useEffect } from 'react';
 import React from 'react';
+
 import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import configureStore from './redux/store';
--- 00-TEAMS/02-TEAM/solid-comics/src/components/header/Header.jsx
+++ 00-TEAMS/02-TEAM/solid-comics/src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.scss';
+
 import { Link } from 'react-router-dom';
 
 export default function Header() {
