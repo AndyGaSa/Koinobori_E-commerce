409345c5adbd919a40327f09ee5b7bd4348eb1e5
ignacioND <ignacio.navarro.vfx@gmail.com>
2021-08-04 09:33:43 +0200

    Fix filter problems for calling API

--- 00-TEAMS/02-TEAM/solid-comics/src/redux/reducers/comics.reducer.js
+++ 00-TEAMS/02-TEAM/solid-comics/src/redux/reducers/comics.reducer.js
@@ -1,15 +1,14 @@
 import actionTypes from '../actions/comics.types';
 
 export default function comicsReducer(comicsList = [], action) {
-  // eslint-disable-next-line no-debugger
-  debugger;
   let newComicList = comicsList;
   switch (action.type) {
     case actionTypes.LOAD_COMICS:
       newComicList = action.comics.data.results?.map((comic) => comic);
       break;
     case actionTypes.FILTER_COMICS:
-      newComicList = action.comics.data.results?.map((comic) => comic);
+      // eslint-disable-next-line no-debugger
+      newComicList = action.filtered.data.results?.map((comic) => comic);
       break;
     default:
       break;
