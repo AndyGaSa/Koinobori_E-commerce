31ad67d95f5751f10aad61eb3ebd8b988def7470
Adrian Pinto <adripintogonzalez@gmail.com>
2021-08-04 10:35:37 +0200

    Fix Dashboard and EventList

--- 00-TEAMS/07-TEAM/child-connect/src/components/Auth0/LogInButton.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/components/Auth0/LogInButton.jsx
@@ -2,18 +2,15 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function LoginButton() {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
   return (
-    <>
-      <button
-        type="button"
-        onClick={() => loginWithRedirect()}
-      >
-        Log In
-      </button>
-      <p>{isAuthenticated}</p>
-    </>
+    <button
+      type="button"
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </button>
   );
 }
 
--- 00-TEAMS/07-TEAM/child-connect/src/pages/Dashboard.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/pages/Dashboard.jsx
@@ -1,29 +1,13 @@
-/* eslint-disable no-console */
-/* eslint-disable import/no-named-as-default */
 import React from 'react';
+import EventList from '../components/EventList';
 import './Dashboard.css';
-import { useSelector } from 'react-redux';
-import EventList from './EventList';
-// import fetchAllEvents from '../redux/actions/events.creators';
 
-export default function Dashboard() {
-//  const dispatch = useDispatch();
-  // dispatch(fetchAllEvents());
+const Dashboard = () => (
+  <>
+    <EventList />
+    {/* Map */}
+    {/* Comments */}
+  </>
+);
 
-  const currentEvents = useSelector(({ events }) => events);
-  console.log(currentEvents);
-
-  return (
-    <>
-      {currentEvents && (
-        <>
-          <h3>{currentEvents[0]?.title}</h3>
-          <h4>{currentEvents[0]?.activity}</h4>
-          <hr />
-          <p>{currentEvents[0]?.description}</p>
-        </>
-      )}
-      <EventList />
-    </>
-  );
-}
+export default Dashboard;
