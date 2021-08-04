df71614b4199483063b368b926c4a1030f927932
ricardSala <riki_sala@icloud.com>
2021-08-04 12:55:30 +0200

    Resolve some problems of header links nav

--- 00-TEAMS/02-TEAM/solid-comics/src/components/header/Header.jsx
+++ 00-TEAMS/02-TEAM/solid-comics/src/components/header/Header.jsx
@@ -6,21 +6,21 @@ export default function Header() {
   return (
     <header className="header">
       <nav className="header-nav">
-        <Link to="profile" className="nav-profile">
+        <Link to="../profile" className="nav-profile">
           <span className="nav-profile__text">Profile</span>
           <img className="nav-profile__img" src="https://i.ibb.co/YLZR3hF/jarvis.png" alt="Profile" />
         </Link>
-        <Link to="comic-list" className="nav-comics">
+        <Link to="../comic-list" className="nav-comics">
           <span className="nav-comics__text">Comics</span>
           <img className="nav-comics__img" src="https://i.ibb.co/mh9w0nC/comics.jpg" alt="Comics" />
 
         </Link>
-        <Link to="battle" className="nav-battle">
+        <Link to="../battle" className="nav-battle">
           <span className="nav-battle__text">Battle</span>
           <img className="nav-battle__img" src="https://i.ibb.co/xDHnz2m/battle.jpg" alt="Battle" />
 
         </Link>
-        <Link to="log-out" className="nav-logout">
+        <Link to="../log-out" className="nav-logout">
           <span className="nav-logout__text">LogOut</span>
           <img className="nav-logout__img" src="https://i.ibb.co/vLGB8Sm/logOut.jpg" alt="Log Out" />
         </Link>
