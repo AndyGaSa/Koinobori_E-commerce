aedb75b605b8e01337f39b7d98785dba6f365a9e
JordiTrassi <j.trassi@telefonica.net>
2021-08-04 09:22:37 +0200

    Delet UserProfile file and modify Perfil style

--- 00-TEAMS/07-TEAM/child-connect/src/components/Header.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/components/Header.jsx
@@ -7,21 +7,23 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <header>
-      <nav>
+    <>
+      <header>
         <Link to="/" className="header__home-button">
           <img src="https://i.ibb.co/M7Cprgj/Child-Connect-2.png" width="213px" height="auto" alt="Child Connect Header Logo" />
         </Link>
-        <section className="header__menu-buttons">
-          <Link to="/" className="buttons__crear-evento">Crear Evento</Link>
-          <div className="header__buttons">
-            <Link to="/perfil">Perfil</Link>
-          </div>
-        </section>
-      </nav>
+        <nav>
+          <section className="header__menu-buttons">
+            <Link to="/" className="buttons__crear-evento">Crear Evento</Link>
+            <div className="header__buttons">
+              <Link to="/perfil">Perfil</Link>
+            </div>
+          </section>
+        </nav>
+      </header>
       <div>
         <h1>CHILD CONNECT, la red social para conectar a los más pequeños.</h1>
       </div>
-    </header>
+    </>
   );
 }
--- 00-TEAMS/07-TEAM/child-connect/src/pages/Perfil.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/pages/Perfil.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable no-console */
 import React from 'react';
 import { useSelector } from 'react-redux';
-/* import EventList from './EventList'; */
+import EventList from './EventList';
+
+import './Perfil.css';
 
 export default function Perfil() {
   const currentEvents = useSelector(({ events }) => events);
@@ -9,6 +11,19 @@ export default function Perfil() {
   console.log(currentEvents);
   return (
     <>
+
+      <header>
+        <h2>Nombre del Usuario</h2>
+        <section>
+          <img src="#" alt="Imagen de perfil de Usuario" />
+          <div>
+            {/* info de la api */}
+          </div>
+        </section>
+      </header>
+      <h3>Descripción Personal:</h3>
+      <p> </p>
+      <EventList />
       <p>{currentEvents[0]?.title}</p>
 
     </>
