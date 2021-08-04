63ebb529a592fb1a195b09dc44a1145640f6b5e5
JordiTrassi <j.trassi@telefonica.net>
2021-08-04 11:44:58 +0200

    Add new styles in header

--- 00-TEAMS/07-TEAM/child-connect/src/components/Header.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Link
 } from 'react-router-dom';
+import LogOutButton from './Auth0/LogOutButton';
 
 import './Header.css';
 
@@ -9,21 +10,26 @@ export default function Header() {
   return (
     <>
       <header>
-        <Link to="/" className="header__home-button">
-          <img src="https://i.ibb.co/M7Cprgj/Child-Connect-2.png" width="213px" height="auto" alt="Child Connect Header Logo" />
-        </Link>
-        <nav>
-          <section className="header__menu-buttons">
-            <Link to="/" className="buttons__crear-evento">Crear Evento</Link>
-            <div className="header__buttons">
-              <Link to="/perfil">Perfil</Link>
-            </div>
-          </section>
-        </nav>
+        <div className="header__menu">
+          <Link to="/" className="header__home-button">
+            <img src="https://i.ibb.co/M7Cprgj/Child-Connect-2.png" width="213px" height="auto" alt="Child Connect Header Logo" />
+          </Link>
+          <nav>
+            <section className="header__menu-buttons">
+              <Link to="/" className="button__crear-evento">Crear Evento</Link>
+              <div className="header__buttons">
+                <Link to="/perfil" className="header__button-perfil">Perfil</Link>
+                <LogOutButton className="header__button-logout">Log Out</LogOutButton>
+              </div>
+            </section>
+          </nav>
+        </div>
+        <div>
+          <h1>CHILD CONNECT, la red social para conectar a los más pequeños.</h1>
+        </div>
+
       </header>
-      <div>
-        <h1>CHILD CONNECT, la red social para conectar a los más pequeños.</h1>
-      </div>
+
     </>
   );
 }
