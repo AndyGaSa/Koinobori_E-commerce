d2c29d869cf93171bf1dd49b50a594fc4328974c
“Andy <garcia.andy.tecnologies10@gmail.com>
2021-08-04 16:18:30 +0200

    Add delete to crud

--- 00-TEAMS/01-TEAM/koinobori/src/components/FilledCart.jsx
+++ 00-TEAMS/01-TEAM/koinobori/src/components/FilledCart.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './FilledCart.scss';
 
 export default function FilledCart() {
   const LocalStorage = JSON.parse(localStorage.getItem('cart'));
+  function deleteArticle(name) {
+    const newLocalStorage = LocalStorage.filter((item) => item.name !== name);
+    localStorage.setItem('cart', JSON.stringify(newLocalStorage));
+    window.location.reload();
+  }
   return (
     <main className="main">
       <ul className="items__container">
         {LocalStorage.map((item) => (
           <li className="item__container">
-            <img className="item__image" src={item.imageFront} alt={item.name} />
+            <Link to={`/details/${item.category}/${item.id}`}><img className="item__image" src={item.imageFront} alt={item.name} /></Link>
             <div className="nameprice__container">
               <span className="item__name">{item.name}</span>
               <span className="item__price">{item.price}</span>
@@ -19,12 +25,13 @@ export default function FilledCart() {
                 className="item__input"
                 type="number"
                 name="quantity"
+                placeholder={item.quantity}
                 min="1"
                 max="100"
               />
             </div>
             <div className="delete__button">
-              <button className="item__delete" type="button">X</button>
+              <button onClick={() => deleteArticle(item.name)} className="item__delete" type="button">X</button>
               <div className="item__empty" />
             </div>
           </li>
