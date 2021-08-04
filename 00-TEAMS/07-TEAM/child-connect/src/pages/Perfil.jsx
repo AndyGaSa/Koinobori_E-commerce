8c2258abb44fe5f1c8b60650a474582ae3b92fae
Adrian Pinto <adripintogonzalez@gmail.com>
2021-08-04 10:34:15 +0200

    Fix Perfil

--- 00-TEAMS/07-TEAM/child-connect/src/pages/Perfil.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/pages/Perfil.jsx
@@ -1,9 +1,7 @@
 /* eslint-disable no-console */
 import React from 'react';
 import { useSelector } from 'react-redux';
-import EventList from './EventList';
-
-import './Perfil.css';
+/* import EventList from './EventList'; */
 
 export default function Perfil() {
   const currentEvents = useSelector(({ events }) => events);
@@ -11,19 +9,6 @@ export default function Perfil() {
   console.log(currentEvents);
   return (
     <>
-
-      <header>
-        <h2>Nombre del Usuario</h2>
-        <section>
-          <img src="#" alt="Imagen de perfil de Usuario" />
-          <div>
-            {/* info de la api */}
-          </div>
-        </section>
-      </header>
-      <h3>Descripción Personal:</h3>
-      <p> </p>
-      <EventList />
       <p>{currentEvents[0]?.title}</p>
 
     </>
