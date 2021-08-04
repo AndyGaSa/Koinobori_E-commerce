bbb2a4ac97fc0ea12dfe7d75a21a2fc612b9f04b
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-04 09:14:28 +0200

    Add  testid on 3 buttons, and create test for the footer

--- 00-TEAMS/05-TEAM/05-sport5/src/components/Footer/Footer.jsx
+++ 00-TEAMS/05-TEAM/05-sport5/src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import './Footer.scss';
 
@@ -5,13 +6,13 @@ export default function Footer() {
   return (
     <footer>
       <nav className="footer">
-        <button type="button" className="footer__navigation-home-button">
+        <button type="button" className="footer__navigation-home-button" data-testid="button-home">
           <i className="fas fa-home" />
         </button>
-        <button type="button" className="footer__navigation-backpage-button">
+        <button type="button" className="footer__navigation-backpage-button" data-testid="button-back">
           <i className="fas fa-chevron-left" />
         </button>
-        <button type="button" className="footer__navigation-profile-button">
+        <button type="button" className="footer__navigation-profile-button" data-testid="button-profile">
           <i className="fas fa-user" />
         </button>
 
---
+++ 00-TEAMS/05-TEAM/05-sport5/src/components/Footer/Footer.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
+
+describe('Given a Footer component', () => {
+  describe('When page is loaded', () => {
+    // arrange
+    beforeEach(() => {
+      // act
+      render(<Footer />);
+    });
+
+    test('Then home button should be in the document', () => {
+      // assert
+      expect(screen.getByTestId('button-home')).toBeInTheDocument();
+    });
+
+    test('Then go back button should be in the document', () => {
+      // assert
+      expect(screen.getByTestId('button-back')).toBeInTheDocument();
+    });
+
+    test('Then profile button should be in the document', () => {
+      // assert
+      expect(screen.getByTestId('button-profile')).toBeInTheDocument();
+    });
+  });
+});
