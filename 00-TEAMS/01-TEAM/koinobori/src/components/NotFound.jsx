9c56aacb7c5c8c9299331d83b9728be749cc5f1f
“Chej <chejahamed@gmail.com>
2021-08-04 15:53:54 +0200

    Add img to NotFund

--- 00-TEAMS/01-TEAM/koinobori/src/components/NotFound.jsx
+++ 00-TEAMS/01-TEAM/koinobori/src/components/NotFound.jsx
@@ -4,14 +4,6 @@ import './EmptyCart.scss';
 
 export default function NotFound() {
   return (
-<<<<<<< HEAD
-    <>
-      <h3>404 - Page not found</h3>
-
-      <Link to="/">Back to home</Link>
-
-    </>
-=======
     <main>
       <section className="emptyCartSection">
         <h1>404 - Page not found</h1>
@@ -21,6 +13,5 @@ export default function NotFound() {
         <Link to="/"><button className="backButton" type="button">Back to home</button></Link>
       </section>
     </main>
->>>>>>> 94698f12ee96ad3d90c6b086995d5026c6ea2e20
   );
 }
