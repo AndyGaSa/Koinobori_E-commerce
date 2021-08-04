9a6b8060311d29f93edce240d14fafc8bd9f59bd
pabloapausa <pabloapausa@outlook.com>
2021-08-04 16:04:43 +0200

    Create header

--- 00-TEAMS/03-TEAM/sonary/src/redux/actions/information.creator.js
+++ 00-TEAMS/03-TEAM/sonary/src/redux/actions/information.creator.js
@@ -3,7 +3,7 @@ import actionTypes from './actionTypes';
 
 export default function loadInformation() {
   return async (dispatch) => {
-    const response = await fetch('https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=callback&page_size=10&country=us&f_has_lyrics=f_has_lyrics&apikey=ac9bd389d8c264b9d3fe211a16c14560');
+    const response = await fetch('https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=callback&page_size=5&country=us&f_has_lyrics=f_has_lyrics&apikey=b6147311dbfaffd8721603c89d845ed8');
     let mod = await response.text();
     mod = await mod.substring(9, (mod.length - 2));
     const { message: { body: { track_list } } } = await JSON.parse(mod);
