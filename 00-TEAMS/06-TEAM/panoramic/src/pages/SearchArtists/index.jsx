95b7957769c0a0a0ea51b8a97065028431f4be72
Manuel Barra <manuelbarralazo@gmail.com>
2021-08-05 12:26:17 +0200

    Add search component

--- 00-TEAMS/06-TEAM/panoramic/src/pages/SearchArtists/index.jsx
+++ 00-TEAMS/06-TEAM/panoramic/src/pages/SearchArtists/index.jsx
@@ -3,33 +3,45 @@ import { useDispatch, useSelector } from 'react-redux';
 import getSpotifyToken from '../../redux/actions/actionCreatorsHome';
 import { addFavArtist } from '../../redux/actions/actionCreatorsFavList';
 import loadSearchedArtists from '../../redux/actions/actionCreatorsSearchedList';
+import './Search.scss';
 
 export default function SearchArtists() {
   const dispatch = useDispatch();
   const searchedArtistsList = useSelector((store) => store.searchedArtists);
+
   useEffect(() => {
     getSpotifyToken();
   }, []);
 
+  function getImageFromArtist({ images }) {
+    return images && images[0] ? images[0].url : '';
+  }
+
   return (
-    <>
+    <main className="main">
+      <h2 className="main__list-title">Search your favourite artist list</h2>
       <input
+        className="main__input-search"
         placeholder="Search your favourite artist"
         onChange={(event) => dispatch(loadSearchedArtists(event.target.value))}
       />
-      <ul>
+      <ul className="main__artists-list">
         {searchedArtistsList.map((artist) => (
-          <li>
-            {artist.name}
+          <li className="main__artist-item artist-item">
+
+            <img src={getImageFromArtist(artist)} className="artist-item__photo" alt="photoartist" />
+            <p className="artist-item__name">{artist.name}</p>
             <button
+              className="artist-item__button-add"
               type="button"
               onClick={() => dispatch(addFavArtist(artist))}
             >
               + Add
             </button>
           </li>
+
         ))}
       </ul>
-    </>
+    </main>
   );
 }
