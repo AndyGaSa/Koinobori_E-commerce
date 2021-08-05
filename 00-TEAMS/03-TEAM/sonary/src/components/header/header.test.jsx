33df84a622558a07949efe662fcd0c5cd78b9d19
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-05 22:35:19 +0200

    Add header test

--- 00-TEAMS/03-TEAM/sonary/src/components/header/header.test.jsx
+++ 00-TEAMS/03-TEAM/sonary/src/components/header/header.test.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { render, screen } from '../../utils/test.utils';
+import { fireEvent, render, screen } from '../../utils/test.utils';
 import Header from './Header';
+import Dashboard from '../main/dashboard/Dashboard';
 
-describe('Given a header component ', () => {
-  test('Then an image should be in the document', () => {
-    render(
-      <Header />
-    );
+describe('Given a Header component', () => {
+  render(<Header />);
+  test('Then create button should be in the document', () => {
     expect(screen.getByTestId('profile-image')).toBeInTheDocument();
+  });
+  describe('And create button is clicked', () => {
+    beforeEach(() => {
+      const logoImage = screen.getByTestId('logo-image');
+      fireEvent.click(logoImage);
+      render(<Dashboard />);
+    });
+    test('Then the text Bad Habit should be in dashboard', () => {
+      const li = screen.getByTestId('list-item-2');
+      expect(li).toBeInTheDocument();
+    });
   });
 });
