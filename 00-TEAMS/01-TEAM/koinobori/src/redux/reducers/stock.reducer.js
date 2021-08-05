b0441ad18fd364f6900020af25972c999bd70f4a
“Andy <garcia.andy.tecnologies10@gmail.com>
2021-08-05 11:43:43 +0200

    Solve reducer undefined problem

--- 00-TEAMS/01-TEAM/koinobori/src/redux/reducers/stock.reducer.js
+++ 00-TEAMS/01-TEAM/koinobori/src/redux/reducers/stock.reducer.js
@@ -2,9 +2,13 @@ import actionTypes from '../actions/action.types';
 
 export default function StockReducer({ type, payload }, stockList = {}) {
   let newStockList = stockList;
+  switch (type) {
+    case actionTypes.LOAD_STOCK:
+      newStockList = { ...newStockList, ...payload };
+      break;
 
-  if (type === actionTypes.LOAD_STOCK) {
-    newStockList = { ...newStockList, ...payload };
+    default:
+      break;
   }
 
   return newStockList;
