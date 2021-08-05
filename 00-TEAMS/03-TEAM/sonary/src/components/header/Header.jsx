59442541c29decaeb894a8973e2787e60f5f7602
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-05 15:38:00 +0200

    Add ternary to have the user icon when logged out and user picture when logged in in header

--- 00-TEAMS/03-TEAM/sonary/src/components/header/Header.jsx
+++ 00-TEAMS/03-TEAM/sonary/src/components/header/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './header.input.scss';
+import { useAuth0 } from '@auth0/auth0-react';
 import logo from './sonary.svg';
+import iconUser from './user.svg';
 
 export default function Header() {
+  const { user, isAuthenticated } = useAuth0();
   return (
     <header className="header">
       <nav className="header__top">
@@ -15,7 +18,7 @@ export default function Header() {
 
         <figure className="header__profile">
           <a className="profile__anchor" href="/profile">
-            <img className="profile__icon" alt="profile icon" />
+            <img src={isAuthenticated ? user.picture : iconUser} className="header__profile" alt="profile icon" />
           </a>
         </figure>
       </nav>
