1334e6bdaf4f8033f1962abccd6b8bd393324643
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-05 23:30:39 +0200

    Change heade test

--- 00-TEAMS/03-TEAM/sonary/src/components/header/header.test.jsx
+++ 00-TEAMS/03-TEAM/sonary/src/components/header/header.test.jsx
@@ -8,11 +8,15 @@ describe('Given a Header component', () => {
   test('Then create button should be in the document', () => {
     expect(screen.getByTestId('profile-image')).toBeInTheDocument();
   });
-  describe('And logo image is clicked', () => {
+  describe('And create button is clicked', () => {
     beforeEach(() => {
       const logoImage = screen.getByTestId('logo-image');
       fireEvent.click(logoImage);
       render(<Dashboard />);
+    });
+    test('Then the text Bad Habit should be in dashboard', () => {
+      const li = screen.getByTestId('list-item-2');
+      expect(li).toBeInTheDocument();
     });
   });
 });
