013a4b0f69e4ce86f710772c8e2a3e9fef2210fa
Alex <alexmunoz91@gmail.com>
2021-08-05 19:35:26 +0200

    Modify some styles

--- 00-TEAMS/06-TEAM/panoramic/src/components/BannerLanding/index.jsx
+++ 00-TEAMS/06-TEAM/panoramic/src/components/BannerLanding/index.jsx
@@ -5,7 +5,10 @@ import styles from './BannerLanding.scss';
 export default function BannerLanding() {
   return (
     <section className="section-banner">
-      <img className="section-banner__logo-item" src="https://i.ibb.co/JpZSLf5/LOGO-PANORAMIC.png" alt="LOGO-PANORAMIC" border="0" />
+      <div className="section-banner__logo-item">
+        <img className="logo-item__main-logo" src="https://i.ibb.co/N12DWzQ/LOGO-PANORAMIC-3.png" width="150" alt="LOGO-PANORAMIC" />
+        <h2 className="logo-item__logo-text">Panoramic</h2>
+      </div>
       <p className="section-banner__text-item">
         BUSCA TUS ARTISTAS FAVORITOS,
         ENTERATE DE SUS EVENTOS Y SIGUELOS PARA QUE ASISTAS A TUS MEJORES PANORAMAS
