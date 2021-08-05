7bb8a4f4c985975bf89fc449804dce098ec76278
Xavier Lasierra <xlp1998@gmail.com>
2021-08-05 23:08:04 +0200

    Add footer authenticated case test

--- 00-TEAMS/05-TEAM/05-sport5/src/components/Footer/Footer.test.jsx
+++ 00-TEAMS/05-TEAM/05-sport5/src/components/Footer/Footer.test.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
 import { render, screen } from '../../utils/test.utils';
 import Footer from './Footer';
 
+jest.mock('@auth0/auth0-react');
+
 describe('Given a Footer component', () => {
   describe('When page is loaded', () => {
-    // arrange
-    beforeEach(() => {
-      // act
-      render(<Footer />);
-    });
-
-    test('Then home button should be in the document', () => {
-      // assert
-      expect(screen.getByTestId('button-home')).toBeInTheDocument();
-    });
-
-    test('Then go back button should be in the document', () => {
-      // assert
-      expect(screen.getByTestId('button-back')).toBeInTheDocument();
-    });
-
     describe('And the user is not Aunthenticated', () => {
+      beforeEach(() => {
+        useAuth0.mockReturnValue({ isAuthenticated: false });
+        render(<Footer />);
+      });
+
+      test('Then home button should be in the document', () => {
+        expect(screen.getByTestId('button-home')).toBeInTheDocument();
+      });
+
+      test('Then go back button should be in the document', () => {
+        expect(screen.getByTestId('button-back')).toBeInTheDocument();
+      });
+
       test('Then profile button should be in the document', () => {
-        // assert
         expect(screen.getByText('Login')).toBeInTheDocument();
+      });
+    });
+    describe('And the user is Aunthenticated with user name Xavi', () => {
+      beforeEach(() => {
+        useAuth0.mockReturnValue({ isAuthenticated: true, user: { name: 'Xavi' } });
+        render(<Footer />);
+      });
+
+      test('Then Xavi should be in the document', () => {
+        expect(screen.getByText('Xavi')).toBeInTheDocument();
       });
     });
   });
