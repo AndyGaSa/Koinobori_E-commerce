30f6482582a8428984bcf04b879d7ec49c0fec2b
Alex <alexmunoz91@gmail.com>
2021-08-05 19:53:21 +0200

    Add bannerlanding test with 100% coverage

--- 00-TEAMS/06-TEAM/panoramic/src/components/BannerLanding/index.jsx
+++ 00-TEAMS/06-TEAM/panoramic/src/components/BannerLanding/index.jsx
@@ -6,12 +6,12 @@ export default function BannerLanding() {
   return (
     <section className="section-banner">
       <div className="section-banner__logo-item">
-        <img className="logo-item__main-logo" src="https://i.ibb.co/N12DWzQ/LOGO-PANORAMIC-3.png" width="150" alt="LOGO-PANORAMIC" />
+        <img data-testid="main-logo" className="logo-item__main-logo" src="https://i.ibb.co/N12DWzQ/LOGO-PANORAMIC-3.png" width="150" alt="LOGO-PANORAMIC" />
         <h2 className="logo-item__logo-text">Panoramic</h2>
       </div>
       <p className="section-banner__text-item">
-        BUSCA TUS ARTISTAS FAVORITOS,
-        ENTERATE DE SUS EVENTOS Y SIGUELOS PARA QUE ASISTAS A TUS MEJORES PANORAMAS
+        FIND YOUR FAVORITE ARTISTS,
+        LEARN ABOUT THEIR EVENTS AND FOLLOW THEM SO YOU CAN ATTEND YOUR BEST VIEWS
       </p>
 
     </section>
