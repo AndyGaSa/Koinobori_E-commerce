3902485f9a2f4f4a8062814609764a0ff3fe9582
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-05 17:18:18 +0200

    Create suggested artist test
    - Test onclick function
    - Test item renderization

--- 00-TEAMS/06-TEAM/panoramic/src/components/SuggestedArtist/SuggestedArtist.test.jsx
+++ 00-TEAMS/06-TEAM/panoramic/src/components/SuggestedArtist/SuggestedArtist.test.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { render, screen } from '../../utils/test.utils';
-import <SuggestedArtist></SuggestedArtist> from './index';
+import { render, screen, fireEvent } from '../../utils/test.utils';
+import SuggestedArtist from './index';
+import { addFavArtist } from '../../redux/actions/actionCreatorsFavList';
+
+jest.mock('../../redux/actions/actionCreatorsFavList');
 
 describe('Given a Suggestion Artist component', () => {
   describe('When the component is rendered', () => {
@@ -11,7 +14,35 @@ describe('Given a Suggestion Artist component', () => {
         );
       });
       test('And should contain the "Suggestion Artist"text', () => {
-        expect(screen.getByText(/Suggestion Artist/i)).toBeInTheDocument();
+        expect(screen.getByText(/Suggested Artist/i)).toBeInTheDocument();
+      });
+    });
+  });
+});
+
+describe('Given a Suggestion Artist component', () => {
+  describe('When the component is rendered', () => {
+    beforeEach(() => {
+      const initialState = {
+        searchedArtists: [],
+        favouriteArtists: [],
+        suggestedArtists: [{
+          name: 'alex', genres: [1, 2], followers: { total: 1 }, images: [{ url: 'hola' }]
+        }]
+      };
+      render(<SuggestedArtist />, { initialState });
+    });
+    describe('And suggestedArtists is not void', () => {
+      test('Then should render li with testid "list-item', () => {
+        expect(screen.getByTestId('list-item')).toBeInTheDocument();
+      });
+    });
+    describe('And the add button is clicked', () => {
+      test('Then addFavArtist should be called', () => {
+        const addButton = screen.getByTestId('add-button');
+        addFavArtist.mockReturnValue({ type: '' });
+        fireEvent.click(addButton);
+        expect(addFavArtist).toHaveBeenCalled();
       });
     });
   });
