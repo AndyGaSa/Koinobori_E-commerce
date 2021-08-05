3d9960292775cc65013d9d6088977cb45bfefb04
pabloapausa <pabloapausa@outlook.com>
2021-08-05 16:52:07 +0200

    Style 'profile' page

--- 00-TEAMS/03-TEAM/sonary/src/components/main/profile/Profile.jsx
+++ 00-TEAMS/03-TEAM/sonary/src/components/main/profile/Profile.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
-import LogoutButton from './LogoutButton';
 import LoginButton from './LoginButton';
 import Favorites from './Favorites';
 import User from './User';
@@ -15,7 +14,6 @@ export default function Profile() {
         <main className="profile">
           <User />
           <Favorites />
-          <LogoutButton />
         </main>
       )
       : (
--- 00-TEAMS/03-TEAM/sonary/src/components/main/profile/User.jsx
+++ 00-TEAMS/03-TEAM/sonary/src/components/main/profile/User.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import LogoutButton from './LogoutButton';
 
 export default function User() {
   const { user, isLoading } = useAuth0();
@@ -11,12 +12,15 @@ export default function User() {
   return (
     <aside className="user">
       <img className="user__image" src={user.picture} alt={user.name} />
-      <h2 className="user__name">{user.name}</h2>
-      <p className="user__name">
-        Email:
-        {' '}
-        {user.email}
-      </p>
+      <div className="user__information">
+        <div>
+          <h2 className="information__name">{user.name}</h2>
+          <p className="information__mail">
+            {user.email}
+          </p>
+        </div>
+        <LogoutButton />
+      </div>
     </aside>
   );
 }
