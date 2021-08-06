1734a06a22ef4c3412f3c5a94dc0180f83c82386
Alex <alexmunoz91@gmail.com>
2021-08-06 09:56:16 +0200

    Add notfound test

--- 00-TEAMS/06-TEAM/panoramic/src/pages/NotFound/index.jsx
+++ 00-TEAMS/06-TEAM/panoramic/src/pages/NotFound/index.jsx
@@ -5,8 +5,9 @@ import { Link } from 'react-router-dom';
 export default function NotFound() {
   return (
     <section className="not-found-section">
-      <h1 className="not-found-title">NotFound</h1>
-      <p className="not-found-404">404 error</p>
+      <h1 className="not-found-404">404 error</h1>
+      <h2 className="not-found-title">Page Not Found</h2>
+      <p className="not-found-description">Sorry, We Couldn’t Find this Page</p>
       <Link to="/" className="not-found-button" type="button">Back to safety</Link>
     </section>
   );
