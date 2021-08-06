2b33933d369b98b48f65a687a7fe6248f8547ec3
JordiTrassi <j.trassi@telefonica.net>
2021-08-06 08:11:30 +0200

    Correct BEM in header component

--- 00-TEAMS/07-TEAM/child-connect/src/components/Header.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/components/Header.jsx
@@ -11,38 +11,38 @@ export default function Header() {
   const { isAuthenticated } = useAuth0();
   return (
     <>
-      <header className="main__header">
-        <div className="header__menu">
+      <header className="main-header">
+        <div className="header-menu">
           {isAuthenticated && (
           <>
-            <Link to="/dashboard" className="header__home-button">
+            <Link to="/dashboard" className="header-home__button">
               <img src="https://i.ibb.co/M7Cprgj/Child-Connect-2.png" width="213px" height="auto" alt="Child Connect Header Logo" />
             </Link>
           </>
           )}
           {!isAuthenticated && (
           <>
-            <Link to="/" className="header__home-button">
+            <Link to="/" className="header-home-button">
               <img src="https://i.ibb.co/M7Cprgj/Child-Connect-2.png" width="213px" height="auto" alt="Child Connect Header Logo" />
             </Link>
           </>
           )}
 
-          <nav className="header__nav-menu">
-            <section className="header__menu-buttons">
+          <nav className="header-nav__menu">
+            <section className="header-menu__buttons">
               {isAuthenticated && (
               <>
-                <Link to="/addevent" className="button__crear-evento">Crear Evento</Link>
-                <div className="header__menu_buttons--buttons">
-                  <Link to="/perfil" className="header__button-perfil">Perfil</Link>
-                  <LogOutButton className="header__button-logout" />
+                <Link to="/addevent" className="button-crear__evento">Crear Evento</Link>
+                <div className="header-menu-buttons__buttons">
+                  <Link to="/perfil" className="header-button__perfil">Perfil</Link>
+                  <LogOutButton className="header-button__logout" />
                 </div>
               </>
               )}
             </section>
           </nav>
         </div>
-        <div className="header__title">
+        <div className="header-title">
           <h1>CHILD CONNECT, la red social para conectar a los más pequeños.</h1>
         </div>
 
