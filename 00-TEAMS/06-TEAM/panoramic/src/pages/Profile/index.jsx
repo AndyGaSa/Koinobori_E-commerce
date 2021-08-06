3fc742534cb07ba1c345d2f624ed38c44932d63d
Alex <alexmunoz91@gmail.com>
2021-08-06 10:41:19 +0200

    Add profile styles

--- 00-TEAMS/06-TEAM/panoramic/src/pages/Profile/index.jsx
+++ 00-TEAMS/06-TEAM/panoramic/src/pages/Profile/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProfileComp from '../../components/ProfileComp';
 
+import './Profile.scss';
+
 export default function Profile() {
   return (
-    <>
+    <div className="profile-container">
       <h1>Profile</h1>
       <ProfileComp />
-    </>
+    </div>
   );
 }
