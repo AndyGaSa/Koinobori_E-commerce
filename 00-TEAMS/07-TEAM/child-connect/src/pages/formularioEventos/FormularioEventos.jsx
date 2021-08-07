fc8d2b3b4f955df2051100206fc762419ae898d4
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-07 15:38:34 +0200

    Modify name in inputs.
    -Modify name to make it the same used in JSON.

--- 00-TEAMS/07-TEAM/child-connect/src/pages/formularioEventos/FormularioEventos.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/pages/formularioEventos/FormularioEventos.jsx
@@ -7,13 +7,13 @@ import { createEvent } from '../../redux/actions/index';
 export default function FormularioEventos() {
   const dispatch = useDispatch();
   const [data, setData] = useState({
-    event_title: '',
-    event_place: '',
-    event_date: 'lunes',
-    event_hour: '17',
-    event_type: 'dancing',
-    event_description: '',
-    event_max_child: '5'
+    title: '',
+    location: '',
+    fecha: 'lunes',
+    hora: '17',
+    activity: 'dancing',
+    description: '',
+    max_children: '5'
   });
 
   const handleInputChange = (event) => {
@@ -30,21 +30,21 @@ export default function FormularioEventos() {
         <p>Completa todos los campos</p>
       </section>
       <form className="event-form__form">
-        <label htmlFor="event_title">
+        <label htmlFor="title">
           Titulo del Evento:
-          <input type="text" name="event_title" placeholder="Baile en casa" className="event-form__input" onChange={handleInputChange} />
+          <input type="text" name="title" placeholder="Baile en casa" className="event-form__input" onChange={handleInputChange} />
         </label>
-        <label htmlFor="event_place">
+        <label htmlFor="location">
           Lugar:
           {' '}
           <img className="event-form__img" src="https://i.ibb.co/G3B81Bh/pin-1.png" alt="" />
-          <input type="text" name="event_place" placeholder="Parque de la España Industrial" className="event-form__input" onChange={handleInputChange} />
+          <input type="text" name="location" placeholder="Parque de la España Industrial" className="event-form__input" onChange={handleInputChange} />
         </label>
-        <label htmlFor="event_date">
+        <label htmlFor="fecha">
           Dia:
           {' '}
           <img className="event-form__img" src="https://i.ibb.co/hBwg4PJ/calender-4021509-640.png" alt="" />
-          <select onChange={handleInputChange} name="event_date" id="event_date" className="event-form__selector">
+          <select onChange={handleInputChange} name="fecha" id="fecha" className="event-form__selector">
             <option defaultValue value="lunes">Lunes</option>
             <option value="martes">Martes</option>
             <option value="miercoles">Miércoles</option>
@@ -54,11 +54,11 @@ export default function FormularioEventos() {
             <option value="domingo">Domingo</option>
           </select>
         </label>
-        <label htmlFor="event_hour">
+        <label htmlFor="hora">
           Hora:
           {' '}
           <img className="event-form__img" src="https://i.ibb.co/Jcm9rR1/flat-2442462-640.png" alt="" />
-          <select onChange={handleInputChange} name="event_hour" id="event_hour" className="event-form__selector">
+          <select onChange={handleInputChange} name="hora" id="hora" className="event-form__selector">
             <option defaultValue value="14">14</option>
             <option value="15">15</option>
             <option value="16">16</option>
@@ -67,9 +67,9 @@ export default function FormularioEventos() {
             <option value="19">19</option>
           </select>
         </label>
-        <label htmlFor="event_type">
+        <label htmlFor="activity">
           Tipo de Evento:
-          <select onChange={handleInputChange} name="event_type" id="event_type" className="event-form__selector">
+          <select onChange={handleInputChange} name="activity" id="activity" className="event-form__selector">
             <option defaultValue value="sports">sports</option>
             <option value="coocking">cooking</option>
             <option value="dancing">dancing</option>
@@ -80,13 +80,13 @@ export default function FormularioEventos() {
             <option value="console">console</option>
           </select>
         </label>
-        <label htmlFor="event_description">
+        <label htmlFor="description">
           Descripción del Evento:
-          <textarea name="event_description" onChange={handleInputChange} placeholder="Describe en qué consiste el evento, cuantas personas participarán, qué elementos brindarás y cuáles debe traer cada niña/o, y toda la información que creas relevante." className="event-form__textarea" />
+          <textarea name="description" onChange={handleInputChange} placeholder="Describe en qué consiste el evento, cuantas personas participarán, qué elementos brindarás y cuáles debe traer cada niña/o, y toda la información que creas relevante." className="event-form__textarea" />
         </label>
-        <label htmlFor="event_max_child">
+        <label htmlFor="max_children">
           Palzas disponibles:
-          <select onChange={handleInputChange} name="event_max_child" id="event_max_child" className="event-form__selector">
+          <select onChange={handleInputChange} name="max_children" id="max_children" className="event-form__selector">
             <option defaultValue value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
