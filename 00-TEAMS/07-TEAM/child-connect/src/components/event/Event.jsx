eed436bb2e03e3d78af324a7929f7aa64ac3cabe
JordiTrassi <j.trassi@telefonica.net>
2021-08-07 21:32:50 +0200

    Add new styles in Event List
    - Dislay flex
    - Responsive
    - Add delete button

--- 00-TEAMS/07-TEAM/child-connect/src/components/event/Event.jsx
+++ 00-TEAMS/07-TEAM/child-connect/src/components/event/Event.jsx
@@ -8,18 +8,21 @@ const Event = ({ data }) => (
   <>
     <article className="eventList__event">
       <header className="event__header">
+        <button type="button" className="event-button__eliminar">X</button>
         <h3>{data.title}</h3>
         <section>
           <img src="https://i.ibb.co/G3B81Bh/pin-1.png" alt="pin-icon" />
-          <p>Barcelona City</p>
+          <p>&nbsp;Barcelona City</p>
         </section>
         <section>
           <img src="https://i.ibb.co/hBwg4PJ/calender-4021509-640.png" alt="calendar-icon" />
+          &nbsp;
           <p>{data.fecha}</p>
         </section>
         <section>
           <img src="https://i.ibb.co/Jcm9rR1/flat-2442462-640.png" alt="clock-icon" />
           <p>
+            &nbsp;
             {data.hora}
             h
           </p>
@@ -28,17 +31,19 @@ const Event = ({ data }) => (
       <section className="event__info">
         <div>
           <img src="" alt="" />
+          &nbsp;
           <p>{data.activity}</p>
         </div>
         <div>
-          <div>{data.max_childs}</div>
-          <p>Niños</p>
+          <div>
+            {data.max_childs}
+          </div>
+          <p>&nbsp;Niños</p>
         </div>
       </section>
       <p className="event__description">{data.description}</p>
       <footer className="event__more-btn">
         <button type="button" className="event-button__saber-mas">Saber más</button>
-        {/* <button type="button" className="event-button__eliminar">Eliminar</button> */}
       </footer>
     </article>
   </>
