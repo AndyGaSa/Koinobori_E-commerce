e38be1e8414ad814f774c2343c63e2b844fc78a4
pabloapausa <pabloapausa@outlook.com>
2021-08-08 15:25:27 +0200

    Delete 'team' folder

--- pabloapausa/team/todo-list-react/src/component/Header.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
-
-export default function Header() {
-  return (
-    <h1>ToDo List</h1>
-  );
-}
--- pabloapausa/team/todo-list-react/src/redux/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import todosReducer from './todos.reducer';
-
-export default combineReducers({
-  toDos: todosReducer,
-});
