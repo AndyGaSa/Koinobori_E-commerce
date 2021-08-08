9dec8ecc0c6c5639789ffae25626f34fe7027455
Alex <alexmunoz91@gmail.com>
2021-08-08 22:55:15 +0200

    Add first details styles

--- 00-TEAMS/06-TEAM/panoramic/src/components/ArtistDetails/index.jsx
+++ 00-TEAMS/06-TEAM/panoramic/src/components/ArtistDetails/index.jsx
@@ -8,7 +8,7 @@ import './ArtistDetails.scss';
 export default function ArtistDetails() {
   const searchedArtist = useSelector((store) => store.artistDetails);
   const dispatch = useDispatch();
-
+  console.log(searchedArtist);
   useEffect(() => {
     getSpotifyToken();
   }, []);
@@ -17,26 +17,57 @@ export default function ArtistDetails() {
     return images && images[0] ? images[0].url : '';
   }
   return (
-    <div className="main-artist-details">
-      <h1>{' Details'}</h1>
-      <ul className="main__artists-list">
+    <div className="artist-details-container">
+      <h1 className="artist-details-container__title">Artist Details</h1>
+      <ul>
         {searchedArtist.map((artist) => (
           <li
-            className="main__artist-item artist-item"
+            className="artist-details-container__artist-card"
             data-testid={`list-item-${artist.name}`}
             key={`list-item-${artist.name}`}
           >
+            <div className="artist-card__artist">
+              <img src={getImageFromArtist(artist)} className="artist-item__photo" alt="photoartist" />
+              <p className="artist-item__name">{artist.name}</p>
+            </div>
 
-            <img src={getImageFromArtist(artist)} className="artist-item__photo" alt="photoartist" />
-            <p className="artist-item__name">{artist.name}</p>
-            <button
-              className="artist-item__button-add"
-              data-testid="add-button"
-              type="button"
-              onClick={() => dispatch(addFavArtist(artist))}
-            >
-              + Add
-            </button>
+            <ul className="artist-card__genres">
+              Genres:
+              {artist.genres.map((genre) => (
+                <li>
+                  {genre}
+                </li>
+              ))}
+            </ul>
+
+            <ul className="artist-card__followers">
+              Followers:
+              <li>
+                {artist.followers.total}
+              </li>
+            </ul>
+            <ul className="artist-card__popularity">
+              Popularity:
+              <li>
+                {artist.popularity}
+              </li>
+            </ul>
+            <div className="artist-card__buttons">
+              <a
+                className="artist-card__spotify-link"
+                href={`${artist.external_urls.spotify}`}
+              >
+                View on Spotify
+              </a>
+              <button
+                className="artist-item__button-add"
+                data-testid="add-button"
+                type="button"
+                onClick={() => dispatch(addFavArtist(artist))}
+              >
+                + Add to favourites
+              </button>
+            </div>
           </li>
 
         ))}
