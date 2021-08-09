12b6ddcab191d592c5f46706912c1de3d9e0034b
Xavier Lasierra <xlp1998@gmail.com>
2021-08-09 18:57:43 +0200

    Add getOneBeer tests

--- xavi-lasierra/lectures/beer-fullstack/backend/src/controllers/beersController.test.js
+++ xavi-lasierra/lectures/beer-fullstack/backend/src/controllers/beersController.test.js
@@ -14,11 +14,34 @@ describe('Given getBeers function', () => {
 
 describe('Given postBeer function', () => {
   describe('When it is invoked', () => {
-    test('Then should call res.send with beersMock[beersMock.length-1].id = 25 as an argument', () => {
-      const res = { send: jest.fn() };
-      const req = { body: { name: 'Leffe' } };
-      controller.postBeer(req, res);
+    const res = { send: jest.fn() };
+    const req = { body: { name: 'Leffe' } };
+    controller.postBeer(req, res);
+
+    test('Then should call res.send with an object with property id = 25 as an argument', () => {
       expect(res.send.mock.calls[0][0].id).toBe(26);
+    });
+    test('Then should call res.send with an object with property name = Leffe as an argument', () => {
+      expect(res.send.mock.calls[0][0].name).toBe('Leffe');
+    });
+  });
+});
+
+describe('Given getOneBeer function', () => {
+  describe('When it is invoked with parameter 1', () => {
+    test('Then should call res.send with an object with property name = Leffe as an argument', () => {
+      const res = { send: jest.fn() };
+      const req = { params: { beerId: '1' } };
+      controller.getOneBeer(req, res);
+      expect(res.send.mock.calls[0][0].name).toBe('Buzz');
+    });
+  });
+  describe('When it is invoked with parameter 26', () => {
+    test('Then should call res.send with an object with property name = Leffe as an argument', () => {
+      const res = { send: jest.fn() };
+      const req = { params: { beerId: '26' } };
+      controller.getOneBeer(req, res);
+      expect(res.send.mock.calls[0][0].name).toBe('Leffe');
     });
   });
 });
