6e1842ad50dc9921aa4ac09099f2145ed3b39ea9
Xavier Lasierra <xlp1998@gmail.com>
2021-08-09 23:25:25 +0200

    Add beersController tests
    -Add filter functionality test in getBeers.
    -Add a test to getOneBeer function completating an id that does not exist in the API.
    -Add deleteOneBeer testing.
    -Add updateBeer testing.

--- xavi-lasierra/lectures/beer-fullstack/backend/src/controllers/beersController.test.js
+++ xavi-lasierra/lectures/beer-fullstack/backend/src/controllers/beersController.test.js
@@ -1,13 +1,28 @@
 const controller = require('./beersController');
-const beersMock = require('../mocks/beersMock');
 
 describe('Given getBeers function', () => {
   describe('When it is invoked', () => {
-    test('Then should call res.send with beersMock as an argument', () => {
-      const res = { send: jest.fn() };
-      controller.getBeers(null, res);
+    describe('And there are not filter queries', () => {
+      test('Then should call res.send with beersMock as an argument', () => {
+        const res = {
+          send: jest.fn()
+        };
+        const req = { query: { } };
+        controller.getBeers(req, res);
 
-      expect(res.send).toHaveBeenCalledWith(beersMock);
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+    describe('And there is a filter query beerName = ab:12', () => {
+      test('Then should call res.send with an object with id = 7 as an argument', () => {
+        const res = {
+          send: jest.fn()
+        };
+        const req = { query: { beerName: 'ab:12' } };
+        controller.getBeers(req, res);
+
+        expect(res.send.mock.calls[0][0][0].id).toBe(7);
+      });
     });
   });
 });
@@ -36,12 +51,53 @@ describe('Given getOneBeer function', () => {
       expect(res.send.mock.calls[0][0].name).toBe('Buzz');
     });
   });
+
   describe('When it is invoked with parameter 26', () => {
     test('Then should call res.send with an object with property name = Leffe as an argument', () => {
       const res = { send: jest.fn() };
       const req = { params: { beerId: '26' } };
       controller.getOneBeer(req, res);
       expect(res.send.mock.calls[0][0].name).toBe('Leffe');
+    });
+  });
+
+  describe('When it is invoked with parameter 99', () => {
+    test('Then should call res.send with undefined', () => {
+      const res = { send: jest.fn() };
+      const req = { params: { beerId: '99' } };
+      controller.getOneBeer(req, res);
+      expect(res.send.mock.calls[0][0]).toBe(undefined);
+    });
+  });
+});
+
+describe('Given deleteOneBeer function', () => {
+  describe('When it is invoked with parameter 2', () => {
+    const res = { send: jest.fn() };
+    const req = { params: { beerId: '2' } };
+    controller.deleteOneBeer(req, res);
+
+    test('Then should call res.send with an object that do not contains Trashy Blonde', () => {
+      expect(res.send.mock.calls[0][0].some(({ name }) => name === 'Trashy Blonde')).toBe(false);
+    });
+  });
+});
+
+describe('Given updateBeer function', () => {
+  describe('When it is invoked with parameter 6', () => {
+    const res = { send: jest.fn() };
+    const req = {
+      params: { beerId: '6' },
+      body: { name: 'Moritz' }
+    };
+    controller.updateBeer(req, res);
+
+    test('Then should call res.send with an object that do not contains Buzz', () => {
+      expect(res.send.mock.calls[0][0].some(({ name }) => name === 'Electric India')).toBe(false);
+    });
+
+    test('Then should call res.send with an object that contains Moritz', () => {
+      expect(res.send.mock.calls[0][0].some(({ name }) => name === 'Moritz')).toBe(true);
     });
   });
 });
