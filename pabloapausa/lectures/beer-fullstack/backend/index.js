013ac6b7dc684edf10da6b0930ff4a1d8a2c2093
pabloapausa <pabloapausa@outlook.com>
2021-08-10 10:05:06 +0200

    Configure 'dotenv' and 'debug' dependencies

--- pabloapausa/lectures/beer-fullstack/backend/index.js
+++ pabloapausa/lectures/beer-fullstack/backend/index.js
@@ -1,1 +1,17 @@
-console.clear();
+/* eslint-disable no-console */
+
+const express = require('express');
+const debug = require('debug')('beers');
+require('dotenv').config();
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(express.json());
+const beersRouter = require('./src/routes/beersRouter');
+
+server.use('/api/beers', beersRouter);
+server.listen(
+  port,
+  () => debug(`Server is running on http://localhost:${process.env.PORT}`),
+);
