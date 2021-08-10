6daa52059845fc997fc9e979d50b383e20d24f21
Lola Rufino <lola.ruf.ar@gmail.com>
2021-08-10 10:58:40 +0200

    Tooling for restApi

--- lola-rufino/lectures/beer-fullstack/backend/index.js
+++ lola-rufino/lectures/beer-fullstack/backend/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 require('dotenv').config();
 
 const debug = require('debug')('beersApi');
-const debugDev = require('debug')('dev');
+const chalk = require('chalk');
+const morgan = require('morgan');
 
 const server = express();
 const port = process.env.PORT || 5000;
 
+server.use(morgan('tiny'));
 server.use(express.json());
-debugDev('Esto es un cambio de desarrollooooo');
 
 const beersRouter = require('./src/routes/beersRouter');
 
@@ -16,5 +17,5 @@ server.use('/api/beers', beersRouter);
 
 server.listen(
   port,
-  () => debug(`Server is running on http://localhost:${port}`)
+  () => debug(`Server is running on ${chalk.bgMagenta(`http://localhost:${port}`)}`)
 );
