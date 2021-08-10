1d80aa15c654796d3413d7fe9ae71ed06d3aae5a
ignacioND <ignacio.navarro.vfx@gmail.com>
2021-08-10 10:38:28 +0200

    Install morgan and chalk packages

--- ignacio-navarro/lectures/beer-fullstack/backend/index.js
+++ ignacio-navarro/lectures/beer-fullstack/backend/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const debug = require('debug')('beersApi');
 const dev = require('debug')('dev');
+const chalk = require('chalk');
+const morgan = require('morgan');
 
 const server = express();
 const port = process.env.PORT || 5000;
 dev('mensaje de desarrollo');
+server.use(morgan('tiny'));
 server.use(express.json());
 
 const beersRouter = require('./src/routes/beersRouter');
@@ -14,5 +17,5 @@ server.use('/api/beers', beersRouter);
 
 server.listen(
   port,
-  () => debug(`Server is running on http://localhost:${port}`),
+  () => debug(`Server is running on ${chalk.blue(`http://localhost:${port}`)}`),
 );
