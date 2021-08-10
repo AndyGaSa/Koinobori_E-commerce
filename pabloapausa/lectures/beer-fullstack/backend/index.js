8fd02ec2de2fc84875f92a1c277ebc6211bf8f0c
pabloapausa <pabloapausa@outlook.com>
2021-08-10 10:56:59 +0200

    Configure 'jest', 'morgan' and 'nodemon' dependencies

--- pabloapausa/lectures/beer-fullstack/backend/index.js
+++ pabloapausa/lectures/beer-fullstack/backend/index.js
@@ -3,15 +3,18 @@
 const express = require('express');
 const debug = require('debug')('beers');
 require('dotenv').config();
+const chalk = require('chalk');
+const morgan = require('morgan');
 
 const server = express();
 const port = process.env.PORT || 5000;
 
+server.use(morgan('dev'));
 server.use(express.json());
 const beersRouter = require('./src/routes/beersRouter');
 
 server.use('/api/beers', beersRouter);
 server.listen(
   port,
-  () => debug(`Server is running on http://localhost:${process.env.PORT}`),
+  () => debug(`Server is running on ${chalk.magenta(`http://localhost:${port}`)}`),
 );
