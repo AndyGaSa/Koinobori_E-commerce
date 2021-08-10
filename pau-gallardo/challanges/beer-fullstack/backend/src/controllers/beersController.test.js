f39f68b592e7effab141bd0ce92294121a899e8c
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-10 18:53:13 +0200

    Rephrase getBeers test

--- pau-gallardo/challanges/beer-fullstack/backend/src/controllers/beersController.test.js
+++ pau-gallardo/challanges/beer-fullstack/backend/src/controllers/beersController.test.js
@@ -1,16 +1,26 @@
 const controller = require('./beersController');
 
-test('GetBeers > Should call res.send', () => {
-  const res = { send: jest.fn() };
-  controller.getBeers(null, res);
-  expect(res.send).toHaveBeenCalled();
+describe('Given getBeers', () => {
+  describe('When is triggered', () => {
+    test('Then getBeers should call res.send', () => {
+      const res = { send: jest.fn() };
+
+      controller.getBeers(null, res);
+
+      expect(res.send).toHaveBeenCalled();
+    });
+  });
 });
 
-test('getOneBeer > Should call res.send', () => {
-  const req = { params: { beerId: 11 } };
-  const res = { send: jest.fn() };
+describe('Given getBeers', () => {
+  describe('When is triggered', () => {
+    test('Then getGetOneBeer should call res.send', () => {
+      const req = { params: { beerId: 11 } };
+      const res = { send: jest.fn() };
 
-  controller.getOneBeer(req, res);
+      controller.getOneBeer(req, res);
 
-  expect(res.send).toHaveBeenCalled();
+      expect(res.send).toHaveBeenCalled();
+    });
+  });
 });
