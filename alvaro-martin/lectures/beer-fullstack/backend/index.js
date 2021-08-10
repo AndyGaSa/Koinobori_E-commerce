cf2ef2883a4399094ce820c7d5f1d3a6dd7af3c3
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-08-10 10:40:06 +0200

    Add morgan , chalk and nodemon to index

--- alvaro-martin/lectures/beer-fullstack/backend/index.js
+++ alvaro-martin/lectures/beer-fullstack/backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 require('dotenv').config();
 const debug = require('debug')('beersApi');
+const chalk = require('chalk');
+const morgan = require('morgan');
 
 const server = express();
 const port = process.env.PORT || 4242;
 
+server.use(morgan('dev'));
 server.use(express.json());
 
 const beersRouter = require('./src/routes/beersRouter');
@@ -13,5 +16,5 @@ server.use('/api/beers', beersRouter);
 
 server.listen(
   port,
-  () => debug(`Server is running on http://localhost:${port}`),
+  () => debug(`Server is running on ${chalk.blue(`http://localhost:${port}`)}`),
 );
