dd38b28aa7d94cf4e6e05ad53d3a239a36f8a3aa
Xavier Lasierra <xlp1998@gmail.com>
2021-08-10 10:56:56 +0200

    Install tooling for restApi

--- xavi-lasierra/lectures/beer-fullstack/backend/index.js
+++ xavi-lasierra/lectures/beer-fullstack/backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 require('dotenv').config();
+const morgan = require('morgan');
+const chalk = require('chalk');
 const debug = require('debug')('beersApi');
 
 const server = express();
 const port = process.env.PORT || 5000;
 
+server.use(morgan('dev'));
 server.use(express.json());
 
 const beersRouter = require('./src/routes/beersRouter');
@@ -13,5 +16,5 @@ server.use('/api/beers', beersRouter);
 
 server.listen(
   port,
-  () => debug(`Server is running on http://localhost:${port}`)
+  () => debug(`Server is running on ${chalk.magenta(`http://localhost:${port}`)}`)
 );
