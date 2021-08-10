85ab0c18715c975b2d52d00a1dc604285ed07492
Pere08 <pripoll91@gmail.com>
2021-08-10 09:27:34 +0200

    Add new test functions

--- pere-ripoll-cortes/lectures/apiRestBeers/beerFullStack/backend/src/controllers/beersController.js
+++ pere-ripoll-cortes/lectures/apiRestBeers/beerFullStack/backend/src/controllers/beersController.js
@@ -1,11 +1,13 @@
-const beersMock = require('../mocks/beersMock');
+let beersMock = require('../mocks/beersMock');
 
-const getBeers = (req, res) => res.send(beersMock);
+const getBeers = (req, res) => {
+  res.send(beersMock);
+};
 
 const postBeer = (req, res) => {
   const { name } = req.body;
   const newBeer = {
-    id: Math.random() * 10,
+    id: beersMock.length + 1,
     name,
   };
 
@@ -14,13 +16,41 @@ const postBeer = (req, res) => {
   res.send(newBeer);
 };
 
+const deleteBeer = (req, res) => {
+  const { name } = req.body;
+  const newBeer = beersMock.filter((e) => e.name !== name);
+  beersMock = newBeer;
+  res.send(beersMock);
+};
+
+const putBeer = (req, res) => {
+  const { name, newName } = req.body;
+  const newBeer = beersMock.map((e) => {
+    if (e.name === name) {
+      e.name = newName;
+    }
+    return beersMock;
+  });
+  beersMock = newBeer;
+  res.send(beersMock);
+};
+
 const getOneBeer = (req, res) => {
   const { beerId } = req.params;
   res.send(beersMock.find(({ id }) => id === +beerId));
 };
 
-module.export = {
+const filterBeer = (req, res) => {
+  const { beerName } = req.query;
+  const newBeer = beersMock.find(({ name }) => name === beerName);
+  res.send(newBeer);
+};
+
+module.exports = {
   getBeers,
   postBeer,
   getOneBeer,
+  deleteBeer,
+  putBeer,
+  filterBeer,
 };
--- pere-ripoll-cortes/lectures/apiRestBeers/beerFullStack/backend/src/controllers/beersController.test.js
+++ pere-ripoll-cortes/lectures/apiRestBeers/beerFullStack/backend/src/controllers/beersController.test.js
@@ -14,3 +14,49 @@ test('getOneBeer > should call res.send', () => {
 
   expect(res.send).toHaveBeenCalled();
 });
+
+describe('Given a post function', () => {
+  const req = { body: { name: 'Buzz' } };
+  const res = { send: jest.fn() };
+
+  controller.postBeer(req, res);
+
+  test('postBeer > should call res.send', () => {
+    expect(res.send).toHaveBeenCalled();
+  });
+
+  test('postBeer > should return Buzz', () => {
+    expect(res.send.mock.calls[0][0].name).toBe('Buzz');
+  });
+});
+
+describe('Given a delete function', () => {
+  const req = { body: { name: 'Buzz' } };
+  const res = { send: jest.fn() };
+
+  controller.deleteBeer(req, res);
+
+  test('deleteBeer > should call res.send', () => {
+    expect(res.send).toHaveBeenCalled();
+  });
+
+  test('deleteBeer should not return Buzz in array', () => {
+    expect(res.send.mock.calls[0][0][0].name).not.toBe('Buzz');
+  });
+});
+
+describe('Given a PUT function', () => {
+  const req = { body: { name: 'Pilsen Lager', newName: 'NewPilsen Lager' } };
+  const res = { send: jest.fn() };
+
+  controller.putBeer(req, res);
+
+  test('putBeer > should call res.send', () => {
+    expect(res.send).toHaveBeenCalled();
+  });
+
+  test('putBeer should change the Buzz beer name to newBuzz', () => {
+    const value = res.send.mock.calls[0][0][0].find((e) => e.name === 'NewPilsen Lager');
+    expect(value.name).toBe('NewPilsen Lager');
+  });
+});
