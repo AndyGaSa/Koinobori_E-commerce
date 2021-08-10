49993da59b43419544edd67f923728aad5032239
Adrian Pinto <adripintogonzalez@gmail.com>
2021-08-10 10:39:04 +0200

    Add morgan package  - Add morgan require  - Add mmorgan how middleware and call it with 'dev' argument

--- adrian-pinto/lecture/rest/beer-fullstack/backend/main.js
+++ adrian-pinto/lecture/rest/beer-fullstack/backend/main.js
@@ -2,14 +2,16 @@ const express = require('express');
 require('dotenv').config();
 const debug = require('debug')('beers');
 const chalk = require('chalk');
+const morgan = require('morgan');
 
 const server = express();
 const port = process.env.PORT || 3001;
 
+server.use(morgan('dev'));
 server.use(express.json());
 
 const beerRouter = require('./src/routes/beersRouter');
 
 server.use('/api/beers', beerRouter);
 
-server.listen(port, () => debug(`Server runing on ${chalk.gren(`127:${port}`)}`));
+server.listen(port, () => debug(`Server runing on ${chalk.green(`127:${port}`)}`));
