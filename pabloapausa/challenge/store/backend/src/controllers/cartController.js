8819b62fdba5daf4d6426e3f1756c2033ef7fc93
pabloapausa <pabloapausa@outlook.com>
2021-08-11 19:01:09 +0200

    Create test files
    Develop cart's controller

--- pabloapausa/challenge/store/backend/src/controllers/cartController.js
+++ pabloapausa/challenge/store/backend/src/controllers/cartController.js
@@ -1,1 +1,21 @@
-// const Cart = require('../models/cartModel');
+const Cart = require('../models/cartModel');
+
+// Adds items to the cart.
+async function postCart({ body }, res) {
+  const newItem = await Cart.create(body);
+
+  res.status(201);
+  // Que lo adjunte en 'products'.
+  return res.send(newItem);
+}
+
+async function getCart({ query }, res) {
+  const foundItem = await { products }.filter(query);
+
+  return res.send(foundItem);
+}
+
+module.exports = {
+  postCart,
+  getCart,
+};
