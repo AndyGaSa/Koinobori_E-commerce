9b9c2c7e70618294230fa182fbbca4de431059ff
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-11 18:46:27 +0200

    Add index.js

--- nacho-martin/challenges/shop-api/backend/index.js
+++ nacho-martin/challenges/shop-api/backend/index.js
@@ -4,3 +4,20 @@ const debug = require('debug')('toDos');
 const dev = require('debug')('dev');
 const chalk = require('chalk');
 const morgan = require('morgan');
+
+require('./src/config/configMongoose');
+
+const server = express();
+const port = process.env.PORT || 5000;
+dev('mensaje de desarrollo');
+server.use(morgan('dev'));
+server.use(express.json());
+
+const usersRouter = require('./src/routes/usersRouter');
+
+server.use('/api/users', usersRouter);
+
+server.listen(
+  port,
+  () => debug(`Server is running on ${chalk.blue(`http://localhost:${port}`)}`)
+);
