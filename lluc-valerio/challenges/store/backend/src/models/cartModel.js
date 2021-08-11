3170fd052b9330693d8c38d2e8933ae6792f8727
Lluc Valério <lluc.valerio@gmail.com>
2021-08-11 21:12:49 +0200

    Add models
     - cart model
     - product model
     - user model

---
+++ lluc-valerio/challenges/store/backend/src/models/cartModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const User = require('./userModel');
+const Product = require('./productModel');
+
+// const { Schema } = mongoose;
+
+const cartSchema = mongoose.Schema({
+  name: { type: mongoose.Schema.ObjectId, ref: User },
+  products: [{
+    product: { type: mongoose.Schema.ObjectId, ref: Product },
+    amount: Number
+  }]
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
