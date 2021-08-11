39c61e4646c61bd4027dd9546a82a83808845850
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-11 18:43:36 +0200

    Create userRouter

---
+++ pau-gallardo/challanges/shop-backend/backend/scr/routes/userRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const userController = require('./.......');
+
+const beersRouter = express.Router();
+
+beersRouter
+  .route('/');
+
+module.exports = beersRouter;
