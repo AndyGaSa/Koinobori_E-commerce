8a6bfa08acedcb115f97c8d02d001d1909e0d8f9
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-11 17:13:11 +0200

    Add updateToDos to route

--- laura-arcidiacono/lectures/toDo-Pau-Laura/backend/src/routes/toDosRoutes.js
+++ laura-arcidiacono/lectures/toDo-Pau-Laura/backend/src/routes/toDosRoutes.js
@@ -4,8 +4,13 @@ const controllers = require('../controllers/toDosControllers');
 const toDosRouter = express.Router();
 
 toDosRouter
+    .route('/:toDoId')
+    .get(controllers.getOneToDo)
+
+toDosRouter
     .route('/')
     .post(controllers.postToDos)
-    .get(controllers.postToDos)
+    .get(controllers.getToDos)
+    .put(controllers.updateToDos)
 
 module.exports = toDosRouter;
