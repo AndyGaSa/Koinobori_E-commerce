d228198c3b7521949dd8138522d4c41c71f6d9dd
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-11 20:53:23 +0200

    Add index.js server, port and userRouter

---
+++ laura-arcidiacono/challenges/carrito-de-compras-laura/backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config();
+const debug = require('debug')('cartItem');
+const morgan = require('morgan');
+
+require('./src/config/mongooseConfig');
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(morgan('dev'));
+server.use(express.json());
+
+// const cartItemRouter = require('./src/routes/cartItemRouter');
+
+// server.use('/api/cartItem', cartItemRouter);
+
+const userRouter = require('./src/routes/userRouter');
+
+server.use('/api/user', userRouter);
+
+// const productRouter = require('./src/routes/productRouter');
+
+// server.use('/api/productRouter', productRouter);
+
+server.listen(
+  port,
+  () => debug(`Server is running on ${`http://localhost:${port}`}`)
+);
