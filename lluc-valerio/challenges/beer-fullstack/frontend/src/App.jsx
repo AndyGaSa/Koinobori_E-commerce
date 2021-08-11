2d4dc37b20379eaad2d69221eea70bd676f81cea
Lluc Valério <lluc.valerio@gmail.com>
2021-08-11 12:17:34 +0200

    Add frontend modifications

--- lluc-valerio/challenges/beer-fullstack/frontend/src/App.jsx
+++ lluc-valerio/challenges/beer-fullstack/frontend/src/App.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect((() => {
+    fetch('api/beers')
+      .then((response) => {
+        // eslint-disable-next-line no-debugger
+        // debugger;
+        // eslint-disable-next-line no-console
+        console.log('************');
+        console.log('JSON Response:');
+        console.log('************');
+        console.log(response.json);
+        return response.json();
+      })
+      .then((data) => {
+      // eslint-disable-next-line no-debugger
+        // debugger;
+        // eslint-disable-next-line no-console
+        console.log('************');
+        console.log('JSON Data:');
+        console.log('************');
+        console.log(data);
+      })
+      .catch((error) => {
+      // eslint-disable-next-line no-debugger
+        // debugger;
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
+  }), []);
   return (
     <div className="App">
       <header className="App-header">
