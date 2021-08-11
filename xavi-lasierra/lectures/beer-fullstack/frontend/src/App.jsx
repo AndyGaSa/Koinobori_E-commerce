87ba06d72200eed350b096cd3c999e27457a2420
Xavier Lasierra <xlp1998@gmail.com>
2021-08-11 12:05:35 +0200

    Fech data in app.jsx from the backend

--- xavi-lasierra/lectures/beer-fullstack/frontend/src/App.jsx
+++ xavi-lasierra/lectures/beer-fullstack/frontend/src/App.jsx
@@ -1,13 +1,23 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('/api/beers')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
