cfae51b4f981872f872beb39b67d85d7b32be75d
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-08-12 18:08:35 +0200

    Update cartModel

--- alvaro-martin/lectures/e-comerce-fullstack/backend/src/models/cartModel.js
+++ alvaro-martin/lectures/e-comerce-fullstack/backend/src/models/cartModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cartSchema = mongoose.Schema({
   users: [{
     user: { type: mongoose.Schema.ObjectId, ref: 'user' },
-    surname: String,
+
   }],
   products: [{
     product: { type: mongoose.Schema.ObjectId, ref: 'product' },
