b931a70f61be61c0313c8d1072d734056769a0b9
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-12 20:17:24 +0200

    Add methods to route /:cartId

--- nacho-martin/challenges/shop-api/backend/src/routes/cartRouter.js
+++ nacho-martin/challenges/shop-api/backend/src/routes/cartRouter.js
@@ -1,8 +1,15 @@
 const express = require('express');
 
 const cartRouter = express.Router();
+const cartController = require('../controllers/cartController');
 
 cartRouter
-  .route('/');
+  .route('/')
+  .post(cartController.createOne);
 
+cartRouter
+  .route('/:cartId')
+  .get(cartController.getOneById)
+  .delete(cartController.deleteById)
+  .put(cartController.updateById);
 module.exports = cartRouter;
