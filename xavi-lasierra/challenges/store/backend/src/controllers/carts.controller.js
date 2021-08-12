3b241e9a42421ac83086aed4270c2acf77fdbf5b
Xavier Lasierra <xlp1998@gmail.com>
2021-08-12 19:38:26 +0200

    Modify createCart function
    -The function checks if the user already has a cart.
    -If the user has not a cart a new one is created with the demanded products.
    -If the user has a cart checks if it already has de product of the petition in it.
    -If the cart has the product adds the amount of the petition to its property.
    -If not creates a new product in the products array.
    -The stock is decreased in all cases.

--- xavi-lasierra/challenges/store/backend/src/controllers/carts.controller.js
+++ xavi-lasierra/challenges/store/backend/src/controllers/carts.controller.js
@@ -1,12 +1,12 @@
+/* eslint-disable no-underscore-dangle */
 const Cart = require('../models/cart.model');
 const Product = require('../models/product.model');
 
 async function getCarts({ query }, res) {
   try {
-    const foundCarts = await Cart.find(query).populate('user').populate({
-      path: 'products',
-      populate: { path: 'product' }
-    });
+    const foundCarts = await Cart.find(query)
+      .populate('user')
+      .populate('products.product');
 
     return res.json(foundCarts);
   } catch (error) {
@@ -15,44 +15,54 @@ async function getCarts({ query }, res) {
   }
 }
 
-async function stockCheck(products, res) {
-  products.forEach(async (product) => {
-    const productFound = await Product.findById(product.product);
-    if (!productFound) {
-      res.status(404);
-      res.send(new Error('Product not found'));
-      return false;
-    }
-    if (productFound.stock < product.amount) {
-      res.send(new Error(`Not enougth stock in product ${productFound.name}`));
-      return false;
-    }
-    return Product.findByIdAndUpdate(
-      product.product,
-      { stock: productFound.stock - product.amount }
+function updateCart(body, products) {
+  const newProducts = products;
+  const foundProductIndex = newProducts
+    .findIndex(({ product }) => product.toString() === body.products[0].product);
+
+  if (foundProductIndex !== -1) {
+    newProducts[foundProductIndex].amount += body.products[0].amount;
+
+    return Cart.findOneAndUpdate(
+      { user: body.user },
+      { products: newProducts },
+      { new: true }
     );
-  });
-  return true;
+  }
+
+  return Cart.findOneAndUpdate(
+    { user: body.user },
+    {
+      $push: {
+        products: body.products
+      }
+    },
+    { new: true }
+  );
+}
+
+function updateStock(updatedProduct) {
+  return Product.findByIdAndUpdate(
+    updatedProduct.product,
+    { $inc: { stock: -updatedProduct.amount } },
+    { new: true }
+  );
 }
 
 async function createCart({ body }, res) {
   try {
-    if (await stockCheck(body.products, res)) {
-      let newCart;
-      if (await Cart.findOne({ user: body.user })) {
-        newCart = await Cart.findOneAndUpdate(
-          { user: body.user },
-          { $push: { products: body.products } },
-          { new: true }
-        );
-      } else {
-        newCart = await Cart.create(body);
-      }
-
-      res.status(201);
-      res.json(newCart);
+    let newCart;
+    const existingCart = await Cart.findOne({ user: body.user });
+    if (existingCart) {
+      newCart = await updateCart(body, existingCart.products);
+    } else {
+      newCart = await Cart.create(body);
     }
-    res.send();
+
+    await updateStock(body.products[0]);
+
+    res.status(201);
+    res.json(newCart);
   } catch (error) {
     res.status(500);
     res.send(error);
@@ -67,8 +77,15 @@ function updateCartById(req, res) {
   res.send('a');
 }
 
-function deleteCartById(req, res) {
-  res.send('a');
+async function deleteCartById({ params: { cartId } }, res) {
+  try {
+    await Cart.findByIdAndDelete(cartId);
+    res.status(204);
+    res.send();
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
 }
 
 module.exports = {
