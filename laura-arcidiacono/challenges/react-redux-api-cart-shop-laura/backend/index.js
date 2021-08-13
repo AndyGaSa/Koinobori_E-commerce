0e176def54df5c649e18a2052c0aab05b50af0c4
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-13 19:11:22 +0200

    Config server in index.js

---
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config();
+const debug = require('debug')('bookShop');
+const morgan = require('morgan');
+const chalk = require('chalk');
+
+require('./src/config/ddbbConfig');
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(morgan('dev'));
+server.use(express.json());
+
+server.listen(
+  port,
+  () => debug(`Server is running on ${chalk.blue(`http://localhost:${port}`)}`)
+);
