0613403e379b28dadd563898d6b9be8b03cadf10
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-13 17:52:14 +0200

    Add Cart component

---
+++ pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/components/Cart/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Cart() {
+  return (
+    <>
+      <h2>Esto es un carro</h2>
+    </>
+  );
+}
--- pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/pages/Desktop.jsx
+++ pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/pages/Desktop.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ProductList from '../components/ProductList';
+import ProductList from '../components/ProductList/ProductList';
+import Cart from '../components/Cart/Cart';
 
 export default function Desktop() {
   return (
     <>
       <h2>Esto es el desktop</h2>
       <ProductList />
+      <Cart />
     </>
   );
 }
