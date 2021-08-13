da61049b42188e60462e857bf40a140932780973
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-13 17:47:43 +0200

    Create ProductList component

---
+++ pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/components/ProductList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ProductList() {
+  return (
+    <>
+      <h2>Esto es una lista de productos</h2>
+    </>
+  );
+}
--- pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/pages/Desktop.jsx
+++ pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/pages/Desktop.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProductList from '../components/ProductList';
 
 export default function Desktop() {
   return (
     <>
       <h2>Esto es el desktop</h2>
+      <ProductList />
     </>
   );
 }
