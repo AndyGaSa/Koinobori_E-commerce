9eca7443f0b888152a69379e934d3ef35f765554
Xavier Lasierra <xlp1998@gmail.com>
2021-08-13 15:27:47 +0200

    Configure frontend environment to develop

---
+++ xavi-lasierra/challenges/ecommerce-api/frontend/src/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
