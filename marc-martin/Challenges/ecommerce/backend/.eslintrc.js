80af614c750eba3bfa783511a3a1b24e8350abc2
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-13 21:07:15 +0200

    Install backend dependencies
    - eslint
    - jest
    - morgan
    - express
    - dotenv
    - debug
    - nodemon
    - chalk
    - cors

---
+++ marc-martin/Challenges/ecommerce/backend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'comma-dangle': [2, 'never'],
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows']
+  }
+};
