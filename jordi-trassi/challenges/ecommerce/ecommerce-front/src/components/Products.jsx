61a0a1f56934574c81ad0b4b66090483192fb4d9
JordiTrassi <j.trassi@telefonica.net>
2021-08-13 18:09:57 +0200

    Crate redux folder:
    - Create ACTIONS folder with files
    - Create REDUCERS folder with files
    - Create STORE folder with files

--- jordi-trassi/challenges/ecommerce/ecommerce-front/src/components/Products.jsx
+++ jordi-trassi/challenges/ecommerce/ecommerce-front/src/components/Products.jsx
@@ -1,1 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadProducts } from '../redux/actions/actionCreator';
+
+export default function Products() {
+  const products = useSelector((store) => store.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadProducts());
+  }, []);
+
+  return (
+    <ul>
+      {products.map((product) => <li key={product.name}>{product.name}</li>)}
+    </ul>
+  );
+}
---
+++ jordi-trassi/challenges/ecommerce/ecommerce-front/src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import productsReducer from './productsReducers';
+
+export default combineReducers({
+  products: productsReducer,
+});
