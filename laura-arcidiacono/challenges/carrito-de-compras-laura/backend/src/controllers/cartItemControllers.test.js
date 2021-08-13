727f8f7657fb029d61522693549f41435e77f2b4
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-13 11:37:52 +0200

    Add deleteOneById function test

--- laura-arcidiacono/challenges/carrito-de-compras-laura/backend/src/controllers/cartItemControllers.test.js
+++ laura-arcidiacono/challenges/carrito-de-compras-laura/backend/src/controllers/cartItemControllers.test.js
@@ -1,26 +1,85 @@
-// const Cart = require('../models/cartItemModel');
-// const controllers = require('./cartItemControllers');
+const Cart = require('../models/cartItemModel');
+const controllers = require('./cartItemControllers');
 
-// jest.mock('../models/cartItemModel');
+jest.mock('../models/cartItemModel');
 
-// describe('Given a getAll function', () => {
-//   describe('When is triggered', () => {
-//     test('Then call json', async () => {
-//       const req = {
-//         query: jest.fn()
-//       };
-//       const res = {
-//         send: jest.fn(),
-//         status: jest.fn(),
-//         json: jest.fn()
-//       };
+describe('Given a getAll function', () => {
+  describe('When is triggered', () => {
+    test('Then call json', async () => {
+      const req = {
+        query: jest.fn()
+      };
+      const res = {
+        send: jest.fn(),
+        status: jest.fn(),
+        json: jest.fn()
+      };
 
-//       Cart.find.mockResolvedValue({}).populate.mockResolvedValue(req)
-//         .populate.mockResolvedValue(req);
+      Cart.find.mockResolvedValue({}).populate.mockResolvedValue(req)
+        .populate.mockResolvedValue(req);
 
-//       await controllers.getAll(req, res);
+      await controllers.getAll(req, res);
 
-//       expect(res.json).toHaveBeenCalled();
-//     });
-//   });
-// });
+      expect(res.json).toHaveBeenCalled();
+    });
+  });
+});
+
+describe('Given a createOne function', () => {
+  describe('When is triggered', () => {
+    test('Then call json', async () => {
+      const req = {
+        body: {}
+      };
+      const res = {
+        json: jest.fn(),
+        status: jest.fn(),
+        send: jest.fn()
+      };
+      Cart.find.mockResolvedValue({});
+
+      await controllers.createOne(req, res);
+
+      expect(res.json).toHaveBeenCalled();
+    });
+  });
+});
+
+describe('Given a getOneById function', () => {
+  describe('When is triggered', () => {
+    test('Then call json', async () => {
+      const req = {
+        query: { cartId: {} }
+      };
+      const res = {
+        json: jest.fn(),
+        status: jest.fn(),
+        send: jest.fn()
+      };
+      Cart.findById.mockResolvedValue({});
+
+      await controllers.getOneById(req, res);
+
+      expect(res.json).toHaveBeenCalled();
+    });
+  });
+});
+
+describe('Given a deleteOneById function', () => {
+  describe('When is triggered', () => {
+    test('Then call send without argument', async () => {
+      const req = {
+        params: { cartId: null }
+      };
+      const res = {
+        send: jest.fn(),
+        status: jest.fn()
+      };
+      Cart.findByIdAndDelete.mockResolvedValue();
+
+      await controllers.deleteOneById(req, res);
+
+      expect(res.send).toHaveBeenCalled();
+    });
+  });
+});
