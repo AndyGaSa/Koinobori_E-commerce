aa7f9600db4c674f963566a45036a7a6efb29a47
“Andy <garcia.andy.tecnologies10@gmail.com>
2021-08-13 20:34:22 +0200

    Create backend config

---
+++ andy-garcia/challenge/fs-shop/backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('dotenv').config();
+const debug = require('debug')('shops');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const cors = require('cors');
+require('./src/config/mongooseConfig');
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(cors());
+server.use(morgan('dev'));
+server.use(express.json());
+
+const usersRouter = require('./src/routes/usersRouter');
+const cartRouter = require('./src/routes/cartRouter');
+const productRouter = require('./src/routes/productRouter');
+
+server.use('/api/user', usersRouter);
+server.use('/api/product', productRouter);
+server.use('/api/cart', cartRouter);
+
+server.listen(
+  port,
+  () => debug(`Server is running on ${chalk.bgBlue(`http://localhost:${port}`)}`),
+);
---
+++ andy-garcia/challenge/fs-shop/backend/src/controllers/productController.js
@@ -0,0 +1,38 @@
+const Product = require('../models/productModel');
+
+async function createProduct(req, res) {
+  const newBeer = await Product.create(req.body);
+
+  return res.send(newBeer);
+}
+
+async function getProduct(req, res) {
+  try {
+    const productItems = await Product.find();
+    res.json(productItems);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+function deleteProduct({ beer }, res) {
+  const { _id } = beer;
+  Product.findByIdAndDelete(_id).exec();
+  res.send(`La cerveza ${beer.name} ha sido eliminada`);
+}
+async function findProduct(req, res, next) {
+  const { beerId } = req.params;
+  const beer = await Product.findOne({ id: +beerId }).exec();
+  try {
+    req.beer = beer;
+    next();
+  } catch (error) {
+    res.status(404);
+    res.send(new Error(`There is no beer with id ${beerId}`));
+  }
+}
+
+module.exports = {
+  createProduct, getProduct, deleteProduct, findProduct,
+};
