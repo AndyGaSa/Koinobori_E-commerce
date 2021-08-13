46c4fcbe49eae388ee8dae06e7f17692fd3ac482
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-13 17:39:16 +0200

    Add desktop page

--- pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/index.jsx
+++ pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 
+import Desktop from './pages/Desktop';
+
 ReactDOM.render(
-  <React.StrictMode />,
+  <React.StrictMode>
+    <Desktop />
+  </React.StrictMode>,
+
   document.getElementById('root'),
 );
---
+++ pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/pages/Desktop.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Desktop() {
+  return (
+    <>
+      <h2>Esto es el desktop</h2>
+    </>
+  );
+}
