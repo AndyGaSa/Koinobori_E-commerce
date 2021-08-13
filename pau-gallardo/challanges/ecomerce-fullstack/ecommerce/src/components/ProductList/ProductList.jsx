650d7156b14c98dc03cf32e9818073de68cc3ac6
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-13 18:19:18 +0200

    Create productList main structure

--- pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/components/ProductList/ProductList.jsx
+++ pau-gallardo/challanges/ecomerce-fullstack/ecommerce/src/components/ProductList/ProductList.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
 
+const productsMock = [
+  {
+    _id: '61140a1c5b8d30faa0dfd80e',
+    name: 'Batamanta',
+    price: 25,
+    stock: -105,
+    __v: 0,
+  },
+  {
+    _id: '61140a2a5b8d30faa0dfd810',
+    name: 'Batamanta 2000',
+    price: 45,
+    stock: 2,
+    __v: 0,
+  },
+  {
+    _id: '6114148540ee0bfcb94ccf4a',
+    __v: 0,
+  },
+  {
+    _id: '61153551b52f8c11010715c1',
+    name: 'Moto',
+    price: 4500,
+    stock: -21,
+    __v: 0,
+  },
+];
+
 export default function ProductList() {
   return (
     <>
-      <h2>Esto es una lista de productos</h2>
+      <h2>Product List</h2>
+      <ul>
+        {
+        productsMock.map((singleProduct) => (
+          <li>
+            <span>{`Product name ${singleProduct.name}`}</span>
+            <span>{`${singleProduct.price}€}`}</span>
+            <span>{`Stock ${singleProduct.stock} units`}</span>
+            <button
+              type="button"
+            >
+              Add to cart
+            </button>
+          </li>
+        ))
+        }
+      </ul>
     </>
   );
 }
