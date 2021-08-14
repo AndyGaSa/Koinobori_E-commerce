238a00ecff2ca2635cbbe361dbd39370bb0d9588
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-08-14 15:20:26 +0200

    Add sneakers component final version

--- alvaro-martin/challenge/sneakers-ecommerce/frontend/src/sneakers-data/data.js
+++ alvaro-martin/challenge/sneakers-ecommerce/frontend/src/sneakers-data/data.js
@@ -5,12 +5,22 @@ export default [{
 },
 {
   name: 'Nike OFF-WHITE x Air Force 1',
-  img: 'https://sportcortez.com/wp-content/uploads/2020/07/n97.png',
-  price: '700€',
+  img: 'https://i.pinimg.com/originals/a9/11/df/a911df54a18776e380cd9bdfff205ce7.png',
+  price: '6.200€',
 },
 {
-  name: 'Air Jordan Dior',
-  img: 'https://sportcortez.com/wp-content/uploads/2020/07/n97.png',
+  name: 'Air Jordan 1',
+  img: 'https://calzatendencias.com/1896-large_default/nike-air-jordan-1-retro-unc.jpg',
+  price: '6.200€',
+},
+{
+  name: 'Air Jordan 1',
+  img: 'https://calzatendencias.com/1896-large_default/nike-air-jordan-1-retro-unc.jpg',
+  price: '6.200€',
+},
+{
+  name: 'Air Jordan 1',
+  img: 'https://calzatendencias.com/1896-large_default/nike-air-jordan-1-retro-unc.jpg',
   price: '6.200€',
 },
 ];
