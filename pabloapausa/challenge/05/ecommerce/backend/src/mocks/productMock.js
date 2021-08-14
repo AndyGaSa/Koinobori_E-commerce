105b433fdbb63fc06f639ea6b7af5e76ce863005
pabloapausa <pabloapausa@outlook.com>
2021-08-14 19:13:39 +0200

    Write 'user', 'product' and 'cart' mocks

--- pabloapausa/challenge/05/ecommerce/backend/src/mocks/productMock.js
+++ pabloapausa/challenge/05/ecommerce/backend/src/mocks/productMock.js
@@ -1,9 +1,47 @@
-const { model, Schema } = require('mongoose');
-
-const product = Schema({
-  name: String,
-  price: Number,
-  stock: Boolean,
-});
-
-module.exports = model('Product', product);
+module.exports = [
+  {
+    name: 'red t-shirt',
+    price: 21,
+    stock: true,
+  },
+  {
+    name: 'blue t-shirt',
+    price: 21,
+    stock: true,
+  },
+  {
+    name: 'yellow t-shirt',
+    price: 21,
+    stock: true,
+  },
+  {
+    name: 'green t-shirt',
+    price: 21,
+    stock: true,
+  },
+  {
+    name: 'violet t-shirt',
+    price: 21,
+    stock: true,
+  },
+  {
+    name: 'orange t-shirt',
+    price: 21,
+    stock: true,
+  },
+  {
+    name: 'black t-shirt',
+    price: 21,
+    stock: true,
+  },
+  {
+    name: 'white t-shirt',
+    price: 21,
+    stock: true,
+  },
+  {
+    name: 'gray t-shirt',
+    price: 21,
+    stock: true,
+  },
+];
--- pabloapausa/challenge/05/ecommerce/backend/src/mocks/userMock.js
+++ pabloapausa/challenge/05/ecommerce/backend/src/mocks/userMock.js
@@ -1,7 +1,68 @@
-const { model, Schema } = require('mongoose');
-
-const user = Schema({
-  name: String,
-});
-
-module.exports = model('User', user);
+module.exports = [
+  {
+    name: 'adrian',
+  },
+  {
+    name: 'alba',
+  },
+  {
+    name: 'alex',
+  },
+  {
+    name: 'alvaro',
+  },
+  {
+    name: 'andy',
+  },
+  {
+    name: 'bryan',
+  },
+  {
+    name: 'chej',
+  },
+  {
+    name: 'daniela',
+  },
+  {
+    name: 'eugenia',
+  },
+  {
+    name: 'ignacio',
+  },
+  {
+    name: 'jordi',
+  },
+  {
+    name: 'laura',
+  },
+  {
+    name: 'lluc',
+  },
+  {
+    name: 'lola',
+  },
+  {
+    name: 'manuel',
+  },
+  {
+    name: 'marc',
+  },
+  {
+    name: 'nacho',
+  },
+  {
+    name: 'pablo',
+  },
+  {
+    name: 'pau',
+  },
+  {
+    name: 'pere',
+  },
+  {
+    name: 'ricard',
+  },
+  {
+    name: 'xavi',
+  },
+];
