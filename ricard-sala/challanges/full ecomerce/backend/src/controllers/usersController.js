3c23aaec4d37c30f5db91b2f6d211f2aaf3b2fb5
ricardSala <riki_sala@icloud.com>
2021-08-14 16:07:32 +0200

    Add user model, router and controllers

---
+++ ricard-sala/challanges/full ecomerce/backend/src/controllers/usersController.js
@@ -0,0 +1,37 @@
+const User = require('../models/userModel');
+
+async function getAll({ query }, res) {
+  try {
+    const users = await User.find(query);
+    res.json(users);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function createOne({ body }, res) {
+  try {
+    const createdUser = await User.create(body);
+    res.json(createdUser);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function getOneById({ params: { userId } }, res) {
+  try {
+    const foundUser = await User.findById(userId);
+    res.json(foundUser);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+module.exports = {
+  getAll,
+  createOne,
+  getOneById,
+};
