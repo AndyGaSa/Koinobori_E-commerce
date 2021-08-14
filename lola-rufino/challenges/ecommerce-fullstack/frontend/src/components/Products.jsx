769781be1c7c899047ea12bf813158fa56a5404b
Lola Rufino <lola.ruf.ar@gmail.com>
2021-08-14 12:19:33 +0200

    Prepare frontend with mocks

--- lola-rufino/challenges/ecommerce-fullstack/frontend/src/components/Products.jsx
+++ lola-rufino/challenges/ecommerce-fullstack/frontend/src/components/Products.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import loadProducts from '../redux/actions/actionCreators';
 
 export default function Products() {
+  const products = useSelector((store) => store.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadProducts());
+  }, []);
   return (
     <main>
       <h2>Product List</h2>
       <ul>
-        <li>Sopa de miso</li>
-        <li>Wasabi</li>
-        <li>Algas nori</li>
+        {products.map((product) => <li>{product.name}</li>)}
       </ul>
     </main>
   );
