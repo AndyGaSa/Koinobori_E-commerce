5b643ac13b4f6fbf2965cdc1d7f15a070d2f2d10
ricardSala <riki_sala@icloud.com>
2021-08-14 16:05:04 +0200

    Add and config routers
    -Cart route
    -User route

--- ricard-sala/challanges/full ecomerce/backend/index.js
+++ ricard-sala/challanges/full ecomerce/backend/index.js
@@ -12,8 +12,13 @@ server.use(morgan('dev'));
 server.use(express.json());
 
 const prouductsRouter = require('./src/routes/productsRouter');
+const userRouter = require('./src/routes/userRouter');
+const cartRouter = require('./src/routes/cartRouter');
 
 server.use('/api/products', prouductsRouter);
+server.use('/api/users', userRouter);
+server.use('/api/cart', cartRouter);
+
 server.listen(
   port,
   () => debug(`server is running on http://localhost:${port}`),
