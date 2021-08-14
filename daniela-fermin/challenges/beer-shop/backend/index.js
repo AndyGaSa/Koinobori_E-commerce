63df0e6afc3a840a23bef5b339219fdc8a386d80
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-14 12:33:03 +0200

    Create the database congiguration model and the index file for the backend

---
+++ daniela-fermin/challenges/beer-shop/backend/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const debug = require('debug')('server');
+
+require('./src/configs/ddbbConfig');
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(morgan('dev'));
+server.use(express.json());
+
+const productRouter = require('./src/routers/productRouter');
+const userRouter = require('./src/routers/userRouter');
+const cartRouter = require('./src/routers/cartRouter');
+
+server.use('/api/products', productRouter);
+server.use('/api/users', userRouter);
+server.use('/api/cart', cartRouter);
+
+server.listen(
+  port,
+  () => debug(`Server is running on http://localhost:${port}`)
+);
