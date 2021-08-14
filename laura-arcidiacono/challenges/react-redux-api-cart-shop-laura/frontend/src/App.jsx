41f4201fe7c9eab2116642528394215e7d6d235d
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-14 16:18:04 +0200

    Add Provider and Products component to App.jsx

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/App.jsx
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import Products from './components/products/Products';
+import configureStore from './redux/store/index';
 
 function App() {
   return (
-    <h1>Hola Lau!</h1>
+    <Provider store={configureStore()}>
+      <h1>Hola Lau!</h1>
+      <Products />
+    </Provider>
+
   );
 }
 
