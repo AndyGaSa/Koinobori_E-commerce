ba3eebd4f6b5729da99b4aed3689ce03a09d9623
Xavier Lasierra <xlp1998@gmail.com>
2021-08-14 15:42:29 +0200

    Create getUser action

--- xavi-lasierra/challenges/ecommerce-api/frontend/src/components/Header/Header.jsx
+++ xavi-lasierra/challenges/ecommerce-api/frontend/src/components/Header/Header.jsx
@@ -1,14 +1,28 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import LoginButton from '../LoginButton/LoginButton';
 import LogoutButton from '../LogoutButton/LogoutButton';
 import Profile from '../Profile/Profile';
 
+import getUser from '../../redux/actions/users.creator';
 import './header.scss';
 
 function Header() {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, user } = useAuth0();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const userData = {
+        name: user.name,
+        email: user.email
+      };
+      dispatch(getUser(userData));
+    }
+  }, [isAuthenticated]);
+
   return (
     <header className="header">
       <h1 className="header__title">shoeShop</h1>
