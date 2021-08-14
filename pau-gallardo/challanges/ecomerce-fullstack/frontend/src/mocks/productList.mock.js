ec11de2bbad3b3c5c6f2759655357d3484ae62b8
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-14 20:39:53 +0200

    Change folder name

--- pau-gallardo/challanges/ecomerce-fullstack/frontend/src/mocks/productList.mock.js
+++ pau-gallardo/challanges/ecomerce-fullstack/frontend/src/mocks/productList.mock.js
@@ -3,7 +3,7 @@ export default [
     _id: '61140a1c5b8d30faa0dfd80e',
     name: 'Batamanta',
     price: 25,
-    stock: -105,
+    stock: 3,
     __v: 0,
   },
   {
@@ -21,7 +21,7 @@ export default [
     _id: '61153551b52f8c11010715c1',
     name: 'Moto',
     price: 4500,
-    stock: -21,
+    stock: 5,
     __v: 0,
   },
 ];
