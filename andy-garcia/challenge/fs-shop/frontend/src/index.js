c1e8b027d2233bd5f1cab86c21a4ba0943ada5f7
Adrian Pinto <adripintogonzalez@gmail.com>
2021-08-14 11:57:34 +0200

    Init Eslint

--- andy-garcia/challenge/fs-shop/frontend/src/index.js
+++ andy-garcia/challenge/fs-shop/frontend/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -8,7 +9,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
