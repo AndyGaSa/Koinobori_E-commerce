98a3d9c52bc9b6411ba2382936bdcb0b583f6a53
Alba del Rio <adelriocastellvi@gmail.com>
2021-08-15 12:20:53 +0200

    Refactor loadArticles actionCreator

--- alba-delrio/challenges/ecomerce-fullStack/frontend/src/Redux/actions/actionCreators.js
+++ alba-delrio/challenges/ecomerce-fullStack/frontend/src/Redux/actions/actionCreators.js
@@ -1,8 +1,12 @@
+import axios from 'axios';
 import actionTypes from './actionTypes';
 
-export default function loadArticles(articles) {
-  return {
-    type: actionTypes.LOAD_ARTICLES,
-    articles
+export default function loadArticles() {
+  return async (dispatch) => {
+    const { articles } = await axios.get('/api/article');
+    dispatch({
+      type: actionTypes.LOAD_ARTICLES,
+      articles
+    });
   };
 }
