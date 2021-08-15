3c84a42f5dd10738133de2005ab30983fad94120
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-15 09:45:05 +0200

    Add Header component to App.js

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/App.jsx
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import configureStore from './redux/store';
+import Header from './components/header/Header';
 import Products from './components/products/Products';
-import configureStore from './redux/store/index';
 
 function App() {
   return (
     <Provider store={configureStore()}>
-      <h1>Hola Lau!</h1>
+      <Header />
       <Products />
     </Provider>
 
