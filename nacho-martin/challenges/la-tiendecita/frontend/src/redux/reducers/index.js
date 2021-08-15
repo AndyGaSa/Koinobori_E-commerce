8900581bc0bfc63b7d0740a2dfd9c3253757ee0d
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-15 12:20:14 +0200

    Modify export to default

--- nacho-martin/challenges/la-tiendecita/frontend/src/redux/reducers/index.js
+++ nacho-martin/challenges/la-tiendecita/frontend/src/redux/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import productReducers from './productReducer';
 
-combineReducers({
-  product: productReducers
+export default combineReducers({
+  products: productReducers
 });
