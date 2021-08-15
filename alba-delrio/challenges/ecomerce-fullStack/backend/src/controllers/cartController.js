76a5c59f0d88d2f4f41c07471582b53cad3b0f2f
Alba del Rio <adelriocastellvi@gmail.com>
2021-08-15 10:29:07 +0200

    Create react App

--- alba-delrio/challenges/ecomerce-fullStack/backend/src/controllers/cartController.js
+++ alba-delrio/challenges/ecomerce-fullStack/backend/src/controllers/cartController.js
@@ -1,7 +1,31 @@
+/* eslint-disable no-param-reassign */
 const Cart = require('../models/cartModel');
+const Article = require('../models/articleModel');
+
+async function updateCart({ params: { _id, quantity } }, res, findCart) {
+  const article = _id;
+  findCart.products.forEach((item) => {
+    if (item.article.equals(article)) {
+      item.amount = quantity;
+    }
+  });
+  await Article.findByIdAndUpdate(
+    _id,
+    { $inc: { stock: -quantity } }
+
+  );
+  findCart.save();
+}
 
 async function createCart(req, res) {
   try {
+    const [findCart] = await Cart.find({ user: req.body.user })
+      .populate('user')
+      .populate('articles,article');
+
+    if (findCart) {
+      return updateCart(req, res, findCart);
+    }
     const newCart = await Cart.create(req.body);
     return res.send(newCart);
   } catch (error) {
@@ -10,6 +34,16 @@ async function createCart(req, res) {
   }
 }
 
+async function deleteCart({ params: { _id } }, res) {
+  try {
+    await Cart.findByIdAndDelete(_id);
+    res.send('The cart has been deleted');
+  } catch (error) {
+    res.status(404);
+    res.send(new Error('There is no cart'));
+  }
+}
+
 module.exports = {
-  createCart
+  createCart, updateCart, deleteCart
 };
--- alba-delrio/challenges/ecomerce-fullStack/backend/src/routers/cartRouter.js
+++ alba-delrio/challenges/ecomerce-fullStack/backend/src/routers/cartRouter.js
@@ -4,7 +4,11 @@ const cartController = require('../controllers/cartController');
 const cartRouter = express.Router();
 
 cartRouter
-  .route('/')
-  .post(cartController.createCart);
+  .route('/:_id')
+  .delete(cartController.deleteCart);
 
+cartRouter
+  .route('/:_id/:quantity')
+  .get(cartController.createCart)
+  .post(cartController.updateCart);
 module.exports = cartRouter;
