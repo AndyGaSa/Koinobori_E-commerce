6dcdb1f3cdb1537016873a353321424a7a988df9
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-15 09:40:03 +0200

    Fix import of rootReducer

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/redux/store/index.js
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/redux/store/index.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers/productsReducer';
+import rootReducer from '../reducers/index';
 
 export default function configureStore(preloadedState) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
