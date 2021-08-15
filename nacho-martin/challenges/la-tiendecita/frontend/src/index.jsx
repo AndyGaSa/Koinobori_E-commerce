d4d322ef218d567eb3ed1b70023a2acf20cd105f
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-15 12:16:38 +0200

    Add provider and store

--- nacho-martin/challenges/la-tiendecita/frontend/src/index.jsx
+++ nacho-martin/challenges/la-tiendecita/frontend/src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
