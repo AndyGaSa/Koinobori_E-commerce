c3ab52264a159248c949b12235abd8f052e538e7
Alba del Rio <adelriocastellvi@gmail.com>
2021-08-15 11:36:13 +0200

    Add Redux folders
    -actions
    -reducers
    -store

---
+++ alba-delrio/challenges/ecomerce-fullStack/frontend/src/Redux/actions/actionCreators.js
@@ -0,0 +1,8 @@
+import actionTypes from './actionTypes';
+
+export function loadArticles() {
+  return {
+    type: actionTypes.LOAD_ARTICLES,
+    articles
+  };
+}
---
+++ alba-delrio/challenges/ecomerce-fullStack/frontend/src/Redux/actions/actionTypes.js
@@ -0,0 +1,3 @@
+export default {
+  LOAD_ARTICLES: 'LOAD_ARTICLES'
+};
