ba5ffa7d0dde7dd9505a6742fe9388c9505f07f0
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-15 19:07:40 +0200

    Add username input frontend functionality

--- pau-gallardo/challanges/ecomerce-fullstack/frontend/src/components/UsernameInput/UsernameInput.jsx
+++ pau-gallardo/challanges/ecomerce-fullstack/frontend/src/components/UsernameInput/UsernameInput.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import setUsername from '../../redux/actions/username.creator';
 
 export default function UsernameInput() {
+  const dispatch = useDispatch();
+  const [user, setUser] = useState();
+
+  function userChange(event) {
+    setUser(event.target.value);
+  }
+
   return (
     <>
       <h1>Introduce your Name</h1>
       <input
         type="text"
+        name="username"
         placeholder="Introduce your name"
+        onChange={userChange}
       />
       <button
         type="button"
+        onClick={() => dispatch(setUsername(user))}
       >
         Save
       </button>
--- pau-gallardo/challanges/ecomerce-fullstack/frontend/src/redux/reducers/index.js
+++ pau-gallardo/challanges/ecomerce-fullstack/frontend/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import productListReducer from './productList.reducer';
 import cartReducer from './cart.reducer';
+import usernameReducer from './username.reducer';
 
 export default combineReducers({
   products: productListReducer,
   cartProducts: cartReducer,
+  username: usernameReducer,
 });
