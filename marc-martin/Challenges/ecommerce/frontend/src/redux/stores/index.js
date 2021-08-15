3d7848514eee50764edbb51683b13ce1c1179096
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-15 11:55:52 +0200

    Create configureStore set up

---
+++ marc-martin/Challenges/ecommerce/frontend/src/redux/stores/index.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/index';
+
+export default function configureStore(preloadedState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(applyMiddleware(thunk))
+
+  );
+}
