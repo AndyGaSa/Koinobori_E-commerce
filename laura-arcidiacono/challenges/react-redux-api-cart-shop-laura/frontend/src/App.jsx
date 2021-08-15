01c425c966db037d22c87993ea4049a2ad43d0c8
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-15 14:58:58 +0200

    Refactor App.jsx
    -Remove Products component
    -Add MainContainer component

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/App.jsx
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
 import Header from './components/header/Header';
-import Products from './components/products/Products';
+import MainContainer from './components/mainContainer/MainContainer';
 
 function App() {
   return (
     <Provider store={configureStore()}>
       <Header />
-      <Products />
+      <MainContainer />
     </Provider>
 
   );
