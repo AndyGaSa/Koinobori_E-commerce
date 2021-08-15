ff9e4e4af3396e53d394e58d674d0302b4e4a063
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-15 09:48:10 +0200

    Implement cors and add routers
    -Implement cors to server.use
    -Add user and cart routers

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/backend/index.js
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const debug = require('debug')('bookShop');
 const morgan = require('morgan');
+const cors = require('cors');
 const chalk = require('chalk');
 
 require('./src/config/ddbbConfig');
@@ -9,12 +10,21 @@ require('./src/config/ddbbConfig');
 const server = express();
 const port = process.env.PORT || 5000;
 
+server.use(cors());
 server.use(morgan('dev'));
 server.use(express.json());
+
+const userRouter = require('./src/routes/userRouter');
+
+server.use('/api/users', userRouter);
 
 const productRouter = require('./src/routes/productRouter');
 
 server.use('/api/products', productRouter);
+
+const cartRouter = require('./src/routes/cartRouter');
+
+server.use('/api/carts', cartRouter);
 
 server.listen(
   port,
