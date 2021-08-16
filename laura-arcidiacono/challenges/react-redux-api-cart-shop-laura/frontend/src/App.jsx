f46889ba15b5cff4f54ffa07bb75778c1dc3268f
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-16 18:56:55 +0200

    Add footer component to App.jsx

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/App.jsx
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux';
 import configureStore from './redux/store';
 import Header from './components/header/Header';
 import MainContainer from './components/mainContainer/MainContainer';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <Provider store={configureStore()}>
       <Header />
       <MainContainer />
+      <Footer />
     </Provider>
 
   );
