7a4af78e6f3390923234d6d2cdcda6be32fec4ec
Xavier Lasierra <xlp1998@gmail.com>
2021-08-16 11:51:12 +0200

    Add destructuring when importing render in  ReactDom in index.js

--- xavi-lasierra/challenges/ecommerce-api/frontend/src/index.js
+++ xavi-lasierra/challenges/ecommerce-api/frontend/src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Auth0Provider } from '@auth0/auth0-react';
 
@@ -8,7 +8,7 @@ import App from './App';
 
 import configureStore from './redux/store';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <Auth0Provider
       domain={`${process.env.REACT_APP_AUTH0_DOMAIN}`}
