f44f2a4c4eed05e67ce3f2f3d1fa07adab49a521
Xavier Lasierra <xlp1998@gmail.com>
2021-08-16 12:43:57 +0200

    Add footer component and adapt style

---
+++ xavi-lasierra/challenges/ecommerce-api/frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './footer.scss';
+
+function Footer() {
+  return (
+    <footer>
+      <small>Copyright © 2021 shoeShop</small>
+      <address>
+        1821 Adamsville Road
+        <br />
+        Harlingen, Texas
+      </address>
+    </footer>
+  );
+}
+
+export default Footer;
