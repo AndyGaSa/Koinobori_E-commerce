c2d1ea48b03346d36b9d30f13b23dec69b51ea24
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-16 20:33:08 +0200

    Add cases to productsReducer
    -Add  ADD_TO_CART and PAY_PRODUCTS

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/redux/reducers/productsReducer.js
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/redux/reducers/productsReducer.js
@@ -1,10 +1,30 @@
+/* eslint-disable no-unreachable */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 import actionTypes from '../actions/actionTypes';
 
 export default function productsReducer(products = [], action) {
+  let nextProducts = products;
   switch (action.type) {
     case actionTypes.LOAD_PRODUCTS:
       return action.data;
+      break;
+
+    case actionTypes.ADD_TO_CART:
+      nextProducts = products.map((product) => {
+        if (product._id === action.product.product) {
+          product.stock -= action.product.amount;
+        }
+        return product;
+      });
+      break;
+
+    case actionTypes.PAY_PRODUCTS:
+      nextProducts = action.products;
+      break;
+
     default:
-      return products;
+      break;
   }
+  return nextProducts;
 }
