af14376b18d359ae5754a0ee3e3e2d52b97b8733
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-16 09:16:12 +0200

    Add test to rejected cases

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/backend/src/controllers/userControllers.test.js
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/backend/src/controllers/userControllers.test.js
@@ -5,109 +5,215 @@ jest.mock('../models/userModel');
 
 describe('Given a getAll function', () => {
   describe('When is triggered', () => {
-    test('Then call json', async () => {
-      const req = {
-        query: jest.fn()
-      };
-      const res = {
-        json: jest.fn(),
-        status: jest.fn(),
-        send: jest.fn()
-      };
+    describe('And find resolved', () => {
+      test('Then call json', async () => {
+        const req = {
+          query: jest.fn()
+        };
+        const res = {
+          json: jest.fn(),
+          status: jest.fn(),
+          send: jest.fn()
+        };
 
-      User.find.mockResolvedValue();
+        User.find.mockResolvedValue();
 
-      await controllers.getAll(req, res);
+        await controllers.getAll(req, res);
 
-      expect(res.json).toHaveBeenCalled();
+        expect(res.json).toHaveBeenCalled();
+      });
+    });
+
+    describe('And find rejects', () => {
+      test('Then call status 500', async () => {
+        const req = {
+          query: jest.fn()
+        };
+        const res = {
+          json: jest.fn(),
+          status: jest.fn(),
+          send: jest.fn()
+        };
+
+        User.find.mockRejectedValue();
+
+        await controllers.getAll(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+      });
     });
   });
 });
 
 describe('Given a createOne function', () => {
   describe('When is triggered', () => {
-    test('Then call json', async () => {
-      const req = {
-        body: {}
-      };
-      const res = {
-        json: jest.fn(),
-        status: jest.fn(),
-        send: jest.fn()
-      };
+    describe('And find resolved', () => {
+      test('Then call json', async () => {
+        const req = {
+          body: {}
+        };
+        const res = {
+          json: jest.fn(),
+          status: jest.fn(),
+          send: jest.fn()
+        };
 
-      User.find.mockResolvedValue({});
+        User.find.mockResolvedValue({});
 
-      await controllers.createOne(req, res);
+        await controllers.createOne(req, res);
 
-      expect(res.json).toHaveBeenCalled();
+        expect(res.json).toHaveBeenCalled();
+      });
+    });
+
+    describe('And create rejected', () => {
+      test('Then call status with 500', async () => {
+        const req = {
+          body: {}
+        };
+        const res = {
+          json: jest.fn(),
+          status: jest.fn(),
+          send: jest.fn()
+        };
+
+        User.create.mockRejectedValue();
+
+        await controllers.createOne(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+      });
     });
   });
 });
 
 describe('Given a getById function', () => {
   describe('When is triggered', () => {
-    test('Then call json', async () => {
-      const req = {
-        query: {
-          userId: {}
-        }
-      };
-      const res = {
-        json: jest.fn(),
-        status: jest.fn(),
-        send: jest.fn()
-      };
-      User.findById.mockResolvedValue({});
+    describe('And findById respons', () => {
+      test('Then call json', async () => {
+        const req = {
+          query: {
+            userId: {}
+          }
+        };
+        const res = {
+          json: jest.fn(),
+          status: jest.fn(),
+          send: jest.fn()
+        };
+        User.findById.mockResolvedValue({});
 
-      await controllers.getById(req, res);
+        await controllers.getById(req, res);
 
-      expect(res.json).toHaveBeenCalled();
+        expect(res.json).toHaveBeenCalled();
+      });
+    });
+
+    describe('And findById reject', () => {
+      test('Then call status with 500', async () => {
+        const req = {
+          query: jest.fn()
+        };
+        const res = {
+          json: jest.fn(),
+          status: jest.fn(),
+          send: jest.fn()
+        };
+
+        User.findById.mockRejectedValue();
+
+        await controllers.getById(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+      });
     });
   });
 });
 
 describe('Given a updatedOneById', () => {
   describe('When is triggered', () => {
-    test('Then call json', async () => {
-      const req = {
-        query: {
-          userId: {}
-        },
-        body: {}
-      };
-      const res = {
-        json: jest.fn(),
-        status: jest.fn(),
-        send: jest.fn()
-      };
-      User.findByIdAndUpdate.mockResolvedValue({});
+    describe('And findByIdAndUpdate response', () => {
+      test('Then call json', async () => {
+        const req = {
+          query: {
+            userId: {}
+          },
+          body: {}
+        };
+        const res = {
+          json: jest.fn(),
+          status: jest.fn(),
+          send: jest.fn()
+        };
+        User.findByIdAndUpdate.mockResolvedValue({});
 
-      await controllers.updateOneById(req, res);
+        await controllers.updateOneById(req, res);
 
-      expect(res.json).toHaveBeenCalled();
+        expect(res.json).toHaveBeenCalled();
+      });
+    });
+    describe('And findByIdAndUpdate reject', () => {
+      test('Then call status with 500', async () => {
+        const req = {
+          query: {
+            userId: {}
+          },
+          body: {}
+        };
+        const res = {
+          json: jest.fn(),
+          status: jest.fn(),
+          send: jest.fn()
+        };
+        User.findByIdAndUpdate.mockRejectedValue();
+
+        await controllers.updateOneById(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+      });
     });
   });
 });
 
 describe('Given a deleteOneById function', () => {
   describe('When is triggered', () => {
-    test('Then call json', async () => {
-      const req = {
-        query: {
-          userId: {}
-        }
-      };
-      const res = {
-        json: jest.fn(),
-        status: jest.fn(),
-        send: jest.fn()
-      };
-      User.findByIdAndDelete.mockResolvedValue({});
+    describe('And findByIdAndDelete response', () => {
+      test('Then call json', async () => {
+        const req = {
+          query: {
+            userId: {}
+          }
+        };
+        const res = {
+          json: jest.fn(),
+          status: jest.fn(),
+          send: jest.fn()
+        };
+        User.findByIdAndDelete.mockResolvedValue({});
 
-      await controllers.deleteOneById(req, res);
+        await controllers.deleteOneById(req, res);
 
-      expect(res.json).toHaveBeenCalled();
+        expect(res.json).toHaveBeenCalled();
+      });
+    });
+    describe('And findByIdAndDelete reject', () => {
+      test('Then call json', async () => {
+        const req = {
+          query: {
+            userId: {}
+          }
+        };
+        const res = {
+          json: jest.fn(),
+          status: jest.fn(),
+          send: jest.fn()
+        };
+        User.findByIdAndDelete.mockRejectedValue();
+
+        await controllers.deleteOneById(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+      });
     });
   });
 });
