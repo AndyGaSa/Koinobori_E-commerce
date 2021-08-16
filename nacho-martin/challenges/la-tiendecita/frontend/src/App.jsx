8ef87011be361536aa2d0588f2c96fec7cd281ad
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-16 19:02:14 +0200

    Add user components and logic

--- nacho-martin/challenges/la-tiendecita/frontend/src/App.jsx
+++ nacho-martin/challenges/la-tiendecita/frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Cart from './components/Cart';
 import FunkoList from './components/FunkoList';
+import Header from './components/Header';
 
 import('./styles/global.scss');
 
 function App() {
   return (
     <>
+      <Header />
       <FunkoList />
       <Cart />
     </>
