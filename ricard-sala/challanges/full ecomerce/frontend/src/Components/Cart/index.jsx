5bf79828ed730104840d88ad3b0f680cb04b568c
ricardSala <riki_sala@icloud.com>
2021-08-16 11:30:32 +0200

    Add delete button

--- ricard-sala/challanges/full ecomerce/frontend/src/Components/Cart/index.jsx
+++ ricard-sala/challanges/full ecomerce/frontend/src/Components/Cart/index.jsx
@@ -10,9 +10,18 @@ export default function Cart() {
   }, [cartRedux]);
   return (
     <div className="cart">
-      <h3 className="cart__user">name:riki</h3>
+      <h3 className="cart__user">riki CART</h3>
       <div className="cart__products">
-        {productsToCart.map((element) => (<p>{element.name}</p>))}
+        {productsToCart.map((element) => (
+          <div className="cart-item">
+            <img className="cart-img" src={element.img} alt="imagen" />
+            <div className="cart-info">
+              <span className="cart-name">{element.name}</span>
+              <span className="cart-price">{element.price}</span>
+            </div>
+            <button className="delete-button" type="button">X</button>
+          </div>
+        )) }
       </div>
       <button className="button-cart" type="button">
         <img className="img-button" src="https://img.icons8.com/material-outlined/24/000000/fast-cart.png" alt="cart" />
