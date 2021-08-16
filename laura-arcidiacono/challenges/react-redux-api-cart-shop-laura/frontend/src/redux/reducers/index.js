bc21e281c1f4abea9471e1e5422be2778c4b0cec
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-16 20:34:20 +0200

    Add cartReducer to index.js

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/redux/reducers/index.js
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-import productsReducer from './productsReducer';
+import products from './productsReducer';
+import cart from './cartReducer';
 
 export default combineReducers({
-  products: productsReducer
+  products,
+  cart
 });
