b5e3baac73c00dc85002a25a5430684d5e969318
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-16 11:35:54 +0200

    Create buy products function on action Creator

--- marc-martin/Challenges/ecommerce/frontend/src/redux/actions/actionCreator.js
+++ marc-martin/Challenges/ecommerce/frontend/src/redux/actions/actionCreator.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
-export default function loadProducts() {
+export function loadProducts() {
   return async (dispatch) => {
-    const { data } = await axios.get('/api/productlist');
+    const { data } = await axios.get('http://localhost:5000/api/productlist');
 
     dispatch({
       type: actionTypes.LOAD_PRODUCTLIST,
+      data
+    });
+  };
+}
+
+export function buyProducts(cartData) {
+  return async (dispatch) => {
+    const { data } = await axios.post('/api/cart', cartData);
+
+    dispatch({
+      type: actionTypes.BUY_PRODUCTS,
       data
     });
   };
