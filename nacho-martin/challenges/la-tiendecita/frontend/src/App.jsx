8509aef5000c4c31d905e15d8eda76fbfad8f8fa
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-16 12:36:04 +0200

    Import style rules

--- nacho-martin/challenges/la-tiendecita/frontend/src/App.jsx
+++ nacho-martin/challenges/la-tiendecita/frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Cart from './components/Cart';
 import FunkoList from './components/FunkoList';
 
+import('./styles/global.scss');
+
 function App() {
   return (
     <>
