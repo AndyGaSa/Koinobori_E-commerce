2e1f54ac49bd282bc0408f4e75b4c54d17f18487
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-16 20:37:08 +0200

    Add functions to actionCreator
    -Add addToCart function
    -Add payProductsfunction

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/redux/actions/actionCreator.js
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/redux/actions/actionCreator.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
@@ -12,12 +13,33 @@ export function loadProducts() {
   };
 }
 
-export function buyProducts(cartData) {
+export function addToCart(product, amount = 1) {
+  return {
+    type: actionTypes.ADD_TO_CART,
+    product: {
+      name: product.name,
+      author: product.author,
+      price: product.price,
+      product: product._id,
+      amount
+    }
+  };
+}
+
+export function payProducts(cart) {
   return async (dispatch) => {
-    const { data } = await axios.post('/api/cart', cartData);
-    dispatch({
-      type: actionTypes.BUY_PRODUCTS,
-      data
-    });
+    try {
+      const { data: products } = await axios.post(process.env.REACT_APP_CART_ENDPOINT, cart);
+      dispatch({
+        type: actionTypes.PAY_PRODUCTS,
+        products
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.PRODUCT_API_ERROR,
+        products: [],
+        error: error.message
+      });
+    }
   };
 }
