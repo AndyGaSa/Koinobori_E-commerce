c17dd0f378a38550e6864f42bd1f1ebb392eec2b
Xavier Lasierra <xlp1998@gmail.com>
2021-08-16 13:43:33 +0200

    Add ADD_TO_CART and SUBSTRACT_FROM_CART actions in products reducer
    -Adds or substracts stock from the product.

--- xavi-lasierra/challenges/ecommerce-api/frontend/src/redux/reducers/products.reducer.js
+++ xavi-lasierra/challenges/ecommerce-api/frontend/src/redux/reducers/products.reducer.js
@@ -1,10 +1,31 @@
+/* eslint-disable no-underscore-dangle */
 import actionTypes from '../actions/products.actions';
+import cartTypes from '../actions/cart.actions';
 
 function productsReducer(products = [], action) {
   let newProducts = products;
-  if (action.type === actionTypes.LOAD_PRODUCTS) {
-    newProducts = action.data;
+  switch (action.type) {
+    case actionTypes.LOAD_PRODUCTS:
+      newProducts = action.data;
+      break;
+    case cartTypes.ADD_TO_CART:
+      newProducts = newProducts.map((product) => (
+        (product._id === action.data._id && product.stock > 0)
+          ? { ...product, stock: product.stock - 1 }
+          : product
+      ));
+      break;
+    case cartTypes.SUBSTRACT_FROM_CART:
+      newProducts = newProducts.map((product) => (
+        product._id === action.data
+          ? { ...product, stock: product.stock + 1 }
+          : product
+      ));
+      break;
+    default:
+      break;
   }
+
   return newProducts;
 }
 
