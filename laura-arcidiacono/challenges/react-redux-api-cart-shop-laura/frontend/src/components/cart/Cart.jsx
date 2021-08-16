6f3fc8bc1c4f4533fb8f1c3463319c0362c14f68
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-16 20:38:30 +0200

    Modify template and functions to Cart component

--- laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/components/cart/Cart.jsx
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/components/cart/Cart.jsx
@@ -1,29 +1,62 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, payProducts } from '../../redux/actions/actionCreator';
+
+import './Cart.css';
 
 export default function Cart() {
+  const cart = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  function handlePay() {
+    dispatch(payProducts(cart));
+  }
+
   return (
-    <main className="main_userCart">
-      <section className="cartItems">
+    <main className="main__user-cart">
+      <section className="user-cart__cart-items">
         <h3>Tus productos seleccionados</h3>
-        <ul className="cartItems_list">
-          <div className="list_cartItems" key="item_name">
-            <li className="cartItem_item">
-              <h3>La casa de los espíritus</h3>
-              <h4>Isabel Allende</h4>
-              <button type="button" className="item_button">-</button>
-              <div className="item_amount">3</div>
-              <button type="button" className="item_button">+</button>
-            </li>
-          </div>
+        <ul className="cart-items__list">
+          {cart?.items?.map((item) => (
+            <div className="list__cart-item" key="item_name">
+              <li className="cart-item__item">
+                <h3>{item.name}</h3>
+                <h4>{item.author}</h4>
+                <button
+                  type="button"
+                  className="item__button"
+                >
+                  -
+                </button>
+                <div className="item__amount">{item.amount}</div>
+                <button
+                  type="button"
+                  className="item__button"
+                  onClick={addToCart}
+                >
+                  +
+                </button>
+              </li>
+            </div>
+          ))}
         </ul>
+
       </section>
-      <section className="totalPrice">
-        <h3>Precio Total:</h3>
-        <div className="totalPrice_total">20€</div>
+      <section>
+        <h3 className="totalPrice">
+          Precio Total:
+          {' '}
+          {cart?.totalPrice}
+        </h3>
       </section>
       <section className="buttons">
-        <button type="button" className="buttons_pay">Pagar</button>
-        <button type="button" className="buttons_removeItem">Quitar todos</button>
+        <button
+          type="button"
+          className="buttons_pay"
+          onClick={handlePay}
+        >
+          Pagar
+        </button>
       </section>
     </main>
   );
