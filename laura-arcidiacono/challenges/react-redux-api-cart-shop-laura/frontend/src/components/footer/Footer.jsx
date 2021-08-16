b6bcc8738a3654db19c377831ce612ce0052cdf4
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-16 18:55:27 +0200

    Add component Footer and styles

---
+++ laura-arcidiacono/challenges/react-redux-api-cart-shop-laura/frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <img className="footer__img" src="https://static.vecteezy.com/system/resources/previews/002/219/582/non_2x/illustration-of-book-icon-free-vector.jpg" alt="Icono Libro" />
+      <h3 className="footer__title">Book Shop</h3>
+    </footer>
+  );
+}
