7db24f7608f28e0cc2ec772e458ab04ac181f319
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-16 19:55:18 +0200

    Add Search styles

--- pau-gallardo/challanges/punk-entrevista/punk-entrevista/src/components/Search/Search.jsx
+++ pau-gallardo/challanges/punk-entrevista/punk-entrevista/src/components/Search/Search.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 
+import './Search.scss';
+
 export default function Search() {
   return (
     <>
-      <input type="text" />
-      <label htmlFor="by_name">
-        by name
-        <input type="radio" id="by_name" />
-      </label>
-      <label htmlFor="by_description">
-        by description
-        <input type="radio" id="by_description" />
-      </label>
-      <button
-        type="button"
-      >
-        Search
-      </button>
+      <div className="search-container">
+        <div className="search-container__left">
+          <input
+            type="text"
+            placeholder="Search"
+          />
+          <section className="search-container__left__radios">
+            <label htmlFor="by_name">
+              <input type="radio" id="by_name" />
+              by name
+            </label>
+            <label htmlFor="by_description">
+              <input type="radio" id="by_description" />
+              by description
+            </label>
+          </section>
+        </div>
+        <div className="search-container__right">
+          <button
+            type="button"
+          >
+            Search
+          </button>
+        </div>
+      </div>
     </>
   );
 }
