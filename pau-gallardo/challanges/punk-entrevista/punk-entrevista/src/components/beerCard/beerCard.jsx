b143909373f60551cc6ce53c5335c4c99cc8b5ba
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-16 19:23:21 +0200

    Add BeerCard styles

--- pau-gallardo/challanges/punk-entrevista/punk-entrevista/src/components/beerCard/beerCard.jsx
+++ pau-gallardo/challanges/punk-entrevista/punk-entrevista/src/components/beerCard/beerCard.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-export default function beerCard(){
-    return(
+import './BeerCard.scss';
 
-    );
+export default function beerCard() {
+  return (
+    <section className="card-container">
+      <div className="card-container__left">
+        <h2>Beer Name</h2>
+        <img src="" alt="" />
+      </div>
+      <p className="card-container__center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ad cumque esse ullam incidunt nemo quo quisquam. Et voluptatem in vitae
+        placeat itaque maiores ex, debitis repellat vero explicabo aliquid iusto?
+      </p>
+      <div className="card-container__right">
+        <button
+          type="button"
+        >
+          Another Beer
+        </button>
+        <button
+          type="button"
+        >
+          Random non alchoolic
+        </button>
+      </div>
+    </section>
+  );
 }
--- pau-gallardo/challanges/punk-entrevista/punk-entrevista/src/index.jsx
+++ pau-gallardo/challanges/punk-entrevista/punk-entrevista/src/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import BeerCard from './components/BeerCard/BeerCard';
+
 ReactDOM.render(
-  <React.StrictMode />,
+  <React.StrictMode>
+    <BeerCard />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
