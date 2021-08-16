916aca7ce0a5daada6b9066693b41f23df6f4cf8
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-16 11:36:34 +0200

    Import configure Store
    - Add provider to App

--- marc-martin/Challenges/ecommerce/frontend/src/App.jsx
+++ marc-martin/Challenges/ecommerce/frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import Products from './components/Products';
+import configureStore from './redux/stores';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Skylab E-commerce</h1>
-    </div>
+    <Provider store={configureStore()}>
+      <main className="App">
+        <h1>Skylab E-commerce</h1>
+        <Products />
+      </main>
+    </Provider>
   );
 }
 
