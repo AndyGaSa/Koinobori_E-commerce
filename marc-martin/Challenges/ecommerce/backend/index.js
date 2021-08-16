02d84a37a937ffa75f5119da7ff958b92c0f81ca
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-16 11:36:52 +0200

    Add routes for users and cart

--- marc-martin/Challenges/ecommerce/backend/index.js
+++ marc-martin/Challenges/ecommerce/backend/index.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 const debug = require('debug')('ecommerce-server');
 const morgan = require('morgan');
 
-require('./src/config/ddbbConfig');
+require('./src/configs/ddbbConfig');
 
 const server = express();
 const port = process.env.PORT || 5000;
@@ -13,8 +13,12 @@ server.use(morgan('dev'));
 server.use(express.json());
 
 const productListRouter = require('./src/routers/productlistRouter');
+const userRouter = require('./src/routers/userRouter');
+const cartRouter = require('./src/routers/cartRouter');
 
 server.use('/api/productlist', productListRouter);
+server.use('/api/users', userRouter);
+server.use('/api/cart', cartRouter);
 
 server.listen(
   port,
