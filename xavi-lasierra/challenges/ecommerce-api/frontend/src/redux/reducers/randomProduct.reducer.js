740937435bf626aa3a540e3d803caeb2622c74c8
Xavier Lasierra <xlp1998@gmail.com>
2021-08-17 10:12:50 +0200

    Add ADD_TO_CART and SUBSTRACT_FROM_CART actions in randomProduct reducer

--- xavi-lasierra/challenges/ecommerce-api/frontend/src/redux/reducers/randomProduct.reducer.js
+++ xavi-lasierra/challenges/ecommerce-api/frontend/src/redux/reducers/randomProduct.reducer.js
@@ -1,10 +1,31 @@
+/* eslint-disable no-underscore-dangle */
 import actionTypes from '../actions/randomProduct.actions';
+import cartTypes from '../actions/cart.actions';
 
 function randomProductReducer(randomProduct = [], action) {
   let newRandomProduct = randomProduct;
-  if (action.type === actionTypes.GET_RANDOM_PRODUCT) {
-    newRandomProduct = action.data;
+  switch (action.type) {
+    case actionTypes.GET_RANDOM_PRODUCT:
+      newRandomProduct = action.data;
+      break;
+    case cartTypes.ADD_TO_CART:
+      newRandomProduct = newRandomProduct.map((product) => (
+        (product._id === action.data._id && product.stock > 0)
+          ? { ...product, stock: product.stock - 1 }
+          : product
+      ));
+      break;
+    case cartTypes.SUBSTRACT_FROM_CART:
+      newRandomProduct = newRandomProduct.map((product) => (
+        product._id === action.data
+          ? { ...product, stock: product.stock + 1 }
+          : product
+      ));
+      break;
+    default:
+      break;
   }
+
   return newRandomProduct;
 }
 
