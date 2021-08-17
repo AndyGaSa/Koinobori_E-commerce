c963ed20e51c94400e99e1b20b81789220cd75eb
Bryan Lc <bryanlc9808@gmail.com>
2021-08-17 17:07:45 +0200

    check the environment install dependencies set index

---
+++ bryanlc/challenges/nomos/backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+require('./src/config/mongoose.config');
+
+const debug = require('debug')('nomosSN');
+const morgan = require('morgan');
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(morgan('dev'));
+server.use(express.json());
+
+const userRoutes = require('./src/routes/usersRoute');
+
+server.use('/api/users', userRoutes);
+
+server.listen(
+  port,
+  () => debug(`Server is runing on http://localhost:${port}`)
+
+);
