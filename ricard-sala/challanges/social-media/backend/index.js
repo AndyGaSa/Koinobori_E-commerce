4c2d730ec8d04cb185208a0874453d7b7f4589ad
ricardSala <riki_sala@icloud.com>
2021-08-17 14:31:39 +0200

    Add Index

---
+++ ricard-sala/challanges/social-media/backend/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const debug = require('debug')('server');
+
+require('./src/config/ddbb-config');
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(morgan('dev'));
+server.use(express.json());
+
+const userRouter = require('./src/routes/userRouter');
+
+server.use('/api/user', userRouter);
+
+server.listen(
+  port,
+  () => debug(`server is running on http://localhost:${port}`),
+);
