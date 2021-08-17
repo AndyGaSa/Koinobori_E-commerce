8cf216433fe871628a902b6c166884a1466f51e7
Lluc Valério <lluc.valerio@gmail.com>
2021-08-17 20:14:56 +0200

    Add html structure

---
+++ lluc-valerio/challenges/social-network/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>user image and name</header>
+      <section>user data</section>
+      <aside>People</aside>
+      <a href="#">friends</a>
+      <a href="#">adversaries</a>
+
+    </>
+  );
+}
+
+export default App;
