ce8ddc0393bd8b13104cbce426d9742b7dc6c8b5
Alex <alexmunoz91@gmail.com>
2021-08-17 16:14:45 +0200

    Add userController functions - Add updateById function - Add deleteById function - Add getById function

--- alex-muñoz/challenges/social-app-fullstack/backend/src/controllers/userController.js
+++ alex-muñoz/challenges/social-app-fullstack/backend/src/controllers/userController.js
@@ -19,8 +19,61 @@ async function getAll({ query }, res) {
     res.send(error);
   }
 }
+async function getOneById({ params: { userId } }, res) {
+  try {
+    const user = await User.findById(userId)
+      .populate({
+        path: 'friends',
+        select: ['name']
+      })
+      .populate({
+        path: 'adversaries',
+        select: ['name']
+      });
+    res.json(user);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+async function updateOneById(
+  {
+    body,
+    params: { userId }
+  },
+  res
+) {
+  try {
+    const userToUpdate = await User.findByIdAndUpdate(
+      userId,
+      body,
+      {
+        new: true,
+        useFindAndModify: false
+      }
+    );
+    res.json(userToUpdate);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function deleteOneById({ params: { userId } }, res) {
+  try {
+    await User.findByIdAndDelete(userId);
+    res.status(204);
+    res.json();
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
 
 module.exports = {
   createOne,
-  getAll
+  getAll,
+  getOneById,
+  updateOneById,
+  deleteOneById
 };
