4b84dbec0700f0c3f5bf641005168e81b2b38371
ricardSala <riki_sala@icloud.com>
2021-08-18 01:03:10 +0200

    Refactor home page

--- ricard-sala/challanges/social-media/frontend/src/pages/Home.jsx
+++ ricard-sala/challanges/social-media/frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from '../redux/store';
+import Dashboard from '../components/dashboard/Dashboard';
 
 export default function Home() {
   return (
-    <div>hola</div>
+    <Provider store={configureStore()}>
+      <Dashboard />
+    </Provider>
   );
 }
