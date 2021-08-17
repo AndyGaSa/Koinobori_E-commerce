dbad5f9a635953de2064bcdf1dfd93aa202b691a
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-17 19:49:13 +0200

    Add styles to Header.jsx and fix test

--- laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/components/header/Header.jsx
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/components/header/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import './header.css';
 
 export default function Header() {
   return (
-    <h1 data-testid="header__title">Este es el Header</h1>
+    <header className="header">
+
+      <img className="header__img-gnomos" src="https://image.freepik.com/vector-gratis/gnomo-navidad-dibujos-animados-lindo_125446-557.jpg" alt="Logo de Gnomos Net" />
+      <div className="header__title-div">
+        <h1 className="header__title" data-testid="header__title">Gnomos</h1>
+        <p> the original Social Net</p>
+      </div>
+
+    </header>
   );
 }
--- laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/components/header/header.test.jsx
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/components/header/header.test.jsx
@@ -11,9 +11,9 @@ describe('Given a Header component', () => {
     test('Then h1 will be in the document', () => {
       expect(screen.getByTestId('header__title')).toBeInTheDocument();
     });
-    test('And the value will be Este es el Header', () => {
+    test('And the value will be Gnomos', () => {
       const titleH2 = screen.getByTestId('header__title');
-      expect(titleH2.textContent).toBe('Este es el Header');
+      expect(titleH2.textContent).toBe('Gnomos');
     });
   });
 });
