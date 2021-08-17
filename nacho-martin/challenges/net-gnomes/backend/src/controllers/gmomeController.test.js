8ec4237346fa121d6840c73d76be8a5ac6d4257d
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-17 15:17:37 +0200

    Refactor tests with beforeEach

--- nacho-martin/challenges/net-gnomes/backend/src/controllers/gmomeController.test.js
+++ nacho-martin/challenges/net-gnomes/backend/src/controllers/gmomeController.test.js
@@ -2,14 +2,15 @@ const gnomeController = require('./gnomeController');
 const Gnome = require('../models/gnomeModels');
 
 jest.mock('../models/gnomeModels');
-
+let req;
+let res;
 describe('Given a getAll function', () => {
   describe('When getAll is invoked with valid arguments', () => {
     test('Then res.json() should be called', async () => {
-      const req = {
+      req = {
         body: {}
       };
-      const res = {
+      res = {
         json: jest.fn(),
         status: jest.fn(),
         send: jest.fn()
@@ -20,17 +21,20 @@ describe('Given a getAll function', () => {
       expect(res.json).toHaveBeenCalled();
     });
   });
+});
+describe('Given a createOne function', () => {
   describe('When createOne is invoked', () => {
-    test('Then res.send should be called with 500 ', async () => {
-      const req = {
+    beforeEach(() => {
+      req = {
         body: {}
       };
-      const res = {
+      res = {
         json: jest.fn(),
         status: jest.fn(),
         send: jest.fn()
       };
-
+    });
+    test('Then res.send should be called with 500 ', async () => {
       Gnome.create.mockResolvedValue({ gnome: 'Pepa' });
 
       await gnomeController.createOne(req, res);
@@ -38,15 +42,6 @@ describe('Given a getAll function', () => {
       expect(res.json).toHaveBeenCalledWith({ gnome: 'Pepa' });
     });
     test('Then res.send should be called with 500 ', async () => {
-      const req = {
-        body: {}
-      };
-      const res = {
-        json: jest.fn(),
-        status: jest.fn(),
-        send: jest.fn()
-      };
-
       Gnome.create.mockRejectedValue(new Error('No gnomes found'));
 
       await gnomeController.createOne(req, res);
