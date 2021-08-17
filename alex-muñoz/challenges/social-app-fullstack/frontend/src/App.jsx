66b663cf7f1275d555ddd3d48751380735d5620f
Alex <alexmunoz91@gmail.com>
2021-08-17 18:34:39 +0200

    Add frontend develop enviroment

---
+++ alex-muñoz/challenges/social-app-fullstack/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hola Panitas</h1>
+      </header>
+    </div>
+  );
+}
