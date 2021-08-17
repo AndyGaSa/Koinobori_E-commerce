ce05f10fe84ac07c00cdd0788703a3ec69eef652
Xavier Lasierra <xlp1998@gmail.com>
2021-08-17 11:40:36 +0200

    Create gnome model

---
+++ xavi-lasierra/lectures/gnomes-social-fullstack/backend/src/models/gnome.model.js
@@ -0,0 +1,18 @@
+const { model, Schema } = require('mongoose');
+
+const gnomeSchema = Schema({
+  picture: { type: String, default: 'http://placehold.it/32x32' },
+  age: Number,
+  eyeColor: String,
+  name: String,
+  gender: String,
+  about: String,
+  registered: { type: Date, default: Date.now() },
+  tags: [String],
+  friends: [{ type: Schema.Types.ObjectId, ref: 'Gnome' }],
+  adversaries: [{ type: Schema.Types.ObjectId, ref: 'Gnome' }],
+  greeting: String,
+  favouriteFruit: String
+});
+
+module.exports = model('Gnome', gnomeSchema);
