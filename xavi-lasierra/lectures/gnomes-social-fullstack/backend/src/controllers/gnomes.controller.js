2fc865240fdd1369c5ed5e8603cebb0aec777b56
Xavier Lasierra <xlp1998@gmail.com>
2021-08-17 12:40:38 +0200

    Create controller functions for /api/gnomes route
    -Created and tested getGnomes, createGnome, getGnomeById, deleteGnomeById and updateGnomeById

--- xavi-lasierra/lectures/gnomes-social-fullstack/backend/src/controllers/gnomes.controller.js
+++ xavi-lasierra/lectures/gnomes-social-fullstack/backend/src/controllers/gnomes.controller.js
@@ -1,9 +1,21 @@
 const Gnome = require('../models/gnome.model');
 
+async function getGnomes({ query }, res) {
+  try {
+    const foundGnomes = await Gnome.find(query)
+      .populate({ path: 'friends', select: 'name' })
+      .populate({ path: 'adversaries', select: 'name' });
+
+    res.json(foundGnomes);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
 async function createGnome({ body }, res) {
   try {
     const newGnome = await Gnome.create(body);
-
     res.json(newGnome);
   } catch (error) {
     res.status(500);
@@ -11,6 +23,45 @@ async function createGnome({ body }, res) {
   }
 }
 
+async function getGnomeById({ params: { gnomeId } }, res) {
+  try {
+    const foundGnome = await Gnome.findById(gnomeId);
+    res.json(foundGnome);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function deleteGnomeById({ params: { gnomeId } }, res) {
+  try {
+    await Gnome.findByIdAndDelete(gnomeId);
+    res.status(201);
+    res.send();
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function updateGnomeById({ params: { gnomeId }, body }, res) {
+  try {
+    const updatedGnome = await Gnome.findByIdAndUpdate(
+      gnomeId,
+      body,
+      { new: true }
+    );
+    res.json(updatedGnome);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
 module.exports = {
-  createGnome
+  getGnomes,
+  createGnome,
+  getGnomeById,
+  deleteGnomeById,
+  updateGnomeById
 };
--- xavi-lasierra/lectures/gnomes-social-fullstack/backend/src/routes/gnomes.router.js
+++ xavi-lasierra/lectures/gnomes-social-fullstack/backend/src/routes/gnomes.router.js
@@ -1,12 +1,23 @@
 const { Router } = require('express');
 const {
-  createGnome
+  getGnomes,
+  createGnome,
+  getGnomeById,
+  deleteGnomeById,
+  updateGnomeById
 } = require('../controllers/gnomes.controller');
 
 const gnomesRouter = Router();
 
 gnomesRouter
   .route('/')
+  .get(getGnomes)
   .post(createGnome);
+
+gnomesRouter
+  .route('/:gnomeId')
+  .get(getGnomeById)
+  .delete(deleteGnomeById)
+  .put(updateGnomeById);
 
 module.exports = gnomesRouter;
