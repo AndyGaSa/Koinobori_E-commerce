4d4a759e25a3a46d4b97c9a98a4304d0aa025ab8
ricardSala <riki_sala@icloud.com>
2021-08-18 00:06:47 +0200

    Create a dashboard component
    -mock render
    -BEM

---
+++ ricard-sala/challanges/social-media/frontend/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './dashboard.scss';
+
+export default function Dashboard() {
+  return (
+    <>
+      <main className="user">
+        <header>friendsbook</header>
+        <div className="user__img-container">
+          <img className="img" src="https://www.antiagingya.com/es/wp-content/uploads/2015/01/img-default-autores.jpg" alt="profile" />
+        </div>
+      </main>
+      <section className="basic__info">
+        <span className="info__name">RICARD</span>
+        <span className="info__gender">male</span>
+      </section>
+      <div className="info__container">
+        <span className="info__about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odio similique doloremque vitae dolores. Dignissimos, iusto voluptates corrupti error incidunt pariatur? Adipisci molestiae deleniti eum cumque sint magni autem voluptas?</span>
+      </div>
+    </>
+  );
+}
