0a1f6605927758250d8baff42c9dd8dd988d204e
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-17 19:19:07 +0200

    Add components
    -Add Header component
    -Add MainContainer component
    -Add Footer component

---
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <h2 data-testid="footer__title">Este es el Footer</h2>
+  );
+}
---
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/components/header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <h1 data-testid="header__title">Este es el Header</h1>
+  );
+}
