89acc5265b9daa432d34ce3b266ce860c6d9eba6
“Chej <chejahamed@gmail.com>
2021-08-17 11:57:52 +0200

    Add index and config in the backend

---
+++ chej-ahamed/lectures/social-net/backend/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+
+const debug = require('debug')('server');
+
+require('./src/configs/mongooseConfig');
+
+const server = express();
+const port = process.env.Port || 5000;
+server.use(morgan('dev'));
+server.use(express.json());
+
+server.get('/', (res, req) => res.send('api funciona'));
+server.listen(
+  port,
+  () => debug(`Server is running on http:localhost:${port}`)
+);
--- chej-ahamed/lectures/social-net/backend/src/models/userModel.js
+++ chej-ahamed/lectures/social-net/backend/src/models/userModel.js
@@ -1,6 +1,25 @@
 const { model, Schema } = require('mongoose');
 
 const userSchema = new Schema({
+  picture: { type: String, default: 'ffff' },
+  age: Number,
+  eyeColor: String,
+  name: String,
+  gender: String,
+  about: String,
+  register: Date,
+  tags: [String],
+  friends: [
+
+    { type: Schema.Types.ObjectId, ref: 'User' }
+
+  ],
+  adversaries: [
+    { type: Schema.Types.ObjectId, ref: 'User' }
+
+  ],
+  greetings: String,
+  favouriteFruit: String
 
 });
 
