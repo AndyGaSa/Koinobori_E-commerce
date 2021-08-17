019180d890a4173172f0c393003cda9d9036f4a0
ricardSala <riki_sala@icloud.com>
2021-08-17 19:18:22 +0200

    Install react and frame works

--- ricard-sala/challanges/social-media/frontend/src/index.jsx
+++ ricard-sala/challanges/social-media/frontend/src/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
