8c971037f0acf95521cf1010cd41bf6ddb0c6cd6
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-17 18:43:00 +0200

    Configure work space in frontend

---
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/App.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Holaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
+  );
+}
+
+export default App;
