d7faa116b35772049b8d8da35bf9b3d2b4a76cf4
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-18 09:30:53 +0200

    Refactor the app addig the provider and configStor

--- daniela-fermin/challenges/gnomes-network/frontend/src/App.jsx
+++ daniela-fermin/challenges/gnomes-network/frontend/src/App.jsx
@@ -1,7 +1,14 @@
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store';
+import ListOfGnomes from './components/ListOfGnomes';
 
 function App() {
-
+  return (
+    <Provider store={configureStore()}>
+      <ListOfGnomes />
+    </Provider>
+  );
 }
 
 export default App;
