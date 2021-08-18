ff6e7377a71826f0981a2270adbd98f4b56c3c49
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-18 17:42:55 +0200

    Change path for details in App

--- daniela-fermin/challenges/gnomes-network/frontend/src/App.jsx
+++ daniela-fermin/challenges/gnomes-network/frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import {
+  BrowserRouter, Route, Redirect, Switch
+} from 'react-router-dom';
 import configureStore from './redux/store';
-import ListOfGnomes from './components/ListOfGnomes';
+import ListOfGnomes from './components/List/ListOfGnomes';
+import GnomeDetail from './components/GnomeDetail/GnomeDetail';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
   return (
     <Provider store={configureStore()}>
-      <ListOfGnomes />
+      <h1>The Gnome Network</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={ListOfGnomes} />
+          <Redirect path="/gnomes" to="/" />
+          <Route path="/details/:gnome_id" component={GnomeDetail} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   );
 }
