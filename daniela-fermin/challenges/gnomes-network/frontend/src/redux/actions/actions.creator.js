99f283c51061693a5c5cefa3c145c8c93c48d929
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-18 17:39:40 +0200

    Add loadCurrentGnome function

--- daniela-fermin/challenges/gnomes-network/frontend/src/redux/actions/actions.creator.js
+++ daniela-fermin/challenges/gnomes-network/frontend/src/redux/actions/actions.creator.js
@@ -1,12 +1,23 @@
+/* eslint-disable camelcase */
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
-export default function loadGnomes() {
+export function loadGnomes() {
   return async (dispatch) => {
     const { data } = await axios.get('/gnomesApi/gnomes');
     dispatch({
       type: actionTypes.LOAD_GNOMES,
       data
+    });
+  };
+}
+
+export function loadCurrentGnome(gnome_id) {
+  return async (dispatch) => {
+    const { data } = await axios.get(`/gnomesApi/gnomes/${gnome_id}`);
+    dispatch({
+      type: actionTypes.LOAD_CURRENT_GNOME,
+      currentGnome: data
     });
   };
 }
