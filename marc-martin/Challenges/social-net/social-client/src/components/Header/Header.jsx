5d86ed5b246b5f641cbbef3ebfe4756d794a8422
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-18 17:34:02 +0200

    Create component Header with sass

---
+++ marc-martin/Challenges/social-net/social-client/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className="main-title">User List</h1>
+      <img src="challenges\social-net\social-client\src\components\Header\broken-1.png" alt="" />
+    </header>
+  );
+}
