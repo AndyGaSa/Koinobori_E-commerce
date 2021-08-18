9087e84e759715236e1254b46b4f6cc339168fa2
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-18 18:05:03 +0200

    Refactor MainContainer component
    -Add className to h2
    -Add callback to buttons
    -Add Link to button-details
    -Import css styles

--- laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/components/mainContainer/MainContainer.jsx
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/components/mainContainer/MainContainer.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import loadUsers from '../../redux/actions/actionCreators';
+
+import './mainContainer.css';
 
 export default function MainContainer() {
   const users = useSelector((store) => store.users);
@@ -13,7 +17,7 @@ export default function MainContainer() {
 
   return (
     <main>
-      <h2 data-testid="main__title">Nuestros Gnomo Users Destacados</h2>
+      <h2 data-testid="main__title" className="main__title">Nuestros Gnomo Users Destacados</h2>
       <ul className="users-list">
         {users?.map((user) => (
           <div key={user.name} className="users-list__container">
@@ -21,18 +25,36 @@ export default function MainContainer() {
               <img src={user.img} alt={user.name} className="item__img" />
               <article className="users-list__article">
                 <h3>{user.name}</h3>
-                <h4>{user.age}</h4>
-                <p>{user.about}</p>
+                <h4>
+                  {user.age}
+                  {' '}
+                  años
+                </h4>
+                <p>
+                  Algo sobre mi:
+                  {' '}
+                  {user.about}
+                </p>
               </article>
               <button
                 type="button"
                 className="users-list__button"
-                data-testid="users-list__button"
-                onClick={console.log('Add to my gnomos friends')}
+                data-testid="users-list__button-add"
+                onClick={() => console.log('Add to my gnomos friends')}
               >
                 Add to my friends
 
               </button>
+              <Link to={`/details/${user._id}`}>
+                <button
+                  type="button"
+                  className="users-list__button"
+                  data-testid="users-list__button-details"
+                  onClick={() => console.log(user._id)}
+                >
+                  Saber más sobre mi...
+                </button>
+              </Link>
             </li>
           </div>
         ))}
