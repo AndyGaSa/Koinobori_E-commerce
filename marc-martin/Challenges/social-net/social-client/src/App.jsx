2ad826e33a11eec9940a1db217d778c85add0745
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-18 09:25:50 +0200

    Add imports, title and component UsersList

--- marc-martin/Challenges/social-net/social-client/src/App.jsx
+++ marc-martin/Challenges/social-net/social-client/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import UsersList from './components/UsersList';
+import configureStore from './redux/stores/index';
 
 function App() {
   return (
-    <div className="App" />
+    <Provider store={configureStore()}>
+      <div className="App">
+        <h1>Skybook</h1>
+        <UsersList />
+      </div>
+    </Provider>
   );
 }
 
