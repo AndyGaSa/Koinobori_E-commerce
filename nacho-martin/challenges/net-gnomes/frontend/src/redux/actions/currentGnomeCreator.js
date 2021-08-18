840046ce7ef516240c06fc222fa7b4a69a57a691
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-19 01:06:00 +0200

    Add add/remove friends/foes functions and logic

--- nacho-martin/challenges/net-gnomes/frontend/src/redux/actions/currentGnomeCreator.js
+++ nacho-martin/challenges/net-gnomes/frontend/src/redux/actions/currentGnomeCreator.js
@@ -1,12 +1,53 @@
+/* eslint-disable no-underscore-dangle */
 import axios from 'axios';
+import requestTypes from '../../utils/requestTypes';
 import actionTypes from './currentgnomeTypes';
+import loadGnomes from './gnomeCreators';
 
-export default function loadCurrentGnome(gnomeId) {
+const {
+  ADD_FRIEND,
+  ADD_ADVERSARY,
+  REMOVE_FRIEND,
+  REMOVE_ADVERSARY
+} = requestTypes;
+
+export function loadCurrentGnome(gnomeId) {
   return async (dispatch) => {
     const { data } = await axios.get((`/api/gnomes/${gnomeId}`));
     dispatch({
       type: actionTypes.LOAD_CURRENT_GNOME,
       currentGnome: data
     });
+    dispatch(loadGnomes());
+  };
+}
+
+export function addFriend(gnomeId, currentRelated, id, action) {
+  const newRelated = [...currentRelated, id];
+  let relation;
+
+  if (action === ADD_FRIEND) {
+    relation = { friends: newRelated };
+  } else if (action === ADD_ADVERSARY) {
+    relation = { adversaries: newRelated };
+  }
+  return async (dispatch) => {
+    await axios.put(`/api/gnomes/${gnomeId}`, relation);
+    dispatch(loadCurrentGnome(gnomeId));
+  };
+}
+
+export function removeRelated(gnomeId, currentRelated, id, action) {
+  const newRelated = currentRelated.filter((related) => related._id !== id);
+  let relation;
+
+  if (action === REMOVE_FRIEND) {
+    relation = { friends: newRelated };
+  } else if (action === REMOVE_ADVERSARY) {
+    relation = { adversaries: newRelated };
+  }
+  return async (dispatch) => {
+    await axios.put(`/api/gnomes/${gnomeId}`, relation);
+    dispatch(loadCurrentGnome(gnomeId));
   };
 }
