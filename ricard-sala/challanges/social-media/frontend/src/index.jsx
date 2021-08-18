5b0d076640b3e6bc27df9bf35226423a63693eea
ricardSala <riki_sala@icloud.com>
2021-08-18 13:02:34 +0200

    Install browser router

--- ricard-sala/challanges/social-media/frontend/src/index.jsx
+++ ricard-sala/challanges/social-media/frontend/src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
