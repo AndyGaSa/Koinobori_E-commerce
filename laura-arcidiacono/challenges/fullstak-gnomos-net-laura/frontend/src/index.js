d7b7053c7a8267f46bebea0f5381344db250c00d
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-18 18:05:33 +0200

    Add Provider in index.js

--- laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/index.js
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/index.js
@@ -1,12 +1,16 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
