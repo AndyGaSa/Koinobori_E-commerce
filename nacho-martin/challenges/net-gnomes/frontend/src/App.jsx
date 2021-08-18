aad6d227d78b5df75bab52e721b510d7c4d7e99c
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-18 17:26:56 +0200

    Add currentGnome component and define routes

--- nacho-martin/challenges/net-gnomes/frontend/src/App.jsx
+++ nacho-martin/challenges/net-gnomes/frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter, Route, Switch
+} from 'react-router-dom';
+import CurrentGnome from './components/CurrentGnome';
 import GnomesList from './components/GnomesList';
 
 function App() {
   return (
     <>
       <h1>The Gnome Network</h1>
-      <GnomesList />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={GnomesList} />
+          <Route path="/:gnomeId" component={CurrentGnome} />
+        </Switch>
+
+      </BrowserRouter>
     </>
   );
 }
--- nacho-martin/challenges/net-gnomes/frontend/src/components/GnomesList.jsx
+++ nacho-martin/challenges/net-gnomes/frontend/src/components/GnomesList.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import loadGnomes from '../redux/actions/creators';
+import { Link } from 'react-router-dom';
+import loadGnomes from '../redux/actions/gnomeCreators';
 
 export default function gnomesList() {
   const gnomesUsers = useSelector((store) => store.gnomes);
@@ -15,7 +16,7 @@ export default function gnomesList() {
     <>
       <h2>Gnomes</h2>
       <ul>
-        {gnomesUsers?.map((gnome) => <li key={gnome._id}>{gnome.name}</li>)}
+        {gnomesUsers.map((gnome) => <li><Link to={`${gnome._id}`}>{gnome.name}</Link></li>)}
       </ul>
     </>
   );
