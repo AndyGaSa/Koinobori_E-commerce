fae44a0834512daefdd5d0ec42c28f660121e09a
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-18 18:06:43 +0200

    Refactor App.jsx
    -Add Route, Switch and Route  to connect the components

--- laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/App.jsx
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/App.jsx
@@ -1,19 +1,31 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import Header from './components/header/Header';
 import MainContainer from './components/mainContainer/MainContainer';
+import Details from './components/details/Details';
+import NotFound from './components/notFound/NotFound';
 import Footer from './components/footer/Footer';
-import configureStore from './redux/store';
+
 import './App.css';
 
 function App() {
   return (
-    <Provider store={configureStore()}>
+    <>
       <Header />
-      <MainContainer />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainContainer} />
+          <Route path="/details/:userId" component={Details} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
       <Footer />
-    </Provider>
+    </>
   );
 }
 
