5fb6158d7f023c6d30b97b54d4b2bd234e44a917
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-19 01:04:36 +0200

    Add currentGnome (user) component

--- nacho-martin/challenges/net-gnomes/frontend/src/components/CurrentGnome.jsx
+++ nacho-martin/challenges/net-gnomes/frontend/src/components/CurrentGnome.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { loadCurrentGnome, addFriend, removeRelated } from '../redux/actions/currentGnomeCreator';
+import requestTypes from '../utils/requestTypes';
 
 export default function CurrentGnome() {
+  const userGnome = useSelector(({ currentGnome }) => currentGnome);
+  const gnomesList = useSelector(({ gnomes }) => gnomes);
+  const dispatch = useDispatch();
+  const { gnomeId } = useParams();
+  useEffect(() => { (dispatch(loadCurrentGnome(gnomeId))); }, []);
+
   return (
-    <div />
+    <>
+      <h2>{userGnome?.name}</h2>
+
+      <ul>
+        {gnomesList.map((gnome) => (
+          <li key={gnome._id}>
+            {gnome.name}
+            {userGnome?.friends?.some((friend) => friend._id === gnome._id)
+              ? <button type="button" onClick={() => dispatch(removeRelated(gnomeId, userGnome?.friends, gnome._id, requestTypes.REMOVE_FRIEND))}>Remove friend</button>
+              : <button type="button" onClick={() => dispatch(addFriend(gnomeId, userGnome?.friends, gnome._id, requestTypes.ADD_FRIEND))}>Add friend</button>}
+
+            {userGnome?.adversaries?.some((friend) => friend._id === gnome._id)
+              ? <button type="button" onClick={() => dispatch(removeRelated(gnomeId, userGnome?.friends, gnome._id, requestTypes.REMOVE_ADVERSARY))}>Remove adversary</button>
+              : <button type="button" onClick={() => dispatch(addFriend(gnomeId, userGnome?.adversaries, gnome._id, requestTypes.ADD_ADVERSARY))}>Add adversary</button>}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <h3>Friends:</h3>
+        <ul>
+          {userGnome?.friends?.map((friend) => <li key={friend._id}>{ friend.name}</li>)}
+        </ul>
+        <h3>Adversaries:</h3>
+        <ul>
+          {userGnome?.adversaries?.map(
+            (adversary) => <li key={adversary._id}>{ adversary.name}</li>
+          )}
+        </ul>
+      </div>
+    </>
   );
 }
