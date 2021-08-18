99284c9281e32b9ee3cb6e316e0c7da5eb14749c
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-18 13:01:45 +0200

    Modify http

--- laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/redux/actions/actionCreators.js
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/redux/actions/actionCreators.js
@@ -3,7 +3,7 @@ import actionTypes from './actionTypes';
 
 export default function loadUsers() {
   return async (dispatch) => {
-    const { data } = await axios.get('/api/users');
+    const { data } = await axios.get('http://localhost:5000/api/users');
 
     dispatch({
       type: actionTypes.LOAD_USERS,
--- laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/redux/reducers/userReducers.js
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/redux/reducers/userReducers.js
@@ -2,7 +2,7 @@ import actionTypes from '../actions/actionTypes';
 
 export default function userReducers(users = {}, action) {
   if (action.type === actionTypes.LOAD_USERS) {
-    return action.data[0];
+    return action.data;
   }
   return users;
 }
