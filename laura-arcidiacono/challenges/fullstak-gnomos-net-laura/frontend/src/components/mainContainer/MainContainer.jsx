f279092926a3f9459534cbc0d9c41a5958a1baeb
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-19 22:17:57 +0200

    Add button and functions
    -Add logout button and function
    -Refactor useSelector

--- laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/components/mainContainer/MainContainer.jsx
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/components/mainContainer/MainContainer.jsx
@@ -3,54 +3,74 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import loadUsers from '../../redux/actions/actionCreators';
+import { loadUsers, logout } from '../../redux/actions/actionCreators';
 
 import './mainContainer.css';
 
 export default function MainContainer() {
-  const users = useSelector((store) => store.users);
+  const { user, users } = useSelector((store) => ({
+    user: store.auth.user,
+    users: store.users
+  }));
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadUsers());
   }, []);
 
+  function handleLogout() {
+    dispatch(logout());
+  }
+
   return (
     <main>
-      <h2 data-testid="main__title" className="main__title">Nuestros Gnomo Users Destacados</h2>
+      <button
+        type="button"
+        className="logout-button"
+        data-testid="logout-button"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
+      <h2 data-testid="main__title" className="main__title">
+        {user.name}
+        :
+        {' '}
+        Mis Gnomos Amigos
+      </h2>
       <ul className="users-list">
-        {users?.map((user) => (
-          <div key={user.name} className="users-list__container">
+        {users?.map((neighbour) => (
+          <div key={neighbour.name} className="users-list__container">
             <li className="users-list__item">
-              <img src={user.img} alt={user.name} className="item__img" />
+              <img src={neighbour.img} alt={neighbour.name} className="item__img" />
               <article className="users-list__article">
-                <h3>{user.name}</h3>
+                <h3>{neighbour.name}</h3>
                 <h4>
-                  {user.age}
+                  {neighbour.age}
                   {' '}
                   años
                 </h4>
                 <p>
                   Algo sobre mi:
                   {' '}
-                  {user.about}
+                  {neighbour.about}
                 </p>
               </article>
               <button
                 type="button"
                 className="users-list__button"
                 data-testid="users-list__button-add"
-                onClick={() => console.log('Add to my gnomos friends')}
+                onClick={() => console.log(neighbour._id)}
               >
                 Add to my friends
 
               </button>
-              <Link to={`/details/${user._id}`}>
+              <Link to={`/details/${neighbour._id}`}>
                 <button
                   type="button"
                   className="users-list__button"
                   data-testid="users-list__button-details"
-                  onClick={() => console.log(user._id)}
+                  onClick={() => console.log(neighbour._id)}
                 >
                   Saber más sobre mi...
                 </button>
