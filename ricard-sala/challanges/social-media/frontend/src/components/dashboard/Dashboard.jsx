d02217547bd64800a0bb2999c404530a4f9e0120
ricardSala <riki_sala@icloud.com>
2021-08-19 10:13:18 +0200

    Link dashboard with backend

--- ricard-sala/challanges/social-media/frontend/src/components/dashboard/Dashboard.jsx
+++ ricard-sala/challanges/social-media/frontend/src/components/dashboard/Dashboard.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import loadUser from '../../redux/actions/actionCreators';
 import './dashboard.scss';
 
 export default function Dashboard() {
+  const users = useSelector((store) => store.users);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, []);
   return (
     <>
       <main className="user">
         <header>friendsbook</header>
         <div className="user__img-container">
-          <img className="img" src="https://www.antiagingya.com/es/wp-content/uploads/2015/01/img-default-autores.jpg" alt="profile" />
+          <img className="img" src={users[1]?.picture} alt="profile" />
+        </div>
+        <div className="container">
+          <img className="container__nav" alt="menu-nav" src="https://img.icons8.com/ios-glyphs/30/ffffff/menu--v1.png" />
         </div>
       </main>
       <section className="basic__info">
-        <span className="info__name">RICARD</span>
-        <span className="info__gender">male</span>
+        <span className="info__name">
+          {users[1]?.name}
+        </span>
+        <span className="info__gender">{users[1]?.gender}</span>
       </section>
       <div className="info__container">
-        <span className="info__about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odio similique doloremque vitae dolores. Dignissimos, iusto voluptates corrupti error incidunt pariatur? Adipisci molestiae deleniti eum cumque sint magni autem voluptas?</span>
+        <span className="info__about">{users[1]?.about}</span>
       </div>
       <div className="friends__button__container">
         <button
