f230fb8253b14ba41dbf52281039fa3c63ae8760
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-19 22:15:41 +0200

    Add login and logout functions to actionCreators

--- laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/redux/actions/actionCreators.js
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/redux/actions/actionCreators.js
@@ -1,13 +1,43 @@
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
-export default function loadUsers() {
+export function loadUsers() {
   return async (dispatch) => {
     const { data } = await axios.get('http://localhost:5000/api/users');
 
     dispatch({
       type: actionTypes.LOAD_USERS,
-      data
+      users: data
     });
+  };
+}
+
+export function login(name) {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post('http://localhost:5000/api/login', { name });
+      return dispatch({
+        type: actionTypes.AUTH_LOGIN,
+        user: data
+      });
+    } catch (error) {
+      if (error?.response?.status === 401) {
+        return dispatch({
+          type: actionTypes.AUTH_LOGIN_ERROR,
+          message: error.message
+        });
+      }
+
+      return dispatch({
+        type: actionTypes.ERROR_GENERIC,
+        message: error.message
+      });
+    }
+  };
+}
+
+export function logout() {
+  return {
+    type: actionTypes.AUTH_LOGOUT
   };
 }
