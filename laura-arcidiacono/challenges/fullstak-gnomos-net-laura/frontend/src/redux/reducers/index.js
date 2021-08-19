0f81d013d1af5755969ec4111ff5b648a6c773ef
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-19 22:14:10 +0200

    Add auth and notification reducers

--- laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/redux/reducers/index.js
+++ laura-arcidiacono/challenges/fullstak-gnomos-net-laura/frontend/src/redux/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import users from './userReducers';
+import auth from './authReducer';
+import notification from './notificationReducer';
 
 export default combineReducers({
-  users
+  users,
+  auth,
+  notification
 });
