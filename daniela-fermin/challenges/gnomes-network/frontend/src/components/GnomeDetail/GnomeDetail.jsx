4e10536a94d39d32a0d989caea5aa1513d817a13
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-19 10:10:18 +0200

    Add to the list of possible friends and adversarie
    -Map to render friends and adversaries.
    -Ternary expression to determine which button to render

--- daniela-fermin/challenges/gnomes-network/frontend/src/components/GnomeDetail/GnomeDetail.jsx
+++ daniela-fermin/challenges/gnomes-network/frontend/src/components/GnomeDetail/GnomeDetail.jsx
@@ -4,7 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { loadGnomes, loadCurrentGnome, addFriend } from '../../redux/actions/actions.creator';
+import {
+  loadGnomes, loadCurrentGnome, addRelated, removeRelated
+} from '../../redux/actions/actions.creator';
+import actionTypes from '../../redux/actions/actionTypes';
 
 export default function GnomeDetail() {
   const userGnome = useSelector((store) => store.currentGnome);
@@ -15,46 +18,41 @@ export default function GnomeDetail() {
   useEffect(() => {
     if (!gnomes.length) dispatch(loadGnomes());
     dispatch(loadCurrentGnome(gnome_id));
-  }, [gnome_id]);
+  }, []);
 
   return (
-    <main>
 
-      <div>
-
-        <h2>{userGnome.name}</h2>
-        <span>{userGnome.age}</span>
-        <span>{userGnome.eyecolor}</span>
-        <span>{userGnome.gender}</span>
-        <span>{userGnome.tags}</span>
-        <span>
-          Friends:
-          {' '}
-          {userGnome.friends}
-        </span>
-        <span>
-          Adversaries:
-          {' '}
-          {userGnome.friends}
-        </span>
-
-      </div>
-      <div>
-        {gnomes.map((otherGnome) => (
-          <li key={otherGnome._id}>
-            <h3>{otherGnome.name}</h3>
-            <button
-              type="button"
-              onClick={() => { addFriend(gnome_id, otherGnome._id); }}
-            >
-              Add to Friends
-
-            </button>
-            <button type="button">Add to Adversaries</button>
+    <>
+      <h2>{userGnome?.name}</h2>
+      <span>{userGnome?.age}</span>
+      <span>{userGnome?.eyecolor}</span>
+      <span>{userGnome?.gender}</span>
+      <span>{userGnome?.tags}</span>
+      <ul>
+        {gnomes?.map((gnome) => (
+          <li key={gnome._id}>
+            {gnome.name}
+            {userGnome?.friends?.some((friend) => friend._id === gnome._id)
+              ? <button type="button" onClick={() => dispatch(removeRelated(gnome_id, userGnome?.friends, gnome._id, actionTypes.REMOVE_FRIEND))}>Remove friend</button>
+              : <button type="button" onClick={() => dispatch(addRelated(gnome_id, userGnome?.friends, gnome._id, actionTypes.ADD_FRIEND))}>Add friend</button>}
+            {userGnome?.adversaries?.some((friend) => friend._id === gnome._id)
+              ? <button type="button" onClick={() => dispatch(removeRelated(gnome_id, userGnome?.adversaries, gnome._id, actionTypes.REMOVE_ADVERSARY))}>Remove adversary</button>
+              : <button type="button" onClick={() => dispatch(addRelated(gnome_id, userGnome?.adversaries, gnome._id, actionTypes.ADD_ADVERSARY))}>Add adversary</button>}
           </li>
-
         ))}
+      </ul>
+      <div>
+        <h3>Friends:</h3>
+        <ul>
+          {userGnome?.friends?.map((friend) => <li key={friend._id}>{ friend.name}</li>)}
+        </ul>
+        <h3>Adversaries:</h3>
+        <ul>
+          {userGnome?.adversaries?.map(
+            (adversary) => <li key={adversary._id}>{ adversary.name}</li>
+          )}
+        </ul>
       </div>
-    </main>
+    </>
   );
 }
