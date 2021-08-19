75ada491824f39248fbd123cab80bf98c4d69632
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-19 10:11:38 +0200

    Refactor the addRelated function -Create the removeRelated function

--- daniela-fermin/challenges/gnomes-network/frontend/src/redux/actions/actions.creator.js
+++ daniela-fermin/challenges/gnomes-network/frontend/src/redux/actions/actions.creator.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable camelcase */
 import axios from 'axios';
 import actionTypes from './actionTypes';
@@ -22,13 +23,30 @@ export function loadCurrentGnome(gnome_id) {
   };
 }
 
-export function addFriend(gnome_id, id) {
+export function addRelated(gnome_id, currentRelated, id, action) {
+  const newRelated = [...currentRelated, id];
+  let relation;
+  if (action === actionTypes.ADD_FRIEND) {
+    relation = { friends: newRelated };
+  } else if (action === actionTypes.ADD_ADVERSARY) {
+    relation = { adversaries: newRelated };
+  }
   return async (dispatch) => {
-    await axios.put(`/gnomesApi/gnomes/${gnome_id}`, id);
-    const { data } = await axios.get(`/gnomesApi/gnomes/${gnome_id}`);
-    dispatch({
-      type: actionTypes.LOAD_CURRENT_GNOME,
-      currentGnome: data
-    });
+    await axios.put(`/gnomesApi/gnomes/${gnome_id}`, relation);
+    dispatch(loadCurrentGnome(gnome_id));
+  };
+}
+
+export function removeRelated(gnome_id, currentRelated, id, action) {
+  const newRelated = currentRelated.filter((related) => related._id !== id);
+  let relation;
+  if (action === actionTypes.REMOVE_FRIEND) {
+    relation = { friends: newRelated };
+  } else if (action === actionTypes.REMOVE_ADVERSARY) {
+    relation = { adversaries: newRelated };
+  }
+  return async (dispatch) => {
+    await axios.put(`/gnomesApi/gnomes/${gnome_id}`, relation);
+    dispatch(loadCurrentGnome(gnome_id));
   };
 }
