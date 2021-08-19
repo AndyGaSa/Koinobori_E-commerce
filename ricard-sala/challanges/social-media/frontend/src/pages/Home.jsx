be37e80a7f017f9c86b4c5024438eb3608c81c77
ricardSala <riki_sala@icloud.com>
2021-08-19 10:13:43 +0200

    Add friends list file too see in preview

--- ricard-sala/challanges/social-media/frontend/src/pages/Home.jsx
+++ ricard-sala/challanges/social-media/frontend/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../redux/store';
 import Dashboard from '../components/dashboard/Dashboard';
+import FriendsList from '../components/friendsList/FriendsList';
 
 export default function Home() {
   return (
     <Provider store={configureStore()}>
       <Dashboard />
+      <FriendsList />
     </Provider>
   );
 }
