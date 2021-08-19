bc0f8e5ebc8613cf3c53109fbd72b885a0d4ef42
Manuel Barra <manuelbarralazo@gmail.com>
2021-08-19 17:33:16 +0200

    Add Header component

---
+++ manuel-barra/challenges/social-network/frontend/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Gnomos</h1>
+      <h2>Friends & Adversaries</h2>
+    </header>
+  );
+}
