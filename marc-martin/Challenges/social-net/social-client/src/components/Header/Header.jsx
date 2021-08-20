18e00ed1e2616aefdee68580aad5883cdba491aa
Marc Martin <marcmartinbarrios@gmail.com>
2021-08-20 09:34:28 +0200

    Delete unused image

--- marc-martin/Challenges/social-net/social-client/src/components/Header/Header.jsx
+++ marc-martin/Challenges/social-net/social-client/src/components/Header/Header.jsx
@@ -6,7 +6,6 @@ export default function Header() {
   return (
     <header>
       <h1 className="main-title">User List</h1>
-      <img src="challenges\social-net\social-client\src\components\Header\broken-1.png" alt="" />
     </header>
   );
 }
