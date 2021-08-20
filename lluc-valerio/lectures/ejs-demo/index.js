21ab26621bde7db5750d2c99c361c25a99593811
Lluc Valério <lluc.valerio@gmail.com>
2021-08-20 14:19:04 +0200

    Add EJS example render

--- lluc-valerio/lectures/ejs-demo/index.js
+++ lluc-valerio/lectures/ejs-demo/index.js
@@ -3,16 +3,41 @@ const express = require('express');
 const morgan = require('morgan');
 const debug = require('debug')('serverSSR');
 const chalk = require('chalk');
+const path = require('path');
 
 // server data
 const server = express();
 const port = process.env.PORT || 5000;
 
-server.use(morgan('dev'));
+server
+  .use(morgan('dev'))
+  .use(
+    express.static(path.join(__dirname, '/public'))
+  )
+  .use(
+    '/js',
+    express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'))
+  )
+  .use(
+    '/css',
+    express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css'))
+  );
 // server.use(express.json());
 
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index');
+});
+
+server.get('/profile', (req, res) => {
+  const userData = {
+    name: 'Lluc',
+    age: 40,
+    wife: 'none of your business',
+    ps5: false
+  };
+  res.render('profile', { user: userData });
 });
 
 // start server
