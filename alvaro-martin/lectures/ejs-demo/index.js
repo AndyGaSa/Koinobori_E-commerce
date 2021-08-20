27e3f5cb9684a9fe3c43b0e0d07c5d7588a345ff
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-08-20 12:46:43 +0200

    Add bootstrap to the files

--- alvaro-martin/lectures/ejs-demo/index.js
+++ alvaro-martin/lectures/ejs-demo/index.js
@@ -1,7 +1,25 @@
-const http = require('http');
+const express = require('express');
+require('dotenv').config();
+const morgan = require('morgan');
+const debug = require('debug')('server');
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World');
+const server = express();
+const port = process.env.PORT || 5000;
+server.use(morgan('dev'));
+server.use(
+  '/css',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')),
+);
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+  res.render('index');
 });
-
-server.listen(4300, () => console.log('serveris running...'));
+server.get('/profile', (req, res) => {
+  res.render('profile');
+});
+server.listen(
+  port,
+  () => debug(`Server is running in http://localhost:${port}`),
+);
