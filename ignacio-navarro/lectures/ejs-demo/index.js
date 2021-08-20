9415a1efe85fa079d799e87d9b7f1874a50feac4
ignacioND <ignacio.navarro@gmail.com>
2021-08-20 16:59:30 +0200

    Add public styles and route for css

--- ignacio-navarro/lectures/ejs-demo/index.js
+++ ignacio-navarro/lectures/ejs-demo/index.js
@@ -9,8 +9,15 @@ const port = process.env.PORT || 5000;
 
 server.use(morgan('dev'));
 server.use(
+  express.static(path.join(__dirname, '/public')),
+);
+server.use(
   '/css',
   express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')),
+);
+server.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')),
 );
 server.set('view engine', 'ejs');
 server.get('/', (req, res) => {
