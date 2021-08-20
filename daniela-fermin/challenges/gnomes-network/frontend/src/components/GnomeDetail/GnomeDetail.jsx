24cab05a011e6fa2a920f8ae7aae07fc03e2545a
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-20 10:15:34 +0200

    Add styles from material ui

--- daniela-fermin/challenges/gnomes-network/frontend/src/components/GnomeDetail/GnomeDetail.jsx
+++ daniela-fermin/challenges/gnomes-network/frontend/src/components/GnomeDetail/GnomeDetail.jsx
@@ -3,17 +3,51 @@
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import { green, red } from '@material-ui/core/colors';
+import { IconButton } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 
+import Avatar from '@material-ui/core/Avatar';
+
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import actionTypes from '../../redux/actions/actionTypes';
 import {
   loadGnomes, loadCurrentGnome, addRelated, removeRelated
 } from '../../redux/actions/actions.creator';
-import actionTypes from '../../redux/actions/actionTypes';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%' // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
+  },
+  avatar: {
+    backgroundColor: red[500]
+  }
+}));
 
 export default function GnomeDetail() {
   const userGnome = useSelector((store) => store.currentGnome);
   const gnomes = useSelector((store) => store.gnomes);
   const { gnome_id } = useParams();
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     if (!gnomes.length) dispatch(loadGnomes());
@@ -30,15 +64,56 @@ export default function GnomeDetail() {
       <span>{userGnome?.tags}</span>
       <ul>
         {gnomes?.map((gnome) => (
-          <li key={gnome._id}>
+          <Card
+            className={classes.root}
+            key={gnome._id}
+          >
+            <CardHeader
+              avatar={(
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  {gnome.picture}
+                </Avatar>
+        )}
+              action={(
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+        )}
+            />
             {gnome.name}
             {userGnome?.friends?.some((friend) => friend._id === gnome._id)
-              ? <button type="button" onClick={() => dispatch(removeRelated(gnome_id, userGnome?.friends, gnome._id, actionTypes.REMOVE_FRIEND))}>Remove friend</button>
-              : <button type="button" onClick={() => dispatch(addRelated(gnome_id, userGnome?.friends, gnome._id, actionTypes.ADD_FRIEND))}>Add friend</button>}
+              ? (
+                <IconButton
+                  style={{ color: green[500] }}
+                  onClick={() => dispatch(removeRelated(gnome_id,
+                    userGnome?.friends, gnome._id, actionTypes.REMOVE_FRIEND))}
+                >
+                  <RemoveCircleIcon />
+
+                </IconButton>
+              )
+              : (
+                <IconButton
+                  className="fa fa-plus-circle"
+                  style={{ color: green[500] }}
+                  onClick={() => dispatch(addRelated(gnome_id, userGnome?.friends,
+                    gnome._id, actionTypes.ADD_FRIEND))}
+                >
+                  <AddCircleIcon />
+                </IconButton>
+              )}
             {userGnome?.adversaries?.some((friend) => friend._id === gnome._id)
-              ? <button type="button" onClick={() => dispatch(removeRelated(gnome_id, userGnome?.adversaries, gnome._id, actionTypes.REMOVE_ADVERSARY))}>Remove adversary</button>
-              : <button type="button" onClick={() => dispatch(addRelated(gnome_id, userGnome?.adversaries, gnome._id, actionTypes.ADD_ADVERSARY))}>Add adversary</button>}
-          </li>
+              ? (
+                <IconButton
+                  color="secondary"
+                  onClick={() => dispatch(removeRelated(gnome_id,
+                    userGnome?.adversaries, gnome._id, actionTypes.REMOVE_ADVERSARY))}
+                >
+                  <RemoveCircleIcon />
+                </IconButton>
+              )
+              : <IconButton color="secondary" onClick={() => dispatch(addRelated(gnome_id, userGnome?.adversaries, gnome._id, actionTypes.ADD_ADVERSARY))}><AddCircleIcon /></IconButton>}
+          </Card>
         ))}
       </ul>
       <div>
--- daniela-fermin/challenges/gnomes-network/frontend/src/components/List/ListOfGnomes.jsx
+++ daniela-fermin/challenges/gnomes-network/frontend/src/components/List/ListOfGnomes.jsx
@@ -3,11 +3,35 @@ import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import { loadGnomes } from '../../redux/actions/actions.creator';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
 
 export default function ListOfGnomes() {
   const gnomes = useSelector((store) => store.gnomes);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   const [filteredGnomes, setFilterGnomes] = useState();
   useEffect(() => {
@@ -44,7 +68,18 @@ export default function ListOfGnomes() {
                   filteredGnomes && filteredGnomes.map((gnome) => (
                     <li key={gnome._id}>
                       <Link to={`/details/${gnome._id}`}>
-                        <span>{gnome.name}</span>
+                        <Card className={classes.root}>
+                          <CardContent>
+
+                            <Typography variant="h5" component="h2">
+                              {gnome.name}
+                            </Typography>
+                          </CardContent>
+                          <CardActions>
+                            <Button size="small">Friends</Button>
+                            <Button size="small">Adversaries</Button>
+                          </CardActions>
+                        </Card>
                       </Link>
                     </li>
                   ))
