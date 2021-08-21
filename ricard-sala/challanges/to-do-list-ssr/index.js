2cb04d0d91b9784b89f2a1712130d9fd83146575
ricardSala <riki_sala@icloud.com>
2021-08-21 12:21:27 +0200

    Refactor index to improve develop enviroment

--- ricard-sala/challanges/to-do-list-ssr/index.js
+++ ricard-sala/challanges/to-do-list-ssr/index.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
+const express = require('express');
 const debug = require('debug')('server');
 const morgan = require('morgan');
-const express = require('express');
 
 const userRouter = require('./src/routers/userRouters');
 const toDoRouter = require('./src/routers/toDoRouters');
+const { dashboardRouter } = require('./src/routers/viewRouter');
 
 require('./src/config/ddbbConfig');
 
 const server = express();
 const port = process.env.PORT || 5014;
+
 server.use(morgan('dev'));
+server.set('view engine', 'ejs');
 server.use(express.json());
 
+server.use('/', dashboardRouter);
 server.use('/api', userRouter);
 server.use('/api', toDoRouter);
 
