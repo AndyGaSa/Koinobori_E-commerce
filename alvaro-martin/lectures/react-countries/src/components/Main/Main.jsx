3351d15c3c0177ff45976190593b954e8a83b515
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-08-21 19:28:02 +0200

    Add countries and flags

--- alvaro-martin/lectures/react-countries/src/components/Main/Main.jsx
+++ alvaro-martin/lectures/react-countries/src/components/Main/Main.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './style.css';
 
 export default function Main() {
   const countries = useSelector((store) => store.countries);
   return (
     <>
       <h1>funciona</h1>
-      {countries?.map(({ name }) => (
-        <p>{name}</p>
-      ))}
+      <section className="countries">
+
+        {countries?.map(({ name, flag }) => (
+          <div className="country">
+            <p>{name}</p>
+            <img className="countries-flag" src={flag} alt={name} />
+          </div>
+        ))}
+      </section>
     </>
   );
 }
