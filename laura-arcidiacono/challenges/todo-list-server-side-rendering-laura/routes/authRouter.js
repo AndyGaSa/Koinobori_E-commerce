2c67c0f5cf3fc96f259102315db81fae5852805b
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-21 20:59:09 +0200

    Add authRouter

---
+++ laura-arcidiacono/challenges/todo-list-server-side-rendering-laura/routes/authRouter.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'TACRkbZUi1lLOUzYtYg5lXMtJXOJ4Is0',
+  issuerBaseURL: 'https://dev-5sr6mzhw.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
