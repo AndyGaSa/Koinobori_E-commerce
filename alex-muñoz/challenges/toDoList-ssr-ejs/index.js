58d887ee3b1ee8b4ca9b44afd42ba947fe8e984b
Alex <alexmunoz91@gmail.com>
2021-08-21 17:07:09 +0200

    Add routers

--- alex-muñoz/challenges/toDoList-ssr-ejs/index.js
+++ alex-muñoz/challenges/toDoList-ssr-ejs/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const debug = require('debug')('toDo');
-const path = require('path');
 const chalk = require('chalk');
 
 const server = express();
@@ -12,6 +11,16 @@ require('./src/config/mongooseConfig');
 
 server.use(morgan('dev'));
 server.use(express.json());
+
+const userRouter = require('./src/routers/user');
+const taskListRouter = require('./src/routers/taskList');
+const taskRouter = require('./src/routers/task');
+
+server.use('/user', userRouter);
+server.use('/taskList', taskListRouter);
+server.use('/task', taskRouter);
+
+server.set('view engine', 'ejs');
 
 server.listen(
   port, () => debug(`Server is running on ${chalk.bgYellow(`http://localhost:${port}`)}`)
