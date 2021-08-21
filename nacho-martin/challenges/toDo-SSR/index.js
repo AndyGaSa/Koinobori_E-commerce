54785d1c0ce32c6212722f199affd580edd6909d
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-21 17:06:05 +0200

    Add server routes

--- nacho-martin/challenges/toDo-SSR/index.js
+++ nacho-martin/challenges/toDo-SSR/index.js
@@ -6,9 +6,23 @@ const morgan = require('morgan');
 require('./src/config/mongooseConfig');
 
 const server = express();
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 5001;
 server.use(morgan('dev'));
 server.use(express.json());
+
+server.set('view engine', 'ejs');
+
+const userRouter = require('./src/routers/user');
+
+server.use('/user', userRouter);
+
+const taskListRouter = require('./src/routers/taskList');
+
+server.use('/taskList', taskListRouter);
+
+const taskRouter = require('./src/routers/task');
+
+server.use('/task', taskRouter);
 
 server.listen(
   port,
