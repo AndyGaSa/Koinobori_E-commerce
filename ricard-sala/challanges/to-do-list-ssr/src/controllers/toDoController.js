5a52a477268310f60af99ee4ff26ede3d540bac7
ricardSala <riki_sala@icloud.com>
2021-08-21 18:11:32 +0200

    Add get all controller

--- ricard-sala/challanges/to-do-list-ssr/src/controllers/toDoController.js
+++ ricard-sala/challanges/to-do-list-ssr/src/controllers/toDoController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const ToDo = require('../models/toDoModel');
 
 const createTask = async ({ body }, res) => {
@@ -9,6 +10,28 @@ const createTask = async ({ body }, res) => {
     res.send(error);
   }
 };
+const getAll = async ({ params }, res) => {
+  try {
+    const allTask = await ToDo.find(params)
+      .populate({ path: 'user', select: ['name', 'email'] });
+    res.json(allTask);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+};
+const getOneById = async ({ params }, res) => {
+  try {
+    const { userId } = params;
+    const getOne = await ToDo.findOne({ user: { _id: userId } });
+    res.json(getOne);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+};
 module.exports = {
   createTask,
+  getAll,
+  getOneById,
 };
