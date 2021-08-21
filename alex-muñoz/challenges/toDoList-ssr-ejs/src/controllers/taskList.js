c21681d8c9a8d07392c12057ff4d9d366d230dba
Alex <alexmunoz91@gmail.com>
2021-08-21 17:00:42 +0200

    Add tasklist controller

---
+++ alex-muñoz/challenges/toDoList-ssr-ejs/src/controllers/taskList.js
@@ -0,0 +1,30 @@
+const TaskList = require('../models/taskList');
+
+async function createOne({ body }, res) {
+  try {
+    const newTaskList = await TaskList.create(body);
+    res.json(newTaskList);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function getOne({ params: { taskListId } }, res) {
+  try {
+    const currentTaskList = await TaskList.findById(taskListId)
+      .populate({
+        path: 'tasks',
+        select: 'name'
+      });
+    res.render('details', { taskList: currentTaskList });
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+module.exports = {
+  createOne,
+  getOne
+};
