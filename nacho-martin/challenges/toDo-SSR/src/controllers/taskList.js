b30ed23d5bc3bd64fba33b16c582d37a0105e34f
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-21 21:45:07 +0200

    Add updateListById function
    This function allows to add a new task to the list

--- nacho-martin/challenges/toDo-SSR/src/controllers/taskList.js
+++ nacho-martin/challenges/toDo-SSR/src/controllers/taskList.js
@@ -1,4 +1,5 @@
 const TaskList = require('../models/taskList');
+const Task = require('../models/task');
 
 async function createOne({ body }, res) {
   try {
@@ -24,6 +25,27 @@ async function getOne({ params: { taskListId } }, res) {
   }
 }
 
+async function updateListbyId({ params: { taskListId }, body }, res) {
+  try {
+    const { newTaskName } = body;
+    const newTask = await Task.create({ name: newTaskName });
+    const taskListToUpdate = await TaskList.findById(taskListId);
+    taskListToUpdate.tasks.push(newTask);
+    taskListToUpdate.save();
+    const updatedTaskList = await TaskList.findById(taskListId)
+      .populate({
+        path: 'tasks',
+        select: 'name'
+      });
+    res.render('taskList', { taskList: updatedTaskList });
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
 module.exports = {
-  createOne, getOne
+  createOne,
+  getOne,
+  updateListbyId
 };
