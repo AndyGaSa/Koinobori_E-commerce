9ff08e28e1874fad3e256277495c171f572454aa
Alex <alexmunoz91@gmail.com>
2021-08-21 17:00:27 +0200

    Add task controller

---
+++ alex-muñoz/challenges/toDoList-ssr-ejs/src/controllers/task.js
@@ -0,0 +1,15 @@
+const Task = require('../models/task');
+
+async function createOne({ body }, res) {
+  try {
+    const newTask = await Task.create(body);
+    res.json(newTask);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+module.exports = {
+  createOne
+};
