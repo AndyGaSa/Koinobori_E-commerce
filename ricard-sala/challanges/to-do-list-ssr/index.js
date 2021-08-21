dde7384542e37d597fbfde3cc51f7f473864ae6c
ricardSala <riki_sala@icloud.com>
2021-08-21 11:24:23 +0200

    refactor index.js

--- ricard-sala/challanges/to-do-list-ssr/index.js
+++ ricard-sala/challanges/to-do-list-ssr/index.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 const debug = require('debug')('server');
-
+const morgan = require('morgan');
 const express = require('express');
+
+const userRouter = require('./src/routers/userRouters');
+const toDoRouter = require('./src/routers/toDoRouters');
+
+require('./src/config/ddbbConfig');
 
 const server = express();
 const port = process.env.PORT || 5014;
+server.use(morgan('dev'));
+server.use(express.json());
 
-server.use('/', (req, res) => {
-  res.send('hello kitty');
-});
+server.use('/api', userRouter);
+server.use('/api', toDoRouter);
 
 server.listen(
   port,
