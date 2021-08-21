1d24d508ed8fd0b81b29f1bcf24b58261d015e27
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-21 21:01:30 +0200

    Add routes
    -Add userRouter
    -Add taskRouter
    -Add indexRouter

--- laura-arcidiacono/challenges/todo-list-server-side-rendering-laura/index.js
+++ laura-arcidiacono/challenges/todo-list-server-side-rendering-laura/index.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const debug = require('debug')('toDoList');
+const cors = require('cors');
 const path = require('path');
 
 const server = express();
 const port = process.env.PORT || 5000;
 
+require('./configDDBB/configDDBB');
+
+server.use(cors());
 server.use(morgan('dev'));
+server.use(express.json());
 
 server.use(
   express.static(path.join(__dirname, '/public'))
@@ -29,8 +34,16 @@ server.unsubscribe(
 
 server.set('view engine', 'ejs');
 
-const router = require('./routes/userRouter');
+const indexRouter = require('./routes/indexRouter');
 
-server.use('/', router);
+server.use('/index', indexRouter);
+
+const userRouter = require('./routes/userRouter');
+
+server.use('/user', userRouter);
+
+const taskRouter = require('./routes/taskRouter');
+
+server.use('/task', taskRouter);
 
 server.listen(port, () => debug(`Server is running on http://localhost:${port}`));
