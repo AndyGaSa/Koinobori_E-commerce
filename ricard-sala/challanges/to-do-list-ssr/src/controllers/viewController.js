36025ab50a92fba94d72fca998a0b6b3c5983a0a
ricardSala <riki_sala@icloud.com>
2021-08-21 18:11:42 +0200

    Add veiw controller

--- ricard-sala/challanges/to-do-list-ssr/src/controllers/viewController.js
+++ ricard-sala/challanges/to-do-list-ssr/src/controllers/viewController.js
@@ -1,5 +1,17 @@
-const dashboard = (req, res) => {
-  res.render('dashboard');
+/* eslint-disable no-console */
+const axios = require('axios');
+
+const dashboard = async (req, res) => {
+  const user = '61210cb43375e8872a8a7b23';
+  try {
+    const { data } = await axios.get(`http://localhost:5013/api/todo/${user}`);
+    const { tasks } = data;
+
+    res.render('dashboard', { todos: tasks });
+  } catch (error) {
+    res.status(500);
+    res.render('notFound');
+  }
 };
 
 module.exports = {
