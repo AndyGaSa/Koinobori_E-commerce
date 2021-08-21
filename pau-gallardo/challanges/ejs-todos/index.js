d3bc8baffb1f9305b5da7222ba1dc87abefd87b4
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-21 10:01:18 +0200

    Set up enviroment and tooling

--- pau-gallardo/challanges/ejs-todos/index.js
+++ pau-gallardo/challanges/ejs-todos/index.js
@@ -1,1 +1,11 @@
 require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const debug = require('debug')('server');
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(morgan('dev'));
+
+server.listen(port, () => debug(`Server is running on http://localhost:${port}`));
