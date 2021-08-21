c950fd24a8fa59b129bebf08e5bd9cce4ee0e09a
Alex <alexmunoz91@gmail.com>
2021-08-21 16:59:50 +0200

    Modify ref un tasklist property

--- alex-muñoz/challenges/toDoList-ssr-ejs/src/models/user.js
+++ alex-muñoz/challenges/toDoList-ssr-ejs/src/models/user.js
@@ -3,7 +3,7 @@ const { model, Schema } = require('mongoose');
 const userSchema = Schema({
   name: String,
   email: String,
-  taskList: [{ type: Schema.Types.ObjectId, ref: 'taskList' }]
+  taskList: [{ type: Schema.Types.ObjectId, ref: 'TaskList' }]
 });
 
 module.exports = model('User', userSchema);
