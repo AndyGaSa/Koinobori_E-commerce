3df8e4f5dc0b0e02a637381000f425e68ec9951b
Alex <alexmunoz91@gmail.com>
2021-08-22 16:29:40 +0200

    Add default cases

--- alex-muñoz/challenges/toDoList-ssr-ejs/src/models/taskList.js
+++ alex-muñoz/challenges/toDoList-ssr-ejs/src/models/taskList.js
@@ -1,8 +1,8 @@
 const { model, Schema } = require('mongoose');
 
 const taskListSchema = Schema({
-  name: String,
-  category: String,
+  name: { type: String, default: 'Unnamed' },
+  category: { type: String, default: 'General' },
   tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }]
 });
 
