1b14b85ed742d880295124a1f9494f1a9ba90083
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-22 10:47:57 +0200

    Add controller tests

--- pau-gallardo/challanges/ejs-todos/scr/controllers/todosController.test.js
+++ pau-gallardo/challanges/ejs-todos/scr/controllers/todosController.test.js
@@ -21,5 +21,131 @@ describe('Given a postTodo function', () => {
         expect(res.status).toHaveBeenCalled();
       });
     });
+    describe('And rejectes', () => {
+      test('Then res.status should have been called', async () => {
+        const req = {
+          body: { content: {} },
+        };
+        const res = {
+          status: jest.fn(),
+        };
+
+        Todo.create.mockRejectedValue({});
+        await controller.postTodo(req, res);
+
+        expect(res.status).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a getTodos function', () => {
+  describe('When is invoked', () => {
+    describe('And resolves', () => {
+      test('Then res.status should have been called', async () => {
+        const req = {
+          body: { content: {} },
+        };
+        const res = {
+          status: jest.fn(),
+          render: jest.fn(),
+        };
+
+        Todo.find.mockResolvedValue({});
+        await controller.getTodos(req, res);
+
+        expect(res.status).toHaveBeenCalled();
+      });
+    });
+    describe('And rejectes', () => {
+      test('Then res.status should have been called', async () => {
+        const req = {
+          body: { content: {} },
+        };
+        const res = {
+          status: jest.fn(),
+        };
+
+        Todo.find.mockRejectedValue({});
+        await controller.getTodos(req, res);
+
+        expect(res.status).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a deleteTodos function', () => {
+  describe('When is invoked', () => {
+    describe('And resolves', () => {
+      test('Then res.status should have been called', async () => {
+        const req = {
+          params: { todoId: {} },
+        };
+        const res = {
+          status: jest.fn(),
+          redirect: jest.fn(),
+        };
+
+        Todo.findByIdAndDelete.mockResolvedValue({});
+        await controller.deleteTodo(req, res);
+
+        expect(res.status).toHaveBeenCalled();
+        expect(res.redirect).toHaveBeenCalled();
+      });
+    });
+    describe('And rejectes', () => {
+      test('Then res.status should have been called', async () => {
+        const req = {
+          params: { todoId: {} },
+        };
+        const res = {
+          status: jest.fn(),
+          render: jest.fn(),
+        };
+
+        Todo.findByIdAndDelete.mockRejectedValue({});
+        await controller.deleteTodo(req, res);
+
+        expect(res.status).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a updateTodo function', () => {
+  describe('When is invoked', () => {
+    describe('And resolves', () => {
+      test('Then res.status should have been called', async () => {
+        const req = {
+          params: { todoId: {} },
+        };
+        const res = {
+          redirect: jest.fn(),
+          status: jest.fn(),
+        };
+
+        Todo.findById.mockResolvedValue({});
+        await controller.updateTodo(req, res);
+
+        expect(res.status).toHaveBeenCalled();
+      });
+    });
+    describe('And rejectes', () => {
+      test('Then res.status should have been called', async () => {
+        const req = {
+          params: { todoId: {} },
+        };
+        const res = {
+          status: jest.fn(),
+          render: jest.fn(),
+        };
+
+        Todo.findById.mockRejectedValue({});
+        await controller.updateTodo(req, res);
+
+        expect(res.status).toHaveBeenCalled();
+      });
+    });
   });
 });
