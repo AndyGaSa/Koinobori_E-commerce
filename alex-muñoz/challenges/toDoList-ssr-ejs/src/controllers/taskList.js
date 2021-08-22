0c196add0b59e74abf034eb2ea7f892bf95e2820
Alex <alexmunoz91@gmail.com>
2021-08-22 16:30:45 +0200

    Add delete, done, update functions

--- alex-muñoz/challenges/toDoList-ssr-ejs/src/controllers/taskList.js
+++ alex-muñoz/challenges/toDoList-ssr-ejs/src/controllers/taskList.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 const TaskList = require('../models/taskList');
 const Task = require('../models/task');
 
@@ -27,16 +28,41 @@ async function getOne({ params: { taskListId } }, res) {
 
 async function updateTaskList({ params: { taskListId }, body }, res) {
   try {
-    const { newTaskName } = body;
-    const newTask = await Task.create({ name: newTaskName });
-    const taskListToUpdate = await TaskList.findById(taskListId)
+    const updatedTaskList = await TaskList.findById(taskListId)
       .populate({
         path: 'tasks',
         select: 'name'
       });
-    taskListToUpdate.tasks.push(newTask);
-    taskListToUpdate.save();
-    res.render('details', { taskList: taskListToUpdate });
+    if (Object.keys(body).some((key) => key === 'newTaskName')) {
+      const { newTaskName } = body;
+      const newTask = await Task.create({ name: newTaskName });
+      const taskListToUpdate = await TaskList.findById(taskListId)
+        .populate({
+          path: 'tasks',
+          select: 'name'
+        });
+      taskListToUpdate.tasks.push(newTask);
+      taskListToUpdate.save();
+      res.render('details', { taskList: taskListToUpdate });
+    } else if (Object.keys(body).some((key) => key === 'delete')) {
+      const taskToDelete = body.delete;
+      const taskRemoved = await Task.findByIdAndDelete(taskToDelete);
+      const taskListToUpdate = await TaskList.findById(taskListId);
+      const { tasks } = taskListToUpdate;
+      tasks.filter((currentTask) => currentTask._id !== taskRemoved);
+      taskListToUpdate.save();
+
+      res.render('details', { taskList: updatedTaskList });
+    } else if (Object.keys(body).some((key) => key === 'done')) {
+      const taskToUpdate = body.done;
+      await Task.findByIdAndUpdate(taskToUpdate, { status: 'done' }, { new: true });
+      res.render('details', { taskList: updatedTaskList });
+    } else if (Object.keys(body).some((key) => key === 'save')) {
+      const taskId = body.save;
+      const newTaskName = body.updatedTaskName;
+      await Task.findByIdAndUpdate(taskId, { name: newTaskName }, { new: true });
+      res.render('details', { taskList: updatedTaskList });
+    }
   } catch (error) {
     res.status(500);
     res.send(error);
