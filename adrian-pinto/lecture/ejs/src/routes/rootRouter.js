23991c3408b7ae61890e626d28ed1c7748e8509c
Adrian Pinto <adripintogonzalez@gmail.com>
2021-08-22 16:44:18 +0200

    Add rootRouter

---
+++ adrian-pinto/lecture/ejs/src/routes/rootRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const rootController = require('../controllers/rootController');
+
+const rootRouter = express.Router();
+
+rootRouter
+  .route('/')
+  .get(rootController.getTemplate);
+
+module.exports = rootRouter;
