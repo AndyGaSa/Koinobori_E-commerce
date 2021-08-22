174de7634bad8899f13ef00078c0de7771a30c22
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-22 16:31:24 +0200

    Add forms to CRUD functionalities

--- nacho-martin/challenges/toDo-SSR/src/routers/task.js
+++ nacho-martin/challenges/toDo-SSR/src/routers/task.js
@@ -2,6 +2,7 @@ const express = require('express');
 const controller = require('../controllers/task');
 
 const taskRouter = express.Router();
+
 taskRouter
   .route('/')
   .post(controller.createOne);
