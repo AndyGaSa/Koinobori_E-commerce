24f336643ff4f04be341882c16f67d4f0627a594
Nacho Martín <martin.nacho.31@gmail.com>
2021-08-23 18:43:25 +0200

    Configure project initial structure

---
+++ nacho-martin/challenges/passport-auth-challenge/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const debug = require('debug')
+const morgan = require('morgan')
+
+require('./src/config/mongooseConfig')
+
+const server = express()
+const port = process.env.PORT || 5000
+
+require('./src/config/passportConfig')
+
+server.use(morgan('dev'))
+server.use(express.json())
+
+const router = require('./src/routes/router')
+
+server.use('/', router)
+
+server.listen(port, debug(`sever is running on port ${port}`))
