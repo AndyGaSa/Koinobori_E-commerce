38df56975b8ca6108177cc790a1c86f7fee16bc0
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-24 18:55:02 +0200

    Add Provider and Header component

--- laura-arcidiacono/lectures/firebase-auth-laura/src/index.jsx
+++ laura-arcidiacono/lectures/firebase-auth-laura/src/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/header/Header';
+import configureStore from './redux/store';
+import './firebase';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStore()}>
+      <Header />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
