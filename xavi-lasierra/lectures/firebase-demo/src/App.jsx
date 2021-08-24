e64e77a0214c0e65bcfbea112ea45ef3160b7638
Xavier Lasierra <xlp1998@gmail.com>
2021-08-24 15:48:42 +0200

    Add header styles

--- xavi-lasierra/lectures/firebase-demo/src/App.jsx
+++ xavi-lasierra/lectures/firebase-demo/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Header from './components/Header/Header';
 
+import './app.scss';
+
 function App() {
   return (
     <Header />
--- xavi-lasierra/lectures/firebase-demo/src/components/Header/Header.jsx
+++ xavi-lasierra/lectures/firebase-demo/src/components/Header/Header.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser, logoutUser } from '../../redux/actions/user.creator';
 
+import './header.scss';
+
 export default function Header() {
   const dispatch = useDispatch();
   const { isAuthenticated } = useSelector((store) => store.user);
 
+  function handleLoginClick() {
+    dispatch(loginUser());
+  }
+  function handleLogoutClick() {
+    dispatch(logoutUser());
+  }
+
   return (
-    <header>
-      <h1>To Do List</h1>
-      {isAuthenticated
-        ? <button type="button" onClick={() => dispatch(logoutUser())}>Logout</button>
-        : <button type="button" onClick={() => dispatch(loginUser())}>Login</button>}
+    <header className="header">
+      <h1 className="header__title">To Do List</h1>
+      <div className="header__button-container">
+        {isAuthenticated
+          ? <button className="header__logout" type="button" onClick={handleLogoutClick}>Logout</button>
+          : <button className="header__login" type="button" onClick={handleLoginClick}>Login</button>}
+      </div>
     </header>
   );
 }
