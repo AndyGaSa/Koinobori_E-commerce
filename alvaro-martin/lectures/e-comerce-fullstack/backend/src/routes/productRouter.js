6ed2094cdf742fea871dd1fb655606dee545bd44
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-08-24 12:18:08 +0200

    Add Auth routers

--- alvaro-martin/lectures/e-comerce-fullstack/backend/src/routes/productRouter.js
+++ alvaro-martin/lectures/e-comerce-fullstack/backend/src/routes/productRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const passport = require('passport');
 const productController = require('../controllers/productController');
 
 const ProductsRouter = express.Router();
 ProductsRouter
   .route('/')
+  .all(passport.authenticate('jwt', { session: false }))
   .get(productController.getProducts)
   .post(productController.postProduct);
 
