b61adbb21ad514120667d8cf9a6e925b851f5e0e
pabloapausa <pabloapausa@outlook.com>
2021-08-24 12:10:27 +0200

    Start 'authentication' project

---
+++ pabloapausa/challenge/06/authentication/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const debug = require('debug')('server');
+const router = require('./src/routes/router');
+
+const server = express();
+const port = process.env.PORT || 4000;
+
+require('./src/config/mongooseConfig');
+require('./src/config/passportConfig')(server);
+
+server.use(morgan('dev'));
+server.use(express.json());
+server.use('/', router);
+server.listen(port, debug(`https://localhost:${port}`));
