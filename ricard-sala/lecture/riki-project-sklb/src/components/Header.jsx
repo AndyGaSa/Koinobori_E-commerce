ef728258ad23f8c86809bd740edebe21802de198
ricardSala <riki_sala@icloud.com>
2021-08-24 13:41:20 +0200

    Add buttons login and loguot

--- ricard-sala/lecture/riki-project-sklb/src/components/Header.jsx
+++ ricard-sala/lecture/riki-project-sklb/src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Header() {
   return (
     <>
-    <h1>header</h1>
+      <h1>header</h1>
+      <button type="button">login</button>
+      <button type="button">logout</button>
     </>
   );
 }
