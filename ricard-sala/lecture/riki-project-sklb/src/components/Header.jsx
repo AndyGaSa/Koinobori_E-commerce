84316183fc87fcc93f7501d19056c187581ffda0
ricardSala <riki_sala@icloud.com>
2021-08-24 17:49:11 +0200

    Add variable islogged

--- ricard-sala/lecture/riki-project-sklb/src/components/Header.jsx
+++ ricard-sala/lecture/riki-project-sklb/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import LoginBtn from './loginBtn';
+import LogoutBtn from './logoutBtn';
 
 export default function Header() {
+  const islogged = useSelector((store) => store.user);
   return (
-    <>
+    <header>
       <h1>header</h1>
-      <button type="button">login</button>
-      <button type="button">logout</button>
-    </>
+      {islogged ? <LogoutBtn /> : <LoginBtn />}
+
+    </header>
   );
 }
