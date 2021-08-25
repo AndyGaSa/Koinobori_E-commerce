e7bab70d0117d1555a43b068c58135f4f29a6a4e
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-25 11:35:48 +0200

    change provider from App to index

--- daniela-fermin/lectures/firebase-excercise/frontend/src/App.jsx
+++ daniela-fermin/lectures/firebase-excercise/frontend/src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import firebase from 'firebase';
+import { currentUser } from './redux/actions/action.creator';
 import Login from './components/Login/Login';
-import configureStore from './redux/stores';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(currentUser(user));
+      }
+    });
+  }, []);
+
   return (
-    <Provider store={configureStore()}>
-      <Login />
-    </Provider>
+
+    <Login />
 
   );
 }
