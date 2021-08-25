23be93b6920e6f70b97c78512c7b151e90b92f23
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-25 11:26:45 +0200

    Create login component

--- daniela-fermin/lectures/firebase-excercise/frontend/src/components/Login/Login.jsx
+++ daniela-fermin/lectures/firebase-excercise/frontend/src/components/Login/Login.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { useDispatch, useSelector } from 'react-redux';
-import loginf from '../../redux/actions/action.creator';
+import { logout } from '../../redux/actions/action.creator';
+import login from '../../service/login';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -31,70 +31,65 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Login() {
-  const isAuthenticated = useSelector(({ login }) => login.isAuthenticated);
+  const { isAuthenticated, user } = useSelector(({ auth }) => auth);
   const classes = useStyles();
-  const [name, setName] = useState('');
-
   const dispatch = useDispatch();
-
-  function handleLoginClick(event) {
-    event.preventDefault();
-    dispatch(loginf(name));
+  function handleLoginClick() {
+    login();
+  }
+  function handleLogoutClick() {
+    dispatch(logout());
   }
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form
-          className={classes.form}
-          noValidate
-          onSubmit={handleLoginClick}
-        >
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="name"
-            label="Name"
-            name="name"
-            autoFocus
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-          />
-          {isAuthenticated
-            ? (
+
+        {isAuthenticated
+          ? (
+            <>
+              <span>
+                <h2>Welcome</h2>
+                {' '}
+                <br />
+                {user?.displayName}
+              </span>
+              <img src={user?.photoURL} alt={user?.name} />
               <Button
-                type="submit"
+                type="button"
+                onClick={handleLogoutClick}
                 fullWidth
                 variant="contained"
                 color="primary"
                 className={classes.submit}
-                disabled={!name.trim()}
               >
                 Logout
               </Button>
-            )
-            : (
+
+            </>
+          )
+          : (
+            <>
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
               <Button
-                type="submit"
+                type="button"
+                onClick={handleLoginClick}
                 fullWidth
                 variant="contained"
                 color="primary"
                 className={classes.submit}
-                disabled={!name.trim()}
               >
                 Login
               </Button>
-            )}
-        </form>
+            </>
+          )}
+
       </div>
     </Container>
   );
