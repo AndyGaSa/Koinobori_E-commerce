9eea38e6d767ed858bb4f9962b6a0d3a6a6747c6
ricardSala <riki_sala@icloud.com>
2021-08-25 09:45:39 +0200

    Delete unecesary files

--- ricard-sala/lecture/riki-project-sklb/src/redux/reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import actionTypes from '../actions/actionTypes';
-
-export default function rootReducer(state = false, action) {
-  let newUser = state;
-  if (action.type === actionTypes.UPDATE_STATE) { newUser = action.state; }
-  return newUser;
-}
