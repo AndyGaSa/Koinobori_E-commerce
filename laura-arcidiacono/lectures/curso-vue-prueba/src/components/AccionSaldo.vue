b315162a1e8054f7acf676f2fe312689a9242938
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-29 20:49:12 +0200

    Add AccionSaldo.vue

---
+++ laura-arcidiacono/lectures/curso-vue-prueba/src/components/AccionSaldo.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="accion" :disabled="desactivar" > {{texto}} </button>
+</template>
+
+<script>
+export default {
+    props: {
+        texto: String,
+        desactivar: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        accion() {
+            this.$emit('accion');
+        }
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
