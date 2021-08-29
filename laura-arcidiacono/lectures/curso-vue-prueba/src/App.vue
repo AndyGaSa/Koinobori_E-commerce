fd479e6c248e707829f7e832fea3f500f9785e28
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-29 19:48:23 +0200

    Refactor App.vue

--- laura-arcidiacono/lectures/curso-vue-prueba/src/App.vue
+++ laura-arcidiacono/lectures/curso-vue-prueba/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+   <img alt="Vue logo" src="./assets/logo.png">
+   <Titulo texto="Mi banco dinámico 2.0" />
+   <Cuenta />
+  </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Titulo from './components/Titulo.vue';
+import Cuenta from './components/Cuenta.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Titulo,
+    Cuenta
   }
 }
 </script>
