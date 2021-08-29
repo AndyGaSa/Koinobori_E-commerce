c1cd21955f361fb13891c74be32170bb85533d03
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-29 20:48:33 +0200

    Add components and methods
    -Add AccionSaldo components with props and custom events
    -Add methods aumentarSaldo y disminuirSaldo

--- laura-arcidiacono/lectures/curso-vue-prueba/src/components/Cuenta.vue
+++ laura-arcidiacono/lectures/curso-vue-prueba/src/components/Cuenta.vue
@@ -5,17 +5,46 @@
   <div v-for="(servicio, index) in servicios" :key="index" >
       {{index + 1}} - {{ servicio }}
   </div>
+  <AccionSaldo 
+  texto="Aumentar Saldo" 
+  @accion="aumentarSaldo"
+  />
+  <AccionSaldo 
+  texto="Disminuir Saldo" 
+  @accion="disminuirSaldo"
+  :desactivar="desactivar"
+  />
 </template>
 
 <script>
+import AccionSaldo from './AccionSaldo.vue';
 export default {
+    components: {
+        AccionSaldo
+    },
     data() {
         return {
             saldo: 1000,
             cuenta: 'Visa',
             estado: true,
-            servicios: ['Giro', 'Abono', 'Transferencia']
+            servicios: ['Giro', 'Abono', 'Transferencia'],
+            desactivar: false
         }
+    },
+    methods: {
+        aumentarSaldo() {
+            this.saldo = this.saldo + 100
+            this.desactivar = false
+        },
+        disminuirSaldo() {
+            if (this.saldo === 0) {
+                this.desactivar = true;
+                alert('No hay saldo')
+                return
+            }
+            this.saldo = this.saldo - 100
+        },
+
     },
 
 }
