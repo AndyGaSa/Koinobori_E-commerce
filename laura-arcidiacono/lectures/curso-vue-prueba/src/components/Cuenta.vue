ae3c0d67b5ca6e3e655bff71110a083655a3df19
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-29 19:48:51 +0200

    Create Cuenta component

---
+++ laura-arcidiacono/lectures/curso-vue-prueba/src/components/Cuenta.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>Tipo de cuenta: {{cuenta}} </h2>
+  <h2>Saldo: {{saldo}}</h2>
+  <h2>Cuenta {{estado ? 'Activa' : 'Desactivada'}} </h2>
+  <div v-for="(servicio, index) in servicios" :key="index" >
+      {{index + 1}} - {{ servicio }}
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            saldo: 1000,
+            cuenta: 'Visa',
+            estado: true,
+            servicios: ['Giro', 'Abono', 'Transferencia']
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
