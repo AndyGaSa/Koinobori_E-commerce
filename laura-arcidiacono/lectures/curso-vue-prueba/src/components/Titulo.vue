7b1f6733d7c3ec8f674a46ff23cd0ef738e790db
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-29 19:48:39 +0200

    Create Titulo component

---
+++ laura-arcidiacono/lectures/curso-vue-prueba/src/components/Titulo.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{texto}}</h1>
+</template>
+
+<script>
+
+
+export default {
+     name: 'Titulo',
+     props: {   
+    texto: String
+}
+}
+</script>
+
+<style scoped>
+    h1 {color: peru}
+</style>
